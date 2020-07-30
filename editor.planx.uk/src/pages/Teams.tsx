f967e35c930eac3b96c8ac0cb6e72d537f4a1151
Peter Szerzo <szerzo.peter@gmail.com>
2020-07-30 16:49:20 +0200

    Visual tweaks

--- editor.planx.uk/src/pages/Teams.tsx
+++ editor.planx.uk/src/pages/Teams.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Link } from "react-navi";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const Teams: React.FC<{ teams: any }> = ({ teams }) => (
-  <>
+  <Box p={4}>
     {teams.map(({ name, slug }: any) => (
       <Link href={`/${slug}`} key={slug} prefetch={false}>
-        {name}
+        <Box mb={4} component={Card}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              {name}
+            </Typography>
+          </CardContent>
+        </Box>
       </Link>
     ))}
-  </>
+  </Box>
 );
 
 export default Teams;
