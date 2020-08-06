f474ce04b59b8a2d0e1de53c4e3f8bf60a7a2818
John Rees <john@bitsushi.com>
2020-08-06 12:53:16 +0100

    add sign-s3-upload endpoint to API

---
+++ api.planx.uk/s3.js
@@ -0,0 +1,34 @@
+const S3 = require("aws-sdk/clients/s3");
+const { customAlphabet } = require("nanoid");
+const { getType } = require("mime");
+
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 8);
+
+const signS3Upload = (filename) =>
+  new Promise((res, rej) => {
+    const opts = {
+      // apiVersion: "2006-03-01",
+      params: { Bucket: process.env.AWS_S3_BUCKET },
+      region: process.env.AWS_S3_REGION,
+
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+    };
+    const s3 = new S3(opts);
+
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET,
+      ACL: process.env.AWS_S3_ACL,
+      Key: `${process.env.NODE_ENV}/${nanoid()}/${filename}`,
+      ContentType: getType(filename),
+    };
+
+    console.log({ opts, params });
+
+    s3.getSignedUrl("putObject", params, (err, url) => {
+      if (err) return rej(err);
+      return res(url);
+    });
+  });
+
+module.exports = { signS3Upload };
