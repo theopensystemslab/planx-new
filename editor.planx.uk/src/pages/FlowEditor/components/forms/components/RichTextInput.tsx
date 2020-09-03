83d3367934b647f43e25d5d0b2c53c5c0de095c1
Peter Szerzo <szerzo.peter@gmail.com>
2020-09-03 16:55:45 +0200

    Rich text image upload

--- editor.planx.uk/src/pages/FlowEditor/components/forms/components/RichTextInput.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/forms/components/RichTextInput.tsx
@@ -2,8 +2,11 @@ import { Box, InputBaseProps, makeStyles } from "@material-ui/core";
 import { convertToRaw } from "draft-js";
 import { stateToMarkdown } from "draft-js-export-markdown";
 import { stateFromMarkdown } from "draft-js-import-markdown";
-import MUIRichTextEditor from "mui-rte";
-import React, { ChangeEvent, useState } from "react";
+import MUIRichTextEditor, {
+  TAsyncAtomicBlockResponse,
+  TMUIRichTextEditorRef,
+} from "mui-rte";
+import React, { ChangeEvent, useState, useRef } from "react";
 
 /**
  * Important: if the `value` prop changes for a reason other than changes in the editor,
@@ -15,6 +18,7 @@ import React, { ChangeEvent, useState } from "react";
 interface IRichTextInput extends InputBaseProps {
   className?: string;
   onChange?: (ev: ChangeEvent<HTMLInputElement>) => void;
+  onImageUpload?: (file: File) => Promise<string>;
 }
 
 const rteContainerStyles = makeStyles((theme) => ({
@@ -42,6 +46,30 @@ const RichTextInput: React.FC<IRichTextInput> = (props) => {
 
   const [focused, setFocused] = useState(false);
 
+  const editorRef = useRef<TMUIRichTextEditorRef>(null);
+
+  const handleFileUpload = async (file: File) => {
+    await editorRef.current?.insertAtomicBlockAsync(
+      "IMAGE",
+      new Promise<TAsyncAtomicBlockResponse>(async (resolve, reject) => {
+        const url = await props.onImageUpload(file);
+        if (!url) {
+          reject();
+          return;
+        }
+        resolve({
+          type: "image",
+          data: {
+            src: url,
+            url,
+            width: 120,
+          },
+        });
+      }),
+      "Uploading now..."
+    );
+  };
+
   const classes = rteContainerStyles();
 
   return (
@@ -53,6 +81,7 @@ const RichTextInput: React.FC<IRichTextInput> = (props) => {
         onBlur={() => {
           setFocused(false);
         }}
+        ref={editorRef}
         defaultValue={JSON.stringify(defaultValue)}
         toolbarButtonSize="small"
         inlineToolbar={true}
@@ -62,9 +91,18 @@ const RichTextInput: React.FC<IRichTextInput> = (props) => {
           "italic",
           "underline",
           "link",
-          "bulletList",
+          "bulletedList",
         ]}
         label={props.placeholder}
+        draftEditorProps={{
+          handleDroppedFiles: (_selectionState, files: File[]) => {
+            if (props.onImageUpload && files[0]) {
+              handleFileUpload(files[0]);
+              return "handled";
+            }
+            return "not-handled";
+          },
+        }}
         onChange={(newState) => {
           if (!props.onChange) {
             return;
--- editor.planx.uk/src/pages/FlowEditor/components/forms/components/fixtures/RichTextInput.fixture.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/forms/components/fixtures/RichTextInput.fixture.tsx
@@ -4,6 +4,13 @@ import RichTextInput from "../RichTextInput";
 import theme from "../../../../../../theme";
 import ThemeProvider from "@material-ui/styles/ThemeProvider";
 
+const wait = (timeout: number): Promise<unknown> =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, timeout);
+  });
+
 const RichTextSnippet: React.FC<{}> = () => {
   const [value, setValue] = useState("Something");
 
@@ -14,6 +21,9 @@ const RichTextSnippet: React.FC<{}> = () => {
           value={value}
           onChange={(ev) => {
             setValue(ev.target.value);
+          }}
+          onImageUpload={() => {
+            return wait(1500).then(() => "https://image.com/image.png");
           }}
         />
       </ThemeProvider>
