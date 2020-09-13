bea77f17cb74cf5eafb3e8b75b322eb9c5d0770f
Peter Szerzo <szerzo.peter@gmail.com>
2020-09-13 17:38:21 +0200

    Add color picker to list manager fixture

--- editor.planx.uk/src/ui/fixtures/ListManager.fixture.tsx
+++ editor.planx.uk/src/ui/fixtures/ListManager.fixture.tsx
@@ -1,31 +1,52 @@
 import React, { ChangeEvent, useState } from "react";
+import { Box } from "@material-ui/core";
 import FixtureContainer from "./FixtureContainer";
 import ListManager from "../ListManager";
 import Input from "../Input";
+import ColorPicker from "../ColorPicker";
 
-const Editor = (props: {
-  value: string;
-  onChange: (newVal: string) => void;
-}) => (
-  <Input
-    required
-    value={props.value}
-    onChange={(ev: ChangeEvent<HTMLInputElement>) => {
-      props.onChange(ev.target.value);
-    }}
-    placeholder="Title"
-  />
+interface Item {
+  name: string;
+  color: string;
+}
+
+const Editor = (props: { value: Item; onChange: (newVal: Item) => void }) => (
+  <Box>
+    <Input
+      required
+      value={props.value.name}
+      onChange={(ev: ChangeEvent<HTMLInputElement>) => {
+        props.onChange({
+          ...props.value,
+          name: ev.target.value,
+        });
+      }}
+      placeholder="Title"
+    />
+    <ColorPicker
+      color={props.value.color}
+      onChange={(color) => {
+        props.onChange({
+          ...props.value,
+          color,
+        });
+      }}
+    />
+  </Box>
 );
 
 const Fixture = () => {
-  const [tasks, setTasks] = useState<Array<string>>([]);
+  const [tasks, setTasks] = useState<Array<Item>>([]);
   return (
     <FixtureContainer>
       <ListManager
         values={tasks}
         onChange={setTasks}
         Editor={Editor}
-        newValue={() => "abcd"}
+        newValue={() => ({
+          name: "",
+          color: "#fff",
+        })}
       />
     </FixtureContainer>
   );
