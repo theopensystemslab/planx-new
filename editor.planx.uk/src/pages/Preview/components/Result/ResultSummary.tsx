78b1046c22d53267efe25facfa8c97c54e7a71de
John Rees <john@bitsushi.com>
2020-09-23 14:51:34 +0100

    feat: import styled frontend result from planx-backend

---
+++ editor.planx.uk/src/pages/Preview/components/Result/ResultSummary.tsx
@@ -0,0 +1,86 @@
+import {
+  Box,
+  IconButton,
+  makeStyles,
+  Typography,
+  useTheme,
+} from "@material-ui/core";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import HelpIcon from "@material-ui/icons/HelpOutlineOutlined";
+import classNames from "classnames";
+import React from "react";
+
+const resultSummaryStyles = makeStyles((theme) => ({
+  root: {
+    position: "relative",
+    width: "100%",
+    borderTopWidth: "10px",
+    borderTopStyle: "solid",
+    minHeight: theme.spacing(10),
+    marginBottom: theme.spacing(3),
+  },
+  defaultColor: {
+    backgroundColor: theme.palette.background.paper,
+    color: "currentColor",
+  },
+  helpIcon: {
+    position: "absolute",
+    right: theme.spacing(2),
+    top: theme.spacing(2),
+  },
+  description: {
+    textAlign: "left",
+    fontSize: 15,
+    "& a": {
+      textDecoration: "underline",
+      cursor: "pointer",
+      color: "currentColor",
+      "&:hover": {
+        textDecoration: "none",
+      },
+    },
+    "& p": {
+      marginTop: 0,
+      marginBottom: 10,
+      "&:last-child": {
+        marginBottom: 0,
+      },
+    },
+    "& strong": {
+      fontWeight: 700,
+    },
+  },
+}));
+
+const ResultSummary = ({ heading, subheading, children, color }) => {
+  const theme = useTheme();
+  const classes = resultSummaryStyles();
+  return (
+    <Box
+      className={classNames(classes.root, !color && classes.defaultColor)}
+      bgcolor={color && color.background}
+      color={color && color.text}
+      borderColor={fade(color ? color.text : theme.palette.text.primary, 0.4)}
+      textAlign="center"
+      px={2}
+      pt={6}
+      pb={2}
+    >
+      <Typography variant="h1" gutterBottom>
+        {heading}
+      </Typography>
+      {subheading && (
+        <Typography variant="h5" gutterBottom>
+          {subheading}
+        </Typography>
+      )}
+      <Box className={classes.description}>{children}</Box>
+
+      <IconButton className={classes.helpIcon} color="inherit">
+        <HelpIcon />
+      </IconButton>
+    </Box>
+  );
+};
+
+export default ResultSummary;
---
+++ editor.planx.uk/src/pages/Preview/components/Result/index.tsx
@@ -0,0 +1,95 @@
+import { Box, Button, Typography } from "@material-ui/core";
+import React from "react";
+import ReactHtmlParser from "react-html-parser";
+import Card from "../shared/Card";
+import SimpleExpand from "../shared/SimpleExpand";
+import ResultReason from "./ResultReason";
+import ResultSummary from "./ResultSummary";
+
+interface IResult {
+  handleSubmit;
+  headingColor: {
+    text: string;
+    background: string;
+  };
+  headingTitle?: string;
+  subheading?: string;
+  headingDescription?: string;
+  reasonsTitle?: string;
+  responses: {
+    [key: string]: {
+      text: string;
+    }[];
+  }[];
+}
+
+const Result: React.FC<IResult> = ({
+  handleSubmit,
+  headingColor,
+  headingTitle = "",
+  subheading = "",
+  headingDescription = "",
+  reasonsTitle = "",
+  responses,
+}) => {
+  return (
+    <Card>
+      <ResultSummary
+        subheading={subheading}
+        color={headingColor}
+        heading={headingTitle}
+      >
+        {headingDescription}
+      </ResultSummary>
+      <Box mb={2}>
+        <Typography variant="h3" gutterBottom>
+          {reasonsTitle}
+        </Typography>
+        <Box mb={3}>
+          {responses.map((x, i) => {
+            const key = Object.keys(x) as any;
+            const visibleResponses = x[key].filter((y, j) => j <= 1);
+            const hiddenResponses = x[key].filter((y, j) => j > 1);
+
+            return (
+              <React.Fragment>
+                <Box mb={1} color="text.secondary">
+                  {key}
+                </Box>
+                {visibleResponses.map((y) => {
+                  return <ResultReason>{ReactHtmlParser(y.text)}</ResultReason>;
+                })}
+                {hiddenResponses.length !== 0 ? (
+                  <SimpleExpand
+                    buttonText={{
+                      open: "See all responses",
+                      closed: "See less responses",
+                    }}
+                  >
+                    {hiddenResponses.map((y) => {
+                      return (
+                        <ResultReason>{ReactHtmlParser(y.text)}</ResultReason>
+                      );
+                    })}
+                  </SimpleExpand>
+                ) : null}
+              </React.Fragment>
+            );
+          })}
+        </Box>
+      </Box>
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        onClick={handleSubmit}
+      >
+        Continue
+      </Button>
+      {/* <Box color="text.secondary" pt={2}>
+        You will not be able to make further changes
+      </Box> */}
+    </Card>
+  );
+};
+export default Result;
