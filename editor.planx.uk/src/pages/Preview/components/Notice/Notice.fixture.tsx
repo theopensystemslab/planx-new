2db38920b62e10e25c134240a7b2437024ea68c0
Peter Szerzo <szerzo.peter@gmail.com>
2020-09-24 10:35:09 +0200

    Notice preview component

---
+++ editor.planx.uk/src/pages/Preview/components/Notice/Notice.fixture.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Notice from "./index";
+
+export default (
+  <Notice
+    title="A"
+    description="I want to go *home* and play with my [basketball](https://basketball.org)."
+    color="#000"
+  />
+);
--- editor.planx.uk/src/pages/Preview/components/Notice/index.tsx
+++ editor.planx.uk/src/pages/Preview/components/Notice/index.tsx
@@ -1,5 +1,71 @@
 import React from "react";
+import { ErrorOutline } from "@material-ui/icons";
+import ReactMarkdown from "react-markdown";
+import { Theme, makeStyles } from "@material-ui/core";
+import { mostReadable } from "@ctrl/tinycolor";
 
-const Notice: React.FC<any> = () => <p>Abcd</p>;
+interface Props {
+  title: string;
+  description: string;
+  color?: string;
+}
+
+interface StyleProps {
+  color: string;
+}
+
+const useStyles = makeStyles<Theme, StyleProps>((theme) => ({
+  container: {
+    position: "relative",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "space-between",
+    backgroundColor: (props) => props.color,
+    color: (props) => mostReadable(props.color, ["#fff", "#000"]).toHexString(),
+    "&:before": {
+      content: "' '",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: 10,
+      bottom: 0,
+      backgroundColor: "rgba(255, 255, 255, 0.2)",
+    },
+    padding: theme.spacing(2),
+    paddingLeft: theme.spacing(2) + 10,
+  },
+  content: {
+    flex: 1,
+  },
+  title: {
+    fontSize: theme.typography.pxToRem(25),
+    fontWeight: 700,
+    margin: 0,
+  },
+  description: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: 400,
+    margin: `${theme.spacing(2)}px 0 0 0`,
+    "& a": {
+      color: "inherit",
+    },
+  },
+}));
+
+const Notice: React.FC<Props> = (props) => {
+  const styles = useStyles({ color: props.color || "#EFEFEF" });
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h3 className={styles.title}>{props.title}</h3>
+        <ReactMarkdown
+          className={styles.description}
+          source={props.description}
+        />
+      </div>
+      <ErrorOutline />
+    </div>
+  );
+};
 
 export default Notice;
