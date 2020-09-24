6a78aaae942e88fb173e0b4ddee4ee9607903c47
John Rees <john@bitsushi.com>
2020-09-24 11:10:33 +0100

    add debug console toggle to preview

--- editor.planx.uk/src/pages/FlowEditor/components/PreviewBrowser.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/PreviewBrowser.tsx
@@ -1,9 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import ExternalLink from "react-feather/dist/icons/external-link";
 import RefreshCw from "react-feather/dist/icons/refresh-cw";
+import Terminal from "react-feather/dist/icons/terminal";
+
+const DebugConsole = () => {
+  return <div>DebugConsole</div>;
+};
 
 const PreviewBrowser: React.FC<{ url: string }> = React.memo((props) => {
   const ref = useRef(null);
+  const [showDebugConsole, setDebugConsoleVisibility] = useState(false);
 
   return (
     <div id="fake-browser">
@@ -12,11 +18,20 @@ const PreviewBrowser: React.FC<{ url: string }> = React.memo((props) => {
         <RefreshCw
           onClick={() => ref.current.contentDocument.location.reload()}
         />
-        <a href={props.url} target="_blank" rel="noopener noreferrer">
+        <Terminal
+          onClick={() => setDebugConsoleVisibility(!showDebugConsole)}
+        />
+        <a
+          href={props.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "inherit" }}
+        >
           <ExternalLink />
         </a>
       </header>
       <iframe src={props.url} frameBorder="none" title="Preview" ref={ref} />
+      {showDebugConsole && <DebugConsole />}
     </div>
   );
 });
