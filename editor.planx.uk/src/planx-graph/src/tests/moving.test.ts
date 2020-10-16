85d49a6b183ec3d7dd5a1c646975f7b4e2780a4d
John Rees <john@bitsushi.com>
2020-10-16 12:42:03 +0100

    Update moving.test.ts

--- editor.planx.uk/src/planx-graph/src/tests/moving.test.ts
+++ editor.planx.uk/src/planx-graph/src/tests/moving.test.ts
@@ -85,27 +85,38 @@ describe("cloning nodes", () => {
       },
       b: {},
       c: {},
-      d: {},
+      d: {
+        edges: ["e", "f"],
+      },
+      e: {},
+      f: {},
     });
 
     const ops = graph.move("d", {
       fromParent: "_root",
-      toParent: "a",
+      toParent: "c",
       clone: true,
     });
 
-    expect(ops).toEqual([{ p: ["a", "edges", 2], li: "d" }]);
+    expect(ops).toEqual([
+      { p: ["c", "edges"], oi: [] },
+      { p: ["c", "edges", 0], li: "d" },
+    ]);
 
     expect(graph.toObject()).toMatchObject({
       _root: {
         edges: ["a", "d"],
       },
       a: {
-        edges: ["b", "c", "d"],
+        edges: ["b", "c"],
       },
       b: {},
-      c: {},
-      d: {},
+      c: { edges: ["d"] },
+      d: {
+        edges: ["e", "f"],
+      },
+      e: {},
+      f: {},
     });
   });
 });
