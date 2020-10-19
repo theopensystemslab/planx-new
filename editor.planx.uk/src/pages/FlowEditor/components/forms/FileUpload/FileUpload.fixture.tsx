0119b736ba7dca26e402008203431336956adf9a
Gunar C. Gessner <gunargessner@gmail.com>
2020-10-19 16:08:24 +0100

    feat(ui): dumb File Upload component for the editor

---
+++ editor.planx.uk/src/pages/FlowEditor/components/forms/FileUpload/FileUpload.fixture.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FixtureContainer from "ui/fixtures/FixtureContainer";
+
+import FileUpload from "./FileUpload";
+
+export default (props) => {
+  return (
+    <FixtureContainer>
+      <FileUpload />
+    </FixtureContainer>
+  );
+};
