c3da9b0a214d147607aea5cfba1254d40d64bfe0
Gunar C. Gessner <gunargessner@gmail.com>
2020-10-19 16:07:29 +0100

    feat: add a DnD provider to FixtureContainer
    will need this to create fixtures for our flow components

--- editor.planx.uk/src/ui/fixtures/FixtureContainer.tsx
+++ editor.planx.uk/src/ui/fixtures/FixtureContainer.tsx
@@ -1,9 +1,13 @@
 import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
 import theme from "../../theme";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 export default (props) => (
   <div style={{ padding: 80 }}>
-    <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    <DndProvider backend={HTML5Backend}>
+      <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+    </DndProvider>
   </div>
 );
