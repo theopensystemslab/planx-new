31cd1eff0d2fbdf5615c8423177e69f3a2751f6d
John Rees <john@bitsushi.com>
2020-10-21 21:42:40 +0100

    refactor: change Preview/Node to dynamically load Nodes lazily

--- editor.planx.uk/src/pages/Preview/Node.tsx
+++ editor.planx.uk/src/pages/Preview/Node.tsx
@@ -1,19 +1,18 @@
 import mapAccum from "ramda/src/mapAccum";
-import React from "react";
-
+import React, { Suspense } from "react";
 import { TYPES } from "../FlowEditor/data/types";
 import { useStore } from "../FlowEditor/lib/store";
-import Checklist from "./components/Checklist";
-import Content from "./components/Content";
-import FindProperty from "./components/FindProperty";
-import Notice from "./components/Notice";
-import FileUpload from "./components/FileUpload";
-import PropertyInformation from "./components/PropertyInformation";
-import Question from "./components/Question";
-import Result from "./components/Result";
-import TaskList from "./components/TaskList";
 
-let uprn;
+const LazyNode: React.FC = ({ children }) => (
+  <Suspense fallback={<div>Loading</div>}>{children}</Suspense>
+);
+
+interface NodeType {
+  component?: string;
+  props: () => object;
+}
+
+let uprn; // TODO: move this to zustand state!
 
 const Node: React.FC<any> = (props) => {
   const [childNodesOf, flagResult, responsesForReport] = useStore((state) => [
@@ -22,105 +21,116 @@ const Node: React.FC<any> = (props) => {
     state.responsesForReport,
   ]);
 
-  switch (props.$t) {
-    // check SUPPORTED_TYPES in store
-    case TYPES.Statement:
-      return (
-        <Question
-          node={props.node}
-          handleClick={props.handleSubmit}
-          responses={childNodesOf(props.id).map((n, i) => ({
-            id: n.id,
-            responseKey: i + 1,
-            title: n.text,
-          }))}
-        />
-      );
-    case TYPES.Result:
-      const flag = flagResult();
-
-      return (
-        <Result
-          handleSubmit={props.handleSubmit}
-          headingColor={{
+  const nodeTypes: Record<string, NodeType> = {
+    Statement: {
+      component: "Question",
+      props: () => ({
+        node: props.node,
+        handleClick: props.handleSubmit,
+        responses: childNodesOf(props.id).map((n, i) => ({
+          id: n.id,
+          responseKey: i + 1,
+          title: n.text,
+        })),
+      }),
+    },
+    Result: {
+      props: () => {
+        const flag = flagResult();
+        return {
+          handleSubmit: props.handleSubmit,
+          headingColor: {
             text: flag.color.toHexString(),
             background: flag.bgColor,
-          }}
-          headingTitle={flag.text}
-          subheading=""
-          reasonsTitle="Responses"
-          responses={[
+          },
+          headingTitle: flag.text,
+          subheading: "",
+          reasonsTitle: "Responses",
+          responses: [
             {
               "Planning permission": responsesForReport(),
             },
-          ]}
-        />
+          ],
+        };
+      },
+    },
+    TaskList: {
+      props: () => ({
+        node: props.node,
+        tasks: props.taskList.tasks,
+        handleSubmit: props.handleSubmit,
+      }),
+    },
+    Notice: {
+      props: () => props,
+    },
+    FileUpload: {
+      component: "FileUpload/FileUpload", // dynamic import ignores package.json
+      props: () => props,
+    },
+    Content: {
+      props: () => props,
+    },
+    Checklist: {
+      props: () => {
+        const childNodes = childNodesOf(props.node.id);
+
+        return {
+          node: props.node,
+          info: props.info,
+          text: props.text,
+          description: props.description,
+          allRequired: props.allRequired,
+          handleSubmit: props.handleSubmit,
+          options: props.node.categories ? undefined : childNodes,
+          groupedOptions: !props.node.categories
+            ? undefined
+            : mapAccum(
+                (index: number, category: { title: string; count: number }) => [
+                  index + category.count,
+                  {
+                    title: category.title,
+                    children: childNodes.slice(index, index + category.count),
+                  },
+                ],
+                0,
+                props.node.categories
+              )[1],
+        };
+      },
+    },
+    FindProperty: {
+      props: () => ({
+        handleSubmit: (data) => {
+          uprn = data;
+          props.handleSubmit([props.id]);
+        },
+      }),
+    },
+    PropertyInformation: {
+      props: () => ({
+        UPRN: uprn,
+        handleSubmit: () => props.handleSubmit([props.id]),
+      }),
+    },
+  };
+
+  for (let [type, node] of Object.entries(nodeTypes)) {
+    if (TYPES[type] && props.$t === TYPES[type]) {
+      const Component = React.lazy(
+        () => import(`./components/${node.component || type}`)
       );
-    case TYPES.TaskList:
+      const componentProps = node.props();
       return (
-        <TaskList
-          node={props.node}
-          tasks={props.taskList.tasks}
-          handleSubmit={props.handleSubmit}
-        />
+        <LazyNode>
+          <Component {...componentProps} />
+        </LazyNode>
       );
-    case TYPES.Notice:
-      return <Notice {...props} handleSubmit={props.handleSubmit} />;
-    case TYPES.FileUpload:
-      return <FileUpload {...props} handleSubmit={props.handleSubmit} />;
-    case TYPES.Content:
-      return <Content {...props} handleSubmit={props.handleSubmit} />;
-    case TYPES.Checklist:
-      const childNodes = childNodesOf(props.node.id);
-      return (
-        <Checklist
-          node={props.node}
-          info={props.info}
-          text={props.text}
-          description={props.description}
-          allRequired={props.allRequired}
-          handleSubmit={props.handleSubmit}
-          options={props.node.categories ? undefined : childNodes}
-          groupedOptions={
-            !props.node.categories
-              ? undefined
-              : mapAccum(
-                  (
-                    index: number,
-                    category: { title: string; count: number }
-                  ) => [
-                    index + category.count,
-                    {
-                      title: category.title,
-                      children: childNodes.slice(index, index + category.count),
-                    },
-                  ],
-                  0,
-                  props.node.categories
-                )[1]
-          }
-        />
-      );
-    case TYPES.FindProperty:
-      return (
-        <FindProperty
-          handleSubmit={(data) => {
-            uprn = data;
-            props.handleSubmit([props.id]);
-          }}
-        />
-      );
-    case TYPES.PropertyInformation:
-      return (
-        <PropertyInformation
-          UPRN={uprn}
-          handleSubmit={() => props.handleSubmit([props.id])}
-        />
-      );
-    default:
-      console.error({ nodeNotFound: props });
-      return null;
+    }
   }
+
+  console.error({ nodeNotFound: props });
+  return null;
 };
 
 export default Node;
