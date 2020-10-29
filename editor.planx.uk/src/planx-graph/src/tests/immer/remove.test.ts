adba091007b20f3665e39ddef586734ec74640c8
John Rees <john@bitsushi.com>
2020-10-29 16:43:02 +0000

    immer delete with clones

--- editor.planx.uk/src/planx-graph/src/tests/immer/remove.test.ts
+++ editor.planx.uk/src/planx-graph/src/tests/immer/remove.test.ts
@@ -2,6 +2,11 @@ import produce from "immer";
 import { Graph } from "./types";
 // import clone from "lodash/clone"
 
+const connections = (id, graph): number =>
+  Object.values(graph).filter(({ edges = [] }) => edges.includes(id)).length;
+
+// const isClone = (id, graph): boolean => connections(id, graph) > 1;
+
 const remove = (id: string, parent: string) => (graph = {}): Graph => {
   return produce(graph, (draft) => {
     const _remove = (id, parent) => {
@@ -17,19 +22,42 @@ const remove = (id: string, parent: string) => (graph = {}): Graph => {
       if (draft[parent].edges.length === 0) delete draft[parent].edges;
       if (Object.keys(draft[parent]).length === 0) delete draft[parent];
 
-      if (draft[id].edges) {
-        // must be a copy, for some reason?
-        [...draft[id].edges].forEach((child) => {
-          _remove(child, id);
-        });
+      if (connections(id, draft) === 0) {
+        if (draft[id].edges) {
+          // must be a copy, for some reason?
+          [...draft[id].edges].forEach((child) => {
+            _remove(child, id);
+          });
+        }
+        delete draft[id];
       }
-
-      delete draft[id];
     };
 
     _remove(id, parent);
   });
 };
+
+test("with clones", () => {
+  expect(
+    remove(
+      "a",
+      "_root"
+    )({
+      _root: {
+        edges: ["a", "clone"],
+      },
+      a: {
+        edges: ["clone"],
+      },
+      clone: {},
+    })
+  ).toMatchObject({
+    _root: {
+      edges: ["clone"],
+    },
+    clone: {},
+  });
+});
 
 test("with id", () => {
   expect(
