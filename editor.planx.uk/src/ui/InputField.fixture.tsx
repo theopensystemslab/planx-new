f434a2ff3272fd2fa6087024016d44f1c38bd929
Gunar C. Gessner <gunargessner@gmail.com>
2020-11-03 16:28:33 +0000

    feat(InputField): add fixture

---
+++ editor.planx.uk/src/ui/InputField.fixture.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import InputField from "ui/InputField";
+
+export default (
+  <InputField
+    autoFocus
+    name="text"
+    multiline={true}
+    placeholder="Portal name"
+    rows={2}
+    disabled={false}
+    required={false}
+  />
+);
