64cfe9c60529c93b686e4401b504d39997af2100
John Rees <john@bitsushi.com>
2020-11-04 15:27:52 +0000

    wip snowpack

---
+++ editor.planx.uk/snowpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_",
+  },
+  plugins: [
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+  scripts: {
+    "mount:api": "mount src/api --to /_dist_/api",
+  },
+};
