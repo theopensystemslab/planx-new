28bcd61d6eedb5e1295b73b743e81cc8557c8bf8
Gunar C. Gessner <gunargessner@gmail.com>
2020-11-18 15:02:25 +0000

    fix(seed): INSERT has more target columns than expressions

--- hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
+++ hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
@@ -1,5 +1,5 @@
-INSERT INTO public.teams (id, name, slug, theme, created_at, updated_at) VALUES (1, 'Open Systems Lab', 'opensystemslab', '{}');
-INSERT INTO public.teams (id, name, slug, theme, created_at, updated_at) VALUES (2, 'Canterbury', 'canterbury', '{"logo": "https://editor.planx.uk/logos/canterbury.svg", "primary": "#331035"}');
-INSERT INTO public.users (id, first_name, last_name, email, is_admin, created_at, updated_at) VALUES (1, 'John', 'Rees', 'john@opensystemslab.io', true);
-INSERT INTO public.users (id, first_name, last_name, email, is_admin, created_at, updated_at) VALUES (2, 'Alastair', 'Parvin', 'alastair@opensystemslab.io', true);
-INSERT INTO public.users (id, first_name, last_name, email, is_admin, created_at, updated_at) VALUES (3, 'Gunar', 'Gessner', 'gunargessner@gmail.com', true);
+INSERT INTO public.teams (id, name, slug, theme) VALUES (1, 'Open Systems Lab', 'opensystemslab', '{}');
+INSERT INTO public.teams (id, name, slug, theme) VALUES (2, 'Canterbury', 'canterbury', '{"logo": "https://editor.planx.uk/logos/canterbury.svg", "primary": "#331035"}');
+INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (1, 'John', 'Rees', 'john@opensystemslab.io', true);
+INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (2, 'Alastair', 'Parvin', 'alastair@opensystemslab.io', true);
+INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (3, 'Gunar', 'Gessner', 'gunargessner@gmail.com', true);
