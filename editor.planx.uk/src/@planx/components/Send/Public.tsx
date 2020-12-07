36894ea44da91918d234656d65d502a9c6afce04
John Rees <john@bitsushi.com>
2020-12-07 16:13:37 +0000

    bops WIP

--- editor.planx.uk/src/@planx/components/Send/Public.tsx
+++ editor.planx.uk/src/@planx/components/Send/Public.tsx
@@ -1,12 +1,30 @@
 import axios from "axios";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React, { useEffect } from "react";
-
 import Card from "../shared/Preview/Card";
 import { PublicProps } from "../ui";
 import type { Send } from "./model";
 
 export type Props = PublicProps<Send>;
+
+const placeholderize = (x: any) => {
+  if ((x && typeof x === "string") || x instanceof String) {
+    if (new Date(String(x)).toString() === "Invalid Date") {
+      return "PLACEHOLDER";
+    } else {
+      return new Date().toISOString();
+    }
+  } else if ((x && typeof x === "number") || x instanceof Number) {
+    return 0;
+  } else if ((x && typeof x === "object") || x instanceof Object) {
+    return Object.entries(x).reduce((acc, [k, v]) => {
+      acc[k] = placeholderize(v);
+      return acc;
+    }, x);
+  } else {
+    return x;
+  }
+};
 
 const SendComponent: React.FC<Props> = (props) => {
   const [replay, passport] = useStore((state) => [
