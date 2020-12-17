c0ce9709cbae62262e43a0ad957a6cb49d3059cd
John Rees <john@bitsushi.com>
2020-12-17 20:39:09 +0000

    hide preview by default

--- editor.planx.uk/src/pages/FlowEditor/lib/store.ts
+++ editor.planx.uk/src/pages/FlowEditor/lib/store.ts
@@ -96,7 +96,7 @@ export const vanillaStore = vanillaCreate<Store>((set, get) => ({
 
   id: undefined,
 
-  showPreview: true,
+  showPreview: false,
 
   togglePreview: () => {
     set({ showPreview: !get().showPreview });
