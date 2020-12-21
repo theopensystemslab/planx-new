7d91e4a0ab39889211627b873fb28bbf15e4550d
John Rees <john@bitsushi.com>
2020-12-21 15:44:18 +0000

    add http-proxy-middleware for bops endpoint

--- api.planx.uk/server.js
+++ api.planx.uk/server.js
@@ -200,6 +200,19 @@ app.use("/bops/:localAuthority", (req, res) =>
 );
 
 app.use(
+  "/bops",
+  createProxyMiddleware({
+    pathRewrite: {
+      "^/bops": "",
+    },
+    target:
+      "https://southwark.preview.bops.services/api/v1/planning_applications",
+    changeOrigin: true,
+    logLevel: "debug",
+  })
+);
+
+app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 100,
     name: "session",
