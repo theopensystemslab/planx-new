cd683788091596ad880cf1725cc5fa9ac90c34f4
John Rees <john@bitsushi.com>
2021-03-30 17:48:02 +0100

    fix: remove ids from seeds to avoid not-unique sequence error

--- hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
+++ hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
@@ -1,5 +1,5 @@
-INSERT INTO public.teams (id, name, slug, theme) VALUES (1, 'Open Systems Lab', 'opensystemslab', '{}');
-INSERT INTO public.teams (id, name, slug, theme) VALUES (2, 'Canterbury', 'canterbury', '{"logo": "https://editor.planx.uk/logos/canterbury.svg", "primary": "#331035"}');
-INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (1, 'John', 'Rees', 'john@opensystemslab.io', true);
-INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (2, 'Alastair', 'Parvin', 'alastair@opensystemslab.io', true);
-INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (3, 'Gunar', 'Gessner', 'gunargessner@gmail.com', true);
+INSERT INTO public.teams (name, slug, theme) VALUES ('Open Systems Lab', 'opensystemslab', '{}');
+INSERT INTO public.teams (name, slug, theme) VALUES ('Canterbury', 'canterbury', '{"logo": "https://editor.planx.uk/logos/canterbury.svg", "primary": "#331035"}');
+INSERT INTO public.users (first_name, last_name, email, is_admin) VALUES ('John', 'Rees', 'john@opensystemslab.io', true);
+INSERT INTO public.users (first_name, last_name, email, is_admin) VALUES ('Alastair', 'Parvin', 'alastair@opensystemslab.io', true);
+INSERT INTO public.users (first_name, last_name, email, is_admin) VALUES ('Gunar', 'Gessner', 'gunargessner@gmail.com', true);
