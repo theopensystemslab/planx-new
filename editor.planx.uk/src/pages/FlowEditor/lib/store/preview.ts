1ada8ffb8c3879c0fbcbac3ad84697f740ce27ec
John Rees <john@bitsushi.com>
2021-04-13 17:15:48 +0100

    update computePassport

--- editor.planx.uk/src/pages/FlowEditor/lib/store/preview.ts
+++ editor.planx.uk/src/pages/FlowEditor/lib/store/preview.ts
@@ -31,7 +31,7 @@ export interface PreviewStore extends Store.Store {
     {
       answers,
       data,
-    }?: { answers: Store.userData["answers"]; data?: Store.userData["data"] }
+    }?: { answers?: Store.userData["answers"]; data?: Store.userData["data"] }
   ) => void;
   resultData: (
     flagSet?: string,
@@ -131,12 +131,10 @@ export const previewStore = (
           ...acc,
           data: {
             ...acc.data,
-            ...Object.entries(data).reduce(
-              (_acc, [id, value]) => ({
-                [id]: { value },
-              }),
-              {}
-            ),
+            ...Object.entries(data).reduce((_acc, [id, value]) => {
+              _acc![id] = { value };
+              return _acc;
+            }, {} as Store.passport["data"]),
           },
         };
       },
