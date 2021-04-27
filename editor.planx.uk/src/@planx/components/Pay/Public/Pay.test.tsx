a3136924140bd09210fc26d6bf3824ca9af3008a
Sarah Scott <sarahoscott@gmail.com>
2021-04-27 14:25:36 -0700

    basic test

---
+++ editor.planx.uk/src/@planx/components/Pay/Public/Pay.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import Pay from "./Pay";
+
+it("renders correctly", () => {
+  render(<Pay handleSubmit={jest.fn()} url={"test-url"} />);
+
+  const title = screen.getByText("Pay for your application");
+  expect(title).toBeTruthy();
+});
