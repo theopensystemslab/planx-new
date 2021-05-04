cf85732e6b2f579d7b1aa367d2033b4ed0db204b
John Rees <john@bitsushi.com>
2021-05-04 17:43:32 +0100

    add test for public addressinput component

---
+++ editor.planx.uk/src/@planx/components/AddressInput/Public.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+
+import { fillInFieldsUsingPlaceholder } from "../shared/testHelpers";
+import AddressInput from "./Public";
+
+test("submits an address", async () => {
+  const handleSubmit = jest.fn();
+
+  render(<AddressInput handleSubmit={handleSubmit} title="" fn="foo" />);
+
+  await waitFor(async () => {
+    await fillInFieldsUsingPlaceholder({
+      "Line 1": "Flat 1",
+      "Line 2": "221b Baker St",
+      Town: "London",
+      County: "County",
+      "Postal code": "SW1A 2AA",
+    });
+
+    await userEvent.click(screen.getByText("Continue"));
+  });
+
+  expect(handleSubmit).toHaveBeenCalledWith({
+    data: {
+      foo: {
+        line1: "Flat 1",
+        line2: "221b Baker St",
+        town: "London",
+        county: "County",
+        postcode: "SW1A 2AA",
+      },
+    },
+  });
+});
