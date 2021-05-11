1f95a6162cefacb43704b54c11a54666b8f7008c
Sarah Scott <sarahoscott@gmail.com>
2021-05-11 16:13:50 -0700

    localStorage module

---
+++ editor.planx.uk/src/lib/local.ts
@@ -0,0 +1,19 @@
+export const getLocalFlow = (id: string) => {
+  const entry = `flow:${id}`;
+
+  try {
+    const state = localStorage.getItem(entry);
+    if (state) return JSON.parse(state);
+  } catch (e) {
+    // Clean up just in case
+    localStorage.removeItem(entry);
+  }
+};
+
+export const setLocalFlow = (id: string, args: { [key: string]: any }) => {
+  localStorage.setItem(`flow:${id}`, JSON.stringify(args));
+};
+
+export const clearLocalFlow = (id: string) => {
+  localStorage.removeItem(`flow:${id}`);
+};
