e1d1b9969dddf171b8a92714e5fe41bac188317c
John Rees <john@bitsushi.com>
2021-05-13 18:30:20 +0100

    console.error error inside ErrorFallback component

--- editor.planx.uk/src/components/ErrorFallback.tsx
+++ editor.planx.uk/src/components/ErrorFallback.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 function ErrorFallback(props: { error: Error }) {
+  console.error(props.error);
+
   return (
     <div role="alert">
       <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{props.error.message}</pre>
+      {props.error?.message && (
+        <pre style={{ color: "red" }}>{props.error.message}</pre>
+      )}
       <p>
         This bug has been automatically logged and our team will see it soon.
       </p>
