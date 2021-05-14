1a4fdb8cb9cbc520a1732f317def12a64b67a8d3
Gunar C. Gessner <gunar@gunargessner.com>
2021-05-14 10:18:51 +0100

    chore: assert env vars exist before starting

--- api.planx.uk/s3.js
+++ api.planx.uk/s3.js
@@ -1,20 +1,24 @@
+const assert = require('assert')
 const S3 = require("aws-sdk/clients/s3");
 const { customAlphabet } = require("nanoid");
 const { getType } = require("mime");
 
 const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 8);
 
+assert(process.env.AWS_S3_BUCKET);
+assert(process.env.AWS_S3_REGION);
+assert(process.env.AWS_ACCESS_KEY);
+assert(process.env.AWS_SECRET_KEY);
+
 const signS3Upload = (filename) =>
   new Promise((res, rej) => {
-    const opts = {
+    const s3 = new S3({
       // apiVersion: "2006-03-01",
       params: { Bucket: process.env.AWS_S3_BUCKET },
       region: process.env.AWS_S3_REGION,
-
       accessKeyId: process.env.AWS_ACCESS_KEY,
       secretAccessKey: process.env.AWS_SECRET_KEY,
-    };
-    const s3 = new S3(opts);
+    });
 
     const fileType = getType(filename);
     const key = `${process.env.NODE_ENV}/${nanoid()}/${filename}`;
