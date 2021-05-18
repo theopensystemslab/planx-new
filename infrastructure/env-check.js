188cda705dc02e9b430bee8a73c9ac3318bbfa2d
John Rees <john@bitsushi.com>
2021-05-18 20:20:35 +0100

    v2

--- infrastructure/env-check.js
+++ infrastructure/env-check.js
@@ -1,20 +1,18 @@
 const fs = require("fs");
 
-const mightBeEnv = /[A-Z][A-Z0-9_]{3,}/g;
-
 const possibleDockerEnvVars = new Set(
   fs
     .readdirSync("..")
     .filter((fn) => fn.startsWith("docker-"))
-    .flatMap((file) => {
-      const data = fs.readFileSync(`../${file}`);
-      return data.toString().match(mightBeEnv);
-    })
+    .flatMap((file) => extractPossibleEnvVars(`../${file}`))
 );
 
-const possiblePulumiEnvVars = new Set(
-  fs.readFileSync(`./application/index.ts`).toString().match(mightBeEnv)
-);
+const possiblePulumiEnvVars = new Set([
+  ...extractPossibleEnvVars(`./application/index.ts`),
+  ...extractPossibleEnvVars(`./certificates/index.ts`),
+  ...extractPossibleEnvVars(`./data/index.ts`),
+  ...extractPossibleEnvVars(`./networking/index.ts`)
+]);
 
 const inDockerAndPulumi = new Set(
   [...possibleDockerEnvVars].filter((x) => possiblePulumiEnvVars.has(x))
@@ -29,3 +27,8 @@ const inPulumiOnly = new Set(
 );
 
 console.log({ inDockerAndPulumi, inDockerOnly, inPulumiOnly });
+
+function extractPossibleEnvVars(fileName) {
+  const data = fs.readFileSync(fileName);
+  return data.toString().match(/[A-Z][A-Z0-9_]{3,}/g) || [];
+}
