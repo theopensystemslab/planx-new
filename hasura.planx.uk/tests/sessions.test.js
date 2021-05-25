eb1115f9525dff38d32d1336a097590c28a8fa17
jessica <jessica.mcinchak@gmail.com>
2021-05-25 20:45:03 +0200

    add initial teams tests

--- hasura.planx.uk/tests/sessions.test.js
+++ hasura.planx.uk/tests/sessions.test.js
@@ -1,5 +1,4 @@
 const assert = require("assert");
-const { hasUncaughtExceptionCaptureCallback } = require("process");
 
 const { gqlAdmin, gqlPublic } = require("./utils");
 
---
+++ hasura.planx.uk/tests/teams.test.js
@@ -0,0 +1,54 @@
+const assert = require("assert");
+
+const { gqlAdmin, gqlPublic } = require("./utils");
+
+describe("teams", () => {
+  let teamId;
+
+  beforeAll(async () => {
+    ({
+      data: {
+        insert_teams: {
+          returning: [{ id: teamId }],
+        },
+      },
+    } = await gqlAdmin(`
+      mutation InsertTeam {
+        insert_teams(objects: {slug: "test", name: "Test"}) {
+          returning { id }
+        }
+      }
+    `));
+    assert(teamId);
+  });
+
+  afterAll(async () => {
+    // XXX: We're assuming that if we delete the team, then
+    //      deletion will cascade into team_members
+    assert.strictEqual(
+      (
+        await gqlAdmin(
+          `mutation DeleteTeamCascade {delete_teams(where: {id: {_eq: "${teamId}"}}) { affected_rows }}`
+        )
+      ).data.delete_flows.affected_rows,
+      1
+    );
+  });
+
+  test("only admin can query relational team_members", async () => {
+    query = `
+      query GetTeamWithMembers {
+        teams(where: {id: {_eq: "${teamId}"}}) {
+          id
+          name
+          members {
+            user_id
+          }
+        }
+      }
+    `;
+
+    assert((await gqlAdmin(query)).data.teams);
+    assert((await gqlPublic(query)).errors[0].message, `field "members" not found in type: 'teams'`);
+  });
+});
