008e76104842e938f091671646db36a3a7e73e2a
Gunar C. Gessner <gunar@gunargessner.com>
2021-06-17 10:56:06 +0100

    chore: notify airbrake when the error boundary catches something
    See https://trello.com/c/9SuOACbR/1279-map-is-not-a-function-error
    which was not recorded on AirBrake.

---
+++ editor.planx.uk/src/airbrake.ts
@@ -0,0 +1,13 @@
+import { Notifier } from "@airbrake/browser";
+
+// forward all JS errors to airbrake.io
+export const notifier =
+  process.env.NODE_ENV !== "test" &&
+  process.env.REACT_APP_AIRBRAKE_PROJECT_ID &&
+  process.env.REACT_APP_AIRBRAKE_PROJECT_KEY
+    ? new Notifier({
+        projectId: Number(process.env.REACT_APP_AIRBRAKE_PROJECT_ID),
+        projectKey: process.env.REACT_APP_AIRBRAKE_PROJECT_KEY,
+        environment: "production",
+      })
+    : undefined;
