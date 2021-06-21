519f4af40ac81cbd8946ca579bfb664869cba7cd
John Rees <john@bitsushi.com>
2021-06-21 17:04:44 +0100

    fix: send payment_amount as integer in pence to bops

--- editor.planx.uk/src/@planx/components/Send/bops.ts
+++ editor.planx.uk/src/@planx/components/Send/bops.ts
@@ -8,7 +8,7 @@ import { flatFlags } from "pages/FlowEditor/data/flags";
 import { GovUKPayment } from "types";
 
 import { Store } from "../../../pages/FlowEditor/lib/store";
-import { GOV_PAY_PASSPORT_KEY } from "../Pay/model";
+import { GOV_PAY_PASSPORT_KEY, toPence } from "../Pay/model";
 import { TYPES } from "../types";
 
 interface BOPSMinimumPayload {
@@ -283,7 +283,7 @@ export function getParams(
 
   const payment = passport?.data?.[GOV_PAY_PASSPORT_KEY] as GovUKPayment;
   if (payment) {
-    data.payment_amount = payment.amount;
+    data.payment_amount = toPence(payment.amount);
     data.payment_reference = payment.payment_id;
   }
 
