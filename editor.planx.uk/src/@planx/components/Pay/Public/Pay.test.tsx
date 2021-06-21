0f774b0620d379e37d81e671185f880d8df1b4b4
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-06-21 19:01:03 +0200

    oops remove unused deps

--- editor.planx.uk/src/@planx/components/Pay/Public/Pay.test.tsx
+++ editor.planx.uk/src/@planx/components/Pay/Public/Pay.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
 import React from "react";
 
 import Pay from "./Pay";
