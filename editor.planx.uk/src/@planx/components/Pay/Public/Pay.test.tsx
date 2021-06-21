54846c907536849649e4cdf00e641e8ed6c1f4c3
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-06-21 18:58:47 +0200

    pay test passes for £0 fee case

--- editor.planx.uk/src/@planx/components/Pay/Public/Pay.test.tsx
+++ editor.planx.uk/src/@planx/components/Pay/Public/Pay.test.tsx
@@ -1,11 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 
 import Pay from "./Pay";
 
-it("renders correctly", () => {
-  render(<Pay handleSubmit={jest.fn()} title="Pay for your application" />);
+it("renders correctly with <= £0 fee", () => {
+  const handleSubmit = jest.fn();
 
-  const title = screen.getByText("Pay for your application");
-  expect(title).toBeTruthy();
+  // if no props.fn, then fee defaults to 0
+  render(<Pay handleSubmit={handleSubmit} />);
+
+  // handleSubmit is still called to set auto = true so Pay isn't seen in card sequence
+  expect(handleSubmit).toHaveBeenCalled();
 });
