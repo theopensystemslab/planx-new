66b89131c2716ade7bf27a3f36829fe7e05761d0
John Rees <john@bitsushi.com>
2021-06-22 14:10:40 +0100

    move store.resultData logic to getResultData to share functionality

--- editor.planx.uk/src/pages/FlowEditor/lib/store/preview.ts
+++ editor.planx.uk/src/pages/FlowEditor/lib/store/preview.ts
@@ -292,88 +292,7 @@ export const previewStore = (
 
   resultData(flagSet = DEFAULT_FLAG_CATEGORY, overrides) {
     const { breadcrumbs, flow } = get();
-
-    const categories = [flagSet];
-
-    return categories.reduce(
-      (
-        acc: {
-          [category: string]: {
-            flag: Flag;
-            responses: any[];
-            displayText: { heading: string; description: string };
-          };
-        },
-        category: string
-      ) => {
-        // TODO: DRY this up with preceding collectedFlags function?
-        const possibleFlags = flatFlags.filter((f) => f.category === category);
-        const keys = possibleFlags.map((f) => f.value);
-        const collectedFlags = Object.values(
-          breadcrumbs
-        ).flatMap(({ answers = [] }) =>
-          answers.map((id) => flow[id]?.data?.flag)
-        );
-
-        const filteredCollectedFlags = collectedFlags
-          .filter((flag) => flag && keys.includes(flag))
-          .sort((a, b) => keys.indexOf(a) - keys.indexOf(b));
-
-        const flag = possibleFlags.find(
-          (f) => f.value === filteredCollectedFlags[0]
-        ) || {
-          // value: "PP-NO_RESULT",
-          value: undefined,
-          text: "No result",
-          category,
-          bgColor: "#EEEEEE",
-          color: tinycolor("black").toHexString(),
-        };
-
-        const responses = Object.entries(breadcrumbs)
-          .map(([k, { answers = [] }]) => {
-            const question = { id: k, ...flow[k] };
-
-            const questionType = question?.type;
-
-            if (
-              !questionType ||
-              !SUPPORTED_DECISION_TYPES.includes(questionType)
-            )
-              return null;
-
-            const selections = answers.map((id) => ({ id, ...flow[id] }));
-            const hidden = !selections.some(
-              (r) => r.data?.flag && r.data.flag === flag?.value
-            );
-
-            return {
-              question,
-              selections,
-              hidden,
-            };
-          })
-          .filter(Boolean);
-
-        const heading =
-          (flag.value && overrides && overrides[flag.value]?.heading) ||
-          flag.text;
-        const description =
-          (flag.value && overrides && overrides[flag.value]?.description) ||
-          flagSet;
-
-        acc[category] = {
-          flag,
-          displayText: { heading, description },
-          responses: responses.every((r: any) => r.hidden)
-            ? responses.map((r: any) => ({ ...r, hidden: false }))
-            : responses,
-        };
-
-        return acc;
-      },
-      {}
-    );
+    return getResultData(breadcrumbs, flow, flagSet, overrides);
   },
 
   resumeSession(args) {
@@ -679,3 +598,89 @@ const knownNots = (
       ...nots,
     } as Record<string, Array<string>>
   );
+
+export const getResultData = (
+  breadcrumbs: Store.breadcrumbs,
+  flow: Store.flow,
+  flagSet?: any,
+  overrides?: any
+) => {
+  const categories = [flagSet];
+
+  return categories.reduce(
+    (
+      acc: {
+        [category: string]: {
+          flag: Flag;
+          responses: any[];
+          displayText: { heading: string; description: string };
+        };
+      },
+      category: string
+    ) => {
+      // TODO: DRY this up with preceding collectedFlags function?
+      const possibleFlags = flatFlags.filter((f) => f.category === category);
+      const keys = possibleFlags.map((f) => f.value);
+      const collectedFlags = Object.values(
+        breadcrumbs
+      ).flatMap(({ answers = [] }) =>
+        answers.map((id) => flow[id]?.data?.flag)
+      );
+
+      const filteredCollectedFlags = collectedFlags
+        .filter((flag) => flag && keys.includes(flag))
+        .sort((a, b) => keys.indexOf(a) - keys.indexOf(b));
+
+      const flag = possibleFlags.find(
+        (f) => f.value === filteredCollectedFlags[0]
+      ) || {
+        // value: "PP-NO_RESULT",
+        value: undefined,
+        text: "No result",
+        category,
+        bgColor: "#EEEEEE",
+        color: tinycolor("black").toHexString(),
+      };
+
+      const responses = Object.entries(breadcrumbs)
+        .map(([k, { answers = [] }]) => {
+          const question = { id: k, ...flow[k] };
+
+          const questionType = question?.type;
+
+          if (!questionType || !SUPPORTED_DECISION_TYPES.includes(questionType))
+            return null;
+
+          const selections = answers.map((id) => ({ id, ...flow[id] }));
+          const hidden = !selections.some(
+            (r) => r.data?.flag && r.data.flag === flag?.value
+          );
+
+          return {
+            question,
+            selections,
+            hidden,
+          };
+        })
+        .filter(Boolean);
+
+      const heading =
+        (flag.value && overrides && overrides[flag.value]?.heading) ||
+        flag.text;
+      const description =
+        (flag.value && overrides && overrides[flag.value]?.description) ||
+        flagSet;
+
+      acc[category] = {
+        flag,
+        displayText: { heading, description },
+        responses: responses.every((r: any) => r.hidden)
+          ? responses.map((r: any) => ({ ...r, hidden: false }))
+          : responses,
+      };
+
+      return acc;
+    },
+    {}
+  );
+};
