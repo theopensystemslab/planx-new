9081eb63dda00e15a9aa33ac5c8c140ca161d382
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-06-22 13:14:10 +0200

    quick & simple network page with link back to index route

---
+++ editor.planx.uk/src/pages/NetworkError.tsx
@@ -0,0 +1,45 @@
+import Box from "@material-ui/core/Box";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import { Link } from "react-navi";
+
+const useStyles = makeStyles<Theme>((theme) => ({
+  root: {
+    backgroundColor: "#fff",
+    color: "#2C2C2C",
+    width: "100%",
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  dashboard: {
+    backgroundColor: "#fff",
+    color: "#2C2C2C",
+    width: "100%",
+    maxWidth: 600,
+    margin: "auto",
+    padding: theme.spacing(8, 0, 4, 0),
+  },
+}));
+
+const NetworkError: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.dashboard}>
+        <Box pl={2} pb={2}>
+          <Typography variant="h1" gutterBottom>
+            Network error
+          </Typography>
+          <Typography variant="body1">
+            Something went wrong. <Link href={"/"}>Go back home.</Link>
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default NetworkError;
