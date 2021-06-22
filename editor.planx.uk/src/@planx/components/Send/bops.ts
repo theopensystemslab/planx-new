7ed6304862afe8d2e3020e7100190083557a0a8c
John Rees <john@bitsushi.com>
2021-06-22 20:35:29 +0100

    refactor: move various types to Send/model

--- editor.planx.uk/src/@planx/components/Send/bops.ts
+++ editor.planx.uk/src/@planx/components/Send/bops.ts
@@ -12,73 +12,13 @@ import { Store } from "../../../pages/FlowEditor/lib/store";
 import { PASSPORT_UPLOAD_KEY } from "../DrawBoundary/model";
 import { GOV_PAY_PASSPORT_KEY, toPence } from "../Pay/model";
 import { TYPES } from "../types";
-
-interface BOPSMinimumPayload {
-  application_type: "lawfulness_certificate";
-  site: {
-    uprn: string;
-    address_1: string;
-    address_2?: string;
-    town: string;
-    postcode: string;
-  };
-}
-
-export interface BOPSFullPayload extends BOPSMinimumPayload {
-  description?: string;
-  payment_reference?: string;
-  payment_amount?: number;
-  session_id?: string;
-  ward?: string;
-  work_status?: "proposed" | "existing";
-  applicant_first_name?: string;
-  applicant_last_name?: string;
-  applicant_phone?: string;
-  applicant_email?: string;
-  agent_first_name?: string;
-  agent_last_name?: string;
-  agent_phone?: string;
-  agent_email?: string;
-  proposal_details?: Array<QuestionAndResponses>;
-  constraints?: Record<string, boolean>;
-  files?: Array<File>;
-  boundary_geojson?: Object;
-  result?: {
-    flag?: string;
-    heading?: string;
-    description?: string;
-    override?: string;
-  };
-}
-
-interface QuestionMetaData {
-  notes?: string;
-  auto_answered?: boolean;
-  policy_refs?: Array<{
-    url?: string;
-    text?: string;
-  }>;
-}
-
-interface ResponseMetaData {
-  flags?: Array<string>;
-}
-
-interface Response {
-  value: string;
-  metadata?: ResponseMetaData;
-}
-
-interface QuestionAndResponses {
-  question: string;
-  metadata?: QuestionMetaData;
-  responses: Array<Response>;
-}
-
-interface File {
-  filename: string;
-  tags?: Array<string>;
-}
+import type {
+  BOPSFullPayload,
+  QuestionAndResponses,
+  QuestionMetaData,
+  Response,
+  ResponseMetaData,
+} from "./model";
 
 export const bopsDictionary = {
   applicant_first_name: "applicant.name.first",
