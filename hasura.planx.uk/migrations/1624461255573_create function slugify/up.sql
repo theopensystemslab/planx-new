9356d49de5b8818566a5f3e47a0cee80ebcb2ab0
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-06-28 09:15:39 +0200

    pg slugify replaces underscores with hyphens too

--- hasura.planx.uk/migrations/1624461255573_create function slugify/up.sql
+++ hasura.planx.uk/migrations/1624461255573_create function slugify/up.sql
@@ -9,9 +9,9 @@ RETURNS TEXT AS $$
     SELECT regexp_replace("value", '[''"]+', '', 'gi') AS "value"
     FROM "lowercase"
   ),
-  -- replaces anything that's not a letter, number, hyphen('-'), or underscore('_') with a hyphen('-')
+  -- replaces anything that's not a letter, number or hyphen('-') with a hyphen('-')
   "hyphenated" AS (
-    SELECT regexp_replace("value", '[^a-z0-9\\-_]+', '-', 'gi') AS "value"
+    SELECT regexp_replace("value", '[^a-z0-9\\-]+', '-', 'gi') AS "value"
     FROM "removed_quotes"
   ),
   -- trims hyphens('-') if they exist on the head or tail of the string
