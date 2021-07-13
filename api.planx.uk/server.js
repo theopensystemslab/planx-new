0ece0c70c617f232fa3e83009c426846d072807c
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-07-13 13:06:17 +0200

    add pay response test & return minimum shape Pay component expects

--- api.planx.uk/server.js
+++ api.planx.uk/server.js
@@ -336,7 +336,11 @@ app.get("/pay/:localAuthority/:paymentId", (req, res) => {
           const govUkResponse = JSON.parse(responseBuffer.toString("utf8"));
 
           // only return payment status, filter out PII
-          return JSON.stringify(govUkResponse.state);
+          return JSON.stringify({ 
+            payment_id: govUkResponse.payment_id,
+            amount: govUkResponse.amount,
+            state: govUkResponse.state,
+          });
         }
       )
     },
