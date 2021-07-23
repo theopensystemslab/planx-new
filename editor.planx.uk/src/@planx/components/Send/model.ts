2efeb2b097d02de961d26872020bf6e263937ff4
John Rees <john@bitsushi.com>
2021-07-23 12:17:35 +0100

    feat: include applicant_description with file for BOPS

--- editor.planx.uk/src/@planx/components/Send/model.ts
+++ editor.planx.uk/src/@planx/components/Send/model.ts
@@ -104,4 +104,21 @@ export type FileTag = PlanTag | EvidenceTag;
 interface File {
   filename: string;
   tags?: Array<FileTag>;
+  applicant_description?: string;
+}
+
+// Extracted from:
+// https://github.com/unboxed/bops/blob/master/app/models/document.rb
+export enum BOPS_TAGS {
+  Front = "Front",
+  Rear = "Rear",
+  Side = "Side",
+  Roof = "Roof",
+  Floor = "Floor",
+  Site = "Site",
+  Plan = "Plan",
+  Elevation = "Elevation",
+  Section = "Section",
+  Proposed = "Proposed",
+  Existing = "Existing",
 }
