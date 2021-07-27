ee60454fd41c275698ef6168239bbcf5d2cead3b
Gunar C. Gessner <gunar@gunargessner.com>
2021-07-27 12:07:31 +0100

    chore: remove unused components Report and SignIn

--- editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Node.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Node.tsx
@@ -114,8 +114,6 @@ const Node: React.FC<any> = (props) => {
     case TYPES.AddressInput:
       return <Question {...allProps} text={node?.data?.title ?? "Address"} />;
     case TYPES.Flow:
-    case TYPES.Report:
-    case TYPES.SignIn:
       return null;
     default:
       console.error({ nodeNotFound: props });
