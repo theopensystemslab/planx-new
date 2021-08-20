27e433fecdd78d0600580877fc3505aa6d7865cd
John Rees <john@bitsushi.com>
2021-08-20 11:39:49 +0100

    1.4, remove webhooks

---
+++ scripts/pullrequest/create.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+echo $SSH_PASSWORD | passwd --stdin root
+
+apt-get update -y
+apt update -y
+
+# create swap
+# https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-20-04
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+
+# install docker
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# download repo
+ git clone $REPO_URL
+
+# ----------------------------------------------------------------
+
+# setup
+cd planx-new
+
+git fetch origin pull/${PULLREQUEST_ID}/head && git checkout FETCH_HEAD
+cat .env .env.staging > .env.prod
+
+ROOT_DOMAIN=$(hostname) TLS_EMAIL=devops@opensystemslab.io docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build
+
+# install hasura cli
+curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh | bash
+cd hasura.planx.uk
+hasura seed apply
