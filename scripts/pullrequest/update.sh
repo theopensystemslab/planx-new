9968f927320714413b5b970ab1bcebaff65f970c
John Rees <john@bitsushi.com>
2021-08-20 11:44:04 +0100

    Create update.sh

---
+++ scripts/pullrequest/update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# docker system prune --volumes
+# PULLREQUEST_ID=$(hostname | tr -dc '0-9')
+
+git add . && git stash
+
+git fetch origin pull/${PULLREQUEST_ID}/head && git checkout FETCH_HEAD
+
+cat .env .env.staging > .env.prod
+
+docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml build --no-cache
+
+docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up -d
