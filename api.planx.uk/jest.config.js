18f2c494240218c73d7260d132e0768f9885da23
John Rees <john@bitsushi.com>
2021-08-24 17:58:20 +0100

    esbuild-jest api

---
+++ api.planx.uk/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  transform: {
+    "^.+\\.js$": [
+      "esbuild-jest",
+      {
+        sourcemap: true,
+      },
+    ],
+  },
+  setupFilesAfterEnv: ["./jest.setup.js"],
+};
