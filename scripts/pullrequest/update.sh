3bb4d5277796617883e544ed379966527f854b7d
John Rees <john@bitsushi.com>
2021-08-25 19:36:11 +0100

    uncomment

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -1,10 +1,4 @@
-#!/usr/bin/env bash
-
-# docker system prune --volumes
-# PULLREQUEST_ID=$(hostname | tr -dc '0-9')
-
-# git add . && git stash
-# git fetch origin pull/${PULLREQUEST_ID}/head && git checkout FETCH_HEAD
+#!/bin/sh
 
 cat .env .env.staging > .env.prod
 
