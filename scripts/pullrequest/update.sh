f60dd26a2e0f6378eec76a3baa246a862c47eb9b
John Rees <john@bitsushi.com>
2021-08-25 21:05:47 +0100

    more env

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -2,4 +2,4 @@
 
 cat .env .env.staging > .env.prod
 
-DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --remove-orphans -d
+JWT_SECRET=${JWT_SECRET} SESSION_SECRET=${SESSION_SECRET} DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --remove-orphans -d
