82ea62644e8edaac415602190c233a87c265513d
John Rees <john@bitsushi.com>
2021-08-25 12:57:47 +0100

    try env.REACT_APP_GOOGLE_OAUTH_OVERRIDE

--- editor.planx.uk/src/pages/Login.tsx
+++ editor.planx.uk/src/pages/Login.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Login: React.FC = () => {
   return (
-    <a href={`${process.env.REACT_APP_API_URL}/auth/google`}>
+    <a
+      href={`${
+        process.env.REACT_APP_GOOGLE_OAUTH_OVERRIDE ??
+        process.env.REACT_APP_API_URL
+      }/auth/google`}
+    >
       Login with Google
     </a>
   );
