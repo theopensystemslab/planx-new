e02c5b9ac07002ecdfbfcbb27b7520ba3c9a986c
John Rees <john@bitsushi.com>
2021-08-26 23:54:26 +0100

    tweak update

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-cat .env .env.staging > .env.prod
-
-JWT_SECRET=${JWT_SECRET} SESSION_SECRET=${SESSION_SECRET} DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --remove-orphans -d
+DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --remove-orphans -d
