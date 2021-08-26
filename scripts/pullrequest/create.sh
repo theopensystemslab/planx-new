0143c2fd89bc82dc7d072cd93c787555fb85b0d1
John Rees <john@bitsushi.com>
2021-08-26 23:54:26 +0100

    tweak update

--- scripts/pullrequest/create.sh
+++ scripts/pullrequest/create.sh
@@ -30,8 +30,6 @@ chmod +x /usr/local/bin/docker-compose
 # cd planx-new
 # git fetch origin pull/${PULLREQUEST_ID}/head && git checkout FETCH_HEAD
 
-cat .env .env.staging > .env.prod
-
 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build -d
 
 # install hasura cli
