8fee28f9698a0ccf8314ddd2a4550db21b417926
John Rees <john@bitsushi.com>
2021-09-08 16:54:44 +0200

    add create and update tasks for vultr staging builds

---
+++ scripts/pullrequest/create.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "root:$SSH_PASSWORD" | chpasswd
+
+apt-get update -y
+
+# create swap
+# https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-20-04
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+
+# install docker
+apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build -d
+
+# install hasura cli
+curl -L https://github.com/hasura/graphql-engine/raw/stable/cli/get.sh | bash
+cd hasura.planx.uk
+hasura seed apply
