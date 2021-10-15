a66f0063e797814dce939209dfe94031956f8a01
John Rees <johnrees@users.noreply.github.com>
2021-10-15 13:36:03 +0100

    api error handling (#660)
    adds some error handling on the API and forwards uncaught errors to airbrake for debugging purposes

---
+++ api.planx.uk/tests/serverErrorHandler.test.js
@@ -0,0 +1,65 @@
+const supertest = require("supertest");
+const app = require("../server");
+
+const { get, post } = supertest(app);
+
+describe("bad requests", () => {
+  test.skip(`app.post("/bops/:localAuthority")`, (done) => {
+    post("/bops/wrong").expect(404, done);
+  });
+
+  test(`app.post("/pay/:localAuthority")`, (done) => {
+    post("/pay/wrong").expect(400, done);
+  });
+
+  test.skip(`app.get("/pay/:localAuthority/:paymentId")`, (done) => {
+    get("/pay/wrong/1").expect(400, done);
+  });
+
+  test(`app.get("/hasura")`, (done) => {
+    get("/hasura").expect(500, done);
+  });
+
+  test(`app.get("/me")`, (done) => {
+    get("/me")
+      .expect(401)
+      .then((response) => {
+        expect(response.body).toEqual({
+          error: "No authorization token was found",
+        });
+        done();
+      })
+      .catch(done);
+  });
+
+  test(`app.get("/gis")`, async (done) => {
+    get("/gis").expect(400, done);
+  });
+
+  test(`app.get("/gis/wrong")`, async (done) => {
+    get("/gis/wrong")
+      .expect(400)
+      .then((response) => {
+        expect(response.body).toEqual({
+          error: "wrong is not a supported location authority",
+        });
+        done();
+      });
+  });
+
+  test(`app.get("/throw-error")`, async (done) => {
+    get("/throw-error").expect(500, done);
+  });
+
+  test(`app.post("/flows/:flowId/publish")`, async (done) => {
+    post("/flows/WRONG/publish").expect(401, done);
+  });
+
+  test(`app.post("/flows/:flowId/download-schema")`, (done) => {
+    post("/flows/WRONG/download-schema").expect(404, done);
+  });
+
+  test(`app.post("/sign-s3-upload")`, (done) => {
+    post("/sign-s3-upload").expect(422, done);
+  });
+});
