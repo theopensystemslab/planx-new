62364750150f7b4401290e05d49a4b1fd6995d73
John Rees <john@bitsushi.com>
2021-10-27 12:35:57 +0100

    Update CollapsibleInput.stories.tsx

--- editor.planx.uk/src/ui/CollapsibleInput.stories.tsx
+++ editor.planx.uk/src/ui/CollapsibleInput.stories.tsx
@@ -1,7 +1,5 @@
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import FeedbackInput from "@planx/components/shared/FeedbackInput";
 import { Meta, Story } from "@storybook/react/types-6-0";
-import { submitFeedback } from "lib/feedback";
 import React, { useState } from "react";
 
 import CollapsibleInput, { Props } from "./CollapsibleInput";
@@ -14,23 +12,14 @@ const metadata: Meta = {
 export const Basic: Story<Props> = (args: Props) => {
   const [text, setText] = useState("");
   return (
-    <>
-      <CollapsibleInput
-        {...args}
-        handleChange={(ev: React.ChangeEvent<HTMLInputElement>) =>
-          setText(ev.target.value)
-        }
-        value={text}
-        name="text"
-      >
-        <Typography variant="body2">
-          Is this result inaccurate? <b>tell us why</b>
-        </Typography>
-      </CollapsibleInput>
-      <Button variant="contained" onClick={() => submitFeedback(text)}>
-        Submit
-      </Button>
-    </>
+    <FeedbackInput
+      // {...args}
+      text="Is this result inaccurate? **tell us why**"
+      handleChange={(ev) => {
+        setText(ev.target.value);
+      }}
+      value={text}
+    />
   );
 };
 
