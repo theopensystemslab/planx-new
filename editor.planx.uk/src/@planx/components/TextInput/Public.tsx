de9ef3bf43e2aadcdd2fc36d3b673cde42568239
Sarah Scott <sarahoscott@gmail.com>
2021-11-10 17:27:51 -0800

    GOV.UK react component test run

--- editor.planx.uk/src/@planx/components/TextInput/Public.tsx
+++ editor.planx.uk/src/@planx/components/TextInput/Public.tsx
@@ -2,8 +2,9 @@ import Card from "@planx/components/shared/Preview/Card";
 import QuestionHeader from "@planx/components/shared/Preview/QuestionHeader";
 import { PublicProps } from "@planx/components/ui";
 import { useFormik } from "formik";
+// import Input from "ui/Input";
+import { Input, InputField } from "govuk-react";
 import React from "react";
-import Input from "ui/Input";
 import InputRow from "ui/InputRow";
 import { object } from "yup";
 
@@ -12,6 +13,20 @@ import type { TextInput, UserData } from "./model";
 import { userDataSchema } from "./model";
 
 export type Props = PublicProps<TextInput, UserData>;
+
+import { useEffect } from "react";
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+// Would probably need to do something like this at the layout level, similar to the global Material-UI theme
+const GlobalStyle = createGlobalStyle`
+  font-family: "Inter";
+`;
+
+// Adding styles to existing components is pretty straightforward
+const StyledInput = styled(InputField)`
+  margin-bottom: 20px;
+`;
 
 // TODO: fix this data field bug for all components
 const TextInputComponent: React.FC<Props> = (props) => {
@@ -29,33 +44,44 @@ const TextInputComponent: React.FC<Props> = (props) => {
     }),
   });
 
+  useEffect(() => {
+    console.log(formik.errors);
+  });
+
   return (
-    <Card handleSubmit={formik.handleSubmit} isValid>
-      <QuestionHeader
-        title={props.title}
-        description={props.description}
-        info={props.info}
-        policyRef={props.policyRef}
-        howMeasured={props.howMeasured}
-      />
-      <InputRow>
-        <Input
-          type={((type) => {
-            if (type === "email") return "email";
-            else if (type === "phone") return "tel";
-            return "text";
-          })(props.type)}
-          multiline={props.type === "long"}
-          rows={props.type === "long" ? 5 : undefined}
-          name="text"
-          value={formik.values.text}
-          placeholder={props.placeholder || "Type your answer"}
-          bordered
-          onChange={formik.handleChange}
-          errorMessage={formik.errors.text as string}
+    <>
+      <GlobalStyle />
+
+      <Card handleSubmit={formik.handleSubmit} isValid>
+        <QuestionHeader
+          title={props.title}
+          description={props.description}
+          info={props.info}
+          policyRef={props.policyRef}
+          howMeasured={props.howMeasured}
         />
-      </InputRow>
-    </Card>
+        <InputRow>
+          <StyledInput
+            hint={<b>Hint something</b>}
+            input={{
+              name: "text",
+              value: formik.values.text,
+              placeholder: props.placeholder || "Type your answer",
+              onChange: formik.handleChange,
+
+              // Another option for trickier styles (e.g. the Input component inside InputField)
+              style: { fontFamily: "Inter" },
+            }}
+            meta={{
+              error: formik.errors.text as string,
+              touched: !!formik.errors.text,
+            }}
+          >
+            Extra context or description goes here
+          </StyledInput>
+        </InputRow>
+      </Card>
+    </>
   );
 };
 
