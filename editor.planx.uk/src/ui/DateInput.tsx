57d056da0e9cff4084d8cbd318adea8d04b53043
DafyddLlyr <dafyddllyr@gmail.com>
2021-12-06 12:11:53 +0000

    fix: Add labels to DateInput Update CSS to ensure adequate spacing Remove old label in preview/unpublished views

--- editor.planx.uk/src/ui/DateInput.tsx
+++ editor.planx.uk/src/ui/DateInput.tsx
@@ -1,3 +1,4 @@
+import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import React, { ChangeEvent } from "react";
@@ -21,11 +22,13 @@ const useClasses = makeStyles((theme) => ({
     // Adds a uniform horizontal spacing between all child elements.
     // The `* + *` selector makes sure the first element doesn't get this margin.
     "& > * + *": {
-      marginLeft: theme.spacing(1),
+      marginLeft: theme.spacing(2),
     },
   },
   label: {
     minWidth: 60,
+    alignSelf: "end",
+    marginBottom: theme.spacing(1.5),
   },
 }));
 
@@ -37,42 +40,62 @@ export default function DateInput(props: Props): FCReturn {
     <ErrorWrapper error={props.error}>
       <div className={classes.root}>
         <div className={classes.editor}>
-          <Typography className={classes.label} variant="body1">
-            {props.label || "Date"}:
-          </Typography>
-          <Input
-            style={{ width: 60 }}
-            value={day || ""}
-            placeholder="DD"
-            bordered={props.bordered}
-            onInput={(ev: ChangeEvent<HTMLInputElement>) => {
-              props.onChange(
-                [year || "", month || "", ev.target.value].join("-")
-              );
-            }}
-          />
-          <Input
-            style={{ width: 60 }}
-            value={month || ""}
-            placeholder="MM"
-            bordered={props.bordered}
-            onInput={(ev: ChangeEvent<HTMLInputElement>) => {
-              props.onChange(
-                [year || "", ev.target.value, day || ""].join("-")
-              );
-            }}
-          />
-          <Input
-            style={{ width: 90 }}
-            value={year || ""}
-            placeholder="YYYY"
-            bordered={props.bordered}
-            onInput={(ev: ChangeEvent<HTMLInputElement>) => {
-              props.onChange(
-                [ev.target.value, month || "", day || ""].join("-")
-              );
-            }}
-          />
+          {props.label && (
+            <Typography className={classes.label} variant="body1">
+              {props.label}:
+            </Typography>
+          )}
+          <Box>
+            <Typography variant="body1">
+              <label htmlFor="day">Day</label>
+            </Typography>
+            <Input
+              style={{ width: 60 }}
+              value={day || ""}
+              placeholder="DD"
+              bordered={props.bordered}
+              id="day"
+              onInput={(ev: ChangeEvent<HTMLInputElement>) => {
+                props.onChange(
+                  [year || "", month || "", ev.target.value].join("-")
+                );
+              }}
+            />
+          </Box>
+          <Box>
+            <Typography variant="body1">
+              <label htmlFor="month">Month</label>
+            </Typography>
+            <Input
+              style={{ width: 60 }}
+              value={month || ""}
+              placeholder="MM"
+              bordered={props.bordered}
+              id="month"
+              onInput={(ev: ChangeEvent<HTMLInputElement>) => {
+                props.onChange(
+                  [year || "", ev.target.value, day || ""].join("-")
+                );
+              }}
+            />
+          </Box>
+          <Box>
+            <Typography variant="body1">
+              <label htmlFor="year">Year</label>
+            </Typography>
+            <Input
+              style={{ width: 90 }}
+              value={year || ""}
+              placeholder="YYYY"
+              bordered={props.bordered}
+              id="year"
+              onInput={(ev: ChangeEvent<HTMLInputElement>) => {
+                props.onChange(
+                  [ev.target.value, month || "", day || ""].join("-")
+                );
+              }}
+            />
+          </Box>
         </div>
       </div>
     </ErrorWrapper>
