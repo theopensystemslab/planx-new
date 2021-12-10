831fc6d8e4b66740231e056a7aeee9af96d676f8
Jessica McInchak <jessica.mcinchak@gmail.com>
2021-12-10 08:51:37 +0100

    remove unused import

--- editor.planx.uk/src/@planx/components/TextInput/Public.tsx
+++ editor.planx.uk/src/@planx/components/TextInput/Public.tsx
@@ -1,4 +1,3 @@
-import { visuallyHidden } from "@material-ui/utils";
 import Card from "@planx/components/shared/Preview/Card";
 import QuestionHeader from "@planx/components/shared/Preview/QuestionHeader";
 import { PublicProps } from "@planx/components/ui";
