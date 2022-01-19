1a64afe8fb54b78365bf1938b1cd53f77b6995e1
DafyddLlyr <dafyddllyr@gmail.com>
2022-01-19 14:05:07 +0000

    fix: Ensure users can see correctly styled output in RichTextEditor

--- editor.planx.uk/src/ui/RichTextInput.tsx
+++ editor.planx.uk/src/ui/RichTextInput.tsx
@@ -52,6 +52,8 @@ const useClasses = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.default,
     width: "100%",
     "&:focus-within": focusStyle,
+    "& h1": theme.typography.h3,
+    "& h2": theme.typography.h5,
   },
   bordered: {
     border: `2px solid #000`,
@@ -130,9 +132,10 @@ const RichTextInput: React.FC<Props> = (props) => {
     []
   );
 
-  const inlineToolbarPlugin = useMemo(() => createInlineToolbarPlugin(), [
-    linkPlugin,
-  ]);
+  const inlineToolbarPlugin = useMemo(
+    () => createInlineToolbarPlugin(),
+    [linkPlugin]
+  );
 
   const classes = useClasses();
 
@@ -160,12 +163,12 @@ const RichTextInput: React.FC<Props> = (props) => {
                 : newHtmlContent;
 
             if (props.onChange && newHtmlContent !== props.value) {
-              const changeEvent = ({
+              const changeEvent = {
                 target: {
                   name: props.name,
                   value: newHtmlContentNonEmpty,
                 },
-              } as unknown) as ChangeEvent<HTMLInputElement>;
+              } as unknown as ChangeEvent<HTMLInputElement>;
               props.onChange(changeEvent);
             }
             setEditorState(newEditorState);
