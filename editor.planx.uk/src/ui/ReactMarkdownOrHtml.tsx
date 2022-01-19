7a82da57783cb57bf71fe15d44a24986fd5e1184
DafyddLlyr <dafyddllyr@gmail.com>
2022-01-19 13:54:08 +0000

    fix: Handle user entered h1 and h2 elements to address a11y issues

--- editor.planx.uk/src/ui/ReactMarkdownOrHtml.tsx
+++ editor.planx.uk/src/ui/ReactMarkdownOrHtml.tsx
@@ -3,13 +3,26 @@ import classNames from "classnames";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-const useClasses = makeStyles({
+const useClasses = makeStyles((theme) => ({
   htmlRoot: {
     "& a": {
       color: "inherit",
     },
+    "& h2": theme.typography.h3,
+    "& h3": theme.typography.h5,
   },
-});
+}));
+
+// Increment H1 and H2 elements to meet a11y requirements in user submitted rich text
+export const incrementHeaderElements = (source: string): string => {
+  const regex: RegExp = /(<\/?h)[1-2]/gi;
+  const incrementer = (match: string, p1: string): string => {
+    const currentLevel: number = Number(match.slice(-1));
+    const newLevel: number = currentLevel + 1;
+    return p1 + newLevel;
+  };
+  return source.replace(regex, incrementer);
+};
 
 export default function ReactMarkdownOrHtml(props: {
   source?: string;
@@ -25,11 +38,12 @@ export default function ReactMarkdownOrHtml(props: {
     const replaceTarget = props.openLinksOnNewTab
       ? props.source.replaceAll(`target="_self"`, `target="_blank" external`)
       : props.source;
+    const incrementHeaders = incrementHeaderElements(replaceTarget);
 
     return (
       <div
         className={classNames(props.className, classes.htmlRoot)}
-        dangerouslySetInnerHTML={{ __html: replaceTarget }}
+        dangerouslySetInnerHTML={{ __html: incrementHeaders }}
         id={props.id}
       />
     );
