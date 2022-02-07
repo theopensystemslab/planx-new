c32c370a80e5ece99cd29351987b8ec5a700426a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-02-07 09:55:57 +0000

    feat: Add link to team homepage in logo (#838)
    * feat: Add link to team homepage in logo
    
    * fix: PRs work best if you check in all your changes...

--- hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
+++ hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
@@ -1,5 +1,5 @@
 INSERT INTO public.teams (name, slug, theme) VALUES ('Open Systems Lab', 'opensystemslab', '{}');
-INSERT INTO public.teams (name, slug, theme) VALUES ('Canterbury', 'canterbury', '{"logo": "https://editor.planx.uk/logos/canterbury.svg", "primary": "#331035"}');
+INSERT INTO public.teams (name, slug, theme, settings) VALUES ('Canterbury', 'canterbury', '{"logo": "https://raw.githubusercontent.com/theopensystemslab/planx-team-logos/main/canterbury.svg", "primary": "#331035"}', '{"homepage": "https://www.canterbury.gov.uk/"}');
 INSERT INTO public.teams (name, slug, theme) VALUES ('Southwark', 'southwark', '{}');
 INSERT INTO public.users (first_name, last_name, email, is_admin) VALUES ('John', 'Rees', 'john@opensystemslab.io', true);
 INSERT INTO public.users (first_name, last_name, email, is_admin) VALUES ('Alastair', 'Parvin', 'alastair@opensystemslab.io', true);
