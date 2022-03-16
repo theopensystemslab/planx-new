0870fd74920a1c09cd19c81d5c80e6c0b58f120e
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-03-16 14:55:50 +0100

    filter request on dataset, response formats data as array of possible features

--- api.planx.uk/gis/digitalLand.js
+++ api.planx.uk/gis/digitalLand.js
@@ -2,49 +2,64 @@ require("isomorphic-fetch");
 
 const { makeBboxWkt, omitGeojson } = require("./helpers");
 const { baseSchema } = require("./local_authorities/metadata/base.js");
-const { digitalLandDatasets } = require("./local_authorities/metadata/digital-land-datasets.js");
+
+// generate list of digital land datasets we want to query
+// based on availability here https://digital-land-maturity-model.herokuapp.com/performance/
+let activeDatasets = [];
+Object.keys(baseSchema).forEach(key => {
+  if (baseSchema[key]["active"]) {
+    baseSchema[key]["digital-land-datasets"].forEach(dataset => {
+      activeDatasets.push(dataset);
+    });
+  }
+});
 
 async function go(x, y, siteBoundary, extras) {
   try {
     // use drawn site boundary if available, fallback to buffered address point
+    // digital land expects WKT format using projection 4326
     const geom = siteBoundary ? siteBoundary : makeBboxWkt(x, y, 0.05);
 
     // https://www.digital-land.info/docs#/
     let options = {
-      entries: "current", // all, current, historical
-      geometry: geom, // WKT format
-      geometry_relation: "intersects", // within, equals, disjoint, intersects, touches, contains, covers, coveredby, overlaps, crosses
+      entries: "current",
+      geometry: geom,
+      geometry_relation: "intersects",
       limit: 100,
-      offset: 0,
     };
-    let url = `https://www.digital-land.info/entity.json?${new URLSearchParams(options)}`;
+    let datasets = `&dataset=${activeDatasets.join(`&dataset=`)}`; // dataset param is not array[string] per docs, instead need to re-specify param name per dataset
     
-    // results.entities will ONLY return intersecting aka "positive" constraints
+    let url = `https://www.digital-land.info/entity.json?${new URLSearchParams(options)}${datasets}`;
     const results = await fetch(url)
       .then(response => response.json())
       .catch(error => console.log(error));
 
+
+    // results will ONLY be populated if we have "positive"/intersecting constraints
     let formattedResult = {};
-    results.entities.forEach(entity => {
-      const key = digitalLandDatasets[entity.dataset];
-      
-      // filter the digital land response for datasets included in the planx base schema
-      if (Object.keys(baseSchema).includes(key)) {
-        // because there can be many digital datasets per planx variable, check if we need to add this key to constraints or just append data
-        if (!Object.keys(formattedResult).includes(key)) {
+    if (results && results.entities) {
+      results.entities.forEach(entity => {
+        // get the planx variable that corresponds to this entity's "dataset", should never be null because our initial request is filtered on "dataset"
+        const key = Object.keys(baseSchema).find(key => baseSchema[key]["digital-land-datasets"].includes(entity.dataset));
+  
+        // because there can be many digital land datasets per planx variable, check if this key is already in our result
+        if (Object.keys(formattedResult).includes(key)) {
+          formattedResult[key]["data"].push(omitGeojson(entity));
+        } else {
           formattedResult[key] = {
             value: true,
             text: baseSchema[key].pos,
             data: [omitGeojson(entity)],
           };
-        } else {
-          formattedResult[key]["data"].push(omitGeojson(entity));
         }
-      }
-    });
+      })
+    } else {
+      // TODO: generate list of planx variables with neg text using base schema??
+      formattedResult = { error: "No intersecting constraints found", url: url };
+    }
 
     // TODO: do second request to get denominator of all datasets available for this council (aka "negative" constraints) & concat to formattedResult
-    // use local authority boundary as polygon for this case & get all datasets within??
+    // use local authority boundary as polygon for this case & get all datasets within?? won't hold up if local auth doesn't have a national park for example
 
     return formattedResult;
   } catch (e) {
--- api.planx.uk/gis/local_authorities/metadata/base.js
+++ api.planx.uk/gis/local_authorities/metadata/base.js
@@ -1,76 +1,92 @@
-// planx schema variables and their possible attributes
+// planx schema variables and their attributes (in the future, maybe these are configurable in Editor?)
 const baseSchema = {
   article4: {
+    active: true,
     neg: "is not subject to any Article 4 restrictions",
     pos: "is subject to Article 4 restriction(s)",
     "digital-land-datasets": ["article-4-direction"],
   },
   listed: {
+    active: true,
     neg: "is not in, or within, a Listed Building",
     pos: "is, or is within, a Listed Building",
     "digital-land-datasets": ["listed-building", "listed-building-outline", "locally-listed-building"],
   },
   "designated.conservationArea": {
+    active: true,
     neg: "is not in a Conservation Area",
     pos: "is in a Conservation Area",
     "digital-land-datasets": ["conservation-area"]
   },
   "designated.AONB": {
+    active: true,
     neg: "is not in an Area of Outstanding Natural Beauty",
     pos: "is in an Area of Outstanding Natural Beauty",
     "digital-land-datasets": ["area-of-outstanding-natural-beauty"],
   },
   "designated.nationalPark": {
+    active: true,
     neg: "is not in a National Park",
     pos: "is in a National Park",
     "digital-land-datasets": ["national-park"],
   },
   "designated.broads": {
+    active: false,
     neg: "is not in a Broad",
     pos: "is in a Broad",
-    "digital-land-datasets": ["national-park"] // https://www.digital-land.info/entity/520007
+    "digital-land-datasets": ["national-park"] // TODO: filter on record https://www.digital-land.info/entity/520007
   },
   "designated.WHS": {
+    active: true,
     neg: "is not an UNESCO World Heritage Site",
     pos: "is, or is within, an UNESCO World Heritage Site",
     "digital-land-datasets": ["world-heritage-site"],
   },
   "monument": {
+    active: true,
     neg: "is not the site of a Scheduled Monument",
     pos: "is the site of a Scheduled Monument",
     "digital-land-datasets": ["scheduled-monument"],
   },
   tpo: {
+    active: true,
     neg: "is not in a Tree Preservation Order (TPO) Zone",
     pos: "is in a Tree Preservation Order (TPO) Zone",
     "digital-land-datasets": ["tree-preservation-order", "tree-preservation-zone"],
   },
   "nature.SSSI": {
+    active: true,
     neg: "is not a Site of Special Scientific Interest (SSSI)",
     pos: "is a Site of Special Scientific Interest (SSSI)",
     "digital-land-datasets": ["site-of-special-scientific-interest"],
   },
   "flood.zone1": {
+    active: false,
     neg: "is not within a Flood Zone 1 (low risk)",
     pos: "is within a Flood Zone 1 (low risk)",
   },
   "flood.zone2": {
+    active: false,
     neg: "is not within a Flood Zone 2 (medium risk)",
     pos: "is within a Flood Zone 2 (medium risk)",
   },
   "flood.zone3": {
+    active: false,
     neg: "is not within a Flood Zone 3 (high risk)",
     pos: "is within a Flood Zone 3 (high risk)",
   },
   "defence.explosives": {
+    active: false,
     neg: "is not an Explosives or Ordnance Storage site",
     pos: "is an Explosives or Ordnance Storage site",
   },
   "defence.safeguarded": {
+    active: false,
     neg: "is not on Safeguarded land",
     pos: "is on Safeguarded land",
   },
   hazard: {
+    active: false,
     neg: "is not in, or within, a Safety Hazard area",
     pos: "is, or is within, a Safety Hazard area",
   },
