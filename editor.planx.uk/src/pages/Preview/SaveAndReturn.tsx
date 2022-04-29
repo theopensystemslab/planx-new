1961ce8e4419c83f372fa432e3d8a98a8a927f1c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-04-29 15:41:10 +0100

    Rebase main, add feature flag

---
+++ editor.planx.uk/src/pages/Preview/SaveAndReturn.tsx
@@ -0,0 +1,102 @@
+import Box from "@material-ui/core/Box";
+import Card from "@planx/components/shared/Preview/Card";
+import QuestionHeader from "@planx/components/shared/Preview/QuestionHeader";
+import { useFormik } from "formik";
+import { useStore } from "pages/FlowEditor/lib/store";
+import React from "react";
+import Input from "ui/Input";
+import InputLabel from "ui/InputLabel";
+import InputRow from "ui/InputRow";
+import { object, ref, string } from "yup";
+
+const confirmEmailSchema = object({
+  email: string().email("Invalid email").required("Email address required"),
+  confirmEmail: string()
+    .email("Invalid email")
+    .required("Email address required")
+    .oneOf([ref("email"), null], "Emails must match"),
+});
+
+export const ConfirmEmail: React.FC<{
+  handleSubmit: (email: string) => void;
+}> = ({ handleSubmit }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      confirmEmail: "",
+    },
+    onSubmit: (values) => handleSubmit(values.confirmEmail),
+    validateOnChange: false,
+    validateOnBlur: false,
+    validationSchema: confirmEmailSchema,
+  });
+
+  return (
+    <Box width="100%" role="main">
+      <Card handleSubmit={formik.handleSubmit}>
+        <QuestionHeader
+          title="Enter your email address"
+          description="We will use this to save your application so you can come back to it later. We will also email you updates about your application."
+        ></QuestionHeader>
+        <InputRow>
+          <InputLabel label={"Email Address"} htmlFor={"email"}>
+            <Input
+              bordered
+              errorMessage={
+                Boolean(formik.touched.email && formik.errors.email)
+                  ? formik.errors.email
+                  : undefined
+              }
+              id="email"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.email}
+            ></Input>
+          </InputLabel>
+        </InputRow>
+        <InputRow>
+          <InputLabel label={"Confirm Email Address"} htmlFor={"confirmEmail"}>
+            <Input
+              bordered
+              errorMessage={
+                Boolean(
+                  formik.touched.confirmEmail && formik.errors.confirmEmail
+                )
+                  ? formik.errors.confirmEmail
+                  : undefined
+              }
+              id="confirmEmail"
+              name="confirmEmail"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.confirmEmail}
+            ></Input>
+          </InputLabel>
+        </InputRow>
+      </Card>
+    </Box>
+  );
+};
+
+const SaveAndReturn: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const isEmailCaptured = Boolean(useStore((state) => state.saveToEmail));
+  const handleSubmit = (email: string) =>
+    useStore.getState().setSaveToEmail(email);
+
+  return (
+    <>
+      {isEmailCaptured ? (
+        children
+      ) : (
+        <ConfirmEmail handleSubmit={handleSubmit}></ConfirmEmail>
+      )}
+    </>
+  );
+};
+
+export default SaveAndReturn;
---
+++ editor.planx.uk/src/routes/utils.test.tsx
@@ -0,0 +1,6 @@
+import { isSaveReturnFlow } from "./utils";
+
+describe("isSaveReturnFlow helper function", () => {
+  it.todo("correctly identifies flows with a 'Send' node");
+  it.todo("correctly identifies flows without a 'Send' node");
+});
