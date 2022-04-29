ee61006d75f484d91448f89c4b94c93cc72c860b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-04-29 15:41:10 +0100

    Rebase main, add feature flag

--- editor.planx.uk/src/routes/utils.ts
+++ editor.planx.uk/src/routes/utils.ts
@@ -1,3 +1,6 @@
+import { TYPES as NodeTypes } from "@planx/components/types";
+import { hasFeatureFlag } from "lib/featureFlags";
+
 export const makeTitle = (str: string) =>
   [str, "PlanX"].filter(Boolean).join(" | ");
 
@@ -5,3 +8,11 @@ export const rootFlowPath = (includePortals = false) => {
   const path = window.location.pathname.split("/").slice(0, 3).join("/");
   return includePortals ? path : path.split(",")[0];
 };
+
+export const isSaveReturnFlow = (flowData: Record<string, any>): Boolean =>
+  hasFeatureFlag("SAVE_AND_RETURN") &&
+  Boolean(
+    Object.values(flowData).find(
+      (node: Record<string, any>) => node.type === NodeTypes.Send
+    )
+  );
