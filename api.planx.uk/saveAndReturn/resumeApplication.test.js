a47dda7798f41fa0f7e7225522044e3fb9b1aca0
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-05-09 09:12:22 +0100

    feat: Wire up Save & Return endpoints to lowcal_storage  (#923)

--- api.planx.uk/saveAndReturn/resumeApplication.test.js
+++ api.planx.uk/saveAndReturn/resumeApplication.test.js
@@ -1,8 +1,8 @@
 const supertest = require("supertest");
 const app = require("../server");
 const { queryMock } = require("../tests/graphqlQueryMock");
+const { mockFlow, mockLowcalSession } = require("../tests/mocks/saveAndReturnMocks");
 const { buildContentFromSessions } = require("./resumeApplication");
-
 
 const ENDPOINT = "/resume-application";
 const TEST_EMAIL = "simulate-delivered@notifications.service.gov.uk"
@@ -21,10 +21,12 @@ describe("buildContentFromSessions function", () => {
         }
       },
       id: 123,
+      expiry_date: "2022-05-04T01:02:03.865452+00:00",
     }];
 
     const result = `Address: 1 High Street
       Project Type: house
+      Expiry Date: 04 May 2022
       Link: example.com/team/flow/preview?sessionId=123`
     expect(buildContentFromSessions(sessions, "flow", "team")).toEqual(result);
   });
@@ -42,6 +44,7 @@ describe("buildContentFromSessions function", () => {
         }
       },
       id: 123,
+      expiry_date: "2022-05-04T01:02:03.865452+00:00",
     },
     {
       data: {
@@ -55,6 +58,7 @@ describe("buildContentFromSessions function", () => {
         }
       },
       id: 456,
+      expiry_date: "2022-05-04T01:02:03.865452+00:00",
     },
     {
       data: {
@@ -68,13 +72,17 @@ describe("buildContentFromSessions function", () => {
         }
       },
       id: 789,
+      expiry_date: "2022-05-04T01:02:03.865452+00:00",
     }];
     const result = `Address: 1 High Street
       Project Type: house
+      Expiry Date: 04 May 2022
       Link: example.com/team/flow/preview?sessionId=123\n\nAddress: 2 High Street
       Project Type: flat
+      Expiry Date: 04 May 2022
       Link: example.com/team/flow/preview?sessionId=456\n\nAddress: 3 High Street
       Project Type: farm
+      Expiry Date: 04 May 2022
       Link: example.com/team/flow/preview?sessionId=789`
     expect(buildContentFromSessions(sessions, "flow", "team")).toEqual(result)
   });
@@ -90,10 +98,12 @@ describe("buildContentFromSessions function", () => {
         }
       },
       id: 123,
+      expiry_date: "2022-05-04T01:02:03.865452+00:00",
     }];
 
     const result = `Address: Address not submitted
       Project Type: house
+      Expiry Date: 04 May 2022
       Link: example.com/team/flow/preview?sessionId=123`
     expect(buildContentFromSessions(sessions, "flow", "team")).toEqual(result);
   });
@@ -111,10 +121,12 @@ describe("buildContentFromSessions function", () => {
         }
       },
       id: 123,
+      expiry_date: "2022-05-04T01:02:03.865452+00:00",
     }];
 
     const result = `Address: 1 High Street
       Project Type: Project type not submitted
+      Expiry Date: 04 May 2022
       Link: example.com/team/flow/preview?sessionId=123`
     expect(buildContentFromSessions(sessions, "flow", "team")).toEqual(result);
   });
@@ -122,33 +134,30 @@ describe("buildContentFromSessions function", () => {
 });
 
 describe("Resume Application endpoint", () => {
-  it("throws an error if an email is not provided", async () => {
-    await supertest(app)
-      .post(ENDPOINT)
-      .send({ flowId: "test"})
-      .expect(400)
-      .then(response => {
-        expect(response.body).toHaveProperty("error", "Required value missing");
-    });
-  });
 
-  it("throws an error if a flowId is not provided", async () => {
-    await supertest(app)
+  it("throws an error for if required data is missing", () => {
+
+    const missingEmail = { flowId: "test", sessionId: 123 };
+    const missingFlowId = { email: "test", sessionId: 123 };
+
+    [missingEmail, missingFlowId].forEach(async (invalidBody) => {
+      await supertest(app)
       .post(ENDPOINT)
-      .send({ email: "test@test.xyz"})
+      .send(invalidBody)
       .expect(400)
       .then(response => {
         expect(response.body).toHaveProperty("error", "Required value missing");
-    });
+      });
+    })
   });
 
   it("throws an error if a flowId is invalid", async () => {
     const flowId = 123;
 
     queryMock.mockQuery({
-      name: 'GetFlowByPK',
-      data: { flows_by_pk: null },
-      variables: { flowId: flowId }
+      name: 'ValidateRequest',
+      data: { flows_by_pk: null, lowcal_sessions: null },
+      variables: { flowId: flowId, email: TEST_EMAIL }
     });
 
     await supertest(app)
@@ -164,20 +173,14 @@ describe("Resume Application endpoint", () => {
     const flowId = 123;
 
     queryMock.mockQuery({
-      name: 'GetFlowByPK',
+      name: 'ValidateRequest',
       data: {
-        flows_by_pk: { slug: "slug", team: {
-          slug: "teamName",
-          notifyPersonalisation: {
-            helpPhone: "test",
-            helpEmail: "test",
-            helpOpeningHours: "test",
-          }
-        }
+        lowcal_sessions: [mockLowcalSession],
+        flows_by_pk: mockFlow
       },
-    },
       variables: {
         flowId: flowId,
+        email: TEST_EMAIL,
       }
     });
 
@@ -190,5 +193,18 @@ describe("Resume Application endpoint", () => {
       .expect(200)
   });
 
-  it.todo("throws an error if there is not a matching session");
+  it("give a successful response even if there is not a matching session", async () => {
+    const flowId = 123;
+
+    queryMock.mockQuery({
+      name: 'ValidateRequest',
+      data: { flows_by_pk: mockFlow, lowcal_sessions: null },
+      variables: { flowId: flowId, email: TEST_EMAIL }
+    });
+
+    await supertest(app)
+      .post(ENDPOINT)
+      .send({ email: TEST_EMAIL, flowId: flowId})
+      .expect(200);
+  });
 });
