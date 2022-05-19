51bacda62f99636baf85032530d2fa4a0f84bf78
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-05-19 09:17:07 +0100

    feat: Expiry and reminder emails (#927)

--- api.planx.uk/server.js
+++ api.planx.uk/server.js
@@ -25,7 +25,7 @@ const { locationSearch } = require("./gis/index");
 const { diffFlow, publishFlow } = require("./publish");
 const { findAndReplaceInFlow } = require("./findReplace");
 const { sendToUniform, downloadUniformZip } = require("./send");
-const { saveApplication, resumeApplication, validateSession } = require("./saveAndReturn")
+const { resumeApplication, validateSession, sendSaveAndReturnEmail } = require("./saveAndReturn")
 
 // debug, info, warn, error, silent
 const LOG_LEVEL = process.env.NODE_ENV === "test" ? "silent" : "debug";
@@ -579,7 +579,7 @@ app.post("/analytics/log-user-resume", async (req, res, next) => {
 
 // assert(process.env.GOVUK_NOTIFY_API_KEY_TEAM);
 // assert(process.env.GOVUK_NOTIFY_API_KEY_TEST);
-app.post("/save-application", saveApplication);
+app.post("/send-email", sendSaveAndReturnEmail);
 app.post("/resume-application", resumeApplication);
 app.post("/validate-session", validateSession);
 
