9574b356e908288e43c7eef7ee2eedad1ff287b7
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-05-19 09:17:07 +0100

    feat: Expiry and reminder emails (#927)

--- api.planx.uk/saveAndReturn/validateSession.test.js
+++ api.planx.uk/saveAndReturn/validateSession.test.js
@@ -10,11 +10,10 @@ const ENDPOINT = "/validate-session";
 describe("Validate Session endpoint", () => {
 
   it("throws an error if required data is missing", () => {
-    const missingEmail = { flowId: "test", sessionId: 123 };
-    const missingSessionId = { flowId: "test", email: "test" };
-    const missingFlowId = { email: "test", sessionId: 123 };
+    const missingEmail = { sessionId: 123 };
+    const missingSessionId = { email: "test" };
 
-    [missingEmail, missingSessionId, missingFlowId].forEach(async (invalidBody) => {
+    [missingEmail, missingSessionId].forEach(async (invalidBody) => {
       await supertest(app)
       .post(ENDPOINT)
       .send(invalidBody)
@@ -26,54 +25,32 @@ describe("Validate Session endpoint", () => {
   });
 
   it("Returns a 200 OK for a valid session", async () => {
-    const flowId = 123;
-    const sessionId = 456;
+    const payload = { email: TEST_EMAIL, sessionId: 123 };
 
     queryMock.mockQuery({
       name: 'ValidateRequest',
       data: { flows_by_pk: mockFlow, lowcal_sessions: [mockLowcalSession] },
-      variables: { email: TEST_EMAIL, flowId, sessionId}
+      variables: payload,
     });
 
     await supertest(app)
       .post(ENDPOINT)
-      .send({ email: TEST_EMAIL, flowId, sessionId })
+      .send(payload)
       .expect(200);
   });
 
   it("Returns a 404 NOT FOUND for an invalid session", async () => {
-    const flowId = 123;
-    const sessionId = 456;
+    const payload = { email: TEST_EMAIL, sessionId: 123 };
 
     queryMock.mockQuery({
       name: 'ValidateRequest',
       data: { flows_by_pk: mockFlow, lowcal_sessions: [] },
-      variables: { email: TEST_EMAIL, flowId, sessionId}
+      variables: payload,
     });
 
     await supertest(app)
       .post(ENDPOINT)
-      .send({ email: TEST_EMAIL, flowId, sessionId })
+      .send(payload)
       .expect(404);
   });
-
-  it("Throws an error for an invalid flow", async () => {
-    const flowId = 123;
-    const sessionId = 456;
-
-    queryMock.mockQuery({
-      name: 'ValidateRequest',
-      data: { flows_by_pk: null, lowcal_sessions: [mockLowcalSession] },
-      variables: { email: TEST_EMAIL, flowId, sessionId}
-    });
-
-    await supertest(app)
-      .post(ENDPOINT)
-      .send({ email: TEST_EMAIL, flowId, sessionId })
-      .expect(500)
-      .then(response => {
-        expect(response.body).toHaveProperty("error", "Unable to validate request");
-      });
-  });
-
 });
