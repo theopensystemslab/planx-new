c274178d91dc7ea32f68247619d1c3a8dd0f0fb0
Gunar Gessner <gunar@gunargessner.com>
2022-06-02 15:46:06 -0300

    infra: add cloudfront
    Resolves several issues:
    - Enables HTTP Strict-Transport-Security Header
    - Makes requests that return 404 return 200 instead (because SPA)
    - Allows us to use CloudFlare SSL "Full" instead of "Flexible"
    - Prevents embedding the app in an iFrame (through X-Frame-Options)

--- infrastructure/application/index.ts
+++ infrastructure/application/index.ts
@@ -466,35 +466,9 @@ new pulumi.Config("cloudflare").require("apiToken");
   // ------------------- Frontend
   const frontendBucket = new aws.s3.Bucket(`${DOMAIN}`, {
     bucket: DOMAIN,
-    // TODO: can we remove these cors rules?
-    corsRules: [
-      {
-        allowedHeaders: ["*"],
-        allowedMethods: ["GET", "HEAD"],
-        // TODO: Narrow down allowed origin to the domain we're using
-        allowedOrigins: ["*"],
-        exposeHeaders: ["ETag"],
-        maxAgeSeconds: 3000,
-      },
-    ],
-    // TODO: remove stringify
-    policy: JSON.stringify({
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Sid: "PublicReadGetObject",
-          Effect: "Allow",
-          Principal: "*",
-          Action: ["s3:GetObject"],
-          Resource: [`arn:aws:s3:::${DOMAIN}/*`],
-        },
-      ],
-    }),
-    websiteDomain: DOMAIN,
     website: {
       indexDocument: "index.html",
       errorDocument: "index.html",
-      // XXX: If needed we can use the `routingRules` key here to create forwardings.
     },
   });
 
@@ -525,11 +499,119 @@ new pulumi.Config("cloudflare").require("apiToken");
       );
     });
 
+  const logsBucket = new aws.s3.Bucket("requestLogs", {
+    bucket: `${DOMAIN}-logs`,
+    acl: "private",
+  });
+
+  // Generate Origin Access Identity to access the private s3 bucket.
+  const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity(
+    "originAccessIdentity",
+    {
+      comment: "This is needed to setup s3 polices and make s3 not public.",
+    }
+  );
+  const cdn = new aws.cloudfront.Distribution(`${DOMAIN}-cdn`, {
+    enabled: true,
+    // Could include `www.${DOMAIN}` here if the `www` subdomain is desired
+    aliases: [DOMAIN],
+    origins: [
+      {
+        originId: frontendBucket.arn,
+        domainName: frontendBucket.websiteEndpoint,
+        s3OriginConfig: {
+          originAccessIdentity:
+            originAccessIdentity.cloudfrontAccessIdentityPath,
+        },
+      },
+    ],
+
+    defaultRootObject: "index.html",
+
+    // A CloudFront distribution can configure different cache behaviors based on the request path.
+    // Here we just specify a single, default cache behavior which is just read-only requests to S3.
+    defaultCacheBehavior: {
+      targetOriginId: frontendBucket.arn,
+      viewerProtocolPolicy: "redirect-to-https",
+      allowedMethods: ["GET", "HEAD", "OPTIONS"],
+      cachedMethods: ["GET", "HEAD", "OPTIONS"],
+      forwardedValues: {
+        cookies: { forward: "none" },
+        queryString: false,
+      },
+      compress: true,
+      minTtl: 0,
+      defaultTtl: 60 * 10,
+      maxTtl: 60 * 10,
+      responseHeadersPolicyId: new aws.cloudfront.ResponseHeadersPolicy(
+        `${DOMAIN}-policy`,
+        {
+          corsConfig: {
+            accessControlAllowCredentials: true,
+            accessControlAllowHeaders: {
+              items: ["*"],
+            },
+            accessControlAllowMethods: {
+              items: ["GET", "HEAD", "OPTIONS"],
+            },
+            // TODO: Narrow this down to the list of domain names we're actually using
+            accessControlAllowOrigins: {
+              items: ["*"],
+            },
+            originOverride: true,
+          },
+          securityHeadersConfig: {
+            // Prevent iFrames
+            frameOptions: {
+              frameOption: "DENY",
+              override: true,
+            },
+            // Implements HTTP Strict Transport Security
+            strictTransportSecurity: {
+              accessControlMaxAgeSec: 63072000, // maximum (2 years)
+              override: true,
+              includeSubdomains: true,
+              preload: true,
+            },
+          },
+        }
+      ).id,
+    },
+
+    // "All" is the most broad distribution, and also the most expensive.
+    // "100" is the least broad, and also the least expensive.
+    priceClass: "PriceClass_100",
+
+    // You can customize error responses. When CloudFront receives an error from the origin (e.g. S3 or some other
+    // web service) it can return a different error code, and return the response for a different resource.
+    customErrorResponses: [
+      { errorCode: 404, responseCode: 200, responsePagePath: "/index.html" },
+    ],
+
+    restrictions: {
+      geoRestriction: {
+        restrictionType: "none",
+      },
+    },
+
+    viewerCertificate: {
+      acmCertificateArn: certificates.requireOutput("certificateArn"),
+      sslSupportMethod: "sni-only",
+      minimumProtocolVersion: "TLSv1.2_2021",
+    },
+
+    loggingConfig: {
+      bucket: logsBucket.bucketDomainName,
+      includeCookies: false,
+      prefix: `${DOMAIN}/`,
+    },
+  });
+
   const frontendDnsRecord = new cloudflare.Record("frontend", {
     name: tldjs.getSubdomain(DOMAIN) ?? "@",
     type: "CNAME",
     zoneId: config.require("cloudflare-zone-id"),
-    value: frontendBucket.websiteDomain,
+    value: cdn.domainName,
     ttl: 1,
     proxied: true,
   });
