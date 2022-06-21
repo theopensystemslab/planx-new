0e237a1da13841d563ab7864ee73f0aed32c6ad4
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-06-21 12:14:31 +0100

    test: Pass env var into job

--- api.planx.uk/saveAndReturn/notify.js
+++ api.planx.uk/saveAndReturn/notify.js
@@ -1,9 +1,5 @@
 const { NotifyClient } = require("notifications-node-client");
 
-const notifyClient = new NotifyClient(
-  process.env.NODE_ENV === "test" ? 
-    process.env.GOVUK_NOTIFY_API_KEY_TEAM_TEST : 
-    process.env.GOVUK_NOTIFY_API_KEY_TEAM
-);
+const notifyClient = new NotifyClient(process.env.GOVUK_NOTIFY_API_KEY_TEAM);
 
 module.exports = { notifyClient };
