20710e805857ee706641932ce40df45d55b13c5a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-06-22 13:35:56 +0100

    fix: Temporarily drop buildkit

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --force-recreate --remove-orphans -d
+docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --force-recreate --remove-orphans -d
