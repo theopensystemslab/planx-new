765d8d7520ef50ba21d4643b412f9b7dde23a8a2
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-06-22 10:17:32 +0100

    fix: Try --no-cache flag in update.sh to force clean rebuild

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --force-recreate --remove-orphans -d
+DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --no-cache --force-recreate --remove-orphans -d
