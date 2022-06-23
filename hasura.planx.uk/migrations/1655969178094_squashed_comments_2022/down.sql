2cdcb790a0993a018c409e47cb4c751718c81a1c
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-06-23 09:35:41 +0200

    q&a notes > postgres comments

---
+++ hasura.planx.uk/migrations/1655969178094_squashed_comments_2022/down.sql
@@ -0,0 +1,36 @@
+
+comment on table "public"."team_members" is NULL;
+
+comment on column "public"."users"."email" is NULL;
+
+comment on table "public"."users" is NULL;
+
+comment on column "public"."uniform_applications"."destination" is NULL;
+
+comment on table "public"."uniform_applications" is NULL;
+
+comment on table "public"."teams" is NULL;
+
+comment on column "public"."published_flows"."data" is NULL;
+
+comment on table "public"."published_flows" is NULL;
+
+comment on table "public"."operations" is NULL;
+
+comment on table "public"."lowcal_sessions" is NULL;
+
+comment on table "public"."global_settings" is NULL;
+
+comment on column "public"."flows"."data" is NULL;
+
+comment on column "public"."flows"."version" is NULL;
+
+comment on table "public"."flows" is NULL;
+
+comment on table "public"."flow_schemas" is NULL;
+
+comment on table "public"."bops_applications" is NULL;
+
+comment on table "public"."analytics_logs" is NULL;
+
+comment on table "public"."analytics" is NULL;
