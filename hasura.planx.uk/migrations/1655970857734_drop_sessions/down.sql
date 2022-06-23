cb827e476b36fc504f5409a1cfd4f13dbe687a48
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-06-23 09:57:19 +0200

    let's see what breaks

---
+++ hasura.planx.uk/migrations/1655970857734_drop_sessions/down.sql
@@ -0,0 +1,4 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- DROP TABLE "public"."sessions" CASCADE;
+-- DROP TABLE "public"."session_events" CASCADE;
---
+++ hasura.planx.uk/migrations/1655970857734_drop_sessions/up.sql
@@ -0,0 +1,3 @@
+DROP TABLE "public"."sessions" CASCADE;
+DROP TABLE "public"."session_events" CASCADE;
+-- now using lowcal_sessions & session_backups
