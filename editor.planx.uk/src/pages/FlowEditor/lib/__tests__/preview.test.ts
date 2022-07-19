ba7dd5eb60dbdabfac9fa01e70a40708bd86922f
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-07-19 13:42:11 +0200

    fix: "change" link from Review correctly updates passport and auto-answered nodes (#1011)

--- editor.planx.uk/src/pages/FlowEditor/lib/__tests__/preview.test.ts
+++ editor.planx.uk/src/pages/FlowEditor/lib/__tests__/preview.test.ts
@@ -7,6 +7,7 @@ import {
   removeOrphansFromBreadcrumbs,
 } from "../store/preview";
 import breadcrumbsDependentOnPassportMock from "./mocks/breadcrumbsDependentOnPassport.json";
+import flowWithAutoAnswersMock from "./mocks/flowWithAutoAnswers.json";
 import flowWithPassportComponentsMock from "./mocks/flowWithPassportComponents.json";
 
 const { getState, setState } = vanillaStore;
@@ -16,6 +17,7 @@ let breadcrumbsDependentOnPassport = cloneDeep(
 let flowWithPassportComponents = cloneDeep(
   flowWithPassportComponentsMock
 ) as Store.flow;
+let flowWithAutoAnswers = cloneDeep(flowWithAutoAnswersMock) as Store.flow;
 
 beforeEach(() => {
   getState().resetPreview();
@@ -475,7 +477,6 @@ describe("changeAnswer", () => {
     getState().changeAnswer("findProperty");
 
     expect(getState().changedNode).toEqual("findProperty");
-    expect(getState().restore).toStrictEqual(true);
   });
 
   test("should keep breadcrumbs order", () => {
@@ -485,25 +486,86 @@ describe("changeAnswer", () => {
     setState({
       flow,
       breadcrumbs,
+      cachedBreadcrumbs: {},
     });
 
     getState().changeAnswer("text");
-
     getState().record("text", {
-      ...breadcrumbsDependentOnPassport.text,
+      ...breadcrumbs.text,
       auto: false,
       data: {
         text: "Changed",
       },
     });
 
-    expect(Object.keys(getState().breadcrumbs)).toStrictEqual([
-      "findProperty",
-      "text",
-      "drawBoundary",
-      "planningConstraints",
-      "text2",
-    ]);
+    expect(getState().changedNode).toEqual("text");
+
+    const changedBreadcrumbs = {
+      ...getState().breadcrumbs,
+      ...getState().cachedBreadcrumbs,
+    };
+    const changedBreadcrumbKeys = Object.keys(changedBreadcrumbs);
+    const originalBreadcrumbKeys = Object.keys(breadcrumbsDependentOnPassport);
+    expect(changedBreadcrumbKeys).toStrictEqual(originalBreadcrumbKeys);
+  });
+
+  test("should auto-answer future nodes with the updated passport variable correctly", () => {
+    // See https://trello.com/c/B8xMMJLo/1930-changes-from-the-review-page-that-affect-the-fee-do-not-update-the-fee
+    //   and https://editor.planx.uk/testing/autoanswer-change-test
+    const flow = { ...flowWithAutoAnswers };
+
+    // Mock initial state as if we've initially answered "Yes" to the question and reached the Review component, about to click "change"
+    const breadcrumbs = {
+      rCjETwjwE3: {
+        auto: false,
+        answers: ["b0qdvLAxIL"],
+      },
+      vgj2UNYK9r: {
+        answers: ["X9JjnbPpnd"],
+        auto: true,
+      },
+    } as Store.breadcrumbs;
+    const cachedBreadcrumbs = {} as Store.cachedBreadcrumbs;
+
+    setState({
+      flow,
+      breadcrumbs,
+      cachedBreadcrumbs,
+    });
+
+    // Assert our initial passport state is correct
+    expect(getState().computePassport()).toEqual({
+      data: {
+        "application.fee.exemption.disability": ["true"],
+      },
+    });
+
+    // Change the question answer from "Yes" to "No"
+    getState().changeAnswer("rCjETwjwE3");
+    getState().record("rCjETwjwE3", {
+      answers: ["ykNZocRJtQ"],
+      auto: false,
+    });
+
+    expect(getState().changedNode).toEqual("rCjETwjwE3");
+
+    // Confirm the passport has updated to reflect new answer and has not retained previous answer
+    expect(getState().computePassport()).toEqual({
+      data: {
+        "application.fee.exemption.disability": ["false"],
+      },
+    });
+
+    const originalAnswer = {
+      vgj2UNYK9r: {
+        answers: ["X9JjnbPpnd"],
+        auto: true,
+      },
+    } as Store.cachedBreadcrumbs;
+
+    // Confirm that our original answer is still preserved in cachedBreadcrumbs, but not included in current breadcrumbs
+    expect(getState().breadcrumbs).not.toContain(originalAnswer);
+    expect(getState().cachedBreadcrumbs).toStrictEqual(originalAnswer);
   });
 });
 
