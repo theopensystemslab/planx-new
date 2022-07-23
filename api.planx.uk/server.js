0e5c6eb955731ed918d3d8dd26fb0092ecfcd51b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-07-23 15:19:37 +0100

    build: Update express-pino-logger to v7

--- api.planx.uk/server.js
+++ api.planx.uk/server.js
@@ -231,10 +231,12 @@ app.use(cookieParser());
 // XXX: Currently not checking for JWT and including req.user in every
 //      express endpoint because authentication also uses req.user. More info:
 //      https://github.com/theopensystemslab/planx-new/pull/555#issue-684435760
+// TODO: requestProperty can now be set. This might resolve the above issue.
 const useJWT = expressjwt({
   secret: process.env.JWT_SECRET,
   algorithms: ["HS256"],
   credentialsRequired: true,
+  requestProperty: "user",
   getToken: (req) =>
     req.cookies?.jwt ??
     req.headers.authorization?.match(/^Bearer (\S+)$/)?.[1] ??
