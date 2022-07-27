d25d5a51b6c6c2b7b0579dce9977e4b8d9014e93
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-07-27 23:19:25 +0100

    fix: Display ContentPage on Save and Return path

--- editor.planx.uk/src/routes/preview.tsx
+++ editor.planx.uk/src/routes/preview.tsx
@@ -101,6 +101,7 @@ const routes = compose(
     "/pages/:page": map((req) => {
       return route({
         view: () => <ContentPage page={req.params.page} />,
+        data: { isContentPage: true },
       });
     }),
   })
