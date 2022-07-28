005fb0cde00cab3cfed338f5274038e7da11d43b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-07-28 21:54:38 +0100

    chore: Update Notify API keys to live

--- api.planx.uk/saveAndReturn/notify.js
+++ api.planx.uk/saveAndReturn/notify.js
@@ -1,5 +1,5 @@
 const { NotifyClient } = require("notifications-node-client");
 
-const notifyClient = new NotifyClient(process.env.GOVUK_NOTIFY_API_KEY_TEAM);
+const notifyClient = new NotifyClient(process.env.GOVUK_NOTIFY_API_KEY);
 
 module.exports = { notifyClient };
