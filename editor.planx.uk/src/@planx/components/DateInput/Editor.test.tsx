f75e3a26025ceee8d0af121adb21bbedd7c41ca9
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-08-22 16:08:55 +0100

    fix: Date should not be required in Editor - Update editorValidationSchema to address main issue - Update tests to remove false positives - Update paddedDate() to catch empty deleted dates which were formatted as '--'

--- editor.planx.uk/src/@planx/components/DateInput/Editor.test.tsx
+++ editor.planx.uk/src/@planx/components/DateInput/Editor.test.tsx
@@ -1,14 +1,15 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { clear } from "@testing-library/user-event/dist/clear";
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import { act, Simulate } from "react-dom/test-utils";
 
 import DateInputComponent from "./Editor";
 
 const minError = "Min must be less than max";
 const maxError = "Max must be greater than min";
+const invalidError = "Enter a valid date in DD.MM.YYYY format";
 
 describe("DateInputComponent - Editor Modal", () => {
   it("renders", () => {
@@ -20,7 +21,7 @@ describe("DateInputComponent - Editor Modal", () => {
     expect(screen.getByText("Date Input")).toBeInTheDocument();
   });
 
-  it("throws an error for incompatible date values", () => {
+  it("throws an error for incompatible date values", async () => {
     const handleSubmit = jest.fn();
     render(
       <DndProvider backend={HTML5Backend}>
@@ -43,15 +44,15 @@ describe("DateInputComponent - Editor Modal", () => {
     userEvent.type(maxMonth, "01");
     userEvent.type(maxYear, "1900");
 
-    act(() => Simulate.submit(screen.getByRole("form")));
+    fireEvent.submit(screen.getByRole("form"));
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(screen.getByText(minError)).toBeVisible();
       expect(screen.getByText(maxError)).toBeVisible();
     });
   });
 
-  it("does not show errors if min is less than max", () => {
+  it("does not show errors if min is less than max", async () => {
     const handleSubmit = jest.fn();
     render(
       <DndProvider backend={HTML5Backend}>
@@ -74,11 +75,37 @@ describe("DateInputComponent - Editor Modal", () => {
     userEvent.type(maxMonth, "01");
     userEvent.type(maxYear, "2000");
 
-    act(() => Simulate.submit(screen.getByRole("form")));
+    fireEvent.submit(screen.getByRole("form"));
 
-    waitFor(() => {
+    await waitFor(() => {
       expect(screen.queryByText(minError)).toBeNull();
       expect(screen.queryByText(maxError)).toBeNull();
+    });
+  });
+
+  it("does not show an error if user deletes a date", async () => {
+    const handleSubmit = jest.fn();
+    const node = { data: { min: "1900-02-13", max: "2000-12-14" } };
+    render(
+      <DndProvider backend={HTML5Backend}>
+        <DateInputComponent id="test" handleSubmit={handleSubmit} node={node} />
+      </DndProvider>
+    );
+
+    const [minDay, maxDay] = screen.getAllByPlaceholderText("DD");
+    const [minMonth, maxMonth] = screen.getAllByPlaceholderText("MM");
+    const [minYear, maxYear] = screen.getAllByPlaceholderText("YYYY");
+
+    expect(screen.queryAllByText(invalidError)).toHaveLength(0);
+
+    [minDay, maxDay, minMonth, maxMonth, minYear, maxYear].forEach((el) =>
+      clear(el)
+    );
+
+    fireEvent.submit(screen.getByRole("form"));
+
+    await waitFor(() => {
+      expect(screen.queryAllByText(invalidError)).toHaveLength(0);
     });
   });
 });
