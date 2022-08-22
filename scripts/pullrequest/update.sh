24cf88d2c8797a52407ac522a3b66e7fed0eacb4
Gunar Gessner <gunar@gunargessner.com>
2022-08-22 13:58:58 -0300

    chore: update file name

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.staging.yml up --build --force-recreate --remove-orphans -d
+DOCKER_BUILDKIT=1 docker-compose --env-file .env.prod -f docker-compose.yml -f docker-compose.pizza.yml up --build --force-recreate --remove-orphans -d
