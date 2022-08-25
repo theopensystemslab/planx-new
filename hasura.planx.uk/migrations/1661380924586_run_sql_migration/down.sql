006dc78cdf9da6d150f9c51535f3544f0699743c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-08-25 11:20:24 +0100

    fix: Tidy up following PR comments

--- hasura.planx.uk/migrations/1661380924586_run_sql_migration/down.sql
+++ hasura.planx.uk/migrations/1661380924586_run_sql_migration/down.sql
@@ -1,4 +1,2 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
 DROP TRIGGER IF EXISTS delete_lowcal_session_scheduled_events_trigger ON lowcal_sessions;
 DROP FUNCTION IF EXISTS delete_lowcal_session_scheduled_events;
