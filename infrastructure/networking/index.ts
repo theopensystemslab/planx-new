82418d41d2a837477058fe4ceafcd79cf8266690
Gunar Gessner <gunar@gunargessner.com>
2022-08-26 11:33:55 +0200

    infra: enable CloudWatch Insights (#1128)

--- infrastructure/networking/index.ts
+++ infrastructure/networking/index.ts
@@ -6,13 +6,21 @@ import * as awsx from "@pulumi/awsx";
 const vpc = new awsx.ec2.Vpc("vpc", {
   numberOfNatGateways: 0,
 });
-const cluster = new awsx.ecs.Cluster("cluster", { vpc });
+const cluster = new awsx.ecs.Cluster("cluster", {
+  vpc,
+  settings: [
+    {
+      name: "containerInsights",
+      value: "enabled",
+    },
+  ],
+});
 const subnet = new aws.rds.SubnetGroup("subnet-group", {
   subnetIds: vpc.publicSubnetIds,
 });
 
 export const subnetId = subnet.id;
-export const vpcSecurityGroupIds = cluster.securityGroups.map(g => g.id);
+export const vpcSecurityGroupIds = cluster.securityGroups.map((g) => g.id);
 export const vpcId = vpc.id;
 export const clusterName = cluster.cluster.name;
 export const privateSubnetIds = vpc.privateSubnetIds;
