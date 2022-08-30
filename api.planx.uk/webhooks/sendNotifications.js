ac0ab13c6e774322d612deba52b742c8d26c85dd
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-08-31 00:57:47 +0200

    set up event to send slack message on application insert

---
+++ api.planx.uk/webhooks/sendNotifications.js
@@ -0,0 +1,52 @@
+import SlackNotify from 'slack-notify';
+
+// hook into the #planx-notifications channel
+const slack = SlackNotify(process.env.SLACK_WEBHOOK_URL);
+
+const sendSlackNotification = (req, res, next) => {
+  const supportedTypes = ["bops-submission", "uniform-submission"];
+  if (!req.body || !req.query?.type || !supportedTypes.includes(req.query.type)) {
+    return res.status(404).send({
+      message: "Missing info required to send a Slack notification"
+    });
+  }
+
+  try {
+    const data = req.body?.event?.data?.new;
+
+    if (req.query.type === "bops-submission") {
+      const isBOPSStaging = data?.destination_url?.includes("staging");
+      if (isBOPSStaging) {
+        return res.status(200).send({
+          message: `Staging application submitted, skipping Slack notication`
+        });
+      }
+
+      const bopsMessage = `:incoming_envelope: New BOPS submission *${data?.bops_id}* [${data?.destination_url}]`;
+      slack.send(bopsMessage).then(() => {
+        res.status(200).send({ message: "Posted to Slack", data: bopsMessage });
+      });
+    }
+    
+    if (req.query.type === "uniform-submission") {
+      const isUniformStaging = data?.response?.["_links"]?.self?.href?.includes("staging");
+      if (isUniformStaging) {
+        return res.status(200).send({
+          message: `Staging application submitted, skipping Slack notication`
+        });
+      }
+
+      const uniformMessage = `:incoming_envelope: New Uniform submission *${data?.idox_submission_id}* [${data?.destination?.toUpperCase()}]`;
+      slack.send(uniformMessage).then(() => {
+        res.status(200).send({ message: "Posted to Slack", data: uniformMessage });
+      });
+    }
+  } catch (error) {
+    return next({
+      error,
+      message: `Failed to send ${req.query.type} Slack notification. Error: ${error}`,
+    });
+  }
+};
+
+export { sendSlackNotification };
