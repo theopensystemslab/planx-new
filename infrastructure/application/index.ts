987bbaecf7fc79a60e9814c25fc1e66ac6946354
Gunar Gessner <gunar@gunargessner.com>
2022-09-09 15:03:54 -0300

    chore: set retention to 28 days

--- infrastructure/application/index.ts
+++ infrastructure/application/index.ts
@@ -157,7 +157,7 @@ export = async () => {
     taskDefinitionArgs: {
       logGroup: new aws.cloudwatch.LogGroup("metabase", {
         namePrefix: "metabase",
-        retentionInDays: 0, // 0 == infinity
+        retentionInDays: 28,
       }),
       container: {
         // if changing, also check docker-compose.yml
@@ -293,7 +293,7 @@ export = async () => {
     taskDefinitionArgs: {
       logGroup: new aws.cloudwatch.LogGroup("api", {
         namePrefix: "api",
-        retentionInDays: 0, // 0 == infinity
+        retentionInDays: 28,
       }),
       container: {
         image: repo.buildAndPushImage({
@@ -446,7 +446,7 @@ export = async () => {
     taskDefinitionArgs: {
       logGroup: new aws.cloudwatch.LogGroup("sharedb", {
         namePrefix: "sharedb",
-        retentionInDays: 0, // 0 == infinity
+        retentionInDays: 28,
       }),
       container: {
         image: repo.buildAndPushImage("../../sharedb.planx.uk"),
