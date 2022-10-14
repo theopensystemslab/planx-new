6ee9b4207ce668728715cafed9d04a29ddbb8f8f
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-10-14 08:25:20 +0200

    switch drawing cursor for UAT request

--- editor.planx.uk/src/@planx/components/DrawBoundary/Public/index.tsx
+++ editor.planx.uk/src/@planx/components/DrawBoundary/Public/index.tsx
@@ -135,7 +135,7 @@ export default function Component(props: Props) {
             <my-map
               id="draw-boundary-map"
               drawMode
-              drawPointer="dot"
+              drawPointer="crosshair"
               drawGeojsonData={JSON.stringify(boundary)}
               zoom={20}
               maxZoom={23}
