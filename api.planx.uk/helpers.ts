2dc8d40c55b91d076595dcbbf1dad0978b501983
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-11-14 14:20:32 +0000

    refactor: Sort published flows by version

--- api.planx.uk/helpers.ts
+++ api.planx.uk/helpers.ts
@@ -26,7 +26,7 @@ const getMostRecentPublishedFlow = async (id: string): Promise<Flow["data"]> => 
     gql`
       query GetMostRecentPublishedFlow($id: uuid!) {
         flows_by_pk(id: $id) {
-          published_flows(limit: 1, order_by: { id: desc }) {
+          published_flows(limit: 1, order_by: { version: desc }) {
             data
           }
         }
@@ -47,7 +47,7 @@ const getPublishedFlowByDate = async (id: string, created_at: string) => {
         flows_by_pk(id: $id) {
           published_flows(
             limit: 1,
-            order_by: { id: desc },
+            order_by: { version: desc },
             where: { created_at: {_lte: $created_at} }
           ) {
             data
