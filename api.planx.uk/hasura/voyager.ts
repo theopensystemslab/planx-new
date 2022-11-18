6082be0847e12cebea20cf4c8070b995530588ac
Ben <benjamin@opensystemlabs.io>
2022-11-18 17:01:01 +0000

    Add GRAPHQL_URL_EXT env variable (later removed)

--- api.planx.uk/hasura/voyager.ts
+++ api.planx.uk/hasura/voyager.ts
@@ -1,13 +1,15 @@
 import assert from "assert";
-import url from 'node:url';
 import { express as voyagerMiddleware } from "graphql-voyager/middleware";
 import { NextFunction, Response, Request } from "express";
 
-assert(process.env.HASURA_GRAPHQL_URL);
+assert(process.env.GRAPHQL_URL_EXT);
 export default function graphQLVoyagerHandler(
   adminKey: string | undefined = undefined
 ) {
   return (req: Request, res: Response, next: NextFunction) => {
+    const graphQLURL: string = process.env.GRAPHQL_URL_EXT!;
+    const graphQLHost: string = new URL(graphQLURL).host;
+
     if (adminKey && !req.user?.sub)
       return next({ status: 401, message: "User ID missing from JWT" });
 
@@ -16,20 +18,12 @@ export default function graphQLVoyagerHandler(
       "script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net"
     );
     res.header("Content-Security-Policy", "worker-src blob:");
+    res.header("Access-Control-Allow-Origin", graphQLHost);
 
     const headers: any = {
       "content-type": "application/json",
-      "mode": "no-cors",
     };
     if (adminKey) headers["x-hasura-admin-secret"] = adminKey;
-
-    // this is a somewhat fragile way of constructing a URL that will work outside of docker
-    let graphQLURL = process.env.HASURA_GRAPHQL_URL!;
-    if (graphQLURL.includes("hasura-proxy")) {
-      const host: string = req.hostname;
-      const graphQLHost = host.replace(/^api/, "hasura");
-      graphQLURL = `//${graphQLHost}/v1/graphql`;
-    }
 
     return voyagerMiddleware({
       endpointUrl: graphQLURL,
