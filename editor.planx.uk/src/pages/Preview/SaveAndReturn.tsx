44ddc56e644bc5c96e7b119b096c34aa50c25f90
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-11-25 10:54:54 +0000

    refactor: Tidy up redundant styles

--- editor.planx.uk/src/pages/Preview/SaveAndReturn.tsx
+++ editor.planx.uk/src/pages/Preview/SaveAndReturn.tsx
@@ -1,5 +1,4 @@
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
 import Card from "@planx/components/shared/Preview/Card";
 import QuestionHeader from "@planx/components/shared/Preview/QuestionHeader";
 import { useFormik } from "formik";
