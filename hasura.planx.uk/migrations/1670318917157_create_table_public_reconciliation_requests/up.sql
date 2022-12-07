d7a141e1045906dbf461be5098910347edf69bf2
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-12-07 09:45:12 +0100

    feat: add audit table to track resumed sessions (#1314)

---
+++ hasura.planx.uk/migrations/1670318917157_create_table_public_reconciliation_requests/up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "public"."reconciliation_requests" (
+  "id" serial NOT NULL, 
+  "session_id" text, 
+  "response" jsonb, 
+  "message" text, 
+  "created_at" timestamptz NOT NULL DEFAULT now(), 
+  PRIMARY KEY ("id") , UNIQUE ("id")
+);
+COMMENT ON TABLE "public"."reconciliation_requests" IS E'Audit log of reconcilied session data for successfully resumed lowcal_sessions';
