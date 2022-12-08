b323ef897cbbb0846377c0c773923e07ad06974c
Gunar Gessner <gunar@gunargessner.com>
2022-12-08 14:10:16 -0300

    fix(public): add missing doncaster domain config

--- editor.planx.uk/src/routes/utils.ts
+++ editor.planx.uk/src/routes/utils.ts
@@ -55,6 +55,7 @@ const PREVIEW_ONLY_DOMAINS = [
   "planningservices.buckinghamshire.gov.uk",
   "planningservices.lambeth.gov.uk",
   "planningservices.southwark.gov.uk",
+  "planningservices.doncaster.gov.uk",
   // XXX: un-comment the next line to test custom domains locally
   // "localhost",
 ];
