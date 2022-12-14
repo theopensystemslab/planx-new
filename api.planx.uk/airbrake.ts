aff87e5a6e2148c03283e1fd607a78773baad480
Ben <benjamin@opensystemlabs.io>
2022-12-14 13:22:26 +0000

    Delegate error handling to `reportError`

--- api.planx.uk/airbrake.ts
+++ api.planx.uk/airbrake.ts
@@ -1,16 +1,32 @@
 import { Notifier } from "@airbrake/node";
 
-const airbrake =
-  process.env.NODE_ENV === "production" &&
-  process.env.AIRBRAKE_PROJECT_ID &&
-  process.env.AIRBRAKE_PROJECT_KEY
-    ? new Notifier({
-        projectId: Number(process.env.AIRBRAKE_PROJECT_ID),
-        projectKey: process.env.AIRBRAKE_PROJECT_KEY,
-        environment: process.env.API_URL_EXT!.endsWith("planx.uk")
-          ? "production"
-          : "staging",
-      })
-    : undefined;
+export const reportError = getErrorLogger().notify;
 
-export default airbrake;
+function getErrorLogger(): ErrorLogger {
+  const hasConfig =
+    process.env.NODE_ENV === "production" &&
+    process.env.AIRBRAKE_PROJECT_ID &&
+    process.env.AIRBRAKE_PROJECT_KEY;
+
+  if (!hasConfig) {
+    console.error("Airbrake not configured");
+    return {
+      notify: (error) => {
+        console.error(error);
+        console.log("Error was not sent to Airbrake");
+      },
+    };
+  }
+
+  return new Notifier({
+    projectId: Number(process.env.AIRBRAKE_PROJECT_ID),
+    projectKey: process.env.AIRBRAKE_PROJECT_KEY,
+    environment: process.env.API_URL_EXT!.endsWith("planx.uk")
+      ? "production"
+      : "staging",
+  });
+}
+
+interface ErrorLogger {
+  notify: (args: unknown) => void;
+}
--- api.planx.uk/server.ts
+++ api.planx.uk/server.ts
@@ -47,7 +47,7 @@ import { useFilePermission, useHasuraAuth, useSendEmailAuth } from "./auth";
 // debug, info, warn, error, silent
 const LOG_LEVEL = process.env.NODE_ENV === "test" ? "silent" : "debug";
 
-import airbrake from "./airbrake";
+import { reportError } from "./airbrake";
 import {
   createReminderEvent,
   createExpiryEvent,
@@ -636,13 +636,14 @@ app.post("/webhooks/hasura/send-slack-notification", sendSlackNotification);
 
 const errorHandler: ErrorRequestHandler = (errorObject, _req, res, _next) => {
   const { status = 500, message = "Something went wrong" } = (() => {
-    if (errorObject.error && airbrake) {
-      airbrake.notify(errorObject.error);
+    if (errorObject.error) {
+      reportError(errorObject.error)
       return {
         ...errorObject,
         message: errorObject.message.concat(", this error has been logged"),
       };
     } else {
+      reportError(errorObject)
       return errorObject;
     }
   })();
