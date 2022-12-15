68d5a9c7e5c77474c38cebe690a5870e023eda4a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-12-15 07:36:54 +0000

    feat: Create OS proxy - Currently appends plaintext dummy apikey, not the secret

---
+++ api.planx.uk/proxy/ordnanceSurvey.ts
@@ -0,0 +1,27 @@
+import { useProxy } from './index';
+import { NextFunction, Request, Response } from "express";
+
+const OS_DOMAIN = "https://api.os.uk"
+
+export const useOrdnanceSurveyProxy = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => (
+  useProxy({
+    target: OS_DOMAIN,
+    pathRewrite: (fullPath, req) => appendAPIKey(fullPath, req)
+  })(req, res, next)
+);
+
+const appendAPIKey = (fullPath: string, req: Request): string => {
+  const [path, params] = fullPath.split("?");
+  // Append API key
+  const updatedParams = new URLSearchParams(params);
+  updatedParams.set("key", "my-api-key");
+  // Remove our API baseUrl
+  const updatedPath = path.replace(req.baseUrl, "");
+  // Construct and return rewritten path
+  const resultPath = [updatedPath, updatedParams.toString()].join("?");
+  return resultPath;
+}
