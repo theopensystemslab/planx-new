92611a7de9796ac1bafe609b2faf75ba5d72c9e5
Ben <benjamin@opensystemlabs.io>
2022-12-15 12:55:04 +0000

    Install docker compose plugin via package repository

--- scripts/pullrequest/create.sh
+++ scripts/pullrequest/create.sh
@@ -19,11 +19,7 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
-apt-get install docker-ce docker-ce-cli containerd.io -y
-
-# install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 docker compose --env-file .env.pizza -f docker-compose.yml -f docker-compose.pizza.yml up --build -d
 
