396ed6b4a21bdf9ae40a87d0c5f92c3bb627b7a2
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-12-16 14:21:31 +0100

    stub out tests

---
+++ api.planx.uk/editor/moveFlow.test.ts
@@ -0,0 +1,56 @@
+import supertest from "supertest";
+
+import { queryMock } from "../tests/graphqlQueryMock";
+import { authHeader } from "../tests/mockJWT";
+import app from "../server";
+
+beforeEach(() => {
+  queryMock.mockQuery({
+    name: "GetTeam",
+    variables: {
+      slug: "new-team",
+    },
+    data: {
+      teams: {
+        id: 1,
+      },
+    },
+  });
+
+  queryMock.mockQuery({
+    name: "UpdateFlow",
+    variables: {
+      id: 1,
+      team_id: 2,
+    },
+    data: {
+      update_flows_by_pk: {
+        id: 2,
+      },
+    },
+  });
+});
+
+it("returns an error if authorization headers are not set", async () => {
+  await supertest(app)
+    .post("/flows/1/move/new-team")
+    .expect(401)
+    .then((res) => {
+      expect(res.body).toEqual({
+        error: "No authorization token was found"
+      });
+    });
+});
+
+// debug why this is returning 500
+it.todo("moves a flow to a new team", async () => {
+  await supertest(app)
+    .post("/flows/1/move/new-team")
+    .set(authHeader())
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toEqual({
+        message: "Successfully moved flow to new-team"
+      });
+    });
+});
