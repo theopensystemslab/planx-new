e901a10d5ce369702abb3cf62bbdd402cdca9a4c
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-12-20 11:41:01 +0100

    Revert "fix: Set correct API .env filename (#1326)"
    This reverts commit cfacaa972ebeb18abe32335ddac26f7f8d682d3e.

--- scripts/pull-secrets.sh
+++ scripts/pull-secrets.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 aws s3 cp s3://pizza-secrets/root.env ./../.env
-aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env.test
+aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env
 aws s3 cp s3://pizza-secrets/editor.env ./../editor.planx.uk/.env
 aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test
 echo "Complete: Secrets from S3 copied to local machine"
