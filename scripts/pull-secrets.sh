c9ee973e1d7633234dbcac9cbadf13c742e10979
Jessica McInchak <jessica.mcinchak@gmail.com>
2022-12-20 11:41:01 +0100

    Revert "doc: How to add a secret (#1322)"
    This reverts commit 50dd44f481f3f3fd323962a8c5a3249cc54be491.

--- scripts/pull-secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-aws s3 cp s3://pizza-secrets/root.env ./../.env
-aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env
-aws s3 cp s3://pizza-secrets/editor.env ./../editor.planx.uk/.env
-aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test
-echo "Complete: Secrets from S3 copied to local machine"
