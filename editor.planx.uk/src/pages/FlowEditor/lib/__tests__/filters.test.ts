4cf2e97e90f73930ac2ea942efb2c7869bc8db5c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2022-12-22 08:37:33 +0000

    test(fail): Write flow and test to describe incorrect branching filters logic

--- editor.planx.uk/src/pages/FlowEditor/lib/__tests__/filters.test.ts
+++ editor.planx.uk/src/pages/FlowEditor/lib/__tests__/filters.test.ts
@@ -1,8 +1,10 @@
 import { vanillaStore } from "../store";
+import flowWithBranchingFilters from "./mocks/flowWithBranchingFilters.json";
 import flowWithSingleFilter from "./mocks/flowWithSingleFilter.json";
 
 const { getState, setState } = vanillaStore;
-const { upcomingCardIds, resetPreview } = getState();
+const { upcomingCardIds, resetPreview, record, currentCard, collectedFlags } =
+  getState();
 
 // https://i.imgur.com/k0kkKox.png
 describe("Single filter", () => {
@@ -48,5 +50,31 @@ describe("Single filter", () => {
     });
 
     expect(upcomingCardIds()).toEqual(["lOrm4XmVGv", "nroxFPM2Jx"]);
+  });
+});
+
+describe("Branching filters", () => {
+  beforeEach(() => {
+    resetPreview();
+    setState({ flow: flowWithBranchingFilters });
+  });
+
+  test.skip("Picking up flag routes me correctly through the second filter", () => {
+    let visitedNodes = () => Object.keys(getState().breadcrumbs);
+
+    // Traverse forward to pick up an "IMMUNE" flag
+    record("pickFlag", { answers: ["setImmunity"] });
+    record("immunityPath1", { answers: [] });
+    expect(collectedFlags("immunityPath1", visitedNodes())).toStrictEqual([
+      "IMMUNE",
+    ]);
+
+    // Traverse forward through next filter
+    record("fork", { answers: ["filter2"] });
+
+    // XXX: Test fails here
+    // The currentCard returns as "immunityFlag2" which we should not land on -
+    // the flags on the first filter are skipped, we go direct from "immunityPath1" to "fork"
+    expect(currentCard()?.id).toBe("immunityPath2");
   });
 });
