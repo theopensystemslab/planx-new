39f62c041a4916b4e5fc93ad9ef980e08463e06b
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2023-01-17 08:47:26 +0000

    ADD: new user 43 (#1371)
    Co-authored-by: Ian Jones <ian.jones@substrakt.com>

--- hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
+++ hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
@@ -5,5 +5,6 @@ INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (13
 INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (20, 'Jessica', 'McInchak', 'jessica@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (33, 'Dafydd', 'Pearson', 'dafydd@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (36, 'Benjamin', 'Williams', 'benjamin@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (43, 'Ian', 'Jones', 'hello@ianjo.uk', true) ON CONFLICT (id) DO NOTHING;
 SELECT setval('users_id_seq', max(id)) FROM users;
 SELECT setval('teams_id_seq', max(id)) FROM teams;
