31bc98e96b4575f9d3f1cae4f91a4e14cab33ef9
Ben <benjamin@opensystemlabs.io>
2023-01-20 15:00:20 +0000

    Don't reuse existing server in CI

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -14,7 +14,7 @@ dotenv.config({ path: "../.env" });
 const config: PlaywrightTestConfig = {
   testDir: "./src",
   /* Maximum time one test can run for. */
-  timeout: 60 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -46,8 +46,7 @@ const config: PlaywrightTestConfig = {
   webServer: {
     command: `pnpm test:server`,
     port: 3000,
-    /* Only spin up a webserver if needed */
-    reuseExistingServer: !process.env.CI,
+    reuseExistingServer: false
   },
   /* Configure projects for major browsers */
   projects: [
