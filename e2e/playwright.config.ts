31d68d97b46d05c99d71c983b6ddd8f7ec30fd25
Ben <benjamin@opensystemlabs.io>
2023-01-20 14:19:55 +0000

    Extend e2e timeout

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -14,13 +14,13 @@ dotenv.config({ path: "../.env" });
 const config: PlaywrightTestConfig = {
   testDir: "./src",
   /* Maximum time one test can run for. */
-  timeout: 15 * 1000,
+  timeout: 30 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5 * 1000,
+    timeout: 15 * 1000,
   },
   /* Run tests in files in parallel */
   fullyParallel: false,
