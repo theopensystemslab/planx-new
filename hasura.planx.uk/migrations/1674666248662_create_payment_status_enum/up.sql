0f8d71017f07f36537a785d4458958a1ab555f07
Ben <benjamin@opensystemlabs.io>
2023-01-25 17:16:44 +0000

    Create payment_status enum table

---
+++ hasura.planx.uk/migrations/1674666248662_create_payment_status_enum/up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "public"."payment_status_enum" ("value" text NOT NULL, "comment" text NOT NULL, PRIMARY KEY ("value") );
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'created', E'pending payment created');
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'started', E'pending payment with some user details entered');
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'submitted', E'pending payment authorised but not yet confirmed by the user');
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'capturable', E'pending payment authorized and confirmed by the user');
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'success', E'payment completed');
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'failed', E'payment failed');
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'cancelled', E'payment cancelled');
+
+INSERT INTO "public"."payment_status_enum" ("value", "comment") VALUES (E'error', E'payment failed due to a system error');
