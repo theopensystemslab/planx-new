fa2a55b92b416f713f8a2475cb2c46363935976b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-02-06 15:50:26 +0000

    feat: Session XML endpoint (#1424)

---
+++ api.planx.uk/admin/session/oneAppXML.ts
@@ -0,0 +1,41 @@
+import { gql } from "graphql-request";
+import { Request, Response, NextFunction } from "express";
+import { adminGraphQLClient as client } from "../../hasura"
+
+export const getOneAppXML = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const xml = await fetchSessionXML(req.params.sessionId);
+    res.set("content-type", "text/xml")
+    return res.send(xml)
+  } catch (error) {
+    return next({ message: "Failed to get OneApp XML: " + (error as Error).message });
+  }
+};
+
+const fetchSessionXML = async (sessionId: string) => {
+  try {
+    const query = gql`
+      query GetUniformApplicationBySessionID($submission_reference: String) {
+        uniform_applications(
+          where: { submission_reference: { _eq: $submission_reference } }
+        ) {
+          payload(path: "xml")
+        }
+      }
+    `;
+    const { uniform_applications } = await client.request(
+      query, 
+      { submission_reference: sessionId }
+    );
+
+    if (!uniform_applications?.length) throw Error("Invalid sessionID");
+
+    return uniform_applications[0].payload;
+} catch (error) {
+    throw Error("Unable to fetch session XML: " + (error as Error).message);
+  }
+};
