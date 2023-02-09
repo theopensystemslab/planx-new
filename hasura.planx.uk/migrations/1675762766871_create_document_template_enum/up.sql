a1e64e6f4f6ee1a7ef02f1a04bb10eefd081434a
Ben <benjamin@opensystemlabs.io>
2023-02-09 09:00:03 +0000

    Update temlate names

--- hasura.planx.uk/migrations/1675762766871_create_document_template_enum/up.sql
+++ hasura.planx.uk/migrations/1675762766871_create_document_template_enum/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "public"."document_template" ("value" text NOT NULL, PRIMARY KEY ("value") );COMMENT ON TABLE "public"."document_template" IS E'Enum of predefined document templates available in @opensystemslab/planx-document-templates';
 
-INSERT INTO "public"."document_template"("value") VALUES (E'LDCP');
+-- these values map to exposed templates in @opensystemslab/planx-document-templates
 
-INSERT INTO "public"."document_template"("value") VALUES (E'LDCP_redacted');
+INSERT INTO "public"."document_template"("value") VALUES (E'_blank');
 
 INSERT INTO "public"."document_template"("value") VALUES (E'LDCE');
 
