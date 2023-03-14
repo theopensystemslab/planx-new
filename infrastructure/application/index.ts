8cef0cbcdfbc2f7a4bb1f03c8dab7c9e0629b131
Gunar Gessner <gunar@gunargessner.com>
2023-03-14 18:28:40 -0300

    docs: fix typo

--- infrastructure/application/index.ts
+++ infrastructure/application/index.ts
@@ -545,7 +545,7 @@ export = async () => {
     }
   );
 
-  // XXX: Originally, our certificate (generated in the `certificates` stack) was created in eu-west-2 (London), however, later we wanted to add CloudFlare which only accepts certificates generated in the us-east-1 region. Hence, this here is duplicate code which should be merged into the `certificate` stack.
+  // XXX: Originally, our certificate (generated in the `certificates` stack) was created in eu-west-2 (London), however, later we wanted to add CloudFront which only accepts certificates generated in the us-east-1 region. Hence, this here is duplicate code which should be merged into the `certificate` stack.
   const usEast1 = new aws.Provider("useast1", { region: "us-east-1" });
 
   const customDomains = ((): Array<any> => {
