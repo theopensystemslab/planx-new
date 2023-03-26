8aee2dfdbac85a7ae812f2e871eb44afd9c51143
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-03-26 16:52:54 +0100

    fix: Migration syntax

--- hasura.planx.uk/migrations/1679669504707_create_table_public_payment_requests/up.sql
+++ hasura.planx.uk/migrations/1679669504707_create_table_public_payment_requests/up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE "public"."payment_requests" (
-  "payment_request_id" uuid NOT NULL DEFAULT gen_random_uuid(), 
-  "session_id" uuid NOT NULL, 
-  "created_at" timestamptz NOT NULL DEFAULT now(), 
-  "payee_email" text NOT NULL, 
-  "session_preview_data" jsonb NOT NULL, 
+  "payment_request_id" uuid NOT NULL DEFAULT gen_random_uuid(),
+  "session_id" uuid NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "payee_email" text NOT NULL,
+  "session_preview_data" jsonb NOT NULL,
   "paid_at" timestamptz,
-  PRIMARY KEY ("payment_request_id") , 
-  FOREIGN KEY ("session_id") REFERENCES "public"."lowcal_sessions"("id") 
-    ON UPDATE restrict ON DELETE cascade, 
+  PRIMARY KEY ("payment_request_id"),
+  FOREIGN KEY ("session_id") REFERENCES "public"."lowcal_sessions"("id") ON UPDATE restrict ON DELETE cascade,
   UNIQUE ("payment_request_id")
 );
+
 COMMENT ON TABLE "public"."payment_requests" IS E'Tracks requests for a third-party to pay for applications';
