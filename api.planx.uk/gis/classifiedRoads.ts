9687cc9ce54d7fb377982a8413b2ef82f4fb5cbd
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-03-28 20:46:36 +0200

    convert classifiedRoads.js to ts

--- api.planx.uk/gis/classifiedRoads.ts
+++ api.planx.uk/gis/classifiedRoads.ts
@@ -1,15 +1,55 @@
-import "isomorphic-fetch";
+import { NextFunction, Request, Response } from "express";
+import fetch from "isomorphic-fetch";
+
+type OSFeatures = {
+  type: "FeatureCollection";
+  crs: { 
+    type: "name";
+    properties: {
+      name: "EPSG:4326"
+    };
+  };
+  features: {
+    type: "Feature";
+    geometry: {
+      type: string;
+      coordinates: any[];
+    };
+    properties: OSHighwayFeature;
+  }[];
+};
+
+// this is a meaningful subset of all properties returned, see nock for full sample response object
+type OSHighwayFeature = {
+  OBJECTID: number;
+  Identifier: string;
+  RoadClassification: string;
+  RoadName1: string;
+  FormsPartOf: string;
+};
+
+type PlanningConstraintResponse = Record<string, PlanningConstraintBody>;
+
+type PlanningConstraintBody = {
+  value: boolean;
+  text: string;
+  data?: OSFeatures["features"];
+};
 
 // Passport key comes from Digital Planning Schemas googlesheet
-export const PASSPORT_FN = "road.classified";
+export const PASSPORT_FN: string = "road.classified";
 
-export const classifiedRoadsSearch = async (req, res, next) => {
+export const classifiedRoadsSearch = async (
+  req: Request, 
+  res: Response, 
+  next: NextFunction
+) => {
   if (!req.query.usrn)
     return next({ status: 401, message: "Missing required query param `?usrn=`" });
 
   // Create an OGC XML filter parameter value which will select the road features (lines) that match an USRN
   //   ref https://labs.os.uk/public/os-data-hub-examples/os-features-api/wfs-example-topo-toid-search#maplibre-gl-js
-  const xml = `
+  const xml: string = `
     <ogc:Filter>
       <ogc:PropertyIsLike wildCard="%" singleChar="#" escapeChar="!">
         <ogc:PropertyName>FormsPartOf</ogc:PropertyName>
@@ -19,33 +59,32 @@ export const classifiedRoadsSearch = async (req, res, next) => {
   `;
 
   // Define WFS parameters object
-  const params = {
+  const params: Record<string, string> = {
     service: "WFS",
     request: "GetFeature",
     version: "2.0.0",
     typeNames: "Highways_RoadLink", // sourced from OS MasterMap Highways Network, uniquely includes "RoadClassification" attribute
     outputFormat: "GEOJSON",
     srsName: "urn:ogc:def:crs:EPSG::4326",
-    count: 1, // USRN can match many road segments, but all should share the same classification, so limit to first result
+    count: "1", // USRN can match many road segments, but all should share the same classification, so limit to first result
     filter: xml,
     key: process.env.ORDNANCE_SURVEY_API_KEY || "",
   };
 
   try {
-    const url = `https://api.os.uk/features/v1/wfs?${new URLSearchParams(params).toString()}`;
-    const features = await fetch(url)
-      .then(res => res.json())
-      .then(data => {
+    const url: string = `https://api.os.uk/features/v1/wfs?${new URLSearchParams(params).toString()}`;
+    const features: OSFeatures["features"] = await fetch(url)
+      .then((res: Response) => res.json())
+      .then((data: OSFeatures) => {
         if (!data.features?.length) return;
         
         // Filter out any intersecting roads that are not classified
         //   find all possible values on page 43 https://www.ordnancesurvey.co.uk/documents/os-mastermap-highways-network-roads-technical-specification.pdf 
         //   XX in future consider doing this directly in XML using <ogc:PropertyIsNotEqualTo>
-        const classifiedFeatures = data.features.filter((feature) => !["Unclassified", "Not Classified", "Unknown"].includes(feature.properties["RoadClassification"]));
+        const classifiedFeatures = data.features.filter((feature: OSFeatures["features"][0]) => !["Unclassified", "Not Classified", "Unknown"].includes(feature.properties["RoadClassification"]));
         return classifiedFeatures;
       });
 
-    // Return a response object that's the same shape as a planning constraint
     if (features?.length) {
       return res.json({
         [PASSPORT_FN]: {
@@ -53,16 +92,16 @@ export const classifiedRoadsSearch = async (req, res, next) => {
           text: `is on a Classified Road (${features[0].properties["RoadName1"]} - ${features[0].properties["RoadClassification"]})`,
           data: features,
         }
-      });
+      } as PlanningConstraintResponse)
     } else {
       return res.json({
         [PASSPORT_FN]: {
           value: false,
           text: "is not on a Classified Road",
         }
-      });
+      } as PlanningConstraintResponse)
     }
-  } catch (error) {
+  } catch (error: any) {
     return next({ message: "Failed to fetch classified roads: " + error?.message });
   }
 };
