30466cd1f96aeaeb5e7a54e30f18b0fda740af34
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-03-29 09:35:43 +0100

    chore: Rebase and use feature flag

--- editor.planx.uk/src/routes/index.tsx
+++ editor.planx.uk/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { hasFeatureFlag } from "lib/featureFlags";
 import { lazy, map, mount, NotFoundError, redirect, route } from "navi";
 import * as React from "react";
 
@@ -56,11 +57,9 @@ const editorRoutes = mount({
   ),
 });
 
-const handlePayRoutes = () =>
+const mountPayRoutes = () =>
   map(async () => {
-    // TODO : Pull in actual feature flag
-    const isFeatureFlagActive = true;
-    if (!isFeatureFlagActive) throw new NotFoundError();
+    if (!hasFeatureFlag("INVITE_TO_PAY")) throw new NotFoundError("/pay");
     return lazy(() => import("./pay"));
   });
 
@@ -68,13 +67,13 @@ export default isPreviewOnlyDomain
   ? mount({
       "/:team/:flow/preview": lazy(() => import("./preview")), // XXX: keeps old URL working, but only for the team listed in the domain.
       "/:flow": lazy(() => import("./preview")),
-      "/:flow/pay": handlePayRoutes(),
+      "/:flow/pay": mountPayRoutes(),
       // XXX: We're not sure where to redirect `/` to so for now we'll just return the default 404
       // "/": redirect("somewhere?"),
     })
   : mount({
       "/:team/:flow/preview": lazy(() => import("./preview")), // loads published flow if exists, or current flow
       "/:team/:flow/unpublished": lazy(() => import("./unpublished")), // loads current flow
-      "/:team/:flow/pay": handlePayRoutes(),
+      "/:team/:flow/pay": mountPayRoutes(),
       "*": editorRoutes,
     });
