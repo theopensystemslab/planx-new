66d3fa0e80ca6ebf647bcccdc1a1fd9fc15b60d2
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-03-29 13:27:23 +0200

    first go

---
+++ api.planx.uk/gis/local_authorities/metadata/scotland.js
@@ -0,0 +1,51 @@
+/*
+TEST ONLY currently for TPX Impact
+*/
+
+const scottGovDomain = "https://maps.gov.scot/server";
+
+const planningConstraints = {
+  "article4": { value: false },
+  "designated.nationalPark.cairngorms": {
+    key: "designated.nationalPark.cairngorms",
+    source: scottGovDomain,
+    id: "ScotGov/ProtectedSites",
+    serverIndex: 0,
+    fields: ["objectid", "npcode", "npname"],
+    neg: "is not in a National Park",
+    pos: (data) => ({
+      text: "is in Cairngorms National Park",
+      description: data.npname,
+    }),
+  },
+  "designated.nationalPark.lochLomondTrossachs": {
+    key: "designated.nationalPark.lochLomondTrossachs",
+    source: scottGovDomain,
+    id: "ScotGov/ProtectedSites",
+    serverIndex: 1,
+    fields: ["objectid", "npcode", "npname"],
+    neg: "is not in a National Park",
+    pos: (data) => ({
+      text: "is in Loch Lomond and The Trossachs National Park",
+      description: data.npname, 
+    }),
+  },
+  "designated.nationalScenicArea": {
+    key: "designated.nationalScenicArea",
+    source: scottGovDomain,
+    id: "ScotGov/ProtectedSites",
+    serverIndex: 3,
+    fields: ["objectid", "nsacode", "nsaname"],
+    neg: "is not in a National Scenic Area",
+    pos: (data) => ({
+      text: "is in a National Scenic Area",
+      description: data.nsaname,
+    }),
+  },
+  tpo: { value: false },
+};
+
+export {
+  planningConstraints,
+  A4_KEY
+};
---
+++ api.planx.uk/gis/local_authorities/scotland.js
@@ -0,0 +1,118 @@
+import "isomorphic-fetch";
+import {
+  getQueryableConstraints,
+  makeEsriUrl,
+  setEsriGeometryType,
+  setEsriGeometry,
+  addDesignatedVariable,
+  rollupResultLayers,
+} from "../helpers.js";
+import { planningConstraints } from "./metadata/scotland";
+
+// Process local authority metadata
+const gisLayers = getQueryableConstraints(planningConstraints);
+
+// Fetch a data layer
+async function search(
+  mapServer,
+  featureName,
+  serverIndex,
+  outFields,
+  geometry,
+  geometryType
+) {
+  const { id } = planningConstraints[featureName];
+
+  let url = makeEsriUrl(mapServer, id, serverIndex, {
+    outFields,
+    geometry,
+    geometryType,
+  });
+
+  return fetch(url)
+    .then((response) => response.text())
+    .then((data) => [featureName, data])
+    .catch((error) => {
+      console.log("Error:", error);
+    });
+}
+
+// For this location, iterate through our planning constraints and aggregate/format the responses
+async function go(x, y, siteBoundary, extras) {
+  // If we have a siteBoundary from drawing,
+  //   then query using the polygon, else fallback to the buffered address point
+  const geomType = setEsriGeometryType(siteBoundary);
+  const geom = setEsriGeometry(geomType, x, y, 0.05, siteBoundary);
+
+  const results = await Promise.all(
+    // TODO: Better to have logic here to iterate over listed buildings and TPOs?
+    Object.keys(gisLayers).map((layer) =>
+      search(
+        gisLayers[layer].source,
+        layer,
+        gisLayers[layer].serverIndex,
+        gisLayers[layer].fields,
+        geom,
+        geomType
+      )
+    )
+  );
+
+  const ob = results
+    .filter(([_key, result]) => !(result instanceof Error))
+    .reduce(
+      (acc, [key, result]) => {
+        const data = JSON.parse(result);
+        const k = `${planningConstraints[key].key}`;
+
+        try {
+          if (data.features.length > 0) {
+            const { attributes: properties } = data.features[0];
+            acc[k] = {
+              ...planningConstraints[key].pos(properties),
+              value: true,
+              type: "warning",
+              data: properties,
+            };
+          } else {
+            if (!acc[k]) {
+              acc[k] = {
+                text: planningConstraints[key].neg,
+                value: false,
+                type: "check",
+                data: {},
+              };
+            }
+          }
+        } catch (e) {
+          console.log(e);
+        }
+
+        return acc;
+      },
+      {
+        ...preCheckedLayers,
+        ...extras,
+      }
+    );
+
+  // Scotland hosts multiple national park layers
+  // Roll these up to preserve their granularity when true
+  const nationalParkLayers = ["designated.nationalPark.cairngorms", "designated.nationalPark.lochLomondTrossachs"];
+  const obWithOneNationalPark = rollupResultLayers(
+    ob,
+    nationalParkLayers,
+    "designated.nationalPark",
+  );
+
+  // Add summary "designated" key to response
+  const obWithDesignated = addDesignatedVariable(obWithOneNationalPark);
+
+  return obWithDesignated;
+}
+
+async function locationSearch(x, y, siteBoundary, extras) {
+  return go(x, y, siteBoundary, extras);
+}
+
+export { locationSearch };
