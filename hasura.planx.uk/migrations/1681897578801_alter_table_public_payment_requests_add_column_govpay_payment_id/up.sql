9c4ab95cd013e68638e2b55ff94bace41a81e836
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-04-21 12:11:52 +0100

    fix: Migration script

--- hasura.planx.uk/migrations/1681897578801_alter_table_public_payment_requests_add_column_govpay_payment_id/up.sql
+++ hasura.planx.uk/migrations/1681897578801_alter_table_public_payment_requests_add_column_govpay_payment_id/up.sql
@@ -1,4 +1,4 @@
 alter table "public"."payment_requests" add column "govpay_payment_id" text
  null unique;
 
-comment on column "public"."payment_requests"."govpay_payment_id" is E 'ID returned by GovPay when payment is initialised';
+comment on column "public"."payment_requests"."govpay_payment_id" is E'ID returned by GovPay when payment is initialised';
