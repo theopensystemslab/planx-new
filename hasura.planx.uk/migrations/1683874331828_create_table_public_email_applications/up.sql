52b0899ed6d6e6d7af98b029683becdb2d46ed3b
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-05-15 16:39:12 +0200

    add foreign key relationship on session_id

--- hasura.planx.uk/migrations/1683874331828_create_table_public_email_applications/up.sql
+++ hasura.planx.uk/migrations/1683874331828_create_table_public_email_applications/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "public"."email_applications" (
   "id" serial NOT NULL, 
-  "session_id" text NOT NULL, 
+  "session_id" uuid NOT NULL, 
   "team_slug" text NOT NULL, 
   "recipient" text NOT NULL, 
   "request" jsonb NOT NULL,
@@ -8,6 +8,7 @@ CREATE TABLE "public"."email_applications" (
   "created_at" timestamptz NOT NULL DEFAULT now(), 
   "sanitised_at" timestamptz, 
   PRIMARY KEY ("id"), 
+  FOREIGN KEY ("session_id") REFERENCES "public"."lowcal_sessions"("id") ON UPDATE restrict ON DELETE cascade,
   UNIQUE ("id")
 );
 
