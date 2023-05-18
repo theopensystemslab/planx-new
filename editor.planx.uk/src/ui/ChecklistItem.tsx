5586881277c9f50f66864dd7a2e14563be1e093b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-05-18 21:26:11 +0100

    feat: ChecklistItem

--- editor.planx.uk/src/ui/ChecklistItem.tsx
+++ editor.planx.uk/src/ui/ChecklistItem.tsx
@@ -1,24 +1,23 @@
 import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
 import * as React from "react";
 
 import Checkbox from "./Checkbox";
 
-export const useClasses = makeStyles((theme) => ({
-  root: {
-    width: "100%",
+const Root = styled(Box)(({ theme }) => ({
+  width: "100%",
+  cursor: "pointer",
+  marginBottom: theme.spacing(1.5),
+  paddingRight: theme.spacing(1),
+  display: "inline-flex",
+  alignItems: "center",
+}));
+
+const Label = styled(Typography)(({ theme }) => ({
+  marginLeft: theme.spacing(1.5),
+  "& > label": {
     cursor: "pointer",
-    marginBottom: theme.spacing(1.5),
-    paddingRight: theme.spacing(1),
-    display: "inline-flex",
-    alignItems: "center",
-  },
-  label: {
-    marginLeft: theme.spacing(1.5),
-    "& > label": {
-      cursor: "pointer",
-    },
   },
 }));
 
@@ -26,7 +25,7 @@ interface Props {
   id?: string;
   label: string;
   checked: boolean;
-  onChange: (event?: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
+  onChange: (event?: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 export default function ChecklistItem({
@@ -35,14 +34,12 @@ export default function ChecklistItem({
   checked,
   id,
 }: Props): FCReturn {
-  const classes = useClasses();
-
   return (
-    <Box className={classes.root}>
+    <Root>
       <Checkbox checked={checked} id={id} onChange={onChange} />
-      <Typography variant="body2" className={classes.label}>
+      <Label variant="body2">
         <label htmlFor={id}>{label}</label>
-      </Typography>
-    </Box>
+      </Label>
+    </Root>
   );
 }
