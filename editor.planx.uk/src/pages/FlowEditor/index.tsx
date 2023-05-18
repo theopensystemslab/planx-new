51bf44868adcf521edc40bb54e7ad77adc24873a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-05-18 18:52:45 +0100

    feat: FlowEditor

--- editor.planx.uk/src/pages/FlowEditor/index.tsx
+++ editor.planx.uk/src/pages/FlowEditor/index.tsx
@@ -1,8 +1,7 @@
 import "./components/Settings";
 import "./floweditor.scss";
 
-import { Theme } from "@mui/material/styles";
-import makeStyles from "@mui/styles/makeStyles";
+import Box from "@mui/material/Box";
 import React, { useRef } from "react";
 
 import { rootFlowPath } from "../../routes/utils";
@@ -11,36 +10,23 @@ import PreviewBrowser from "./components/PreviewBrowser";
 import { useStore } from "./lib/store";
 import useScrollControlsAndRememberPosition from "./lib/useScrollControlsAndRememberPosition";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  editor: {
-    position: "relative",
-  },
-  sideLink: {
-    position: "absolute",
-    top: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}));
-
 const FlowEditor: React.FC<any> = ({ flow, breadcrumbs }) => {
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   useScrollControlsAndRememberPosition(scrollContainerRef);
 
   const showPreview = useStore((state) => state.showPreview);
 
-  const classes = useStyles();
-
   return (
-    <div id="editor-container">
-      <div id="editor" className={classes.editor} ref={scrollContainerRef}>
+    <Box id="editor-container">
+      <Box id="editor" ref={scrollContainerRef} sx={{ position: "relative" }}>
         <Flow flow={flow} breadcrumbs={breadcrumbs} />
-      </div>
+      </Box>
       {showPreview && (
         <PreviewBrowser
           url={`${window.location.origin}${rootFlowPath(false)}/preview`}
         />
       )}
-    </div>
+    </Box>
   );
 };
 
