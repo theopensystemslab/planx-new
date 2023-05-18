f2ca10c879740b763dea215707a725bb97e12adb
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-05-18 18:58:14 +0100

    feat: Teams

--- editor.planx.uk/src/pages/Teams.tsx
+++ editor.planx.uk/src/pages/Teams.tsx
@@ -1,8 +1,7 @@
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
-import { Theme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
+import { styled } from "@mui/styles";
 import React from "react";
 import { Link } from "react-navi";
 
@@ -12,55 +11,48 @@ interface Props {
   teams: Array<Team>;
 }
 
-const useStyles = makeStyles<Theme>((theme) => ({
-  root: {
-    backgroundColor: "#2C2C2C",
-    color: "#fff",
-    width: "100%",
-    flex: 1,
-    justifyContent: "flex-start",
-    // paddingTop: "calc(12vh + 75px)",
-    alignItems: "center",
-  },
-  dashboard: {
-    backgroundColor: "#2C2C2C",
-    color: "#fff",
-    width: "100%",
-    maxWidth: 600,
-    margin: "auto",
-    padding: theme.spacing(8, 0, 4, 0),
-  },
-  linkBox: {
-    textDecoration: "none",
-  },
+const Root = styled(Box)(() => ({
+  backgroundColor: "#2C2C2C",
+  color: "#fff",
+  width: "100%",
+  flex: 1,
+  justifyContent: "flex-start",
+  alignItems: "center",
+}));
+
+const Dashboard = styled(Box)(({ theme }) => ({
+  backgroundColor: "#2C2C2C",
+  color: "#fff",
+  width: "100%",
+  maxWidth: 600,
+  margin: "auto",
+  padding: theme.spacing(8, 0, 4, 0),
+}));
+
+const StyledLink = styled(Link)(() => ({
+  textDecoration: "none",
 }));
 
 const Teams: React.FC<Props> = ({ teams }) => {
-  const classes = useStyles();
   return (
-    <Box className={classes.root}>
-      <Box className={classes.dashboard}>
+    <Root>
+      <Dashboard>
         <Box pl={2} pb={2}>
           <Typography variant="h1" gutterBottom>
             Select a team
           </Typography>
         </Box>
         {teams.map(({ name, slug }) => (
-          <Link
-            href={`/${slug}`}
-            key={slug}
-            prefetch={false}
-            className={classes.linkBox}
-          >
+          <StyledLink href={`/${slug}`} key={slug} prefetch={false}>
             <Box mb={4} p={4} component={Card}>
               <Typography variant="h5" component="h2">
                 {name}
               </Typography>
             </Box>
-          </Link>
+          </StyledLink>
         ))}
-      </Box>
-    </Box>
+      </Dashboard>
+    </Root>
   );
 };
 
