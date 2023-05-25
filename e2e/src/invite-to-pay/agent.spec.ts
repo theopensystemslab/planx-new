db0d26b064ba6489f40befa3a4c805a0238c1da0
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-05-25 11:07:20 +0100

    fix: Add "locked application" warning, fix E2E tests (#1723)

--- e2e/src/invite-to-pay/agent.spec.ts
+++ e2e/src/invite-to-pay/agent.spec.ts
@@ -1,11 +1,5 @@
 import { test, expect } from "@playwright/test";
-import {
-  setFeatureFlag,
-  addSessionToContext,
-  cards,
-  fillGovUkCardDetails,
-  modifyFlow,
-} from "../helpers";
+import { setFeatureFlag, addSessionToContext, modifyFlow } from "../helpers";
 import inviteToPayFlow from "../flows/invite-to-pay-flow";
 import {
   Context,
@@ -108,106 +102,65 @@ test.describe("Agent journey", async () => {
     expect(toggleInviteToPayButton).toBeDisabled();
   });
 
-  test.fixme(
-    "agent cannot make changes after sending a payment request - session is locked",
-    async ({ page: firstPage, context: browserContext }) => {
-      const sessionId = await makePaymentRequest({ page: firstPage, context });
+  test("agent cannot make changes after sending a payment request - session is locked", async ({
+    page: firstPage,
+    context: browserContext,
+  }) => {
+    const sessionId = await makePaymentRequest({ page: firstPage, context });
 
-      // Resume session
-      const resumeLink = `/${context.team!.slug!}/${context.flow!
-        .slug!}/preview?analytics=false&sessionId=${sessionId}`;
-      const secondPage = await browserContext.newPage();
-      await secondPage.goto(resumeLink);
-      expect(
-        await secondPage.getByRole("heading", {
-          name: "Resume your application",
-        })
-      ).toBeVisible();
-      await secondPage.getByLabel("Email address").fill(context.user.email);
-      await secondPage.getByTestId("continue-button").click();
+    // Resume session
+    const resumeLink = `/${context.team!.slug!}/${context.flow!
+      .slug!}/preview?analytics=false&sessionId=${sessionId}`;
+    const secondPage = await browserContext.newPage();
+    await secondPage.goto(resumeLink);
+    expect(
+      await secondPage.getByRole("heading", {
+        name: "Resume your application",
+      })
+    ).toBeVisible();
+    await secondPage.getByLabel("Email address").fill(context.user.email);
+    await secondPage.getByTestId("continue-button").click();
 
-      // Cannot click "back" or make changes
-      expect(
-        await secondPage.getByRole("heading", {
-          name: "Resume your application",
-        })
-      ).toBeVisible();
-      await secondPage.getByTestId("continue-button").click();
-      const backButton = await secondPage
-        .locator("#main-content")
-        .getByRole("button", { name: "Back" });
-      expect(backButton).not.toBeVisible();
-    }
-  );
+    expect(
+      await secondPage.getByRole("heading", {
+        name: "Your application is locked",
+      })
+    ).toBeVisible();
+    expect(secondPage.getByTestId("continue-button")).not.toBeVisible();
+  });
 
-  test.fixme(
-    "agent cannot pay after sending a payment request",
-    async ({ page: firstPage, context: browserContext }) => {
-      const sessionId = await makePaymentRequest({ page: firstPage, context });
+  test("reconciliation does not apply to sessions with open payment requests", async ({
+    page: firstPage,
+    context: browserContext,
+  }) => {
+    const sessionId = await makePaymentRequest({ page: firstPage, context });
 
-      // Resume session
-      const resumeLink = `/${context.team!.slug!}/${context.flow!
-        .slug!}/preview?analytics=false&sessionId=${sessionId}`;
-      const secondPage = await browserContext.newPage();
-      await secondPage.goto(resumeLink);
-      expect(
-        await secondPage.getByRole("heading", {
-          name: "Resume your application",
-        })
-      ).toBeVisible();
-      await secondPage.getByLabel("Email address").fill(context.user.email);
-      await secondPage.getByTestId("continue-button").click();
+    // Make change to flow, publish it
+    await modifyFlow({ context, modifiedFlow: modifiedInviteToPayFlow });
 
-      // TODO: Check desired behaviour and fix this
-      // Make payment
-      expect(
-        await secondPage.getByRole("heading", {
-          name: "Resume your application",
-        })
-      ).toBeVisible();
-      await secondPage.getByTestId("continue-button").click();
-      expect(
-        await secondPage.getByRole("heading", {
-          name: "Pay for your application",
-        })
-      ).toBeVisible();
-      await secondPage.getByText("Pay now using GOV.UK Pay").click();
-      await fillGovUkCardDetails({
-        page: secondPage,
-        cardNumber: cards.successful_card_number,
-      });
-      await secondPage.getByRole("button", { name: "Confirm payment" }).click();
+    // Navigate to resume session link
+    const resumeLink = `/${context.team!.slug!}/${context.flow!
+      .slug!}/preview?analytics=false&sessionId=${sessionId}`;
+    const secondPage = await browserContext.newPage();
+    await secondPage.goto(resumeLink);
+    expect(
+      await secondPage.getByRole("heading", { name: "Resume your application" })
+    ).toBeVisible();
+    await secondPage.getByLabel("Email address").fill(context.user.email);
+    await secondPage.getByTestId("continue-button").click();
 
-      // FAIL: Session is locked and cannot be modified so payment not registered
-    }
-  );
+    // Reconciliation ignored
+    const reconciliationText = await secondPage.getByText(
+      "This service has been updated since you last saved your application. We will ask you to answer any updated questions again when you continue."
+    );
+    expect(reconciliationText).not.toBeVisible();
 
-  test.fixme(
-    "reconciliation does not apply to sessions with open payment requests",
-    async ({ page, context: browserContext }) => {
-      const sessionId = await makePaymentRequest({ page, context });
-
-      // Make change to flow, publish it
-      await modifyFlow({ context, modifiedFlow: modifiedInviteToPayFlow });
-
-      // Navigate to resume session link
-      const resumeLink = `/${context.team!.slug!}/${context.flow!
-        .slug!}/preview?analytics=false&sessionId=${sessionId}`;
-      const newPage = await browserContext.newPage();
-      await newPage.goto(resumeLink);
-      expect(
-        await newPage.getByRole("heading", { name: "Resume your application" })
-      ).toBeVisible();
-      await newPage.getByLabel("Email address").fill(context.user.email);
-      await newPage.getByTestId("continue-button").click();
-
-      // Reconciliation ignored
-      const reconciliationText = await newPage.getByText(
-        "This service has been updated since you last saved your application. We will ask you to answer any updated questions again when you continue."
-      );
-      expect(reconciliationText).not.toBeVisible();
-
-      // FAIL: Reconciliation text displayed
-    }
-  );
+    // Locked application message displayed
+    expect(
+      await secondPage.getByRole("heading", {
+        name: "Your application is locked",
+      })
+    ).toBeVisible();
+    expect(secondPage.getByTestId("continue-button")).not.toBeVisible();
+  });
 });
