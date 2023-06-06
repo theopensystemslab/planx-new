e3dba9180841feff53580b0e3e4eef0736f31d9a
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-06-06 16:19:13 +0200

    basic validation and back/change behavior

--- editor.planx.uk/src/@planx/components/MultipleFileUpload/Public.tsx
+++ editor.planx.uk/src/@planx/components/MultipleFileUpload/Public.tsx
@@ -1,8 +1,10 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { PublicProps } from "@planx/components/ui";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FONT_WEIGHT_BOLD } from "theme";
+import ErrorWrapper from "ui/ErrorWrapper";
+import { array } from "yup";
 
 import { FileUploadSlot } from "../FileUpload/Public";
 import Card from "../shared/Preview/Card";
@@ -10,31 +12,98 @@ import QuestionHeader from "../shared/Preview/QuestionHeader";
 import { Dropzone } from "../shared/PrivateFileUpload/Dropzone";
 import { FileStatus } from "../shared/PrivateFileUpload/FileStatus";
 import { UploadedFileCard } from "../shared/PrivateFileUpload/UploadedFileCard";
+import { getPreviouslySubmittedData, makeData } from "../shared/utils";
 import { MultipleFileUpload } from "./model";
 
 type Props = PublicProps<MultipleFileUpload>;
 
 export default Component;
 
+const slotsSchema = array()
+  .required()
+  .test({
+    name: "nonUploading",
+    message: "Upload at least one file",
+    test: (slots?: Array<FileUploadSlot>) => {
+      return Boolean(
+        slots &&
+          slots.length > 0 &&
+          !slots.some((slot) => slot.status === "uploading")
+      );
+    },
+  });
+
 function Component(props: Props) {
-  const [slots, setSlots] = useState<FileUploadSlot[]>([]);
+  const recoveredSlots = getPreviouslySubmittedData(props)?.map(
+    (slot: FileUploadSlot) => slot.cachedSlot
+  );
+  const [slots, setSlots] = useState<FileUploadSlot[]>(recoveredSlots ?? []);
   const [fileUploadStatus, setFileUploadStatus] = useState<string | undefined>(
     undefined
   );
+  const [validationError, setValidationError] = useState<string | undefined>();
+
+  const handleSubmit = () => {
+    slotsSchema
+      .validate(slots)
+      .then(() => {
+        props.handleSubmit?.(
+          makeData(
+            props,
+            slots.map((slot) => ({
+              url: slot.url,
+              filename: slot.file.path,
+              cachedSlot: {
+                ...slot,
+                file: {
+                  path: slot.file.path,
+                  type: slot.file.type,
+                  size: slot.file.size,
+                },
+              },
+            }))
+          )
+        );
+      })
+      .catch((err) => {
+        setValidationError(err.message);
+      });
+  };
+
+  /**
+   * Declare a ref to hold a mutable copy the up-to-date validation error.
+   * The intention is to prevent frequent unnecessary update loops that clears the
+   * validation error state if it is already empty.
+   */
+  const validationErrorRef = useRef(validationError);
+  useEffect(() => {
+    validationErrorRef.current = validationError;
+  }, [validationError]);
+
+  useEffect(() => {
+    if (validationErrorRef.current) {
+      setValidationError(undefined);
+    }
+  }, [slots]);
 
   return (
-    <Card handleSubmit={props.handleSubmit} isValid>
+    <Card
+      handleSubmit={handleSubmit}
+      isValid={slots.every((slot) => slot.url && slot.status === "success")}
+    >
       <QuestionHeader title={props.title} description={props.description} />
       <Box>
         <FileStatus status={fileUploadStatus} />
         <Box sx={{ display: "flex", mb: 4, gap: 2 }}>
-          <Box sx={{ flex: "50%" }}>
-            <Dropzone
-              slots={slots}
-              setSlots={setSlots}
-              setFileUploadStatus={setFileUploadStatus}
-            />
-          </Box>
+          <ErrorWrapper error={validationError} id={props.id}>
+            <Box sx={{ flex: "50%" }}>
+              <Dropzone
+                slots={slots}
+                setSlots={setSlots}
+                setFileUploadStatus={setFileUploadStatus}
+              />
+            </Box>
+          </ErrorWrapper>
           <Box sx={{ flex: "50%" }}>
             <Typography fontWeight={FONT_WEIGHT_BOLD}>
               Required files
