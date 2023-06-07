805734853794f0f5ba943689392f3cb2c0dd5874
builditben <builditben@fastmail.com>
2023-06-07 09:37:24 +0100

    Add LDC mocks for submissions

---
+++ e2e/tests/api-driven/src/invite-to-pay/mocks/index.ts
@@ -0,0 +1,25 @@
+import type {
+  FlowGraph,
+  Breadcrumbs,
+  Passport,
+  Node,
+} from "@opensystemslab/planx-core/types";
+import { ComponentType } from "@opensystemslab/planx-core/types";
+import flow from "./flow.json";
+import session from "./session.json";
+
+export const mockBreadcrumbs = session["data"]["breadcrumbs"] as Breadcrumbs;
+
+export const mockPassport = session["data"]["passport"] as Passport;
+
+export const inviteToPayFlowGraph = flow["data"] as FlowGraph;
+
+export function sendNodeWithDestination(destination): Node {
+  return {
+    type: ComponentType.Send,
+    data: {
+      title: "Send",
+      destinations: [destination.toLowerCase()],
+    },
+  };
+}
