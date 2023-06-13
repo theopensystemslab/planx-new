50ded83d201bed4a4ae646575c1c999a59a0a25f
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2023-06-13 14:13:32 +0100

    update: styles for radio + checkbox (#1739)

--- editor.planx.uk/src/@planx/components/shared/Radio/DescriptionRadio.tsx
+++ editor.planx.uk/src/@planx/components/shared/Radio/DescriptionRadio.tsx
@@ -1,6 +1,7 @@
 import Box from "@mui/material/Box";
 import FormLabel from "@mui/material/FormLabel";
 import Radio, { RadioProps } from "@mui/material/Radio";
+import { useRadioGroup } from "@mui/material/RadioGroup";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import React from "react";
@@ -12,12 +13,25 @@ export interface Props {
   onChange: RadioProps["onChange"];
 }
 
-const StyledFormLabel = styled(FormLabel)(({ theme }) => ({
-  border: "2px lightgray solid",
+interface StyledFormLabelProps {
+  isSelected: boolean;
+}
+
+const StyledFormLabel = styled(FormLabel, {
+  shouldForwardProp: (prop) => prop !== "isSelected",
+})<StyledFormLabelProps>(({ theme, isSelected }) => ({
+  border: "2px solid",
+  borderColor: isSelected
+    ? theme.palette.primary.main
+    : theme.palette.secondary.main,
   padding: theme.spacing(1.5),
   cursor: "pointer",
   display: "block",
   height: "100%",
+  color: theme.palette.text.primary,
+  "& > p": {
+    color: theme.palette.text.secondary,
+  },
 }));
 
 const DescriptionRadio: React.FC<Props> = ({
@@ -25,14 +39,19 @@ const DescriptionRadio: React.FC<Props> = ({
   description,
   onChange,
   id,
-}) => (
-  <StyledFormLabel focused={false}>
-    <Box sx={{ paddingBottom: 2, display: "flex", alignItems: "center" }}>
-      <Radio value={id} onChange={onChange} />
-      <Typography>{title}</Typography>
-    </Box>
-    <Typography variant="body2">{description}</Typography>
-  </StyledFormLabel>
-);
+}) => {
+  const radioGroupState = useRadioGroup();
+  const isSelected = radioGroupState?.value === id;
+
+  return (
+    <StyledFormLabel focused={false} isSelected={isSelected}>
+      <Box sx={{ paddingBottom: 1, display: "flex", alignItems: "center" }}>
+        <Radio value={id} onChange={onChange} />
+        <Typography variant="body1">{title}</Typography>
+      </Box>
+      <Typography variant="body2">{description}</Typography>
+    </StyledFormLabel>
+  );
+};
 
 export default DescriptionRadio;
