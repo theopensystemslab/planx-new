c87177f18b224003b5086db22e08da91818df4d8
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2023-06-14 15:00:47 +0100

    UPDATE: styling for notice component (#1789)
    * UPDATE: styling for notice component
    
    * UPDATE: styling for notice component

--- editor.planx.uk/src/@planx/components/Notice/Public.tsx
+++ editor.planx.uk/src/@planx/components/Notice/Public.tsx
@@ -26,29 +26,45 @@ const useStyles = makeStyles<Theme, StyleProps>((theme) => ({
     display: "flex",
     alignItems: "flex-start",
     justifyContent: "space-between",
+    padding: theme.spacing(2),
     backgroundColor: (props) => props.color,
     color: (props) =>
-      mostReadable(props.color, ["#fff", "#000"])?.toHexString(),
+      mostReadable(props.color, [
+        "#fff",
+        `${theme.palette.text.primary}`,
+      ])?.toHexString(),
     "&:before": {
       content: "' '",
       position: "absolute",
       top: 0,
       left: 0,
-      width: 10,
-      bottom: 0,
-      backgroundColor: (props) =>
-        mostReadable(props.color, ["#fff", "#000"])?.toHexString(),
+      width: "100%",
+      height: "100%",
+      color: (props) =>
+        mostReadable(props.color, [
+          "#fff",
+          `${theme.palette.text.primary}`,
+        ])?.toHexString(),
       opacity: 0.3,
+      border: "2px solid currentColor",
+      pointerEvents: "none",
     },
-    padding: theme.spacing(2),
-    paddingLeft: `calc(${theme.spacing(2)} + 10px)`,
   },
   content: {
     flex: 1,
   },
+  titleWrap: {
+    display: "flex",
+    alignItems: "center",
+  },
+  icon: {
+    width: 34,
+    height: 34,
+  },
   title: {
     fontWeight: FONT_WEIGHT_SEMI_BOLD,
     margin: 0,
+    paddingLeft: theme.spacing(1),
   },
   description: {
     fontWeight: 400,
@@ -57,11 +73,14 @@ const useStyles = makeStyles<Theme, StyleProps>((theme) => ({
       color: (props) =>
         getContrastTextColor(props.color, theme.palette.primary.main),
     },
+    "& p:last-of-type": {
+      marginBottom: 0,
+    },
   },
 }));
 
 const NoticeComponent: React.FC<Props> = (props) => {
-  const styles = useStyles({ color: props.color || "#EFEFEF" });
+  const styles = useStyles({ color: props.color || "#F9F8F8" });
   const handleSubmit = !props.resetButton
     ? () => props.handleSubmit?.()
     : undefined;
@@ -75,15 +94,17 @@ const NoticeComponent: React.FC<Props> = (props) => {
         />
         <div className={styles.container}>
           <div className={styles.content}>
-            <Typography component="h3" variant="h4" className={styles.title}>
-              {props.title}
-            </Typography>
+            <div className={styles.titleWrap}>
+              <ErrorOutline className={styles.icon} />
+              <Typography component="h3" variant="h4" className={styles.title}>
+                {props.title}
+              </Typography>
+            </div>
             <ReactMarkdownOrHtml
               className={styles.description}
               source={props.description}
             />
           </div>
-          <ErrorOutline />
         </div>
         {props.resetButton && (
           <Button
