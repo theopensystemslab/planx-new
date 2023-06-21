4d93fe860b98af4d03c0d2a7f9b7071d30b183eb
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2023-06-21 15:04:02 +0100

    FIX: radio alignment on zoom (#1822)

--- editor.planx.uk/src/theme.ts
+++ editor.planx.uk/src/theme.ts
@@ -315,12 +315,13 @@ const getThemeOptions = (primaryColor: string): ThemeOptions => {
               // Styles for radio icon dot
               content: "''",
               position: "absolute",
-              top: "11px",
-              left: "11px",
+              top: "50%",
+              left: "50%",
               width: 0,
               height: 0,
+              transform: "translate(-50%, -50%)",
               color: TEXT_COLOR_PRIMARY,
-              border: "11px solid currentcolor",
+              border: "10px solid currentcolor",
               borderRadius: "50%",
               background: "currentcolor",
               // Hide by default, show if checked
