8a3bdfcd2d188ed5fc20586aea565821e8c2c8b6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-06-22 10:40:10 +0100

    feat: Styled ResultReason (#1826)

--- editor.planx.uk/src/@planx/components/Result/Public/ResultReason.tsx
+++ editor.planx.uk/src/@planx/components/Result/Public/ResultReason.tsx
@@ -3,9 +3,8 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
-import { Theme } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
 import { visuallyHidden } from "@mui/utils";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
@@ -21,97 +20,101 @@ interface IResultReason {
   showChangeButton?: boolean;
 }
 
-const useClasses = makeStyles((theme: Theme) => ({
-  root: {
-    display: "flex",
-    alignItems: "center",
-    margin: "0",
-    marginRight: theme.spacing(7),
-    position: "relative",
+const PREFIX = "Result";
+
+const classes = {
+  removeTopBorder: `${PREFIX}-removeTopBorder`,
+};
+
+const Root = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  margin: "0",
+  marginRight: theme.spacing(7),
+  position: "relative",
+}));
+
+const ChangeLink = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  right: theme.spacing(-7),
+  top: 0,
+  height: "100%",
+  minWidth: theme.spacing(7),
+  display: "flex",
+  justifyContent: "flex-end",
+  alignItems: "center",
+  flexShrink: "0",
+  "& button": {
+    padding: "1em 0.25em",
+    fontSize: "inherit",
   },
-  accordion: {
-    width: "100%",
-    backgroundColor: "transparent",
-    position: "relative",
-    margin: "0",
-    "&:hover": {
-      background: theme.palette.grey,
-    },
-    "&::after": {
-      content: "''",
-      position: "absolute",
-      bottom: "0",
-      left: "0",
-      width: "100%",
-      height: "1px",
-      backgroundColor: theme.palette.secondary.main,
-      zIndex: "2",
-    },
-    "&.Mui-expanded": {
-      margin: "0",
-    },
+}));
+
+const MoreInfo = styled(Box)(({ theme }) => ({
+  color: theme.palette.text.primary,
+  paddingLeft: theme.spacing(3),
+  paddingBottom: theme.spacing(1),
+}));
+
+const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
+  padding: "0",
+  margin: "0",
+  minHeight: "0",
+  "&.Mui-expanded": {
+    minHeight: "0",
   },
-  changeLink: {
+  "& > div": {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    transition: "none",
+  },
+  "& svg": {
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const SummaryWrap = styled(Box)(({ theme }) => ({
+  paddingLeft: theme.spacing(3),
+  paddingRight: theme.spacing(2),
+  margin: "0",
+}));
+
+const AccordionFlag = styled(Box)(({ theme }) => ({
+  content: "''",
+  position: "absolute",
+  top: "0",
+  left: "0",
+  width: "10px",
+  height: "100%",
+  backgroundColor: theme.palette.background.paper, // TODO match to flag color
+  zIndex: "1",
+}));
+
+const StyledAccordion = styled(Accordion)(({ theme }) => ({
+  width: "100%",
+  backgroundColor: "transparent",
+  position: "relative",
+  margin: "0",
+  "&:hover": {
+    background: theme.palette.grey,
+  },
+  "&::after": {
+    content: "''",
     position: "absolute",
-    right: theme.spacing(-7),
-    top: 0,
-    height: "100%",
-    minWidth: theme.spacing(7),
-    display: "flex",
-    justifyContent: "flex-end",
-    alignItems: "center",
-    flexShrink: "0",
-    "& button": {
-      padding: "1em 0.25em",
-      fontSize: "inherit",
-    },
+    bottom: "0",
+    left: "0",
+    width: "100%",
+    height: "1px",
+    backgroundColor: theme.palette.secondary.main,
+    zIndex: "2",
   },
-  removeTopBorder: {
+  "&.Mui-expanded": {
+    margin: "0",
+  },
+  [`& .${classes.removeTopBorder}`]: {
     "&:before": {
       display: "none",
     },
-  },
-  summary: {
-    padding: "0",
-    margin: "0",
-    minHeight: "0",
-    "&.Mui-expanded": {
-      minHeight: "0",
-    },
-    "& > div": {
-      marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(2),
-      transition: "none",
-    },
-    "& svg": {
-      color: theme.palette.text.secondary,
-    },
-  },
-  summaryWrap: {
-    paddingLeft: theme.spacing(3),
-    paddingRight: theme.spacing(2),
-    margin: "0",
-  },
-  accordionFlag: {
-    content: "''",
-    position: "absolute",
-    top: "0",
-    left: "0",
-    width: "10px",
-    height: "100%",
-    backgroundColor: theme.palette.background.paper, // TODO match to flag color
-    zIndex: "1",
-  },
-  accordionHeader: {
-    position: "relative",
-  },
-  accordionDetails: {
-    padding: "0",
-  },
-  moreInfo: {
-    color: theme.palette.text.primary,
-    paddingLeft: theme.spacing(3),
-    paddingBottom: theme.spacing(1),
   },
 }));
 
@@ -124,8 +127,6 @@ const ResultReason: React.FC<IResultReason> = ({
   const changeAnswer = useStore((state) => state.changeAnswer);
   const [expanded, setExpanded] = React.useState(false);
 
-  const classes = useClasses();
-
   const hasMoreInfo = question.data.info ?? question.data.policyRef;
   const toggleAdditionalInfo = () => setExpanded(!expanded);
 
@@ -136,25 +137,22 @@ const ResultReason: React.FC<IResultReason> = ({
   }`;
 
   return (
-    <Box className={classes.root}>
-      <Accordion
-        className={classes.accordion}
+    <Root>
+      <StyledAccordion
         classes={{ root: classes.removeTopBorder }}
         onChange={() => hasMoreInfo && toggleAdditionalInfo()}
         expanded={expanded}
         elevation={0}
         square
       >
-        <Box className={classes.accordionHeader}>
-          <AccordionSummary
-            className={classes.summary}
+        <Box sx={{ position: "relative" }}>
+          <StyledAccordionSummary
             expandIcon={hasMoreInfo ? <Caret /> : null}
             aria-label={ariaLabel}
             aria-controls={`group-${id}-content`}
             id={`group-${id}-header`}
           >
-            <Box
-              className={classes.summaryWrap}
+            <SummaryWrap
               display="flex"
               justifyContent="space-between"
               alignItems="center"
@@ -168,9 +166,9 @@ const ResultReason: React.FC<IResultReason> = ({
                 {question.data.text} <br />
                 <strong>{response}</strong>
               </Typography>
-            </Box>
-          </AccordionSummary>
-          <Box className={classes.changeLink}>
+            </SummaryWrap>
+          </StyledAccordionSummary>
+          <ChangeLink>
             <Box>
               {showChangeButton && (
                 <Link
@@ -187,11 +185,11 @@ const ResultReason: React.FC<IResultReason> = ({
                 </Link>
               )}
             </Box>
-          </Box>
+          </ChangeLink>
         </Box>
         {hasMoreInfo && (
-          <AccordionDetails className={classes.accordionDetails}>
-            <Box className={classes.moreInfo}>
+          <AccordionDetails sx={{ p: 0 }}>
+            <MoreInfo>
               {question.data.info && (
                 <ReactMarkdownOrHtml
                   source={question.data.info}
@@ -204,12 +202,12 @@ const ResultReason: React.FC<IResultReason> = ({
                   openLinksOnNewTab
                 />
               )}
-            </Box>
+            </MoreInfo>
           </AccordionDetails>
         )}
-      </Accordion>
-      <Box className={classes.accordionFlag}></Box>
-    </Box>
+      </StyledAccordion>
+      <AccordionFlag />
+    </Root>
   );
 };
 
