8c3ef6481803f6a58fd2302f5a1cbc3009e7bf4d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-06-23 16:13:00 +0100

    feat: Styled MoreInfoSection

--- editor.planx.uk/src/@planx/components/shared/Preview/MoreInfoSection.tsx
+++ editor.planx.uk/src/@planx/components/shared/Preview/MoreInfoSection.tsx
@@ -1,30 +1,16 @@
 import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
 import React from "react";
-import { FONT_WEIGHT_SEMI_BOLD } from "theme";
 
-const infoSectionStyles = makeStyles((theme) => ({
-  root: {
-    "& + $root": {
-      paddingTop: theme.spacing(3),
-      borderTop: `1px solid ${theme.palette.text.primary}`,
-    },
+const Root = styled(Box)(({ theme }) => ({
+  "& img, & p": {
+    marginBottom: "1rem",
+    marginTop: 0,
   },
-  title: {
-    paddingBottom: theme.spacing(4),
-  },
-  content: {
-    "& img, & p": {
-      marginBottom: "1rem",
-      marginTop: 0,
-    },
-    "& strong": {
-      fontWeight: FONT_WEIGHT_SEMI_BOLD,
-    },
-    "& em": {
-      fontStyle: "italic",
-    },
+  "& + $MoreInfoSection-root": {
+    paddingTop: theme.spacing(3),
+    borderTop: `1px solid ${theme.palette.text.primary}`,
   },
 }));
 
@@ -34,16 +20,15 @@ interface IMoreInfoSection {
 }
 
 const MoreInfoSection: React.FC<IMoreInfoSection> = ({ title, children }) => {
-  const classes = infoSectionStyles();
   return (
-    <Box className={classes.root} pb={3}>
+    <Root className="MoreInfoSection-root" pb={3}>
       {title && (
-        <Typography className={classes.title} variant="h3" component="h2">
+        <Typography mb={4} variant="h3" component="h2">
           {title}
         </Typography>
       )}
-      <Box className={classes.content}>{children}</Box>
-    </Box>
+      <Box>{children}</Box>
+    </Root>
   );
 };
 
