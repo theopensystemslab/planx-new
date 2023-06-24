3e7e8e7e5905d11589757a2332b7c464f6a639fd
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-06-24 10:02:30 +0100

    feat: Styled Input

--- editor.planx.uk/src/ui/Input.tsx
+++ editor.planx.uk/src/ui/Input.tsx
@@ -1,7 +1,5 @@
 import InputBase, { InputBaseProps } from "@mui/material/InputBase";
-import type { Theme } from "@mui/material/styles";
-import makeStyles from "@mui/styles/makeStyles";
-import classNames from "classnames";
+import { styled } from "@mui/material/styles";
 import React, {
   ChangeEvent,
   forwardRef,
@@ -11,6 +9,14 @@ import React, {
 import { borderedFocusStyle, FONT_WEIGHT_SEMI_BOLD } from "theme";
 
 import ErrorWrapper from "./ErrorWrapper";
+
+const PREFIX = "Input";
+
+const classes = {
+  multiline: `${PREFIX}-multiline`,
+  adornedEnd: `${PREFIX}-adornedEnd`,
+  focused: `${PREFIX}-focused`,
+};
 
 export interface Props extends InputBaseProps {
   format?: "large" | "bold" | "data";
@@ -23,50 +29,56 @@ export interface Props extends InputBaseProps {
   onChange?: (ev: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const useClasses = makeStyles<Theme, Props>((theme) => ({
-  input: {
-    backgroundColor: "#fff",
-    // Maintain 16px minimum input size to prevent zoom on iOS
-    fontSize: "1rem",
-    width: "100%",
-    padding: theme.spacing(0, 1.5),
-    height: 50,
-    "& input": {
-      fontWeight: "inherit",
-    },
+interface StyledInputBase extends InputBaseProps {
+  format?: Props["format"];
+  bordered?: Props["bordered"];
+}
+
+const StyledInputBase = styled(InputBase, {
+  shouldForwardProp: (prop) =>
+    !["format", "bordered"].includes(prop.toString()),
+})<StyledInputBase>(({ theme, format, bordered }) => ({
+  backgroundColor: "#fff",
+  // Maintain 16px minimum input size to prevent zoom on iOS
+  fontSize: "1rem",
+  width: "100%",
+  padding: theme.spacing(0, 1.5),
+  height: 50,
+  "& input": {
+    fontWeight: "inherit",
   },
-  bordered: {
+  ...(bordered && {
     border: `2px solid ${theme.palette.text.primary}`,
-  },
-  inputMultiline: {
+  }),
+  ...(format === "data" && {
+    backgroundColor: "#fafafa",
+  }),
+  ...(format === "bold" && {
+    fontWeight: FONT_WEIGHT_SEMI_BOLD,
+  }),
+  ...(format === "large" && {
+    backgroundColor: "#fff",
+    height: 50,
+    fontSize: 25,
+    width: "100%",
+    fontWeight: FONT_WEIGHT_SEMI_BOLD,
+  }),
+  [`& .${classes.multiline}`]: {
     height: "auto",
     "& textarea": {
       padding: theme.spacing(1.5, 0),
       lineHeight: 1.6,
     },
   },
-  questionInput: {
-    backgroundColor: "#fff",
-    height: 50,
-    fontSize: 25,
-    width: "100%",
-    fontWeight: FONT_WEIGHT_SEMI_BOLD,
-  },
-  bold: {
-    fontWeight: FONT_WEIGHT_SEMI_BOLD,
-  },
-  data: {
-    backgroundColor: "#fafafa",
-  },
-  adornedEnd: {
+  [`& .${classes.adornedEnd}`]: {
     paddingRight: 2,
   },
-  focused: (props) => (props.bordered ? borderedFocusStyle : {}),
+  [`& .${classes.focused}`]: {
+    ...(bordered && borderedFocusStyle),
+  },
 }));
 
 export default forwardRef((props: Props, ref): FCReturn => {
-  const classes = useClasses(props);
-
   const container = useRef<HTMLDivElement | null>(null);
 
   const {
@@ -94,19 +106,10 @@ export default forwardRef((props: Props, ref): FCReturn => {
 
   return (
     <ErrorWrapper error={errorMessage} id={id}>
-      <InputBase
-        className={classNames(
-          classes.input,
-          format === "large" && classes.questionInput,
-          format === "bold" && classes.bold,
-          format === "data" && classes.data,
-          bordered && classes.bordered
-        )}
-        classes={{
-          multiline: classes.inputMultiline,
-          adornedEnd: classes.adornedEnd,
-          focused: classes.focused,
-        }}
+      <StyledInputBase
+        format={format}
+        bordered={bordered}
+        classes={classes}
         inputProps={{
           "aria-label": ariaLabel,
           "aria-describedby": ariaDescribedBy,
