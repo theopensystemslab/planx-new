e7f915d36c5205b922b3e79b7779b250a252b12b
Ian Jones <hello@ianjo.uk>
2023-06-27 13:40:29 +0100

    feat: styled login screen

--- editor.planx.uk/src/pages/Login.tsx
+++ editor.planx.uk/src/pages/Login.tsx
@@ -1,57 +1,51 @@
-import { Theme } from "@mui/material/styles";
-import makeStyles from "@mui/styles/makeStyles";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
 import React from "react";
 
-const useClasses = makeStyles((theme: Theme) => ({
-  wrapper: {
-    width: "100vw",
-    height: "100vh",
-    lineHeight: "1.333",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    background: "#1279ab",
-  },
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    color: "#FFF",
-  },
-  title: {
-    fontWeight: "400",
-    fontSize: "2.5rem",
-    margin: "0.25em 0",
-    "& > span": {
-      color: "rgba(255, 255, 255, 0.49);",
-    },
-  },
-  login: {
-    color: "#FFF",
-    fontSize: "1.125rem",
-  },
+const Wrapper = styled(Box)(() => ({
+  width: "100vw",
+  height: "100vh",
+  lineHeight: "1.333",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  background: "#1279ab",
+}));
+
+const Container = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "#FFF",
+}));
+
+const Beta = styled("span")(() => ({
+  color: "rgba(255, 255, 255, 0.49);",
 }));
 
 const Login: React.FC = () => {
-  const classes = useClasses();
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.container}>
-        <h1 className={classes.title}>
-          Plan✕<span>beta</span>
-        </h1>
-        <a
-          className={classes.login}
+    <Wrapper>
+      <Container>
+        <Typography mb={1} mt={1} variant="h1" fontWeight="400">
+          Plan✕
+          <Beta>beta</Beta>
+        </Typography>
+        <Link
+          variant="body1"
+          color="#FFFFFF"
           href={`${
             process.env.REACT_APP_GOOGLE_OAUTH_OVERRIDE ??
             process.env.REACT_APP_API_URL
           }/auth/google`}
         >
           Login with Google
-        </a>
-      </div>
-    </div>
+        </Link>
+      </Container>
+    </Wrapper>
   );
 };
 
