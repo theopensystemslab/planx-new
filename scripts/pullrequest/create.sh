0d8af136f2244ed6474baea0f7667fcf7ec1135b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-06-28 10:45:28 +0100

    fix: Add back second apt-get (#1866)

--- scripts/pullrequest/create.sh
+++ scripts/pullrequest/create.sh
@@ -16,6 +16,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --batch --yes --de
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # set env for this shell
