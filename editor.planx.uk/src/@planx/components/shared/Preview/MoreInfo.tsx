770cab9b9d108cba7c0af6b65268666b894f7804
Ian Jones <hello@ianjo.uk>
2023-07-03 10:26:28 +0100

    fix: paper drawer width

--- editor.planx.uk/src/@planx/components/shared/Preview/MoreInfo.tsx
+++ editor.planx.uk/src/@planx/components/shared/Preview/MoreInfo.tsx
@@ -31,13 +31,11 @@ const Root = styled(Drawer, {
   },
 
   [`& .${classes.drawerPaper}`]: {
-    width: drawerWidth,
+    width: "100%",
+    maxWidth: drawerWidth,
     backgroundColor: theme.palette.background.default,
     border: 0,
     boxShadow: "-4px 0 0 rgba(0,0,0,0.1)",
-    [theme.breakpoints.only("xs")]: {
-      width: "100%",
-    },
     padding: theme.spacing(1),
   },
 }));
