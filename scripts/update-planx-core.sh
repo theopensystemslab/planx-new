e6f68f411976268750705be38c9b051f59e3da34
Benjamin <109954107+builditben@users.noreply.github.com>
2023-07-05 16:52:43 +0100

    Add script to update planx-core (#1917)

---
+++ scripts/update-planx-core.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -o errexit -o errtrace
+
+# run from project root
+cd $(dirname $0)/..
+rootDir=$(pwd)
+
+directories=(api.planx.uk editor.planx.uk e2e/tests/api-driven e2e/tests/ui-driven)
+
+echo "This command will update planx-core and run \`pnpm install\` in the following directories:"
+for dir in ${directories[@]}; do
+  echo ${dir}
+done
+echo
+
+echo -e "Which planx-core commit ref do you want to upgrade to?"
+read ref
+if [ -z ${ref} ]; then
+  echo "no commit ref supplied";
+  exit 1
+fi
+echo
+
+package="github:theopensystemslab\/planx-core\#${ref}\","
+expression="s/github:theopensystemslab\/planx-core.\+$/${package}/"
+
+for dir in ${directories[@]}; do
+  sed -i ${expression} ${dir}/package.json
+  cd ${dir}
+  echo installing packages in ${dir}
+  pnpm i -s
+  cd ${rootDir}
+done
