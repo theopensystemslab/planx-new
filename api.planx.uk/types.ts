d356c3c1309f0bde0a8913551d7c5cb45fbc09a5
Benjamin <109954107+builditben@users.noreply.github.com>
2023-07-05 11:36:33 +0100

    chore: fix mocked sessionId type (#1912)

--- api.planx.uk/types.ts
+++ api.planx.uk/types.ts
@@ -73,6 +73,7 @@ export interface LowCalSession {
   has_user_saved: boolean;
   flow: {
     slug: string;
+    team?: Team;
   };
   lockedAt?: string;
   paymentRequests?: Pick<PaymentRequest, "id" | "payeeEmail" | "payeeName">[];
