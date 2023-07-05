4e31c8920c6b324b133adfa5700e321d0b828fbf
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2023-07-05 14:43:32 +0100

    UPDATE: styling for task list (#1824)
    * UPDATE: styling for task list
    
    * feat: update task list to styled

--- editor.planx.uk/src/ui/NumberedList.tsx
+++ editor.planx.uk/src/ui/NumberedList.tsx
@@ -1,109 +1,106 @@
 import Box from "@mui/material/Box";
 import ButtonBase from "@mui/material/ButtonBase";
 import Collapse from "@mui/material/Collapse";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import makeStyles from "@mui/styles/makeStyles";
-import classNames from "classnames";
 import React from "react";
 import { FONT_WEIGHT_SEMI_BOLD } from "theme";
 import Caret from "ui/icons/Caret";
 import ReactMarkdownOrHtml from "ui/ReactMarkdownOrHtml";
 
-const useClasses = makeStyles((theme) => ({
-  panel: {
-    backgroundColor: theme.palette.background.default,
-    position: "relative",
-    paddingBottom: theme.spacing(1.25),
-  },
-  stepIndicator: {
+const STEP_DIAMETER = "45px";
+const STEP_SPACER = "60px";
+
+const List = styled("ol")(({ theme }) => ({
+  listStyle: "none",
+  margin: theme.spacing(3, 0, 0, 0),
+  padding: 0,
+}));
+
+const Panel = styled("li")(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  position: "relative",
+  "&::after": {
+    content: "''",
     position: "absolute",
-    width: theme.spacing(7),
-    height: "100%",
-    textAlign: "center",
-    top: 0,
-    left: 0,
-    overflow: "hidden",
-    [theme.breakpoints.up("sm")]: {
-      paddingTop: theme.spacing(1.25),
-      width: theme.spacing(13.5),
-      textAlign: "left",
-    },
-    "&::after": {
-      content: `''`,
-      display: "block",
-      width: 1,
-      backgroundColor: "currentColor",
-      height: "100%",
-      top: 0,
-      position: "absolute",
-      left: "48%",
-      [theme.breakpoints.up("sm")]: {
-        left: "16%",
-      },
-    },
-    "& > i": {
-      width: theme.spacing(4.5),
-      height: theme.spacing(4.5),
-      display: "inline-block",
-      lineHeight: theme.spacing(4.5),
-      border: `1px solid ${theme.palette.text.primary}`,
-      backgroundColor: theme.palette.background.default,
-      borderRadius: "50%",
-      position: "relative",
-      fontStyle: "normal",
-      textAlign: "center",
-      zIndex: 2,
-      [theme.breakpoints.up("sm")]: {
-        lineHeight: theme.spacing(4.5),
-        width: theme.spacing(4.5),
-        height: theme.spacing(4.5),
-      },
+    width: `calc(100% - ${STEP_SPACER})`,
+    height: "1px",
+    background: theme.palette.secondary.main,
+    bottom: "0",
+    left: STEP_SPACER,
+  },
+  // Offset top of vertical border for first indicator in list
+  "&:first-of-type > div::after": {
+    top: STEP_DIAMETER,
+  },
+  // Shorten height of vertical border for last indicator in list
+  "&:last-of-type > div::after": {
+    height: STEP_DIAMETER,
+  },
+}));
+
+const Summary = styled(ButtonBase)(({ theme }) => ({
+  fontWeight: FONT_WEIGHT_SEMI_BOLD,
+  minHeight: STEP_SPACER,
+  padding: theme.spacing(2, 1, 2, 0),
+  paddingLeft: STEP_SPACER,
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+}));
+
+const Content = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  paddingTop: theme.spacing(1.5),
+  paddingBottom: theme.spacing(3),
+  paddingLeft: STEP_SPACER,
+  paddingRight: theme.spacing(2),
+  display: "block",
+  "& p": {
+    marginTop: 0,
+    "&:last-child": {
+      marginBottom: 0,
     },
   },
-  isFirst: {
-    "&::after": {
-      top: theme.spacing(4.5),
-    },
-  },
-  isLast: {
-    "&::after": {
-      height: theme.spacing(4.5),
-    },
-  },
-  summary: {
-    fontWeight: FONT_WEIGHT_SEMI_BOLD,
-    minHeight: theme.spacing(6),
-    padding: theme.spacing(2, 0),
-    paddingLeft: theme.spacing(7),
-    width: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-    [theme.breakpoints.up("sm")]: {},
-  },
-  content: {
-    backgroundColor: theme.palette.background.default,
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-    paddingLeft: theme.spacing(7),
-    paddingRight: theme.spacing(2),
-    lineHeight: 1.6,
+}));
+
+const StepIndicator = styled(Box)(({ theme }) => ({
+  position: "absolute",
+  width: STEP_DIAMETER,
+  height: "100%",
+  textAlign: "center",
+  top: 0,
+  left: 0,
+  overflow: "hidden",
+  paddingTop: theme.spacing(1),
+  paddingBottom: theme.spacing(1),
+  pointerEvents: "none",
+  "&::after": {
+    content: `''`,
     display: "block",
-    [theme.breakpoints.up("sm")]: {
-      paddingLeft: theme.spacing(13.5),
-      paddingRight: theme.spacing(13.5),
-    },
-    "& p": {
-      marginTop: 0,
-      "&:last-child": {
-        marginBottom: 0,
-      },
-    },
+    width: 2,
+    backgroundColor: "currentColor",
+    height: "100%",
+    top: 0,
+    position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%)",
   },
-  numberedList: {
-    listStyle: "none",
-    margin: 0,
-    padding: 0,
-  },
+}));
+
+const StepNumber = styled("i")(({ theme }) => ({
+  width: STEP_DIAMETER,
+  height: STEP_DIAMETER,
+  display: "inline-block",
+  lineHeight: theme.spacing(4.25),
+  border: `2px solid ${theme.palette.text.primary}`,
+  backgroundColor: theme.palette.background.default,
+  borderRadius: "50%",
+  position: "relative",
+  fontStyle: "normal",
+  textAlign: "center",
+  zIndex: 2,
+  fontWeight: FONT_WEIGHT_SEMI_BOLD,
 }));
 
 type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
@@ -112,36 +109,29 @@ interface Item {
   description: string;
 }
 
-function ListItem(
-  props: Item & { index: number; isLast: boolean; heading?: HeadingLevel }
-) {
+function ListItem(props: Item & { index: number; heading?: HeadingLevel }) {
   const [expanded, setExpanded] = React.useState(false);
   const handleChange = () => {
     setExpanded(!expanded);
   };
-  const classes = useClasses();
   return (
-    <li className={classes.panel}>
-      <div
-        className={classNames(
-          classes.stepIndicator,
-          props.index === 0 && classes.isFirst,
-          props.isLast && classes.isLast
-        )}
-      >
-        <i>{props.index + 1}</i>
-      </div>
-      <ButtonBase
-        className={classes.summary}
+    <Panel>
+      <StepIndicator>
+        <StepNumber>{props.index + 1}</StepNumber>
+      </StepIndicator>
+      <Summary
         onClick={handleChange}
         aria-expanded={expanded}
         aria-controls={`group-${props.index}-content`}
+        disableRipple
+        disableTouchRipple
       >
         <Box>
           <Typography
-            variant="h5"
+            variant="h4"
             component={props.heading || "h5"}
             id={`group-${props.index}-heading`}
+            align="left"
           >
             {props.title}
           </Typography>
@@ -149,38 +139,30 @@ function ListItem(
         <Caret
           expanded={expanded}
           titleAccess={expanded ? "Less Information" : "More Information"}
+          color="primary"
         />
-      </ButtonBase>
+      </Summary>
       {props.description && (
         <Collapse in={expanded}>
-          <Box
-            className={classes.content}
+          <Content
             id={`group-${props.index}-content`}
             aria-labelledby={`group-${props.index}-heading`}
           >
             <ReactMarkdownOrHtml source={props.description} />
-          </Box>
+          </Content>
         </Collapse>
       )}
-    </li>
+    </Panel>
   );
 }
 
 function NumberedList(props: { items: Item[]; heading?: HeadingLevel }) {
-  const classes = useClasses();
-
   return (
-    <ol className={classes.numberedList}>
+    <List>
       {props.items?.map((item, i) => (
-        <ListItem
-          {...item}
-          key={i}
-          index={i}
-          isLast={i === props.items.length - 1}
-          heading={props.heading}
-        />
+        <ListItem {...item} key={i} index={i} heading={props.heading} />
       ))}
-    </ol>
+    </List>
   );
 }
 
