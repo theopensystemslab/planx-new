c74b73145eacb21e6b8fa617a8a6c4d6aec2e0d6
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-07-05 13:28:40 +0200

    show a simple warning container for editor-only stories

--- editor.planx.uk/src/@planx/components/Calculate/Calculate.stories.tsx
+++ editor.planx.uk/src/@planx/components/Calculate/Calculate.stories.tsx
@@ -1,7 +1,10 @@
+import ErrorOutline from "@mui/icons-material/ErrorOutline";
+import Typography from "@mui/material/Typography";
 import { Meta } from "@storybook/react";
 import React from "react";
 
 import Wrapper from "../fixtures/Wrapper";
+import { WarningContainer } from "../shared/Preview/WarningContainer";
 import Editor from "./Editor";
 import Public from "./Public/index";
 
@@ -11,5 +14,16 @@ export default {
 } as Meta;
 
 export const WithEditor = () => {
-  return <Wrapper Editor={Editor} Public={Public} />;
+  return (
+    <>
+      <Wrapper Editor={Editor} Public={Public} />
+      <WarningContainer>
+        <ErrorOutline />
+        <Typography variant="body2" ml={2}>
+          This component is only available in the Editor when designing
+          services, it does <strong>not</strong> display in the Public form.
+        </Typography>
+      </WarningContainer>
+    </>
+  );
 };
