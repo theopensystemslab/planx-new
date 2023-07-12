9f937563f1934bc4b6caf02b98d71fee52a16737
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-07-12 09:45:04 +0200

    Notice 'Back to start' should reset email and sessionId

--- editor.planx.uk/src/pages/FlowEditor/lib/__tests__/preview.test.ts
+++ editor.planx.uk/src/pages/FlowEditor/lib/__tests__/preview.test.ts
@@ -579,6 +579,28 @@ describe("changeAnswer", () => {
   });
 });
 
+describe("resetPreview", () => {
+  test("should reset preview state correctly for a local storage session", async () => {
+    setState({
+      sessionId: "123",
+    });
+
+    resetPreview();
+    expect(getState().sessionId).toBe("");
+  });
+
+  test("should reset preview state correctly for a save & return session", async () => {
+    setState({
+      sessionId: "123",
+      saveToEmail: "test@council.gov.uk",
+    });
+
+    resetPreview();
+    expect(getState().sessionId).toBe("");
+    expect(getState().saveToEmail).toBe("");
+  });
+});
+
 const mockBreadcrumbs = {
   mBFPszBssY: {
     auto: false,
