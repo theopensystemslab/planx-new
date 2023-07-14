be76346ef849e15205b781131ec56be9cdae52e1
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-07-14 11:08:57 +0200

    stub out a few more

---
+++ editor.planx.uk/src/pages/Pay/InviteToPay.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta } from "@storybook/react";
+import React from "react";
+
+import InviteToPay from "./InviteToPay";
+
+const meta = {
+  title: "Design System/Pages/InviteToPay",
+  component: InviteToPay,
+} satisfies Meta<typeof InviteToPay>;
+
+export default meta;
+
+// export const Basic = {
+//   render: () => <InviteToPay {...paymentRequestMock} />,
+// };
