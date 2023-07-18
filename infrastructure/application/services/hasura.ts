46136d242e62bd8b30e361f856bcc65021cf7e0c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-07-18 16:21:56 +0100

    wip

--- infrastructure/application/services/hasura.ts
+++ infrastructure/application/services/hasura.ts
@@ -36,18 +36,18 @@ export const createHasuraService = async ({
       path: "/healthz",
     },
   });
-  // Forward HTTP to HTTPS
-  const hasuraListenerHttp = targetHasura.createListener("hasura-http", {
-    protocol: "HTTP",
-    defaultAction: {
-      type: "redirect",
-      redirect: {
-        protocol: "HTTPS",
-        port: "443",
-        statusCode: "HTTP_301",
-      },
-    },
-  });
+  // // Forward HTTP to HTTPS
+  // const hasuraListenerHttp = targetHasura.createListener("hasura-http", {
+  //   protocol: "HTTP",
+  //   defaultAction: {
+  //     type: "redirect",
+  //     redirect: {
+  //       protocol: "HTTPS",
+  //       port: "443",
+  //       statusCode: "HTTP_301",
+  //     },
+  //   },
+  // });
   
   const hasuraListenerHttps = targetHasura.createListener("hasura-https", {
     protocol: "HTTPS",
@@ -129,6 +129,6 @@ export const createHasuraService = async ({
     zoneId: config.require("cloudflare-zone-id"),
     value: hasuraListenerHttps.endpoint.hostname,
     ttl: 1,
-    proxied: false,
+    proxied: true,
   });
 }
