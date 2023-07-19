b1e4076b02c0fab723eec75265f645581e1f0067
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-07-19 21:49:52 +0200

    Content & Notice components

--- editor.planx.uk/src/@planx/components/Content/Content.stories.tsx
+++ editor.planx.uk/src/@planx/components/Content/Content.stories.tsx
@@ -1,26 +1,27 @@
-import { Meta, StoryFn, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 
 import Wrapper from "../fixtures/Wrapper";
 import Editor from "./Editor";
-import type { Props as PublicProps } from "./Public";
 import Public from "./Public";
 
-const metadata: Meta = {
+const meta = {
   title: "PlanX Components/Content",
   component: Public,
   argTypes: {
     handleSubmit: { action: true },
   },
-};
+} satisfies Meta<typeof Public>;
 
-export const Frontend: StoryObj<PublicProps> = {
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic = {
   args: {
-    content: `**Good Morning** _Bom dia_`,
-    color: "white",
+    content: `<h1>Ready to submit?</h1><p>You will not be able to make any further changes.You will not be able to make any further changes.</p>`,
+    color: "#F9F8F8",
   },
-};
+} satisfies Story;
 
 export const WithEditor = () => <Wrapper Editor={Editor} Public={Public} />;
-
-export default metadata;
