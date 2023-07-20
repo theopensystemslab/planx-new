b82f67000d7685e7bb0bacf9085edd8fbcc2e83f
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-07-20 17:26:36 +0200

    setup

---
+++ editor.planx.uk/src/@planx/components/Pay/Public/Pay.stories.tsx
@@ -0,0 +1,45 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import Confirm from "./Confirm";
+
+const meta = {
+  title: "PlanX Components/Pay",
+  component: Confirm,
+} satisfies Meta<typeof Confirm>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const Basic = {
+  args: {
+    title: "Pay for your application",
+    bannerTitle: "The fee is",
+    description: "The fee covers the cost of processing your application",
+    fee: 103,
+    instructionsTitle: "How to pay",
+    instructionsDescription: "Pay via GOV.UK Pay",
+    buttonTitle: "Pay",
+    onConfirm: () => {},
+    error: undefined,
+    showInviteToPay: false,
+  },
+} satisfies Story;
+
+// Needs react-navi mock
+export const WithInviteToPay = {
+  args: {
+    title: "Pay for your application",
+    bannerTitle: "The fee is",
+    description: "The fee covers the cost of processing your application",
+    fee: 103,
+    instructionsTitle: "How to pay",
+    instructionsDescription: "Pay via GOV.UK Pay",
+    buttonTitle: "Pay",
+    onConfirm: () => {},
+    error: undefined,
+    showInviteToPay: true,
+    nomineeTitle: "Details of your nominee",
+    nomineeDescription: "Invite someone else to pay via GOV.UK Pay",
+  },
+} satisfies Story;
