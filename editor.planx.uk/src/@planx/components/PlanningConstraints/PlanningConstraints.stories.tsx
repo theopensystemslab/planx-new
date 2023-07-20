df6ff6cc00ed3e795b94b9e1e9972564e9d81052
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-07-20 20:59:18 +0200

    updated mocks for positive & negative constraint checks

--- editor.planx.uk/src/@planx/components/PlanningConstraints/PlanningConstraints.stories.tsx
+++ editor.planx.uk/src/@planx/components/PlanningConstraints/PlanningConstraints.stories.tsx
@@ -3,30 +3,70 @@ import { Meta } from "@storybook/react";
 import React from "react";
 
 import Editor from "./Editor";
+import classifiedRoadsNegativeResponseMock from "./mocks/classifiedRoadsNegativeResponseMock";
+import classifiedRoadsResponseMock from "./mocks/classifiedRoadsResponseMock";
+import digitalLandNegativeResponseMock from "./mocks/digitalLandNegativeResponseMock";
 import digitalLandResponseMock from "./mocks/digitalLandResponseMock";
 import Public, {
   PlanningConstraintsContent,
   PlanningConstraintsContentProps,
 } from "./Public";
 
+/**
+ * PlanningConstraints fetches data about constraints from DLUHC's planning.data.gov.uk & data about classified roads from the Ordnance Survey Features API.
+ *
+ * We do a spatial query to find constraints that overlap with the red-line site boundary polygon drawn using DrawBoundary.
+ * If an applicant uploads a PDF location plan or skips drawing, then we fallback to checking for constraints that intersect with the address point selected using FindProperty.
+ *
+ * Example sites:
+ * - "With Intersections" is based on a site boundary around LAMBETH PALACE, LAMBETH PALACE ROAD, LONDON, SE1 7JU in July 2023
+ * - "Without Intersections" is based on a site boundary around 2, WALLER ROAD, BEACONSFIELD, BUCKINGHAMSHIRE, HP9 2HE in July 2023
+ */
 export default {
   title: "PlanX Components/PlanningConstraints",
   component: Public,
 } satisfies Meta<typeof Public>;
 
-const props: PlanningConstraintsContentProps = {
+const propsWithIntersections: PlanningConstraintsContentProps = {
   title: "Planning constraints",
   description:
     "Planning constraints might limit how you can develop or use the property",
   fn: "property.constraints.planning",
-  constraints: digitalLandResponseMock["constraints"],
-  metadata: digitalLandResponseMock["metadata"],
-  handleSubmit: () => console.log(""),
-  refreshConstraints: () => console.log(""),
+  constraints: {
+    ...digitalLandResponseMock["constraints"],
+    ...classifiedRoadsResponseMock["constraints"],
+  },
+  metadata: {
+    ...digitalLandResponseMock["metadata"],
+    ...classifiedRoadsResponseMock["metadata"],
+  },
+  handleSubmit: () => {},
+  refreshConstraints: () => {},
 };
 
-export const Basic = {
-  render: () => <PlanningConstraintsContent {...props} />,
+const propsWithoutIntersections: PlanningConstraintsContentProps = {
+  title: "Planning constraints",
+  description:
+    "Planning constraints might limit how you can develop or use the property",
+  fn: "property.constraints.planning",
+  constraints: {
+    ...digitalLandNegativeResponseMock["constraints"],
+    ...classifiedRoadsNegativeResponseMock["constraints"],
+  },
+  metadata: {
+    ...digitalLandNegativeResponseMock["metadata"],
+    ...classifiedRoadsNegativeResponseMock["metadata"],
+  },
+  handleSubmit: () => {},
+  refreshConstraints: () => {},
+};
+
+export const WithIntersections = {
+  render: () => <PlanningConstraintsContent {...propsWithIntersections} />,
+};
+
+export const WithoutIntersections = {
+  render: () => <PlanningConstraintsContent {...propsWithoutIntersections} />,
 };
 
 export const WithEditor = () => <Wrapper Editor={Editor} Public={Public} />;
