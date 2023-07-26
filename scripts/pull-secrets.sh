dee57923e0432d977d79d69ea3b727c4ae007755
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-07-26 11:40:05 +0100

    chore: Update push and pull secrets scripts

--- scripts/pull-secrets.sh
+++ scripts/pull-secrets.sh
@@ -2,7 +2,18 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-aws s3 cp s3://pizza-secrets/root.env ./../.env
-aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env.test
-aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test
+if [ -z "${CI}" ]
+then
+    echo "Fetching secrets for CI"
+    aws s3 cp s3://pizza-secrets/root.env ./../.env
+    aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env.test
+    aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test
+else 
+    echo "Fetching secrets for developer"
+    aws sso login --profile $1
+    aws s3 cp s3://pizza-secrets/root.env ./../.env --profile $1
+    aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env.test --profile $1
+    aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test --profile $1
+fi
+
 echo "Complete: Secrets from S3 copied to local machine"
--- scripts/push-secrets.sh
+++ scripts/push-secrets.sh
@@ -2,7 +2,8 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-aws s3 cp ./../.env s3://pizza-secrets/root.env
-aws s3 cp ./../api.planx.uk/.env.test s3://pizza-secrets/api.env.test
-aws s3 cp ./../hasura.planx.uk/.env.test s3://pizza-secrets/hasura.env.test
+aws sso login --profile $1
+aws s3 cp ./../.env s3://pizza-secrets/root.env --profile $1
+aws s3 cp ./../api.planx.uk/.env.test s3://pizza-secrets/api.env.test --profile $1
+aws s3 cp ./../hasura.planx.uk/.env.test s3://pizza-secrets/hasura.env.test --profile $1
 echo "Complete: Secrets from local machine pushed to S3"
