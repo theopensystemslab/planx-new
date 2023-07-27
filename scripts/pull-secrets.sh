2e23a828556f9de4c64013118b43e9b82e15bef3
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-07-27 10:39:40 +0100

    fix: PR comments

--- scripts/pull-secrets.sh
+++ scripts/pull-secrets.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-if [ -z "${CI}" ]
+if [ -z "${CI}"=true ]
 then
     echo "Fetching secrets for CI"
     aws s3 cp s3://pizza-secrets/root.env ./../.env
@@ -10,10 +10,11 @@ then
     aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test
 else 
     echo "Fetching secrets for developer"
-    aws sso login --profile $1
-    aws s3 cp s3://pizza-secrets/root.env ./../.env --profile $1
-    aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env.test --profile $1
-    aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test --profile $1
+    profile=${1:-default}
+    aws sso login --profile $profile
+    aws s3 cp s3://pizza-secrets/root.env ./../.env --profile $profile
+    aws s3 cp s3://pizza-secrets/api.env.test ./../api.planx.uk/.env.test --profile $profile
+    aws s3 cp s3://pizza-secrets/hasura.env.test ./../hasura.planx.uk/.env.test --profile $profile
 fi
 
 echo "Complete: Secrets from S3 copied to local machine"
