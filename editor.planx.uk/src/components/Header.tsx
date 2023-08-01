9f564327fbfcb0dd911a36aeb498be2529a5a657
Ian Jones <hello@ianjo.uk>
2023-08-01 17:01:31 +0100

    feat: max-width page containers

--- editor.planx.uk/src/components/Header.tsx
+++ editor.planx.uk/src/components/Header.tsx
@@ -266,7 +266,7 @@ const NavBar: React.FC = () => {
       {isVisible && (
         <StyledNavBar data-testid="navigation-bar">
           <Container
-            maxWidth={false}
+            maxWidth="contentWrap"
             sx={{
               display: "flex",
               flexDirection: "column",
@@ -323,7 +323,7 @@ const PublicToolbar: React.FC<{
     <>
       <SkipLink href="#main-content">Skip to main content</SkipLink>
       <StyledToolbar disableGutters>
-        <Container maxWidth={false}>
+        <Container maxWidth="contentWrap">
           <InnerContainer>
             <LeftBox>
               {teamTheme?.logo ? (
@@ -349,7 +349,7 @@ const PublicToolbar: React.FC<{
         </Container>
       </StyledToolbar>
       {!showCentredServiceTitle && (
-        <Container maxWidth={false}>
+        <Container maxWidth="contentWrap">
           <ServiceTitle />
         </Container>
       )}
