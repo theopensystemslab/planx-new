1fc00cb0c80ea0c655155ccdbf2719456bbf0513
Ian Jones <hello@ianjo.uk>
2023-08-02 14:44:04 +0100

    feat: tidy up resume pages

--- editor.planx.uk/src/pages/Preview/ReconciliationPage.tsx
+++ editor.planx.uk/src/pages/Preview/ReconciliationPage.tsx
@@ -1,7 +1,7 @@
 import Warning from "@mui/icons-material/WarningOutlined";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { lighten, useTheme } from "@mui/material/styles";
+import { useTheme } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { SectionsOverviewList } from "@planx/components/Section/Public";
 import Card from "@planx/components/shared/Preview/Card";
@@ -67,7 +67,7 @@ const ReconciliationPage: React.FC<Props> = ({
         <Banner
           heading={bannerHeading}
           color={{
-            background: lighten(theme.palette.info.main, 0.9),
+            background: theme.palette.info.light,
             text: theme.palette.text.primary,
           }}
         />
