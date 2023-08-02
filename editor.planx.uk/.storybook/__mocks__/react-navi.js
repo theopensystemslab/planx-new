b129ce3ec77c0f4024c51d4903ac92e058cc72d7
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-08-02 10:39:16 +0200

    chore: update Storybook [Pay] (#1996)
    Co-authored-by: Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>

--- editor.planx.uk/.storybook/__mocks__/react-navi.js
+++ editor.planx.uk/.storybook/__mocks__/react-navi.js
@@ -3,10 +3,15 @@
 const reactNavi = require('react-navi');
 
 const originalUseCurrentRoute = reactNavi.useCurrentRoute;
+const originalUseNavigation = reactNavi.useNavigation;
 
 function reactNaviDecorator(story, { parameters }) {
-  delete reactNavi['useCurrentRoute'];
+  delete reactNavi["useCurrentRoute"];
   reactNavi.useCurrentRoute = parameters?.reactNavi?.useCurrentRoute ?? originalUseCurrentRoute;
+
+  delete reactNavi["useNavigation"];
+  reactNavi.useNavigation = parameters?.reactNavi?.useNavigation ?? originalUseNavigation;
+
   return story();  
 }
 
---
+++ editor.planx.uk/src/@planx/components/Pay/Public/Pay.stories.tsx
@@ -0,0 +1,58 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import Confirm from "./Confirm";
+
+const meta = {
+  title: "PlanX Components/Pay",
+  component: Confirm,
+  parameters: {
+    reactNavi: {
+      useCurrentRoute: () => ({
+        data: {
+          mountpath: "test mountpath",
+        },
+      }),
+      useNavigation: () => ({
+        navigate: () => console.log("called navigate()"),
+      }),
+    },
+  },
+} satisfies Meta<typeof Confirm>;
+
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const Basic = {
+  args: {
+    title: "Pay for your application",
+    bannerTitle: "The fee is",
+    description: "The fee covers the cost of processing your application",
+    fee: 103,
+    instructionsTitle: "How to pay",
+    instructionsDescription: "Pay via GOV.UK Pay",
+    buttonTitle: "Pay",
+    onConfirm: () => {},
+    error: undefined,
+    showInviteToPay: false,
+  },
+} satisfies Story;
+
+export const WithInviteToPay = {
+  args: {
+    title: "Pay for your application",
+    bannerTitle: "The fee is",
+    description: "The fee covers the cost of processing your application",
+    fee: 103,
+    instructionsTitle: "How to pay",
+    instructionsDescription: "Pay via GOV.UK Pay",
+    buttonTitle: "Pay",
+    onConfirm: () => {},
+    error: undefined,
+    showInviteToPay: true,
+    nomineeTitle: "Details of your nominee",
+    nomineeDescription: "Invite someone else to pay via GOV.UK Pay",
+    yourDetailsTitle: "Your details",
+    yourDetailsLabel: "Your name or organisation name",
+  },
+} satisfies Story;
