627d20062e98de5ad86221631a699d1c2b75d501
Jessica McInchak <jessica.mcinchak@gmail.com>
2023-08-03 15:19:06 +0200

    chore: remove ben's seed user [skip pizza] (#2060)

--- hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
+++ hasura.planx.uk/seeds/1595528762802_teams_and_users.sql
@@ -1,7 +1,6 @@
 INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (2, 'Alastair', 'Parvin', 'alastair@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (20, 'Jessica', 'McInchak', 'jessica@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (33, 'Dafydd', 'Pearson', 'dafydd@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
-INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (36, 'Benjamin', 'Williams', 'benjamin@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
 INSERT INTO public.users (id, first_name, last_name, email, is_admin) VALUES (65, 'Ian', 'Jones', 'ian@opensystemslab.io', true) ON CONFLICT (id) DO NOTHING;
 SELECT setval('users_id_seq', max(id)) FROM users;
 SELECT setval('teams_id_seq', max(id)) FROM teams;
