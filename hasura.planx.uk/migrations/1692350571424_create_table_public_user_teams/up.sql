bdcfc0fd837ac71c8798582495d057eb0b8fbb86
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-18 13:36:55 +0100

    feat: New user_teams table

---
+++ hasura.planx.uk/migrations/1692350571424_create_table_public_user_teams/up.sql
@@ -0,0 +1,2 @@
+CREATE TABLE "public"."user_teams" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "user_id" integer NOT NULL, "team_id" integer NOT NULL, PRIMARY KEY ("id") , FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON UPDATE cascade ON DELETE cascade, FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON UPDATE cascade ON DELETE cascade);COMMENT ON TABLE "public"."user_teams" IS E'Tracks relationship between users and teams';
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
