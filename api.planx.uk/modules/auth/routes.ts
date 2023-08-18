7c3bf3560c84938a970d8ab44f4c41ef051450d9
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-18 19:59:58 +0100

    chore: Refactor auth module into proposed structure

---
+++ api.planx.uk/modules/auth/routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import * as Middleware from "./middleware";
+import * as Controller from "./controller";
+
+const router = Router();
+
+router.get("/logout", Controller.logout);
+router.get("/auth/login/failed", Controller.failedLogin);
+
+// The first step in Google authentication will involve redirecting the user to google.com
+router.get("/auth/google", Middleware.useGoogleAuth);
+
+//After authorization, Google will redirect the user back to this route
+router.get("/auth/google/callback", Middleware.useGoogleCallbackAuth, Controller.handleSuccess);
+
+export default router;
