1e9443f53ae070e6df2b58d6ac8ab28c170e526b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-21 19:49:08 +0100

    fix: Syntax issue in push-secrets.sh [skip pizza]

--- scripts/push-secrets.sh
+++ scripts/push-secrets.sh
@@ -3,8 +3,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 profile=${1:-default}
 
-aws sso login --profile profile
-aws s3 cp ./../.env s3://pizza-secrets/root.env --profile profile
-aws s3 cp ./../api.planx.uk/.env.test s3://pizza-secrets/api.env.test --profile profile
-aws s3 cp ./../hasura.planx.uk/.env.test s3://pizza-secrets/hasura.env.test --profile profile
+aws sso login --profile $profile
+aws s3 cp ./../.env s3://pizza-secrets/root.env --profile $profile
+aws s3 cp ./../api.planx.uk/.env.test s3://pizza-secrets/api.env.test --profile $profile
+aws s3 cp ./../hasura.planx.uk/.env.test s3://pizza-secrets/hasura.env.test --profile $profile
 echo "Complete: Secrets from local machine pushed to S3"
