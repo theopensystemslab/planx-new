4bd07953816700cf106a68ba8f8106e0733b601f
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-21 13:29:29 +0100

    test: Create gqlWithRole() client

--- hasura.planx.uk/tests/utils.js
+++ hasura.planx.uk/tests/utils.js
@@ -1,4 +1,5 @@
 const fetch = require("isomorphic-fetch");
+const jsonwebtoken = require("jsonwebtoken");
 
 async function gqlAdmin(query, variables = {}) {
   const res = await fetch(`http://${process.env.HASURA_HOST}:${process.env.HASURA_PORT}/v1/graphql`, {
@@ -25,13 +26,64 @@ async function gqlPublic(query, variables = {}, headers = {}) {
 }
 
 /**
+ * Get a role-based connection to Hasura
+ * @param {string} role 
+ * @param {number} userId 
+ * @returns A GQL client which authenticates to Hasura with the given role and userId
+ */
+function gqlWithRole(role, userId) {
+  const jwt = buildJWTForRole(role, userId)
+
+  const gql = async (query, variables = {}, headers = {}) => {
+    const res = await fetch(`http://${process.env.HASURA_HOST}:${process.env.HASURA_PORT}/v1/graphql`, {
+      method: "POST",
+      headers: {
+        ...headers,
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify({ query: query, variables }),
+    });
+    return await res.json();
+  }
+
+  return gql;
+}
+
+/**
+ * @param {string} role 
+ * @param {number} userId 
+ * @returns {string}
+ */
+function buildJWTForRole(role, userId = 1) {
+  const hasura = {
+    "x-hasura-allowed-roles": [role],
+    "x-hasura-default-role": role,
+    "x-hasura-role": role,
+    "x-hasura-user-id": userId.toString(),
+  };
+
+  const data = {
+    sub: userId.toString(),
+    "https://hasura.io/jwt/claims": hasura,
+  };
+
+  const jwt = jsonwebtoken.sign(data, process.env.JWT_SECRET);
+  return jwt;
+}
+
+/**
  * Helper method for introspecting as a specific user
- * @param {string} role ("public" or "admin")
+ * @param {string} role ("public", "admin" or "platformManager")
+ * @param {number} userId
  * @example
  * const { types, queries, mutations } = await introspectAs("public")
  */
-const introspectAs = async (role) => {
-  const gql = role === "admin" ? gqlAdmin : gqlPublic;
+const introspectAs = async (role, userId = undefined) => {
+  const gql = {
+    admin: gqlAdmin,
+    public: gqlPublic,
+    platformManager: gqlWithRole("platformManager", userId),
+  }[role]
   const INTROSPECTION_QUERY = `
     query IntrospectionQuery {
       __schema {
