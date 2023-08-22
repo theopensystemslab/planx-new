572718a7623928c627905d7bcd1323473888551a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-22 08:58:50 +0100

    fix: Roles should be array relationship, not object

---
+++ hasura.planx.uk/migrations/1692690813431_alter_table_public_user_roles_alter_column_user_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."user_roles" add constraint "user_roles_user_id_key" unique ("user_id");
---
+++ hasura.planx.uk/migrations/1692690813431_alter_table_public_user_roles_alter_column_user_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."user_roles" drop constraint "user_roles_user_id_key";
