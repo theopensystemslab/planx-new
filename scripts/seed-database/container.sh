7eb67bf1b316ce4d2df972455c1bcfbdc30c147d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-08-30 08:23:21 +0100

    feat(ci): Throw error on sync script failure, wrap in single transaction (#2177)

--- scripts/seed-database/container.sh
+++ scripts/seed-database/container.sh
@@ -10,9 +10,6 @@ REMOTE_PG=${2}
 # RESET set to "reset_all" will truncate all synced tables
 RESET=${3}
 
-# INCLUDE_PUBLISHED_FLOWS set to "include_published_flows" will sync published flows
-INCLUDE_PUBLISHED_FLOWS=${4}
-
 echo downloading data from production
 
 # set-up tmp dir for remote data
@@ -26,27 +23,27 @@ for table in "${tables[@]}"; do
   read_cmd="\\copy (SELECT * FROM ${table}) TO ${target} WITH (FORMAT csv, DELIMITER ';');"
   psql --quiet ${REMOTE_PG} --command="${read_cmd}"
   echo ${table} downloaded
+
   if [[ ${RESET} == "reset_all" ]]; then
     reset_cmd="TRUNCATE TABLE ${table} CASCADE;"
-    psql ${LOCAL_PG} --command="${reset_cmd}"
+
+    # Start building single sync.sql file which will be executed in a single transaction
+    cat $reset_cmd > sync.sql
   fi
 done
 
+psql --quiet ${REMOTE_PG} --command="\\copy (SELECT DISTINCT ON (flow_id) id, data, flow_id, summary, publisher_id, created_at FROM published_flows ORDER BY flow_id, created_at DESC) TO '/tmp/published_flows.csv' (FORMAT csv, DELIMITER ';');"
+echo published_flows downloaded
+
 if [[ ${RESET} == "reset_flows" ]]; then
-  psql ${LOCAL_PG} --command="TRUNCATE TABLE flows CASCADE;"
+  cat write/truncate_flows.sql > sync.sql
 fi
 
-if [[ ${INCLUDE_PUBLISHED_FLOWS} == "include_published_flows" ]]; then
-  psql --quiet ${REMOTE_PG} --command="\\copy (SELECT DISTINCT ON (flow_id) id, data, flow_id, summary, publisher_id, created_at FROM published_flows ORDER BY flow_id, created_at DESC) TO '/tmp/published_flows.csv' (FORMAT csv, DELIMITER ';');"
-  echo published_flows downloaded
-fi
+# Add main operations
+cat write/main.sql > sync.sql
 
-echo beginning write transaction
-psql --quiet ${LOCAL_PG} -f write/main.sql
-
-if [[ ${INCLUDE_PUBLISHED_FLOWS} == "include_published_flows" ]]; then
-  psql --quiet ${LOCAL_PG} -f write/published_flows.sql
-fi
+echo "Beginning write transaction..."
+psql --quiet ${LOCAL_PG} -f sync.sql --single-transaction -v ON_ERROR_STOP=on
 
 # clean-up tmp dir
 rm -rf /tmp
