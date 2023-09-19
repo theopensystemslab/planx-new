401a14effa2c50a64e59be022d431e9193463f0b
Ian Jones <hello@ianjo.uk>
2023-09-19 11:24:51 +0100

    feat: notice component padding for help tab

--- editor.planx.uk/src/@planx/components/Notice/Public.tsx
+++ editor.planx.uk/src/@planx/components/Notice/Public.tsx
@@ -83,7 +83,10 @@ const NoticeComponent: React.FC<Props> = (props) => {
           policyRef={props.policyRef}
           howMeasured={props.howMeasured}
         />
-        <Container customColor={props.color}>
+        <Container
+          customColor={props.color}
+          mt={{ xs: 9, sm: 10, md: 12, contentWrap: 6 }}
+        >
           <Content>
             <TitleWrap>
               <ErrorOutline sx={{ width: 34, height: 34 }} />
