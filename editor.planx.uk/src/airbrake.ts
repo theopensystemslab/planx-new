9c77d09710c79aaab1064f788446419b9dae68b2
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-09-20 10:32:26 +0100

    fix: Incomplete URL substring sanitation (#2227)

--- editor.planx.uk/src/airbrake.ts
+++ editor.planx.uk/src/airbrake.ts
@@ -3,6 +3,29 @@ import { isLiveEnv } from "utils";
 
 export const logger = getErrorLogger();
 
+/**
+ * Checking a partial host can be unsafe, e.g.
+ * window.location.host.endsWith("gov.uk")
+ */
+function getEnvForAllowedHosts(host: string) {
+  switch (host) {
+    case "planningservices.newcastle.gov.uk":
+    case "planningservices.medway.gov.uk":
+    case "planningservices.doncaster.gov.uk":
+    case "planningservices.lambeth.gov.uk":
+    case "planningservices.southwark.gov.uk":
+    case "planningservices.buckinghamshire.gov.uk":
+    case "editor.planx.uk":
+      return "production"
+
+    case "editor.planx.dev":
+      return "staging"
+
+    default:
+      "pullrequest";
+  }
+}
+
 function log(...args: any[]) {
   return process.env.SUPPRESS_LOGS
     ? () => {
@@ -31,13 +54,7 @@ function getErrorLogger(): ErrorLogger {
   return new Notifier({
     projectId: Number(process.env.REACT_APP_AIRBRAKE_PROJECT_ID!),
     projectKey: process.env.REACT_APP_AIRBRAKE_PROJECT_KEY!,
-    environment:
-      window.location.host.endsWith("planx.uk") ||
-      window.location.host.endsWith("gov.uk")
-        ? "production"
-        : window.location.host.endsWith("planx.dev")
-        ? "staging"
-        : "pullrequest",
+    environment: getEnvForAllowedHosts(window.location.host)
   });
 }
 
