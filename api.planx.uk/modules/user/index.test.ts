c6a7a4e34aa19b024955fb8ccf4183f37065d7d6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-09-29 17:09:54 +0100

    test: Add API tests for new endpoint

--- api.planx.uk/modules/user/index.test.ts
+++ api.planx.uk/modules/user/index.test.ts
@@ -2,20 +2,24 @@ import supertest from "supertest";
 import app from "../../server";
 import { authHeader } from "../../tests/mockJWT";
 
-const mockCreateUser = jest.fn();
-
 const mockUser = {
   firstName: "Bilbo",
   lastName: "Baggins",
-  email: "bilbo@bagend.sh",
+  email: "bilbo.baggins@example.com",
   isPlatformAdmin: false,
 };
+
+const mockCreateUser = jest.fn();
+const mockDeleteUser = jest.fn();
+const mockGetByEmail = jest.fn().mockResolvedValue(mockUser);
 
 jest.mock("@opensystemslab/planx-core", () => {
   return {
     CoreDomainClient: jest.fn().mockImplementation(() => ({
       user: {
         create: () => mockCreateUser(),
+        delete: () => mockDeleteUser(),
+        getByEmail: () => mockGetByEmail(),
       },
     })),
   };
@@ -63,6 +67,63 @@ describe("Create user endpoint", () => {
         expect(mockCreateUser).toHaveBeenCalled();
         expect(res.body).toHaveProperty("message");
         expect(res.body.message).toMatch(/Successfully created user/);
+      });
+  });
+});
+
+describe("Delete user endpoint", () => {
+  it("requires authentication", async () => {
+    await supertest(app).delete("/user/bilbo.baggins@example.com").expect(401);
+  });
+
+  it("requires the 'platformAdmin' role", async () => {
+    await supertest(app)
+      .delete("/user/bilbo.baggins@example.com")
+      .set(authHeader({ role: "teamEditor" }))
+      .expect(403);
+  });
+
+  it("handles an invalid email", async () => {
+    mockGetByEmail.mockResolvedValueOnce(null);
+
+    await supertest(app)
+      .delete("/user/bilbo.baggins@example.com")
+      .set(auth)
+      .expect(500)
+      .then((res) => {
+        expect(mockGetByEmail).toHaveBeenCalled();
+        expect(res.body).toHaveProperty("error");
+        expect(res.body.error).toMatch(/Failed to delete user/);
+      });
+  });
+
+  it("handles a failure to delete the user", async () => {
+    mockDeleteUser.mockResolvedValueOnce(false);
+
+    await supertest(app)
+      .delete("/user/bilbo.baggins@example.com")
+      .set(auth)
+      .expect(500)
+      .then((res) => {
+        expect(mockGetByEmail).toHaveBeenCalled();
+        expect(mockDeleteUser).toHaveBeenCalled();
+        expect(res.body).toHaveProperty("error");
+        expect(res.body.error).toMatch(/Failed to delete user/);
+      });
+  });
+
+  it("can successfully delete a user", async () => {
+    mockDeleteUser.mockResolvedValue(true);
+
+    await supertest(app)
+      .delete("/user/bilbo.baggins@example.com")
+      .set(auth)
+      .expect(200)
+      .then((res) => {
+        expect(mockGetByEmail).toHaveBeenCalled();
+        expect(mockDeleteUser).toHaveBeenCalled();
+        expect(res.body).toHaveProperty("message");
+        expect(res.body.message).toMatch(/Successfully deleted user/);
       });
   });
 });
