76783eca41d08556c3e8148da7fe1b27eebca82f
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-10-09 13:43:27 +0100

    chore: Drop redundant endpoints

--- api.planx.uk/server.test.js
+++ api.planx.uk/server.test.js
@@ -1,23 +1,6 @@
 import supertest from "supertest";
 
-import { queryMock } from "./tests/graphqlQueryMock";
 import app from "./server";
-
-it("mocks hasura", async () => {
-  queryMock.mockQuery({
-    name: "GetTeams",
-    data: {
-      teams: [{ id: 1 }],
-    },
-  });
-
-  await supertest(app)
-    .get("/hasura")
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toEqual({ teams: [{ id: 1 }] });
-    });
-});
 
 describe("authentication", () => {
   test("Failed login endpoint", async () => {
