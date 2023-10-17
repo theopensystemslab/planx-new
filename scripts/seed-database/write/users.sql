102393e989978c9cf691c2adbcad1faea08ecd14
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-10-17 10:08:26 +0100

    fix: Disable grant_new_user_template_team_access trigger in DB sync (#2315)

--- scripts/seed-database/write/users.sql
+++ scripts/seed-database/write/users.sql
@@ -11,6 +11,11 @@ CREATE TEMPORARY TABLE sync_users (
 
 \copy sync_users FROM '/tmp/users.csv'  WITH (FORMAT csv, DELIMITER ';');
 
+-- Do not automatically generate team_member records for the templates team
+-- We manually truncate and replace the team_members table in another step
+ALTER TABLE
+  users DISABLE TRIGGER grant_new_user_template_team_access;
+
 INSERT INTO users (
   id,
   first_name,
@@ -26,5 +31,8 @@ SELECT
   is_platform_admin
 FROM sync_users
 ON CONFLICT (id) DO NOTHING;
+
+ALTER TABLE
+  users ENABLE TRIGGER grant_new_user_template_team_access;
 
 SELECT setval('users_id_seq', max(id)) FROM users;
