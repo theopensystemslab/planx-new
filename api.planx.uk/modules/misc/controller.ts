6fe86f7bb2d47e9a145a4cb538f91498bb7d89d6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-11-14 16:59:34 +0000

    feat: Document download-application endpoint (#2422)

--- api.planx.uk/modules/misc/controller.ts
+++ api.planx.uk/modules/misc/controller.ts
@@ -2,6 +2,9 @@ import { RequestHandler } from "express";
 import { getClient } from "../../client";
 import { userContext } from "../auth/middleware";
 import { ServerError } from "../../errors";
+import { stringify } from "csv-stringify";
+import { z } from "zod";
+import { ValidatedRequestHandler } from "../../shared/middleware/validate";
 
 export const getLoggedInUserDetails: RequestHandler = async (
   _req,
@@ -33,3 +36,39 @@ export const getLoggedInUserDetails: RequestHandler = async (
 
 export const healthCheck: RequestHandler = (_req, res) =>
   res.json({ hello: "world" });
+
+export const downloadApplicationSchema = z.object({
+  body: z.object({
+    data: z.array(
+      z.object({
+        question: z.string(),
+        responses: z.any(),
+        metadata: z.any().optional(),
+      }),
+    ),
+  }),
+});
+
+export type DownloadApplicationController = ValidatedRequestHandler<
+  typeof downloadApplicationSchema,
+  string | Record<"message", string>
+>;
+
+/**
+ * Allows an applicant to download their application data on the Confirmation page
+ */
+export const downloadApplicationController: DownloadApplicationController =
+  async (req, res, next) => {
+    const { data } = res.locals.parsedReq.body;
+
+    try {
+      // build a CSV and stream the response
+      stringify(data, {
+        columns: ["question", "responses", "metadata"],
+        header: true,
+      }).pipe(res);
+      res.header("Content-type", "text/csv");
+    } catch (err) {
+      next(err);
+    }
+  };
