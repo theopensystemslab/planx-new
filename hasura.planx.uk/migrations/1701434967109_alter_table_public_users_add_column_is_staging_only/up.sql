283f7a5afca4cf68076a5eb9349168144cbb8dec
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2023-12-04 11:15:24 +0000

    fix: Typo in migration, comment out sync script

--- hasura.planx.uk/migrations/1701434967109_alter_table_public_users_add_column_is_staging_only/up.sql
+++ hasura.planx.uk/migrations/1701434967109_alter_table_public_users_add_column_is_staging_only/up.sql
@@ -1,4 +1,4 @@
 alter table "public"."users" add column "is_staging_only" boolean
  not null default 'false';
 
-comment on column "public"."users"."is_staging_only" is E 'Controls if this user can access all environments. Some users are restricted to Pizza / Staging environments only.';
+comment on column "public"."users"."is_staging_only" is E'Controls if this user can access all environments. Some users are restricted to Pizza / Staging environments only.';
