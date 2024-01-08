c9b7810ceab63f21ac1c9be6e54147ea1d93b6e4
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-01-08 12:01:43 +0000

    chore(wip): Update sync script for `team_themes` table (#2633)

--- scripts/seed-database/container.sh
+++ scripts/seed-database/container.sh
@@ -18,7 +18,7 @@ mkdir -p /tmp
 # Create sync.sql file for all our comnands which will be executed in a single transaction
 touch '/tmp/sync.sql'
 
-tables=(flows users teams flow_document_templates team_members)
+tables=(flows users teams flow_document_templates team_members team_themes)
 
 # run copy commands on remote  db
 for table in "${tables[@]}"; do
