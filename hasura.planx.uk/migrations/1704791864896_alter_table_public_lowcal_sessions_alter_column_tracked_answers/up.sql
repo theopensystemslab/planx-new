841da1e284db12a1ec3160c9166f832531d1299b
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-01-09 10:38:34 +0100

    fix migration json -> jsonb

--- hasura.planx.uk/migrations/1704791864896_alter_table_public_lowcal_sessions_alter_column_tracked_answers/up.sql
+++ hasura.planx.uk/migrations/1704791864896_alter_table_public_lowcal_sessions_alter_column_tracked_answers/up.sql
@@ -1,2 +1,2 @@
-alter table "public"."lowcal_sessions" add column "allow_list_answers" json null;
+alter table "public"."lowcal_sessions" add column "allow_list_answers" jsonb null;
 comment on column "public"."lowcal_sessions"."allow_list_answers" is E'Non-personal passport answers that can be retained for analytics post-sanitation; updated via cron trigger after submission';
