3673affdfe3d9eddb29538d33c41f38a3cdddd86
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-01-10 09:29:05 +0000

    feat: setup a Cron trigger to update allow-list answers for analytics on submitted sessions (#2641)

--- api.planx.uk/modules/webhooks/controller.ts
+++ api.planx.uk/modules/webhooks/controller.ts
@@ -15,6 +15,7 @@ import {
 import { SanitiseApplicationData } from "./service/sanitiseApplicationData/types";
 import { sanitiseApplicationData } from "./service/sanitiseApplicationData";
 import { IsCleanJSONBController } from "./service/validateInput/schema";
+import { analyzeSessions } from "./service/analyzeSessions";
 
 export const sendSlackNotificationController: SendSlackNotification = async (
   _req,
@@ -145,6 +146,25 @@ export const sanitiseApplicationDataController: SanitiseApplicationData =
       );
     }
   };
+
+export const analyzeSessionsController: SanitiseApplicationData = async (
+  _req,
+  res,
+  next,
+) => {
+  try {
+    const { operationFailed, results } = await analyzeSessions();
+    if (operationFailed) res.status(500);
+    return res.json(results);
+  } catch (error) {
+    return next(
+      new ServerError({
+        message: "Failed to update session analytics",
+        cause: error,
+      }),
+    );
+  }
+};
 
 export const isCleanJSONBController: IsCleanJSONBController = async (
   _req,
---
+++ api.planx.uk/modules/webhooks/service/analyzeSessions/index.test.ts
@@ -0,0 +1,192 @@
+import supertest from "supertest";
+
+import app from "../../../../server";
+import * as operations from "./operations";
+import * as sharedOperations from "../sanitiseApplicationData/operations";
+
+const mockSend = jest.fn();
+const mockSlackNotify = jest.fn().mockImplementation(() => {
+  return { send: mockSend };
+});
+jest.mock("slack-notify", () => {
+  return {
+    __esModule: true, // see https://jestjs.io/docs/jest-object#jestmockmodulename-factory-options
+    default: (webhookURL: string) => mockSlackNotify(webhookURL),
+  };
+});
+
+const { post } = supertest(app);
+
+describe("Analyze sessions webhook", () => {
+  const ENDPOINT = "/webhooks/hasura/analyze-sessions";
+
+  it("returns a 401 without correct authentication", async () => {
+    await post(ENDPOINT)
+      .expect(401)
+      .then((response) => {
+        expect(response.body).toEqual({
+          error: "Unauthorised",
+        });
+      });
+  });
+
+  it("returns a 500 if an unhandled error is thrown whilst running operations", async () => {
+    const mockOperationHandler = jest.spyOn(
+      sharedOperations,
+      "operationHandler",
+    );
+    mockOperationHandler.mockRejectedValueOnce("Unhandled error!");
+
+    await post(ENDPOINT)
+      .set({ Authorization: process.env.HASURA_PLANX_API_KEY })
+      .expect(500)
+      .then((response) =>
+        expect(response.body.error).toMatch(
+          /Failed to update session analytics/,
+        ),
+      );
+  });
+
+  it("returns a 200 when all operations are successful", async () => {
+    const mockOperation1 = jest.fn().mockResolvedValue(["123"]);
+    const mockOperation2 = jest.fn().mockResolvedValue(["456", "789"]);
+    const mockOperation3 = jest.fn().mockResolvedValue(["abc", "def", "ghi"]);
+
+    const mockGetOperations = jest.spyOn(
+      operations,
+      "getAnalyzeSessionOperations",
+    );
+    mockGetOperations.mockImplementationOnce(() => [
+      mockOperation1,
+      mockOperation2,
+      mockOperation3,
+    ]);
+
+    await post(ENDPOINT)
+      .set({ Authorization: process.env.HASURA_PLANX_API_KEY })
+      .expect(200)
+      .then((response) => {
+        expect(mockOperation1).toHaveBeenCalled();
+        expect(mockOperation2).toHaveBeenCalled();
+        expect(mockOperation3).toHaveBeenCalled();
+
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              status: "success",
+              count: 1,
+            }),
+            expect.objectContaining({
+              status: "success",
+              count: 2,
+            }),
+            expect.objectContaining({
+              status: "success",
+              count: 3,
+            }),
+          ]),
+        );
+      });
+  });
+
+  it("returns a 500 when only a single operation fails", async () => {
+    const mockOperation1 = jest.fn().mockResolvedValue(["123"]);
+    const mockOperation2 = jest
+      .fn()
+      .mockRejectedValue(new Error("Query failed!"));
+    const mockOperation3 = jest.fn().mockResolvedValue(["abc", "def", "ghi"]);
+
+    const mockGetOperations = jest.spyOn(
+      operations,
+      "getAnalyzeSessionOperations",
+    );
+    mockGetOperations.mockImplementationOnce(() => [
+      mockOperation1,
+      mockOperation2,
+      mockOperation3,
+    ]);
+
+    await post(ENDPOINT)
+      .set({ Authorization: process.env.HASURA_PLANX_API_KEY })
+      .expect(500)
+      .then((response) => {
+        expect(mockOperation1).toHaveBeenCalled();
+        expect(mockOperation2).toHaveBeenCalled();
+        expect(mockOperation3).toHaveBeenCalled();
+
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              status: "success",
+              count: 1,
+            }),
+            expect.objectContaining({
+              status: "failure",
+              errorMessage: "Query failed!",
+            }),
+            expect.objectContaining({
+              status: "success",
+              count: 3,
+            }),
+          ]),
+        );
+
+        expect(mockSlackNotify).toHaveBeenCalledWith(
+          process.env.SLACK_WEBHOOK_URL,
+        );
+        expect(mockSend).toHaveBeenCalledWith(
+          expect.objectContaining({
+            text: expect.stringMatching(/Error: Query failed!/),
+          }),
+        );
+      });
+  });
+
+  it("returns a 500 if all operations fail", async () => {
+    const mockOperation1 = jest
+      .fn()
+      .mockRejectedValue(new Error("Query failed!"));
+    const mockOperation2 = jest
+      .fn()
+      .mockRejectedValue(new Error("Query failed!"));
+    const mockOperation3 = jest
+      .fn()
+      .mockRejectedValue(new Error("Query failed!"));
+
+    const mockGetOperations = jest.spyOn(
+      operations,
+      "getAnalyzeSessionOperations",
+    );
+    mockGetOperations.mockImplementationOnce(() => [
+      mockOperation1,
+      mockOperation2,
+      mockOperation3,
+    ]);
+
+    await post(ENDPOINT)
+      .set({ Authorization: process.env.HASURA_PLANX_API_KEY })
+      .expect(500)
+      .then((response) => {
+        expect(mockOperation1).toHaveBeenCalled();
+        expect(mockOperation2).toHaveBeenCalled();
+        expect(mockOperation3).toHaveBeenCalled();
+
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              status: "failure",
+              errorMessage: "Query failed!",
+            }),
+            expect.objectContaining({
+              status: "failure",
+              errorMessage: "Query failed!",
+            }),
+            expect.objectContaining({
+              status: "failure",
+              errorMessage: "Query failed!",
+            }),
+          ]),
+        );
+      });
+  });
+});
