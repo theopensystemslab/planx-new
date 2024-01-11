ada5fe3cf6ca2e1bf89bc9c3d605b99283781ada
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2024-01-11 11:57:15 +0000

    wip: UI setup for team design settings (#2643)
    * wip: Council theme colours
    
    * feat: Previews for design settings
    
    * feat: UI for design settings
    
    * fix: Remove unneeded z-index

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/DesignSettings.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/DesignSettings.tsx
@@ -1,5 +1,7 @@
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
+import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { useFormik } from "formik";
 import { hasFeatureFlag } from "lib/featureFlags";
@@ -15,10 +17,24 @@ import InputRowItem from "ui/shared/InputRowItem";
 import InputRowLabel from "ui/shared/InputRowLabel";
 import PublicFileUploadButton from "ui/shared/PublicFileUploadButton";
 
+const DesignPreview = styled(Box)(({ theme }) => ({
+  border: `2px solid ${theme.palette.border.input}`,
+  padding: theme.spacing(2),
+  boxShadow: "4px 4px 0px rgba(150, 150, 150, 0.5)",
+}));
+
+const exampleColor = "#007078";
+
 const DesignSettings: React.FC = () => {
-  const formik = useFormik<{ bgColor: string }>({
+  const formik = useFormik<{
+    themeColor: string;
+    buttonColor: string;
+    linkColor: string;
+  }>({
     initialValues: {
-      bgColor: "#000",
+      themeColor: exampleColor,
+      buttonColor: exampleColor,
+      linkColor: exampleColor,
     },
     onSubmit: () => {},
     validate: () => {},
@@ -27,7 +43,7 @@ const DesignSettings: React.FC = () => {
   const isUsingFeatureFlag = () => hasFeatureFlag("SHOW_TEAM_SETTINGS");
 
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <>
       <EditorRow>
         <Typography variant="h2" component="h3" gutterBottom>
           Design
@@ -43,91 +59,226 @@ const DesignSettings: React.FC = () => {
       ) : (
         <>
           <EditorRow background>
-            <InputGroup flowSpacing>
-              <InputLegend>Theme colour</InputLegend>
-              <InputDescription>
-                Set the theme colour. The theme colour should be a dark colour
-                that contrasts with white ("#ffffff").
-              </InputDescription>
-              <InputDescription>
-                <Link href="https://www.planx.uk">
-                  See our guide for setting theme colours
-                </Link>
-              </InputDescription>
-              <InputRow>
-                <InputRowItem>
-                  <ColorPicker
-                    color={formik.values.bgColor}
-                    onChange={(color) => formik.setFieldValue("bgColor", color)}
-                    label="Theme colour"
+            <form onSubmit={formik.handleSubmit}>
+              <InputGroup flowSpacing>
+                <InputLegend>Theme colour & logo</InputLegend>
+                <InputDescription>
+                  The theme colour and logo, are used in the header of the
+                  service. The theme colour should be a dark colour that
+                  contrasts with white ("#ffffff"). The logo should contrast
+                  with a dark background colour (your theme colour) and have a
+                  transparent background.
+                </InputDescription>
+                <InputDescription>
+                  <Link href="https://www.planx.uk">
+                    See our guide for setting theme colours and logos
+                  </Link>
+                </InputDescription>
+                <InputRow>
+                  <InputRowItem>
+                    <ColorPicker
+                      color={formik.values.themeColor}
+                      onChange={(color) =>
+                        formik.setFieldValue("themeColor", color)
+                      }
+                      label="Theme colour"
+                    />
+                  </InputRowItem>
+                </InputRow>
+                <InputRow>
+                  <InputRowLabel>Logo:</InputRowLabel>
+                  <InputRowItem width={50}>
+                    <PublicFileUploadButton />
+                  </InputRowItem>
+                  <Typography
+                    color="text.secondary"
+                    variant="body2"
+                    pl={2}
+                    alignSelf="center"
+                  >
+                    .png or .svg
+                  </Typography>
+                </InputRow>
+              </InputGroup>
+              <Box>
+                <Typography variant="h4" my={1}>
+                  Preview:
+                </Typography>
+                <DesignPreview bgcolor={exampleColor}>
+                  <img
+                    width="140"
+                    src="https://raw.githubusercontent.com/theopensystemslab/planx-team-logos/main/barnet.svg"
+                    alt="council logo"
                   />
-                </InputRowItem>
-              </InputRow>
-            </InputGroup>
+                </DesignPreview>
+              </Box>
+              <Box>
+                <Button type="submit" variant="contained" disabled>
+                  Save
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled
+                  color="secondary"
+                  sx={{ ml: 1.5 }}
+                >
+                  Reset changes
+                </Button>
+              </Box>
+            </form>
           </EditorRow>
           <EditorRow background>
-            <InputGroup flowSpacing>
-              <InputLegend>Logo</InputLegend>
-              <InputDescription>
-                Set the logo to be used in the header of the service. The logo
-                should contrast with a dark background colour and have a
-                transparent background.
-              </InputDescription>
-              <InputDescription>
-                <Link href="https://www.planx.uk">See our guide for logos</Link>
-              </InputDescription>
-              <InputRow>
-                <InputRowLabel>Logo:</InputRowLabel>
-                <InputRowItem width={50}>
-                  <PublicFileUploadButton />
-                </InputRowItem>
-                <Typography
-                  color="text.secondary"
-                  variant="body2"
-                  pl={2}
-                  alignSelf="center"
-                >
-                  .png or .svg
+            <form onSubmit={formik.handleSubmit}>
+              <InputGroup flowSpacing>
+                <InputLegend>Button colour</InputLegend>
+                <InputDescription>
+                  The button background colour should be either a dark or light
+                  colour. The text will be programatically selected to contrast
+                  with the selected colour (being either black or white).
+                </InputDescription>
+                <InputDescription>
+                  <Link href="https://www.planx.uk">
+                    See our guide for setting button colours
+                  </Link>
+                </InputDescription>
+                <InputRow>
+                  <InputRowItem>
+                    <ColorPicker
+                      color={formik.values.buttonColor}
+                      onChange={(color) =>
+                        formik.setFieldValue("buttonColor", color)
+                      }
+                      label="Button colour"
+                    />
+                  </InputRowItem>
+                </InputRow>
+              </InputGroup>
+              <Box>
+                <Typography variant="h4" my={1}>
+                  Preview:
                 </Typography>
-              </InputRow>
-            </InputGroup>
+                <DesignPreview bgcolor="white">
+                  <Button
+                    variant="contained"
+                    sx={{ backgroundColor: exampleColor }}
+                  >
+                    Example primary button
+                  </Button>
+                </DesignPreview>
+              </Box>
+              <Box>
+                <Button type="submit" variant="contained" disabled>
+                  Save
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled
+                  color="secondary"
+                  sx={{ ml: 1.5 }}
+                >
+                  Reset changes
+                </Button>
+              </Box>
+            </form>
           </EditorRow>
           <EditorRow background>
-            <InputGroup flowSpacing>
-              <InputLegend>Favicon</InputLegend>
-              <InputDescription>
-                Set the favicon to be used in the browser tab. The favicon
-                should be 32x32px and in .ico or .png format.
-              </InputDescription>
-              <InputDescription>
-                <Link href="https://www.planx.uk">
-                  See our guide for favicons
-                </Link>
-              </InputDescription>
-              <InputRow>
-                <InputRowLabel>Favicon:</InputRowLabel>
-                <InputRowItem width={50}>
-                  <PublicFileUploadButton />
-                </InputRowItem>
-                <Typography
-                  color="text.secondary"
-                  variant="body2"
-                  pl={2}
-                  alignSelf="center"
-                >
-                  .ico or .png
+            <form onSubmit={formik.handleSubmit}>
+              <InputGroup flowSpacing>
+                <InputLegend>Text link colour</InputLegend>
+                <InputDescription>
+                  The text link colour should be a dark colour that contrasts
+                  with white ("#ffffff").
+                </InputDescription>
+                <InputDescription>
+                  <Link href="https://www.planx.uk">
+                    See our guide for setting text link colours
+                  </Link>
+                </InputDescription>
+                <InputRow>
+                  <InputRowItem>
+                    <ColorPicker
+                      color={formik.values.linkColor}
+                      onChange={(color) =>
+                        formik.setFieldValue("linkColor", color)
+                      }
+                      label="Text link colour"
+                    />
+                  </InputRowItem>
+                </InputRow>
+              </InputGroup>
+              <Box>
+                <Typography variant="h4" my={1}>
+                  Preview:
                 </Typography>
-              </InputRow>
-            </InputGroup>
+                <DesignPreview bgcolor="white">
+                  <Link sx={{ color: exampleColor }}>Example text link</Link>
+                </DesignPreview>
+              </Box>
+              <Box>
+                <Button type="submit" variant="contained" disabled>
+                  Save
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled
+                  color="secondary"
+                  sx={{ ml: 1.5 }}
+                >
+                  Reset changes
+                </Button>
+              </Box>
+            </form>
           </EditorRow>
-          <EditorRow>
-            <Button type="submit" variant="contained" color="primary">
-              Update design settings
-            </Button>
+          <EditorRow background>
+            <form onSubmit={formik.handleSubmit}>
+              <InputGroup flowSpacing>
+                <InputLegend>Favicon</InputLegend>
+                <InputDescription>
+                  Set the favicon to be used in the browser tab. The favicon
+                  should be 32x32px and in .ico or .png format.
+                </InputDescription>
+                <InputDescription>
+                  <Link href="https://www.planx.uk">
+                    See our guide for favicons
+                  </Link>
+                </InputDescription>
+                <InputRow>
+                  <InputRowLabel>Favicon:</InputRowLabel>
+                  <InputRowItem width={50}>
+                    <PublicFileUploadButton />
+                  </InputRowItem>
+                  <Typography
+                    color="text.secondary"
+                    variant="body2"
+                    pl={2}
+                    alignSelf="center"
+                  >
+                    .ico or .png
+                  </Typography>
+                </InputRow>
+              </InputGroup>
+              <Box>
+                <Button type="submit" variant="contained" disabled>
+                  Save
+                </Button>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled
+                  color="secondary"
+                  sx={{ ml: 1.5 }}
+                >
+                  Reset changes
+                </Button>
+              </Box>
+            </form>
           </EditorRow>
         </>
       )}
-    </form>
+    </>
   );
 };
 
