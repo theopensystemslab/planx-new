1dbb181990d86fb373caeea2531e85f24aa05fa0
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-01-16 17:14:56 +0000

    feat: Wire up design settings (link colour)

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/DesignSettings/TextLinkForm.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/DesignSettings/TextLinkForm.tsx
@@ -1,22 +1,56 @@
 import Link from "@mui/material/Link";
+import { getContrastRatio, useTheme } from "@mui/material/styles";
+import { Team, TeamTheme } from "@opensystemslab/planx-core/types";
 import { useFormik } from "formik";
-import React from "react";
+import { useStore } from "pages/FlowEditor/lib/store";
+import React, { useEffect, useState } from "react";
 import ColorPicker from "ui/editor/ColorPicker";
 import InputDescription from "ui/editor/InputDescription";
 import InputRow from "ui/shared/InputRow";
 import InputRowItem from "ui/shared/InputRowItem";
 
-import { DesignPreview, EXAMPLE_COLOUR, SettingsForm } from ".";
+import { DesignPreview, SettingsForm } from ".";
 
-export const TextLinkForm: React.FC = () => {
-  const formik = useFormik<{
-    linkColor: string;
-  }>({
-    initialValues: {
-      linkColor: EXAMPLE_COLOUR,
+type FormValues = Pick<TeamTheme, "linkColour">;
+
+export const TextLinkForm: React.FC<{ team: Team; onSuccess: () => void }> = ({
+  team,
+  onSuccess,
+}) => {
+  const theme = useTheme();
+
+  useEffect(() => {
+    setInitialValues({ linkColour: team.theme?.linkColour });
+  }, [team]);
+
+  const [initialValues, setInitialValues] = useState<FormValues>({
+    linkColour: "",
+  });
+
+  const formik = useFormik<FormValues>({
+    initialValues,
+    onSubmit: async (values, { resetForm }) => {
+      const isSuccess = await useStore.getState().updateTeamTheme(values);
+      if (isSuccess) {
+        onSuccess();
+        // Reset "dirty" status to disable Save & Reset buttons
+        resetForm({ values });
+      }
     },
-    onSubmit: () => {},
-    validate: () => {},
+    validateOnBlur: false,
+    validateOnChange: false,
+    enableReinitialize: true,
+    validate: ({ linkColour }) => {
+      const isContrastThresholdMet =
+        getContrastRatio("#FFF", linkColour) > theme.palette.contrastThreshold;
+
+      if (!isContrastThresholdMet) {
+        return {
+          linkColour:
+            "Colour does not meet accessibility contrast requirements (3:1)",
+        };
+      }
+    },
   });
 
   return (
@@ -40,8 +74,8 @@ export const TextLinkForm: React.FC = () => {
         <InputRow>
           <InputRowItem>
             <ColorPicker
-              color={formik.values.linkColor}
-              onChange={(color) => formik.setFieldValue("linkColor", color)}
+              color={formik.values.linkColour}
+              onChange={(color) => formik.setFieldValue("linkColour", color)}
               label="Text link colour"
             />
           </InputRowItem>
@@ -49,7 +83,9 @@ export const TextLinkForm: React.FC = () => {
       }
       preview={
         <DesignPreview bgcolor="white">
-          <Link sx={{ color: EXAMPLE_COLOUR }}>Example text link</Link>
+          <Link sx={{ color: formik.values.linkColour }}>
+            Example text link
+          </Link>
         </DesignPreview>
       }
     />
