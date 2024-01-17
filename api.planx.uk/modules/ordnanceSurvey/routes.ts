e6046f56b32c930db99bf4fc1109f9d4e8813727
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-01-17 10:30:09 +0100

    disable new CORS rules for OS Proxy endpoint

--- api.planx.uk/modules/ordnanceSurvey/routes.ts
+++ api.planx.uk/modules/ordnanceSurvey/routes.ts
@@ -1,8 +1,16 @@
+import cors from "cors";
 import { Router } from "express";
 import { useOrdnanceSurveyProxy } from "./controller";
 
 const router = Router();
 
-router.use("/proxy/ordnance-survey", useOrdnanceSurveyProxy);
+// Because this route already uses MAP_ALLOWLIST, disable global CORS checks so map repo docs and HTML templates are accessible
+const osProxyCORSOptions = {
+  credentials: true,
+  methods: "*",
+};
+
+router.options("/proxy/ordnance-survey", cors(osProxyCORSOptions));
+router.use("/proxy/ordnance-survey", cors(osProxyCORSOptions), useOrdnanceSurveyProxy);
 
 export default router;
