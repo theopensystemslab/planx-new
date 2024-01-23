f4059660d6bf3fe2b90f5753d5522f04833d59d5
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-01-23 16:22:05 +0000

    fix: Add permissions route guards

--- editor.planx.uk/src/routes/flowSettings.tsx
+++ editor.planx.uk/src/routes/flowSettings.tsx
@@ -1,6 +1,14 @@
 import gql from "graphql-tag";
 import { publicClient } from "lib/graphql";
-import { compose, mount, redirect, route, withData } from "navi";
+import {
+  compose,
+  map,
+  mount,
+  NotFoundError,
+  redirect,
+  route,
+  withData,
+} from "navi";
 import DataManagerSettings from "pages/FlowEditor/components/Settings/DataManagerSettings";
 import ServiceFlags from "pages/FlowEditor/components/Settings/ServiceFlags";
 import ServiceSettings from "pages/FlowEditor/components/Settings/ServiceSettings";
@@ -18,58 +26,66 @@ const flowSettingsRoutes = compose(
 
   mount({
     "/": redirect("./service"),
-    "/:tab": route(async (req) => {
-      const { data } = await publicClient.query({
-        query: gql`
-          query GetFlow($slug: String!, $team_slug: String!) {
-            flows(
-              limit: 1
-              where: {
-                slug: { _eq: $slug }
-                team: { slug: { _eq: $team_slug } }
+    "/:tab": map(async (req) => {
+      const isAuthorised = useStore.getState().canUserEditTeam(req.params.team);
+      if (!isAuthorised)
+        throw new NotFoundError(
+          `User does not have access to ${req.originalUrl}`,
+        );
+
+      return route(async (req) => {
+        const { data } = await publicClient.query({
+          query: gql`
+            query GetFlow($slug: String!, $team_slug: String!) {
+              flows(
+                limit: 1
+                where: {
+                  slug: { _eq: $slug }
+                  team: { slug: { _eq: $team_slug } }
+                }
+              ) {
+                id
+                settings
               }
-            ) {
-              id
-              settings
             }
-          }
-        `,
-        variables: {
-          slug: req.params.flow,
-          team_slug: req.params.team,
-        },
+          `,
+          variables: {
+            slug: req.params.flow,
+            team_slug: req.params.team,
+          },
+        });
+
+        const settings: FlowSettings = data.flows[0].settings;
+        useStore.getState().setFlowSettings(settings);
+
+        return {
+          title: makeTitle(
+            [req.params.team, req.params.flow, "Flow Settings"].join("/"),
+          ),
+          view: (
+            <Settings
+              currentTab={req.params.tab}
+              tabs={[
+                {
+                  name: "Service",
+                  route: "service",
+                  Component: ServiceSettings,
+                },
+                {
+                  name: "Service Flags",
+                  route: "flags",
+                  Component: ServiceFlags,
+                },
+                {
+                  name: "Data",
+                  route: "data-manager",
+                  Component: DataManagerSettings,
+                },
+              ]}
+            />
+          ),
+        };
       });
-
-      const settings: FlowSettings = data.flows[0].settings;
-      useStore.getState().setFlowSettings(settings);
-
-      return {
-        title: makeTitle(
-          [req.params.team, req.params.flow, "Flow Settings"].join("/"),
-        ),
-        view: (
-          <Settings
-            currentTab={req.params.tab}
-            tabs={[
-              {
-                name: "Service",
-                route: "service",
-                Component: ServiceSettings,
-              },
-              {
-                name: "Service Flags",
-                route: "flags",
-                Component: ServiceFlags,
-              },
-              {
-                name: "Data",
-                route: "data-manager",
-                Component: DataManagerSettings,
-              },
-            ]}
-          />
-        ),
-      };
     }),
   }),
 );
