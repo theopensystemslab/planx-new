0695e454cd1fa4dcf8ce97754be1f88c0f894ee3
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-01-25 16:54:47 +0000

    feat: Add new columns to team_integrations table

---
+++ hasura.planx.uk/migrations/1706201208594_alter_table_public_team_integrations_add_column_staging_bops_secret/up.sql
@@ -0,0 +1,9 @@
+alter table "public"."team_integrations" add column "staging_bops_secret" text
+ null;
+
+comment on column "public"."team_integrations"."staging_bops_secret" is E 'Secret required for BOPS submission in the format <API_KEY>:<INITIALIZATION_VECTOR>';
+
+alter table "public"."team_integrations" add column "production_bops_secret" text
+ null;
+
+comment on column "public"."team_integrations"."production_bops_secret" is E'Secret required for BOPS submission in the format <API_KEY>:<INITIALIZATION_VECTOR>';
