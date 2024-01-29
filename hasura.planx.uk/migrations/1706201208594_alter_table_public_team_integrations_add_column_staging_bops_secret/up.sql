73c88a56414407a010c70d35666e8cb44278928d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-01-29 16:35:13 +0000

    fix: Typo in comment

--- hasura.planx.uk/migrations/1706201208594_alter_table_public_team_integrations_add_column_staging_bops_secret/up.sql
+++ hasura.planx.uk/migrations/1706201208594_alter_table_public_team_integrations_add_column_staging_bops_secret/up.sql
@@ -1,7 +1,7 @@
 alter table "public"."team_integrations" add column "staging_bops_secret" text
  null;
 
-comment on column "public"."team_integrations"."staging_bops_secret" is E 'Secret required for BOPS submission in the format <API_KEY>:<INITIALIZATION_VECTOR>';
+comment on column "public"."team_integrations"."staging_bops_secret" is E'Secret required for BOPS submission in the format <API_KEY>:<INITIALIZATION_VECTOR>';
 
 alter table "public"."team_integrations" add column "production_bops_secret" text
  null;
