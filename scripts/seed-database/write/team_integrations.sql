66819b067a012bb2367bed8f9285a311c2ba31a5
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-01-31 18:04:59 +0000

    chore: Sync new `staging_bops_secret` column [skip pizza]

--- scripts/seed-database/write/team_integrations.sql
+++ scripts/seed-database/write/team_integrations.sql
@@ -2,20 +2,23 @@
 CREATE TEMPORARY TABLE sync_team_integrations (
   id serial,
   team_id integer,
-  staging_bops_submission_url text
+  staging_bops_submission_url text,
+  staging_bops_secret text
 );
 
 \COPY sync_team_integrations FROM '/tmp/team_integrations.csv' WITH (FORMAT csv, DELIMITER ';');
 
 INSERT INTO
-  team_integrations (id, team_id, staging_bops_submission_url)
+  team_integrations (id, team_id, staging_bops_submission_url, staging_bops_secret)
 SELECT
   id,
   team_id,
-  staging_bops_submission_url
+  staging_bops_submission_url,
+  staging_bops_secret
 FROM
   sync_team_integrations ON CONFLICT (id) DO
 UPDATE
 SET
   team_id = EXCLUDED.team_id,
-  staging_bops_submission_url = EXCLUDED.staging_bops_submission_url;
+  staging_bops_submission_url = EXCLUDED.staging_bops_submission_url,
+  staging_bops_secret = EXCLUDED.staging_bops_secret;
