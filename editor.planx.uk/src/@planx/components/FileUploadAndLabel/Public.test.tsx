48d11beba8de81b67f4d53eb39b240aa2db9bcb2
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-02-07 14:49:01 +0000

    test: FileUploadAndLabel can save and append `_requestedFiles`

--- editor.planx.uk/src/@planx/components/FileUploadAndLabel/Public.test.tsx
+++ editor.planx.uk/src/@planx/components/FileUploadAndLabel/Public.test.tsx
@@ -1,10 +1,24 @@
-import { fireEvent, screen, waitFor, within } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  screen,
+  waitFor,
+  within,
+} from "@testing-library/react";
+import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
 import axios from "axios";
+import { vanillaStore } from "pages/FlowEditor/lib/store";
+import { FullStore, useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 import { axe, setup } from "testUtils";
+import { Breadcrumbs } from "types";
 
 import { mockFileTypes, mockFileTypesUniqueKeys } from "./mocks";
+import { Condition, PASSPORT_REQUESTED_FILES_KEY } from "./model";
 import FileUploadAndLabelComponent from "./Public";
+
+const { getState, setState } = vanillaStore;
+let initialState: FullStore;
 
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
@@ -595,3 +609,195 @@ describe("Error handling", () => {
     expect(handleSubmit).not.toHaveBeenCalled();
   });
 });
+
+describe("Submitting data", () => {
+  beforeAll(() => (initialState = getState()));
+
+  afterEach(() => waitFor(() => setState(initialState)));
+
+  it("records the user uploaded files", async () => {
+    const handleSubmit = jest.fn();
+    const { user } = setup(
+      <FileUploadAndLabelComponent
+        title="Test title"
+        handleSubmit={handleSubmit}
+        fileTypes={mockFileTypesUniqueKeys}
+      />,
+    );
+
+    await uploadAndTagSingleFile(user);
+
+    await user.click(screen.getByText("Continue"));
+    expect(handleSubmit).toHaveBeenCalledTimes(1);
+
+    const submitted = handleSubmit.mock.calls[0][0];
+    const uploadedFile = submitted?.data?.roofPlan;
+
+    expect(uploadedFile).toBeDefined();
+    expect(uploadedFile).toHaveLength(1);
+    expect(uploadedFile[0]).toEqual(
+      expect.objectContaining({
+        filename: "test1.png",
+        url: "https://api.editor.planx.dev/file/private/gws7l5d1/test1.jpg",
+      }),
+    );
+  });
+
+  it("records the full file type list presented to the user", async () => {
+    const handleSubmit = jest.fn();
+    const { user } = setup(
+      <FileUploadAndLabelComponent
+        title="Test title"
+        handleSubmit={handleSubmit}
+        fileTypes={mockFileTypesUniqueKeys}
+      />,
+    );
+
+    await uploadAndTagSingleFile(user);
+    await user.click(screen.getByText("Continue"));
+
+    const submitted = handleSubmit.mock.calls[0][0];
+    const requestedFiles = submitted?.data?._requestedFiles;
+
+    expect(requestedFiles).toBeDefined();
+    expect(requestedFiles).toHaveLength(3);
+    expect(requestedFiles).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          fn: "roofPlan",
+          condition: Condition.AlwaysRequired,
+        }),
+        expect.objectContaining({
+          fn: "heritage",
+          condition: Condition.AlwaysRecommended,
+        }),
+        expect.objectContaining({
+          fn: "utilityBill",
+          condition: Condition.NotRequired,
+        }),
+      ]),
+    );
+  });
+
+  it("appends to the list of existing requested files", async () => {
+    // Mimic having passed file upload / file upload and label component
+    const breadcrumbs: Breadcrumbs = {
+      previousFileUploadComponent: {
+        auto: false,
+        data: {
+          anotherFileType: [
+            {
+              url: "http://test.com/file.jpg",
+              filename: "file.jpg",
+            },
+          ],
+          [PASSPORT_REQUESTED_FILES_KEY]: [
+            {
+              fn: "anotherFileType",
+              condition: Condition.AlwaysRequired,
+            },
+          ],
+        },
+      },
+    };
+
+    const flow = {
+      _root: {
+        edges: ["previousFileUploadComponent", "currentComponent"],
+      },
+      previousFileUploadComponent: {
+        data: {
+          fn: "anotherFileType",
+          color: "#EFEFEF",
+        },
+        type: 140,
+      },
+      currentComponent: {
+        type: 145,
+        data: {
+          title: "Upload and label",
+          fileTypes: [
+            {
+              name: "Floor Plan",
+              fn: "floorPlan",
+              rule: {
+                condition: "AlwaysRequired",
+              },
+            },
+          ],
+          hideDropZone: false,
+        },
+      },
+    };
+
+    act(() => setState({ flow, breadcrumbs }));
+
+    const passport = useStore.getState().computePassport();
+    console.log({ passport });
+
+    const handleSubmit = jest.fn();
+    const { user } = setup(
+      <FileUploadAndLabelComponent
+        title="Test title"
+        handleSubmit={handleSubmit}
+        fileTypes={mockFileTypesUniqueKeys}
+      />,
+    );
+
+    await uploadAndTagSingleFile(user);
+    await user.click(screen.getByText("Continue"));
+
+    const submitted = handleSubmit.mock.calls[0][0];
+    const requestedFiles = submitted?.data?._requestedFiles;
+
+    expect(requestedFiles).toEqual(
+      expect.arrayContaining([
+        // Existing files from previous components
+        expect.objectContaining({
+          fn: "anotherFileType",
+          condition: Condition.AlwaysRequired,
+        }),
+        // Requested files from this component
+        expect.objectContaining({
+          fn: "roofPlan",
+          condition: Condition.AlwaysRequired,
+        }),
+        expect.objectContaining({
+          fn: "heritage",
+          condition: Condition.AlwaysRecommended,
+        }),
+        expect.objectContaining({
+          fn: "utilityBill",
+          condition: Condition.NotRequired,
+        }),
+      ]),
+    );
+  });
+});
+
+/**
+ * Test helper which steps through the process of uploading and labelling a file
+ * Does not contain assertations - relies on these happening in other, more granular, passing tests
+ */
+const uploadAndTagSingleFile = async (user: UserEvent) => {
+  mockedAxios.post.mockResolvedValueOnce({
+    data: {
+      fileType: "image/png",
+      fileUrl: "https://api.editor.planx.dev/file/private/gws7l5d1/test1.jpg",
+    },
+  });
+
+  const file1 = new File(["test1"], "test1.png", { type: "image/png" });
+  const input = screen.getByTestId("upload-input");
+  await user.upload(input, [file1]);
+
+  const fileTaggingModal = await within(document.body).findByTestId(
+    "file-tagging-dialog",
+  );
+
+  const selects = await within(document.body).findAllByTestId("select");
+  fireEvent.change(selects[0], { target: { value: "Roof plan" } });
+
+  const submitModalButton = await within(fileTaggingModal).findByText("Done");
+  user.click(submitModalButton);
+};
