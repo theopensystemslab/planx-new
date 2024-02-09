10745d2aa571a66d6a8b0ee2dfa77b6a7ce7e1b6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-02-09 14:58:47 +0000

    fix: Typo in down.sql

--- hasura.planx.uk/migrations/1707239828082_update_node_type_names/down.sql
+++ hasura.planx.uk/migrations/1707239828082_update_node_type_names/down.sql
@@ -2,7 +2,7 @@ UPDATE
   public.analytics_logs
 SET
   node_type = CASE
-    WHEN node_type 'Question' THEN 'Statement'
-    WHEN node_type 'Answer' THEN 'Response'
+    WHEN node_type = 'Question' THEN 'Statement'
+    WHEN node_type = 'Answer' THEN 'Response'
     ELSE node_type
   END;
