6916c32652c6498e640d82f0cb757d95fec2e6e1
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-02-14 10:29:18 +0000

    chore: Update sync script for `team_integrations.has_planning_data` (#2778)

--- scripts/seed-database/write/team_integrations.sql
+++ scripts/seed-database/write/team_integrations.sql
@@ -3,25 +3,28 @@ CREATE TEMPORARY TABLE sync_team_integrations (
   id serial,
   team_id integer,
   staging_bops_submission_url text,
-  staging_bops_secret text
+  staging_bops_secret text,
+  has_planning_data boolean
 );
 
 \COPY sync_team_integrations FROM '/tmp/team_integrations.csv' WITH (FORMAT csv, DELIMITER ';');
 
 INSERT INTO
-  team_integrations (id, team_id, staging_bops_submission_url, staging_bops_secret)
+  team_integrations (id, team_id, staging_bops_submission_url, staging_bops_secret, has_planning_data)
 SELECT
   id,
   team_id,
   staging_bops_submission_url,
-  staging_bops_secret
+  staging_bops_secret,
+  has_planning_data
 FROM
   sync_team_integrations ON CONFLICT (id) DO
 UPDATE
 SET
   team_id = EXCLUDED.team_id,
   staging_bops_submission_url = EXCLUDED.staging_bops_submission_url,
-  staging_bops_secret = EXCLUDED.staging_bops_secret;
+  staging_bops_secret = EXCLUDED.staging_bops_secret,
+  has_planning_data = EXCLUDED.has_planning_data;
 SELECT
   setval('team_integrations_id_seq', max(id))
 FROM
