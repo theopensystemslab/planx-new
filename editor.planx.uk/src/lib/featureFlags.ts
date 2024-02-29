1fc66b1d9ed2a58782f114b033e36d410a81e8f5
Mike Heneghan <mike@opensystemslab.io>
2024-02-29 17:00:56 +0000

    chore: add feature flag

--- editor.planx.uk/src/lib/featureFlags.ts
+++ editor.planx.uk/src/lib/featureFlags.ts
@@ -1,5 +1,5 @@
 // add/edit/remove feature flags in array below
-const AVAILABLE_FEATURE_FLAGS = [] as const;
+const AVAILABLE_FEATURE_FLAGS = ["APPLICATION_SUBMISSION_VIEW"] as const;
 
 type FeatureFlag = (typeof AVAILABLE_FEATURE_FLAGS)[number];
 
