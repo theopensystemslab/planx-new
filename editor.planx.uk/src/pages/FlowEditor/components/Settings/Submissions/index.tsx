09e5c83af4e793b89ffce59d3e664a62b6c65565
Mike Heneghan <mike@opensystemslab.io>
2024-03-13 17:01:26 +0000

    feat: query submission_services_summary and log result
    - Setup up the types and function to query the submission_services_summary for the pertinent data
    - For now simply log the data out in the console

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/Submissions/index.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/Submissions/index.tsx
@@ -1,9 +1,35 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FeaturePlaceholder } from "ui/editor/FeaturePlaceholder";
 
+import { useStore } from "../../../lib/store";
+import {
+  fetchSubmittedApplications,
+  SubmissionData,
+} from "./submissionDataTypesAndQueries";
+
 const Submissions: React.FC = () => {
+  const flowSlug = useStore.getState().flowSlug;
+  const teamSlug = useStore.getState().teamSlug;
+  const [applications, setApplications] = useState<SubmissionData[]>();
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (flowSlug && teamSlug) {
+      fetchSubmittedApplications(flowSlug, teamSlug)
+        .then((result) => setApplications(result.submission_services_summary))
+        .catch((error) => {
+          setError(error);
+        });
+    }
+  }, [flowSlug, teamSlug]);
+
+  useEffect(() => {
+    console.log(applications);
+    console.log(error);
+  }, [applications, error]);
+
   return (
     <Box>
       <Typography variant="h2" component="h3" gutterBottom>
