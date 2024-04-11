480b90f0b9ba33c22507356710333f6572b2e286
Ian Jones <hello@ianjo.uk>
2024-04-11 10:18:59 +0100

    wip: Outputting mixed flagsets in the graph

--- editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Option.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Option.tsx
@@ -1,3 +1,5 @@
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 import { flatFlags } from "@opensystemslab/planx-core/types";
 import classNames from "classnames";
 import React from "react";
@@ -7,6 +9,37 @@ import { useStore } from "../../../lib/store";
 import Hanger from "./Hanger";
 import Node from "./Node";
 
+const Flag = styled(Box)(() => ({
+  fontWeight: "700",
+  padding: "6px",
+  height: "100%",
+  display: "flex",
+  alignItems: "center",
+  fontSize: "0.9em",
+  borderLeft: "1px solid #b1b4b6",
+  "&::after": {
+    content: '"N/A"',
+  },
+  "&.planning-permission::after": {
+    content: '"PLA"',
+  },
+  "&.works-to-trees--hedges::after": {
+    content: '"WTH"',
+  },
+  "&.community-infrastructure-levy::after": {
+    content: '"CIL"',
+  },
+  "&.listed-building-consent::after": {
+    content: '"LBC"',
+  },
+  "&.demolition-in-a-conservation-area::after": {
+    content: '"DCA"',
+  },
+  "&.planning-policy::after": {
+    content: '"POL"',
+  },
+}));
+
 const Option: React.FC<any> = (props) => {
   const childNodes = useStore((state) => state.childNodesOf(props.id));
 
@@ -14,12 +47,15 @@ const Option: React.FC<any> = (props) => {
 
   let background = "#F9F8F8"; // no flag color
   let color = "#000";
+  let category = "NA";
   try {
     const flag = flatFlags.find(({ value }) =>
       [props.data?.flag, props.data?.val].filter(Boolean).includes(value),
     );
     background = flag?.bgColor || background;
     color = flag?.color || color;
+    category = flag?.category.toLowerCase().replaceAll(" ", "-").replaceAll("&", "") || category;
+
   } catch (e) {}
 
   return (
@@ -27,8 +63,8 @@ const Option: React.FC<any> = (props) => {
       className={classNames("card", "option", { wasVisited: props.wasVisited })}
     >
       <Link href={href} prefetch={false} onClick={(e) => e.preventDefault()}>
-        <div className="band" style={{ background, color }}></div>
         <div className="text">{props.data.text}</div>
+        <Flag className={category} style={{ background, color }}></Flag>
       </Link>
       <ol className="decisions">
         {childNodes.map((child: any) => (
