ec0856b33ac1e8561be166ff5c2464cec724794c
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-04-14 17:12:40 +0200

    use accordions

--- editor.planx.uk/src/pages/PlatformAdminPanel.tsx
+++ editor.planx.uk/src/pages/PlatformAdminPanel.tsx
@@ -1,5 +1,8 @@
 import Close from "@mui/icons-material/Close";
 import Done from "@mui/icons-material/Done";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
@@ -8,14 +11,21 @@ import { SummaryListTable } from "@planx/components/shared/Preview/SummaryList";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 import { AdminPanelData } from "types";
+import Caret from "ui/icons/Caret";
 
-const StyledTeam = styled(Box, {
+const StyledTeamAccordion = styled(Accordion, {
   shouldForwardProp: (prop) => prop !== "primaryColour",
 })<{ primaryColour?: string }>(({ theme, primaryColour }) => ({
-  border: `10px solid ${theme.palette.divider}`,
   borderLeft: `10px solid ${primaryColour}`,
+  backgroundColor: theme.palette.background.paper,
+  width: "100%",
+  position: "relative",
   marginBottom: theme.spacing(2),
-  padding: theme.spacing(2),
+  padding: theme.spacing(1),
+  "&::after": {
+    position: "absolute",
+    width: "100%",
+  },
 }));
 
 function Component() {
@@ -25,9 +35,11 @@ function Component() {
     <Box p={3}>
       <Typography variant="h1">Platform Admin Panel</Typography>
       <Typography variant="body1" mb={3}>
-        {`This is an overview of each team's integrations and settings for the **${process.env.NODE_ENV}** environment`}
+        {`This is an overview of each team's integrations and settings for the `}
+        <strong>{process.env.NODE_ENV}</strong>
+        {` environment`}
       </Typography>
-      {adminPanelData?.map((team) => <TeamData data={team} />)}
+      {adminPanelData?.map((team) => <TeamData key={team.id} data={team} />)}
     </Box>
   );
 }
@@ -42,88 +54,113 @@ const Configured: React.FC = () => <Done color="success" fontSize="small" />;
 
 const TeamData: React.FC<TeamData> = ({ data }) => {
   return (
-    <StyledTeam primaryColour={data.primaryColour} key={data.id}>
-      <Typography variant="h2" mb={2}>
-        {data.name}
-      </Typography>
-      <Grid
-        container
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="flex-end"
-        spacing={3}
+    <StyledTeamAccordion primaryColour={data.primaryColour}>
+      <AccordionSummary
+        id={`${data.name}-header`}
+        aria-controls={`${data.name}-panel`}
+        expandIcon={<Caret />}
+        sx={{ pr: 1.5 }}
       >
-        <Grid item xs={4}>
-          <SummaryListTable dense={true}>
-            <>
-              <Box component="dt">{"Slug"}</Box>
-              <Box component="dd">
-                <code>
-                  {`/`}
-                  {data.slug}
-                </code>
-              </Box>
-            </>
-            <>
-              <Box component="dt">{"Reference code"}</Box>
-              <Box component="dd">
-                {data.referenceCode || <NotConfigured />}
-              </Box>
-            </>
-            <>
-              <Box component="dt">{"Homepage"}</Box>
-              <Box component="dd">{data.homepage || <NotConfigured />}</Box>
-            </>
-            <>
-              <Box component="dt">{"Subdomain"}</Box>
-              <Box component="dd">{data.subdomain || <NotConfigured />}</Box>
-            </>
-          </SummaryListTable>
+        <Typography variant="h2">{data.name}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          spacing={3}
+        >
+          <Grid item xs={4}>
+            <SummaryListTable dense={true}>
+              <>
+                <Box component="dt">{"Slug"}</Box>
+                <Box component="dd">
+                  <code>
+                    {`/`}
+                    {data.slug}
+                  </code>
+                </Box>
+              </>
+              <>
+                <Box component="dt">{"Homepage"}</Box>
+                <Box component="dd">{data.homepage || <NotConfigured />}</Box>
+              </>
+              <>
+                <Box component="dt">{"Logo"}</Box>
+                <Box component="dd">
+                  {data.logo ? <Configured /> : <NotConfigured />}
+                </Box>
+              </>
+              <>
+                <Box component="dt">{"Favicon"}</Box>
+                <Box component="dd">
+                  {data.favicon ? <Configured /> : <NotConfigured />}
+                </Box>
+              </>
+            </SummaryListTable>
+          </Grid>
+          <Grid item xs={4}>
+            <SummaryListTable dense={true}>
+              <>
+                <Box component="dt">{"Planning constraints"}</Box>
+                <Box component="dd">
+                  {data.planningDataEnabled ? (
+                    <Configured />
+                  ) : (
+                    <NotConfigured />
+                  )}
+                </Box>
+              </>
+              <>
+                <Box component="dt">{"Article 4s"}</Box>
+                <Box component="dd">{"?"}</Box>
+              </>
+              <>
+                <Box component="dt">{"Reference code"}</Box>
+                <Box component="dd">
+                  {data.referenceCode || <NotConfigured />}
+                </Box>
+              </>
+              <>
+                <Box component="dt">{"Subdomain"}</Box>
+                <Box component="dd">{data.subdomain || <NotConfigured />}</Box>
+              </>
+            </SummaryListTable>
+          </Grid>
+          <Grid item xs={4}>
+            <SummaryListTable dense={true}>
+              <>
+                <Box component="dt">{"GOV.UK Notify"}</Box>
+                <Box component="dd">
+                  {data.govnotifyPersonalisation?.helpEmail || (
+                    <NotConfigured />
+                  )}
+                </Box>
+              </>
+              <>
+                <Box component="dt">{"GOV.UK Pay"}</Box>
+                <Box component="dd">
+                  {data.govpayEnabled ? <Configured /> : <NotConfigured />}
+                </Box>
+              </>
+              <>
+                <Box component="dt">{"Send to email"}</Box>
+                <Box component="dd">
+                  {data.sendToEmailAddress || <NotConfigured />}
+                </Box>
+              </>
+              <>
+                <Box component="dt">{"BOPS"}</Box>
+                <Box component="dd">
+                  {data.bopsSubmissionURL ? <Configured /> : <NotConfigured />}
+                </Box>
+              </>
+            </SummaryListTable>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <SummaryListTable dense={true}>
-            <>
-              <Box component="dt">{"Planning constraints"}</Box>
-              <Box component="dd">
-                {data.planningDataEnabled ? <Configured /> : <NotConfigured />}
-              </Box>
-            </>
-            <>
-              <Box component="dt">{"Article 4s"}</Box>
-              <Box component="dd">{"?"}</Box>
-            </>
-            <>
-              <Box component="dt">{"GOV.UK Notify"}</Box>
-              <Box component="dd">
-                {data.govnotifyPersonalisation?.helpEmail || <NotConfigured />}
-              </Box>
-            </>
-            <>
-              <Box component="dt">{"GOV.UK Pay"}</Box>
-              <Box component="dd">
-                {data.govpayEnabled ? <Configured /> : <NotConfigured />}
-              </Box>
-            </>
-          </SummaryListTable>
-        </Grid>
-        <Grid item xs={4}>
-          <SummaryListTable dense={true}>
-            <>
-              <Box component="dt">{"Send to email"}</Box>
-              <Box component="dd">
-                {data.sendToEmailAddress || <NotConfigured />}
-              </Box>
-            </>
-            <>
-              <Box component="dt">{"BOPS"}</Box>
-              <Box component="dd">
-                {data.bopsSubmissionURL ? <Configured /> : <NotConfigured />}
-              </Box>
-            </>
-          </SummaryListTable>
-        </Grid>
-      </Grid>
-    </StyledTeam>
+      </AccordionDetails>
+    </StyledTeamAccordion>
   );
 };
 
