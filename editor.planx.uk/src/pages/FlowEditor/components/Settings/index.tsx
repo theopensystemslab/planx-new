bc6d9394d0108c6ac76e287b5b70a20c2d83b4f9
Ian Jones <hello@ianjo.uk>
2024-04-16 14:20:34 +0100

    fix: Ensure settings panels are not overlapped by elements with explicit z-index values

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/index.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/index.tsx
@@ -97,6 +97,8 @@ const Root = styled(Box)(({ theme }) => ({
   left: 0,
   right: 0,
   minHeight: `calc(100% - ${HEADER_HEIGHT}px)`,
+  // Ensure settings panels sit above editor content with explicit z-index set, will be redundent when we move to side-tabbed settings
+  zIndex: "100",
   [`& .${classes.tabs}`]: {
     backgroundColor: theme.palette.border.main,
   },
