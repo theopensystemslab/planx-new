5422caafe2e56741b5da5371fe9146d1141ca0a2
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-04-23 08:51:40 +0200

    fix migration syntax

--- hasura.planx.uk/migrations/1713793168052_alter_table_public_team_integrations_add_column_staging_file_api_key/up.sql
+++ hasura.planx.uk/migrations/1713793168052_alter_table_public_team_integrations_add_column_staging_file_api_key/up.sql
@@ -1,7 +1,7 @@
 alter table "public"."team_integrations" 
-add column "staging_file_api_key" text null
-add column "production_file_api_key" text null
-add column "power_automate_webhook_url" text null;
+add column staging_file_api_key text null,
+add column production_file_api_key text null,
+add column power_automate_webhook_url text null;
 
 comment on column "public"."team_integrations"."staging_file_api_key" is E'Secret allowing a council to download private S3 user files via the Planx API in the format <API_KEY>:<INITIALIZATION_VECTOR>';
 comment on column "public"."team_integrations"."production_file_api_key" is E'Secret allowing a council to download private S3 user files via the Planx API in the format <API_KEY>:<INITIALIZATION_VECTOR>';
