37c310cae63fa9ad0007d2f7bdc703fc49dd0bb1
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2024-05-17 10:57:44 +0100

    feat: Inline loading indicator for address lookup (#3136)

--- editor.planx.uk/src/components/DelayedLoadingIndicator.tsx
+++ editor.planx.uk/src/components/DelayedLoadingIndicator.tsx
@@ -4,17 +4,27 @@ import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import React, { useEffect, useState } from "react";
 
-const Root = styled(Box)(() => ({
+export interface Props {
+  inline?: boolean;
+}
+
+const Root = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "inline",
+})<Props>(({ inline, theme }) => ({
   padding: 60,
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+  ...(inline && {
+    padding: 0,
+  }),
 }));
 
 const DelayedLoadingIndicator: React.FC<{
   msDelayBeforeVisible?: number;
   text?: string;
-}> = ({ msDelayBeforeVisible = 0, text }) => {
+  inline?: boolean;
+}> = ({ msDelayBeforeVisible = 0, text, inline }) => {
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
@@ -30,8 +40,9 @@ const DelayedLoadingIndicator: React.FC<{
       aria-busy="true"
       aria-live="assertive"
       data-testid="delayed-loading-indicator"
+      inline={inline}
     >
-      <CircularProgress aria-label="Loading" />
+      <CircularProgress size={inline ? 30 : 40} aria-label="Loading" />
       <Typography variant="body2" sx={{ ml: "1rem" }}>
         {text ?? "Loading..."}
       </Typography>
