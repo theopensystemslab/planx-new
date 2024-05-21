c55541aaa4766ee356cf774d1395406a501f15c4
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-05-21 07:39:50 +0100

    fix: check if council has A4 schema set via GIS API and display in admin panel (#3174)

--- api.planx.uk/modules/gis/routes.ts
+++ api.planx.uk/modules/gis/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { locationSearch } from "./service";
+import { hasArticle4Schema } from "./service/article4Schema";
 import { classifiedRoadsSearch } from "./service/classifiedRoads";
 
 const router = Router();
 
 router.get("/gis/:localAuthority", locationSearch);
+router.get("/gis/:localAuthority/article4-schema", hasArticle4Schema);
 router.get("/roads", classifiedRoadsSearch);
 
 export default router;
---
+++ api.planx.uk/modules/gis/service/article4Schema.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+
+import { localAuthorityMetadata } from "./digitalLand";
+
+/**
+ * @swagger
+ * /gis/{localAuthority}/article4-schema:
+ *  get:
+ *    summary: Returns whether Article 4 schema variables are configured for this local authority
+ *    description: Returns whether Article 4 schema variables are configured for this local authority. A positive "status" from this endpoint is a proxy for marking this PlanX onboarding step "complete".
+ *    tags:
+ *      - gis
+ *    parameters:
+ *      - $ref: '#/components/parameters/localAuthority'
+ *        description: Name of the Local Authority, usually the same as Planx `team`. Required until Planning Data is available for any council o article 4 variables are generalised based on permitted development rights removed rather than council
+ */
+export async function hasArticle4Schema(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    if (!req.params.localAuthority) {
+      return next({
+        status: 401,
+        message: "Missing param local authority",
+      });
+    }
+
+    const status = Object.keys(localAuthorityMetadata).includes(
+      req.params.localAuthority,
+    );
+    res.status(200).send({
+      message: `${status ? "Found" : "Did not find"} Article 4 schema for ${req.params.localAuthority}`,
+      status: status,
+    });
+  } catch (error) {
+    next({
+      status: 500,
+      message: `Error getting Article 4 schema for ${req.params.localAuthority}: ${error}`,
+    });
+  }
+}
