dfbd9499409836400bb35ccc750e4078da330216
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-05-24 09:58:56 +0200

    wip disabled display

--- editor.planx.uk/src/@planx/components/PlanningConstraints/Editor.tsx
+++ editor.planx.uk/src/@planx/components/PlanningConstraints/Editor.tsx
@@ -1,7 +1,17 @@
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 import { ComponentType as TYPES } from "@opensystemslab/planx-core/types";
 import { EditorProps, ICONS, InternalNotes } from "@planx/components/ui";
 import { useFormik } from "formik";
 import React from "react";
+import { FONT_WEIGHT_BOLD } from "theme";
 import InputGroup from "ui/editor/InputGroup";
 import ModalSection from "ui/editor/ModalSection";
 import ModalSectionContent from "ui/editor/ModalSectionContent";
@@ -9,7 +19,7 @@ import RichTextInput from "ui/editor/RichTextInput";
 import Input from "ui/shared/Input";
 import InputRow from "ui/shared/InputRow";
 
-import { parseContent, PlanningConstraints } from "./model";
+import { availableDatasets, parseContent, PlanningConstraints } from "./model";
 
 type Props = EditorProps<TYPES.PlanningConstraints, PlanningConstraints>;
 
@@ -33,33 +43,111 @@ function PlanningConstraintsComponent(props: Props) {
           title="Planning constraints"
           Icon={ICONS[TYPES.PlanningConstraints]}
         >
-          <InputRow>
-            <Input
-              format="large"
-              name="title"
-              placeholder={formik.values.title}
-              value={formik.values.title}
-              onChange={formik.handleChange}
-            />
-          </InputRow>
-          <InputRow>
-            <RichTextInput
-              name="description"
-              placeholder="Description"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-            />
-          </InputRow>
-          <InputGroup label="Planning constraints data field">
+          <InputGroup>
+            <InputRow>
+              <Input
+                format="large"
+                name="title"
+                placeholder={formik.values.title}
+                value={formik.values.title}
+                onChange={formik.handleChange}
+              />
+            </InputRow>
+            <InputRow>
+              <RichTextInput
+                name="description"
+                placeholder="Description"
+                value={formik.values.description}
+                onChange={formik.handleChange}
+              />
+            </InputRow>
             <InputRow>
               <Input
                 format="data"
                 name="fn"
                 placeholder={formik.values.fn}
                 value={formik.values.fn}
-                onChange={formik.handleChange}
+                disabled
               />
             </InputRow>
+          </InputGroup>
+          <InputGroup label="Which constraints do you want to check?">
+            <TableContainer>
+              <Table size="small">
+                <TableHead>
+                  <TableRow>
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        color="primary"
+                        checked={true}
+                        disabled={true}
+                        inputProps={{
+                          "aria-label": "select all constraints",
+                        }}
+                      />
+                    </TableCell>
+                    <TableCell sx={{ fontWeight: FONT_WEIGHT_BOLD }}>
+                      Constraint
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {availableDatasets
+                    .sort((a, b) => a.key.localeCompare(b.key))
+                    .map((dataset, i: number) => (
+                      <TableRow key={i}>
+                        <TableCell padding="checkbox">
+                          <Checkbox
+                            color="primary"
+                            checked={true}
+                            disabled={true}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <Box
+                            sx={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                              alignItems: "center",
+                            }}
+                          >
+                            <Typography variant="body2">
+                              {dataset.name}
+                            </Typography>
+                            <Typography
+                              variant="body2"
+                              p={0.5}
+                              sx={{
+                                maxWidth: "fit-content",
+                                backgroundColor: "#f0f0f0",
+                                color: "#00000061",
+                                fontFamily: `"Source Code Pro", monospace;`,
+                              }}
+                            >
+                              {dataset.key}
+                            </Typography>
+                          </Box>
+                          <Box>
+                            <Typography
+                              variant="body2"
+                              color="GrayText"
+                              sx={{ fontSize: ".8em" }}
+                            >
+                              {`via ${dataset.source}: ${dataset.datasets
+                                .filter(Boolean)
+                                .join(", ")} ${
+                                dataset?.entity
+                                  ? `(entity ${dataset.entity})`
+                                  : ``
+                              }`}
+                            </Typography>
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </InputGroup>
           <InputGroup label="Planning conditions disclaimer">
             <InputRow>
--- editor.planx.uk/src/@planx/components/PlanningConstraints/model.ts
+++ editor.planx.uk/src/@planx/components/PlanningConstraints/model.ts
@@ -23,3 +23,135 @@ export type IntersectingConstraints = Record<string, string[]>;
 
 export const DEFAULT_PLANNING_CONDITIONS_DISCLAIMER =
   "<p><strong>This page does not include information about historic planning conditions that may apply to this property.</strong></p>";
+
+interface Dataset {
+  name: string;
+  key: string;
+  source: "Planning Data" | "Ordnance Survey";
+  datasets: string[];
+  entity?: number;
+}
+
+export const availableDatasets: Dataset[] = [
+  {
+    name: "Article 4s",
+    key: "article4",
+    source: "Planning Data",
+    datasets: ["article4-direction-area"],
+  },
+  {
+    name: "Central Activities Zone",
+    key: "article4.caz",
+    source: "Planning Data",
+    datasets: ["central-activities-zone"],
+  },
+  {
+    name: "Brownfield",
+    key: "brownfieldSite",
+    source: "Planning Data",
+    datasets: ["brownfield-land", "brownfield-site"],
+  },
+  {
+    name: "Area of Outstanding Natural Beauty (AONB)",
+    key: "designated.AONB",
+    source: "Planning Data",
+    datasets: ["area-of-outstanding-natural-beauty"],
+  },
+  {
+    name: "Conservation Area",
+    key: "designated.conservationArea",
+    source: "Planning Data",
+    datasets: ["conservation-area"],
+  },
+  {
+    name: "Green Belt",
+    key: "greenBelt",
+    source: "Planning Data",
+    datasets: ["green-belt"],
+  },
+  {
+    name: "National Park",
+    key: "designated.nationalPark",
+    source: "Planning Data",
+    datasets: ["national-park"],
+  },
+  {
+    name: "Broads",
+    key: "designated.nationalPark.broads",
+    source: "Planning Data",
+    datasets: ["national-park"],
+    entity: 520007,
+  },
+  {
+    name: "UNESCO World Heritage Site (WHS)",
+    key: "designated.WHS",
+    source: "Planning Data",
+    datasets: ["world-heritage-site", "world-heritage-site-buffer-zone"],
+  },
+  {
+    name: "Flood Risk Zones 2 & 3",
+    key: "flood",
+    source: "Planning Data",
+    datasets: ["flood-risk-zone"],
+  },
+  {
+    name: "Listed Building",
+    key: "listed",
+    source: "Planning Data",
+    datasets: ["listed-building", "listed-building-outline"],
+  },
+  {
+    name: "Scheduled Monument",
+    key: "monument",
+    source: "Planning Data",
+    datasets: ["scheduled-monument"],
+  },
+  {
+    name: "Ancient Semi-Natural Woodland (ASNW)",
+    key: "nature.ASNW",
+    source: "Planning Data",
+    datasets: ["ancient-woodland"],
+  },
+  {
+    name: "Ramsar Site",
+    key: "nature.ramsar",
+    source: "Planning Data",
+    datasets: ["ramsar"],
+  },
+  {
+    name: "Special Area of Conservation (SAC)",
+    key: "nature.SAC",
+    source: "Planning Data",
+    datasets: ["special-area-of-conservation"],
+  },
+  {
+    name: "Special Protection Area (SPA)",
+    key: "nature.SPA",
+    source: "Planning Data",
+    datasets: ["special-protection-area"],
+  },
+  {
+    name: "Site of Special Scientific Interest (SSSI)",
+    key: "nature.SSSI",
+    source: "Planning Data",
+    datasets: ["site-of-special-scientific-interest"],
+  },
+  {
+    name: "Historic Park or Garden",
+    key: "registeredPark",
+    source: "Planning Data",
+    datasets: ["park-and-garden"],
+  },
+  {
+    name: "Tree Preservation Order (TPO) or Zone",
+    key: "tpo",
+    source: "Planning Data",
+    datasets: ["tree", "tree-preservation-order", "tree-preservation-zone"],
+  },
+  {
+    name: "Classified Road",
+    key: "road.classified",
+    source: "Ordnance Survey",
+    datasets: ["OS Mastermap Highways"],
+  },
+];
