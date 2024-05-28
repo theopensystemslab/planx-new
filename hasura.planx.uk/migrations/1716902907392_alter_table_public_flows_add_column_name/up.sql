83ddc22d6389aa979edcc366ba9743d44666df39
Rory Doak <rorydoak@Rorys-MacBook-Pro.local>
2024-05-28 14:57:54 +0100

    add sql to populate name column with slug data

--- hasura.planx.uk/migrations/1716902907392_alter_table_public_flows_add_column_name/up.sql
+++ hasura.planx.uk/migrations/1716902907392_alter_table_public_flows_add_column_name/up.sql
@@ -1,3 +1,6 @@
 alter table "public"."flows" add column "name" text
  null;
  comment on column "public"."feedback"."node_data" is 'The name of the flow, entered by the user and used to generate the "slug"';
+
+UPDATE flows
+SET name = slug;
