499e31bd11c3d41c9fc786a44ca7a260da6b27ed
Rory Doak <rorydoak@Rorys-MacBook-Pro.local>
2024-05-29 14:32:45 +0100

    alter population script to format slug

--- hasura.planx.uk/migrations/1716902907392_alter_table_public_flows_add_column_name/up.sql
+++ hasura.planx.uk/migrations/1716902907392_alter_table_public_flows_add_column_name/up.sql
@@ -3,7 +3,7 @@ alter table "public"."flows" add column "name" text
  comment on column "public"."flows"."name" is 'The name of the flow, entered by the user and used to generate the "slug"';
 
 UPDATE flows
-SET name = replace(initcap(replace(slug,'-','xax')),'xax',' ')
+SET name = replace(concat(upper(substring(slug,1,1)),right(slug,-1)),'-',' ')
 WHERE name IS NULL
 
 
