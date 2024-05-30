13693b11e48d7832bb6a560243baf84363c128f4
Ian Jones <hello@ianjo.uk>
2024-05-30 11:16:36 +0100

    wip: Replace banners with snackbar

--- editor.planx.uk/src/components/Header.tsx
+++ editor.planx.uk/src/components/Header.tsx
@@ -3,6 +3,7 @@ import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import Person from "@mui/icons-material/Person";
 import Visibility from "@mui/icons-material/Visibility";
+import Alert from "@mui/material/Alert";
 import AppBar from "@mui/material/AppBar";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
@@ -15,6 +16,7 @@ import ListItemText from "@mui/material/ListItemText";
 import MenuItem from "@mui/material/MenuItem";
 import Paper from "@mui/material/Paper";
 import Popover, { popoverClasses } from "@mui/material/Popover";
+import Snackbar from "@mui/material/Snackbar";
 import { styled, Theme } from "@mui/material/styles";
 import MuiToolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -389,6 +391,11 @@ const PublicToolbar: React.FC<{
       <NavBar />
       <AnalyticsDisabledBanner />
       <TestEnvironmentBanner />
+      <Snackbar autoHideDuration={null}>
+        <Alert sx={{ width: "100%" }}>
+          Service settings updated successfully
+        </Alert>
+      </Snackbar>
       <ConfirmationDialog
         open={isDialogOpen}
         onClose={handleRestart}
--- editor.planx.uk/src/components/TestEnvironmentBanner.tsx
+++ editor.planx.uk/src/components/TestEnvironmentBanner.tsx
@@ -1,20 +1,7 @@
-import ReportIcon from "@mui/icons-material/Report";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import { styled } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
-import { visuallyHidden } from "@mui/utils";
+import Snackbar from "@mui/material/Snackbar";
 import { useStore } from "pages/FlowEditor/lib/store";
-import React from "react";
-
-const TestEnvironmentWarning = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-  color: theme.palette.text.primary,
-  justifyContent: "space-between",
-  alignItems: "center",
-  padding: "0.2em 0",
-}));
+import React, { useEffect, useState } from "react";
 
 const TestEnvironmentBanner: React.FC = () => {
   const [isTestEnvBannerVisible, hideTestEnvBanner] = useStore((state) => [
@@ -22,33 +9,39 @@ const TestEnvironmentBanner: React.FC = () => {
     state.hideTestEnvBanner,
   ]);
 
+  const [isSnackbarOpen, setSnackbarOpen] = useState(false);
+
+  useEffect(() => {
+    if (isTestEnvBannerVisible) {
+      setSnackbarOpen(true);
+    }
+  }, [isTestEnvBannerVisible]);
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+    hideTestEnvBanner();
+  };
+
   if (!isTestEnvBannerVisible) return null;
 
   return (
-    <TestEnvironmentWarning>
-      <Container
-        maxWidth={false}
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box display="flex" alignItems="center">
-          <ReportIcon color="error" />
-          <Typography variant="body2" ml={1}>
-            This is a <strong>testing environment</strong> for new features. Do
-            not use it to make permanent content changes.
-          </Typography>
-        </Box>
-        <Button size="small" onClick={hideTestEnvBanner}>
-          Hide
-          <Box sx={visuallyHidden} component="span">
-            the test environment banner
-          </Box>
+    <Snackbar
+      open={isSnackbarOpen}
+      autoHideDuration={null}
+      onClose={handleCloseSnackbar}
+      message="This is a testing environment for new features. Do
+      not use it to make permanent content changes."
+      sx={(theme) => ({ backgroundColor: theme.palette.info.main })}
+      action={
+        <Button
+          sx={{ color: "white" }}
+          size="small"
+          onClick={handleCloseSnackbar}
+        >
+          Close
         </Button>
-      </Container>
-    </TestEnvironmentWarning>
+      }
+    />
   );
 };
 
