cb67c77019455fb26c4e96f1862c9cca6a650f9e
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-05-31 09:57:46 +0100

    feat: show which planning constraints are checked in the editor (non-interactive) (#3191)

--- editor.planx.uk/src/@planx/components/PlanningConstraints/Editor.tsx
+++ editor.planx.uk/src/@planx/components/PlanningConstraints/Editor.tsx
@@ -1,7 +1,17 @@
+import Box from "@mui/material/Box";
+import Checkbox from "@mui/material/Checkbox";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 import { ComponentType as TYPES } from "@opensystemslab/planx-core/types";
 import { EditorProps, ICONS, InternalNotes } from "@planx/components/ui";
 import { useFormik } from "formik";
 import React from "react";
+import { FONT_WEIGHT_BOLD } from "theme";
 import InputGroup from "ui/editor/InputGroup";
 import ModalSection from "ui/editor/ModalSection";
 import ModalSectionContent from "ui/editor/ModalSectionContent";
@@ -9,7 +19,7 @@ import RichTextInput from "ui/editor/RichTextInput";
 import Input from "ui/shared/Input";
 import InputRow from "ui/shared/InputRow";
 
-import { parseContent, PlanningConstraints } from "./model";
+import { availableDatasets, parseContent, PlanningConstraints } from "./model";
 
 type Props = EditorProps<TYPES.PlanningConstraints, PlanningConstraints>;
 
@@ -33,33 +43,116 @@ function PlanningConstraintsComponent(props: Props) {
           title="Planning constraints"
           Icon={ICONS[TYPES.PlanningConstraints]}
         >
-          <InputRow>
-            <Input
-              format="large"
-              name="title"
-              placeholder={formik.values.title}
-              value={formik.values.title}
-              onChange={formik.handleChange}
-            />
-          </InputRow>
-          <InputRow>
-            <RichTextInput
-              name="description"
-              placeholder="Description"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-            />
-          </InputRow>
-          <InputGroup label="Planning constraints data field">
+          <Typography variant="body2" sx={{ mb: 2 }}>
+            Find documentation about{" "}
+            <a
+              href="https://opensystemslab.notion.site/How-Planning-constraints-work-06b64035fe6b40279387375ef3de49e4?pvs=4"
+              target="_blank"
+            >
+              how constraints work in Plan✕ here
+            </a>
+            .
+          </Typography>
+          <InputGroup>
+            <InputRow>
+              <Input
+                format="large"
+                name="title"
+                placeholder={formik.values.title}
+                value={formik.values.title}
+                onChange={formik.handleChange}
+              />
+            </InputRow>
+            <InputRow>
+              <RichTextInput
+                name="description"
+                placeholder="Description"
+                value={formik.values.description}
+                onChange={formik.handleChange}
+              />
+            </InputRow>
             <InputRow>
               <Input
                 format="data"
                 name="fn"
                 placeholder={formik.values.fn}
                 value={formik.values.fn}
-                onChange={formik.handleChange}
+                disabled
               />
             </InputRow>
+          </InputGroup>
+          <InputGroup label="Which constraints do you want to check?">
+            <TableContainer>
+              <Table size="small">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <Checkbox
+                        color="primary"
+                        checked={true}
+                        disabled={true}
+                        inputProps={{
+                          "aria-label": "select all constraints",
+                        }}
+                      />
+                    </TableCell>
+                    <TableCell sx={{ fontWeight: FONT_WEIGHT_BOLD }}>
+                      Constraints
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {availableDatasets
+                    .sort((a, b) => a.val.localeCompare(b.val))
+                    .map((dataset, i: number) => (
+                      <TableRow key={i}>
+                        <TableCell sx={{ verticalAlign: "top" }}>
+                          <Checkbox
+                            color="primary"
+                            checked={true}
+                            disabled={true}
+                          />
+                        </TableCell>
+                        <TableCell>
+                          <InputGroup>
+                            <InputRow>
+                              <Input
+                                format="large"
+                                name="text"
+                                value={dataset.text}
+                                disabled
+                              />
+                            </InputRow>
+                            <InputRow>
+                              <Input
+                                format="data"
+                                name="val"
+                                value={dataset.val}
+                                disabled
+                              />
+                            </InputRow>
+                          </InputGroup>
+                          <Box>
+                            <Typography
+                              variant="body2"
+                              color="GrayText"
+                              sx={{ fontSize: ".8em", pb: 1 }}
+                            >
+                              {`via ${dataset.source}: ${dataset.datasets
+                                .filter(Boolean)
+                                .join(", ")} ${
+                                dataset?.entity
+                                  ? `(entity ${dataset.entity})`
+                                  : ``
+                              }`}
+                            </Typography>
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </InputGroup>
           <InputGroup label="Planning conditions disclaimer">
             <InputRow>
