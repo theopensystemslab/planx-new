e6153d6d6a660754e8d54df7d6321532e9810be9
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-05-31 09:52:04 +0100

    feat: Replace userData in state with formik.userData

--- editor.planx.uk/src/@planx/components/List/Public/Context.tsx
+++ editor.planx.uk/src/@planx/components/List/Public/Context.tsx
@@ -1,16 +1,23 @@
+import { FormikProps, useFormik } from "formik";
 import React, { createContext, ReactNode, useContext, useState } from "react";
 
-import { generateNewItem, Schema, UserData } from "../model";
+import {
+  generateInitialValues,
+  generateValidationSchema,
+  Schema,
+  UserData,
+} from "../model";
 
 interface ListContextValue {
   schema: Schema;
-  activeIndex: number | undefined;
-  userData: UserData;
+  activeIndex: number;
   addNewItem: () => void;
-  saveItem: (index: number, updatedItem: UserData[0]) => void;
+  saveItem: () => Promise<void>;
   removeItem: (index: number) => void;
   editItem: (index: number) => void;
   cancelEditItem: () => void;
+  formik: FormikProps<UserData>;
+  handleSubmit: () => void;
 }
 
 interface ListProviderProps {
@@ -24,50 +31,87 @@ export const ListProvider: React.FC<ListProviderProps> = ({
   children,
   schema,
 }) => {
-  const [activeIndex, setActiveIndex] = useState<number | undefined>(0);
-  const [userData, setUserData] = useState<UserData>(
-    schema.min === 0 ? [] : [generateNewItem(schema)],
-  );
+  const [activeIndex, setActiveIndex] = useState<number>(0);
 
-  const addNewItem = () => {
-    setUserData([...userData, generateNewItem(schema)]);
-    setActiveIndex((prev) => (prev === undefined ? 0 : prev + 1));
+  const setAllFieldsTouched = () => {
+    Object.keys(formik.values.userData[activeIndex]).forEach((key) => {
+      formik.setFieldTouched(formik.values.userData[activeIndex][key], true);
+    });
   };
 
-  const saveItem = (index: number, updatedItem: UserData[0]) => {
-    setUserData((prev) =>
-      prev.map((item, i) => (i === index ? updatedItem : item)),
-    );
+  const addNewItem = async () => {
+    // Do not allow a new item to be added if there's still an active item
+    // TODO: show error to explain this
+    if (activeIndex !== -1) return;
+    await formik.validateForm();
+    if (!formik.isValid) {
+      console.log({ errors: formik.errors });
+    }
+    // Add new item, and set to active
+    formik.values.userData.push(generateInitialValues(schema));
+    setActiveIndex(formik.values.userData.length - 1);
   };
 
-  const editItem = (index: number) => setActiveIndex(index);
+  const saveItem = async () => {
+    // setAllFieldsTouched(activeIndex);
+    await formik.validateForm();
+    if (formik.isValid) {
+      setActiveIndex(-1);
+    }
+  };
 
   const removeItem = (index: number) => {
     if (activeIndex && index < activeIndex) {
       // If item is before currently active card, retain active card
-      setActiveIndex((prev) => (prev === undefined ? 0 : prev - 1));
+      setActiveIndex((prev) => (prev === -1 ? 0 : prev - 1));
     } else if (index === activeIndex || index === 0) {
       // If item is currently in Edit mode, exit Edit mode
       cancelEditItem();
     }
 
     // Remove item from userData
-    setUserData((prev) => prev.filter((_, i) => i !== index));
+    formik.setFieldValue(
+      "userData",
+      formik.values.userData.filter((_, i) => i !== index),
+    );
   };
 
-  const cancelEditItem = () => setActiveIndex(undefined);
+  const handleSubmit = () => {
+    if (activeIndex !== -1) {
+      // TODO: Display error for this
+      console.log("Please save all list items to proceed");
+      return;
+    }
+
+    formik.handleSubmit();
+  };
+
+  const cancelEditItem = () => setActiveIndex(-1);
+  const editItem = (index: number) => setActiveIndex(index);
+
+  const formik = useFormik<UserData>({
+    initialValues: { userData: [generateInitialValues(schema)] },
+    onSubmit: (values) => {
+      console.log("Submit!");
+      console.log({ values });
+    },
+    validateOnBlur: false,
+    validateOnChange: false,
+    validationSchema: generateValidationSchema(schema),
+  });
 
   return (
     <ListContext.Provider
       value={{
         activeIndex,
-        userData,
         addNewItem,
         saveItem,
         schema,
         editItem,
         removeItem,
         cancelEditItem,
+        formik,
+        handleSubmit,
       }}
     >
       {children}
