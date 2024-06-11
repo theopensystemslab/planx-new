f76e2278262bd7db8c7ab57fcc4306f7b26601e8
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2024-06-11 13:50:50 +0100

    Create BuildingDetails.ts

---
+++ editor.planx.uk/src/@planx/components/List/schemas/GLA/BuildingDetails.ts
@@ -0,0 +1,34 @@
+import { Schema } from "@planx/components/List/model";
+
+export const BuildingDetailsGLA: Schema = {
+  type: "Building detail",
+  fields: [
+    {
+      type: "text",
+      data: {
+        title: "Building reference",
+        fn: "reference",
+        type: TextInputType.Short,
+        ],
+      },
+    },
+    {
+      type: "number",
+      data: {
+        title: "What is the maximum height of the building?",
+        units: "m",
+        fn: "height",
+        allowNegatives: false,
+      },
+    },
+    {
+      type: "number",
+      data: {
+        title: "How many storeys does the building have?",
+        fn: "storeys",
+        allowNegatives: false,
+      },
+    },
+  ],
+  min: 1,
+} as const;
