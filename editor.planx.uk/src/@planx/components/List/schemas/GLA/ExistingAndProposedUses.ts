596e1c0a2e105b6819f75ef19bdd180fb325aee2
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2024-06-11 11:32:05 +0100

    Create ExistingAndProposedUses.ts

---
+++ editor.planx.uk/src/@planx/components/List/schemas/GLA/ExistingAndProposedUses.ts
@@ -0,0 +1,53 @@
+import { Schema } from "@planx/components/List/model";
+
+export const ExistingAndProposedUsesGLA: Schema = {
+  type: "Existing and proposed uses",
+  fields: [
+    {
+      type: "question",
+      data: {
+        title: "What is the use class?",
+        fn: "useClass",
+        options: [
+          { id: "bTwo", data: { text: "B2 - General industry", val: "bTwo" } },
+          { id: "bEight", data: { text: "B8 - Storage and distribution", val: "bEight" } },
+          { id: "cOne", data: { text: "C1 - Hotels", val: "cOne" } },
+          { id: "cTwo", data: { text: "C2 - Residential institutions", val: "cTwo" } },
+          { id: "cTwoA", data: { text: "C2a - Secure residential institutions", val: "cTwoA" } },
+          { id: "cThree", data: { text: "C3 - Dwellinghouses", val: "cThree" } },
+          { id: "cFour", data: { text: "C4 - Houses in multiple occupation", val: "cFour" } },
+          { id: "e", data: { text: "E - Commercial, business and service", val: "e" } },
+          { id: "fOne", data: { text: "Learning and non-residential institutions", val: "fOne" } },
+          { id: "fTwo", data: { text: "Local community uses", val: "fTwo" } },
+          { id: "SG", data: { text: "Sui generis", val: "SG" } },
+          { id: "unknown", data: { text: "Unknown", val: "unknown" } },
+        ],
+      },
+    },
+    {
+      type: "number",
+      data: {
+        title: "What is the existing gross internal floor area (in square metres)?",
+        fn: "GIAExisting",
+        allowNegatives: false,
+      },
+    },
+    {
+      type: "number",
+      data: {
+        title: "What is the gross internal floor area lost (in square metres)?",
+        fn: "GIALost",
+        allowNegatives: false,
+      },
+    },
+    {
+      type: "number",
+      data: {
+        title: "What is the gross internal floor area gained (in square metres)?",
+        fn: "GIAGained",
+        allowNegatives: false,
+      },
+    },
+  ],
+  min: 1,
+} as const;
