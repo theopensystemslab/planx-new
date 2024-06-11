0d0bc43c676ed99339f3e769c1f342f1a069374c
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2024-06-11 10:50:15 +0100

    refactor: Standardise error summary (#3259)

--- editor.planx.uk/src/pages/FlowEditor/components/Flow/FeedbackPage.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Flow/FeedbackPage.tsx
@@ -25,6 +25,7 @@ import { client } from "lib/graphql";
 import React, { useState } from "react";
 import { Feedback } from "routes/feedback";
 import EditorRow from "ui/editor/EditorRow";
+import ErrorSummary from "ui/shared/ErrorSummary";
 import ReactMarkdownOrHtml from "ui/shared/ReactMarkdownOrHtml";
 
 interface Props {
@@ -164,35 +165,43 @@ export const FeedbackPage: React.FC<Props> = ({ feedback }) => {
           </Typography>
         </EditorRow>
         <EditorRow>
-          <Feed>
-            <Table stickyHeader sx={{ tableLayout: "fixed" }}>
-              <TableHead>
-                <TableRow
-                  sx={{ "& > *": { borderBottomColor: "black !important" } }}
-                >
-                  <TableCell sx={{ width: 160 }}>
-                    <strong>Type</strong>
-                  </TableCell>
-                  <TableCell sx={{ width: 100 }}>
-                    <strong>Date</strong>
-                  </TableCell>
-                  <TableCell sx={{ width: 340 }}>
-                    <strong>Comment</strong>
-                  </TableCell>
-                  <TableCell sx={{ width: 60 }}></TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {feedback.map((item) => (
-                  <CollapsibleRow
-                    key={item.id}
-                    {...item}
-                    displayFeedbackItems={displayFeedbackItems}
-                  />
-                ))}
-              </TableBody>
-            </Table>
-          </Feed>
+          {feedback.length === 0 ? (
+            <ErrorSummary
+              format="info"
+              heading="No feedback found for this service"
+              message="If you're looking for feedback from more than six months ago, please contact a PlanX developer"
+            />
+          ) : (
+            <Feed>
+              <Table stickyHeader sx={{ tableLayout: "fixed" }}>
+                <TableHead>
+                  <TableRow
+                    sx={{ "& > *": { borderBottomColor: "black !important" } }}
+                  >
+                    <TableCell sx={{ width: 160 }}>
+                      <strong>Type</strong>
+                    </TableCell>
+                    <TableCell sx={{ width: 100 }}>
+                      <strong>Date</strong>
+                    </TableCell>
+                    <TableCell sx={{ width: 340 }}>
+                      <strong>Comment</strong>
+                    </TableCell>
+                    <TableCell sx={{ width: 60 }}></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {feedback.map((item) => (
+                    <CollapsibleRow
+                      key={item.id}
+                      {...item}
+                      displayFeedbackItems={displayFeedbackItems}
+                    />
+                  ))}
+                </TableBody>
+              </Table>
+            </Feed>
+          )}
         </EditorRow>
       </Box>
     </Container>
