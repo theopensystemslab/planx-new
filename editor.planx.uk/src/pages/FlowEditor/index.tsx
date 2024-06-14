cfbd5d9956cf9d320006c4e1bf036125125e089d
Ian Jones <hello@ianjo.uk>
2024-06-14 10:06:13 +0100

    wip: Editor canvas dotted background

--- editor.planx.uk/src/pages/FlowEditor/index.tsx
+++ editor.planx.uk/src/pages/FlowEditor/index.tsx
@@ -4,17 +4,32 @@ import "./floweditor.scss";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 import React, { useRef } from "react";
+import DottedBackground from "ui/images/dotted-bg.svg";
 
 import { rootFlowPath } from "../../routes/utils";
 import Flow from "./components/Flow";
 import Sidebar from "./components/Sidebar";
 import { useStore } from "./lib/store";
 import useScrollControlsAndRememberPosition from "./lib/useScrollControlsAndRememberPosition";
+
 const EditorContainer = styled(Box)(() => ({
   display: "flex",
   alignItems: "stretch",
   overflow: "hidden",
   flexGrow: 1,
+  position: "relative",
+}));
+
+const EditorBackground = styled(Box)(() => ({
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+  backgroundImage: `url('${DottedBackground}')`,
+  backgroundRepeat: "repeat",
+  backgroundSize: "16px",
+  zIndex: -1,
 }));
 
 const FlowEditor: React.FC<any> = ({ flow, breadcrumbs }) => {
@@ -30,10 +45,14 @@ const FlowEditor: React.FC<any> = ({ flow, breadcrumbs }) => {
           flexDirection: "column",
           width: "100%",
           overflowX: "auto",
+          position: "relative",
         }}
       >
         <Box id="editor" ref={scrollContainerRef} sx={{ position: "relative" }}>
-          <Flow flow={flow} breadcrumbs={breadcrumbs} />
+          <Box sx={{ position: "relative", width: "auto", height: "auto" }}>
+            <EditorBackground />
+            <Flow id="flow" flow={flow} breadcrumbs={breadcrumbs} />
+          </Box>
         </Box>
       </Box>
       {showSidebar && (
