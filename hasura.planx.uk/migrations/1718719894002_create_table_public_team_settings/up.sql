ac99e89d91bb6709d4abfc64526b4c48f04ac95b
Rory Doak <rory@opensystemslab.io>
2024-06-19 09:45:28 +0100

    add column comments to create table migration

--- hasura.planx.uk/migrations/1718719894002_create_table_public_team_settings/up.sql
+++ hasura.planx.uk/migrations/1718719894002_create_table_public_team_settings/up.sql
@@ -14,3 +14,12 @@ PRIMARY KEY ("id") ,
 FOREIGN KEY ("team_id") REFERENCES "public"."teams"("id") ON UPDATE cascade ON DELETE cascade, 
 UNIQUE ("id"), UNIQUE ("team_id"), UNIQUE ("reference_code"));
 COMMENT ON TABLE "public"."team_settings" IS E'Global settings for boundary and contact details';
+
+comment on column "public"."team_settings"."reference_code" is E'Team name in three letter short form';
+comment on column "public"."team_settings"."team_id" is E'Linked ID to Teams table';
+comment on column "public"."team_settings"."help_phone" is E'For use in gov notify emails';
+comment on column "public"."team_settings"."help_email" is E'For use in gov notify emails';
+comment on column "public"."team_settings"."help_opening_hours" is E'For use in gov notify emails';
+comment on column "public"."team_settings"."email_reply_to_id" is E'Generate by gov notify and relates to the "reply to" address in notifications';
+comment on column "public"."team_settings"."boundary_url" is E'User entered boundary linked to https://www.planning.data.gov.uk/';
+comment on column "public"."team_settings"."boundary_json" is E'Long form boundary geojson - used to compute boundary__bbox';
