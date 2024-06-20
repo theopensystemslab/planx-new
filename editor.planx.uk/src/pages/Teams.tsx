c533d9e4149a3b7603510de77b33ba84bca61578
Ian Jones <hello@ianjo.uk>
2024-06-20 13:03:25 +0100

    feat: Updated editor teams and team selection pages

--- editor.planx.uk/src/pages/Teams.tsx
+++ editor.planx.uk/src/pages/Teams.tsx
@@ -1,71 +1,102 @@
-import Edit from "@mui/icons-material/Edit";
-import Visibility from "@mui/icons-material/Visibility";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
+import Container from "@mui/material/Container";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { Team } from "@opensystemslab/planx-core/types";
 import React from "react";
 import { Link } from "react-navi";
+import Dashboard from "ui/editor/Dashboard";
 
 import { useStore } from "./FlowEditor/lib/store";
 
-interface Props {
-  teams: Array<Team>;
+interface TeamTheme {
+  slug: string;
+  primaryColour: string;
 }
 
-const Root = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.background.dark,
-  color: "#fff",
-  width: "100%",
-  flex: 1,
-  justifyContent: "flex-start",
-  alignItems: "center",
-}));
+interface Props {
+  teams: Array<Team>;
+  teamTheme: Array<TeamTheme>;
+}
 
-const Dashboard = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.background.dark,
-  color: "#fff",
+export const Root = styled(Box)(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
   width: "100%",
-  maxWidth: 600,
-  margin: "auto",
-  padding: theme.spacing(8, 0, 4, 0),
+  display: "flex",
+  alignItems: "flex-start",
+  flexGrow: 1,
 }));
 
 const StyledLink = styled(Link)(() => ({
   textDecoration: "none",
 }));
 
-const Teams: React.FC<Props> = ({ teams }) => {
+const TeamCard = styled(Card)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  marginBottom: theme.spacing(2),
+  color: theme.palette.text.primary,
+  outline: `1px solid ${theme.palette.border.light}`,
+  outlineOffset: "-1px",
+  borderRadius: "1px",
+}));
+
+const TeamColourBand = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignSelf: "stretch",
+  width: theme.spacing(1.5),
+  zIndex: 1,
+}));
+
+const Teams: React.FC<Props> = ({ teams, teamTheme }) => {
+  const canUserEditTeam = useStore.getState().canUserEditTeam;
+
+  const editableTeams = teams.filter((team) => canUserEditTeam(team.slug));
+  const viewOnlyTeams = teams.filter((team) => !canUserEditTeam(team.slug));
+
+  const renderTeams = (teamsToRender: Array<Team>) =>
+    teamsToRender.map(({ name, slug }) => {
+      const theme = teamTheme.find((t) => t.slug === slug);
+      const primaryColour = theme ? theme.primaryColour : "#000";
+
+      return (
+        <StyledLink href={`/${slug}`} key={slug} prefetch={false}>
+          <TeamCard>
+            <TeamColourBand bgcolor={primaryColour} />
+            <Typography p={2} variant="h3" component="h2">
+              {name}
+            </Typography>
+          </TeamCard>
+        </StyledLink>
+      );
+    });
   return (
     <Root>
       <Dashboard>
-        <Box pl={2} pb={2}>
-          <Typography variant="h2" component="h1" gutterBottom>
+        <Container maxWidth="formWrap">
+          <Typography variant="h2" component="h1" mb={4}>
             Select a team
           </Typography>
-        </Box>
-        {teams.map(({ name, slug }) => (
-          <StyledLink href={`/${slug}`} key={slug} prefetch={false}>
-            <Box
-              mb={2.5}
-              px={2.5}
-              py={3}
-              mx={2}
-              component={Card}
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <Typography variant="h4" component="h2">
-                {name}
+          {editableTeams.length > 0 && (
+            <>
+              <Typography variant="h3" component="h2" mb={2}>
+                My teams
               </Typography>
-              {useStore.getState().canUserEditTeam(slug) ? (
-                <Edit />
-              ) : (
-                <Visibility />
-              )}
-            </Box>
-          </StyledLink>
-        ))}
+              {renderTeams(editableTeams)}
+            </>
+          )}
+
+          {viewOnlyTeams.length > 0 && (
+            <>
+              <Typography variant="h3" component="h2" mt={4} mb={2}>
+                Other teams (view only)
+              </Typography>
+              {renderTeams(viewOnlyTeams)}
+            </>
+          )}
+        </Container>
       </Dashboard>
     </Root>
   );
