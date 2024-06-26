7481245640624366fb8769869fbd1a06ea1cba0f
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-06-26 20:26:48 +0100

    chore: Log full error when building digital planning application [skip pizza]

--- api.planx.uk/modules/admin/session/digitalPlanningData.ts
+++ api.planx.uk/modules/admin/session/digitalPlanningData.ts
@@ -40,9 +40,8 @@ export const getDigitalPlanningApplicationPayload = async (
     return res.send(data);
   } catch (error) {
     return next({
-      message:
-        "Failed to make Digital Planning Application payload: " +
-        (error as Error).message,
+      message: `Failed to make Digital Planning Application payload:
+        ${JSON.stringify(error as Error, null, 2)}.`,
     });
   }
 };
