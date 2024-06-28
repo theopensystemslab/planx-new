470df49a4baa9bd0cd78b298baf9327be2b8ffee
Ian Jones <hello@ianjo.uk>
2024-06-28 15:17:59 +0100

    feat: Fix editor layouts

--- editor.planx.uk/src/pages/layout/FlowEditorLayout.tsx
+++ editor.planx.uk/src/pages/layout/FlowEditorLayout.tsx
@@ -1,22 +1,14 @@
-import Box from "@mui/material/Box";
-import { styled } from "@mui/material/styles";
 import EditorNavMenu, { flowLayoutRoutes } from "components/EditorNavMenu";
 import ErrorFallback from "components/ErrorFallback";
 import React, { PropsWithChildren } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-
-const Root = styled(Box)(() => ({
-  display: "flex",
-  alignItems: "stretch",
-  overflow: "hidden",
-  flexGrow: 1,
-}));
+import Dashboard from "ui/editor/Dashboard";
 
 const FlowEditorLayout: React.FC<PropsWithChildren> = ({ children }) => (
-  <Root>
+  <Dashboard>
     <EditorNavMenu compact routes={flowLayoutRoutes} />
     <ErrorBoundary FallbackComponent={ErrorFallback}>{children}</ErrorBoundary>
-  </Root>
+  </Dashboard>
 );
 
 export default FlowEditorLayout;
