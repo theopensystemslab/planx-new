217f23c3ac276c7c32fbc14b4f7455764e7e404d
Dan Goss <freemvmt@posteo.net>
2024-07-18 20:14:10 +0100

    enable jest override to run tests in shell with env loaded

--- api.planx.uk/jest.setup.js
+++ api.planx.uk/jest.setup.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import { queryMock } from "./tests/graphqlQueryMock";
 
-dotenv.config({ path: "./.env.test" });
+dotenv.config({ override: true, path: "./.env.test" });
 
 beforeEach(() => {
   queryMock.setup(process.env.HASURA_GRAPHQL_URL);
