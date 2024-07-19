5a1906464c0ce5248f04848ec9a91d0559e0d443
Rory Doak <rory@opensystemslab.io>
2024-07-19 11:37:02 +0100

    alter hasura testing in line with permissions changes

--- hasura.planx.uk/tests/team_themes.test.js
+++ hasura.planx.uk/tests/team_themes.test.js
@@ -37,8 +37,8 @@ describe("team_themes", () => {
       expect(i.queries).toContain("team_themes");
     });
 
-    test("cannot insert team_themes", () => {
-      expect(i.queries).not.toContain("insert_team_themes");
+    test("can insert team_themes", () => {
+      expect(i.queries).toContain("insert_team_themes");
     });
 
     test("can query team_themes", async () => {
