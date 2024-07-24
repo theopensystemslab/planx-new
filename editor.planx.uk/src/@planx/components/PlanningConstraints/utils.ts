4472c30bcc127e4339ce42925fb02da81342949d
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-07-24 17:47:33 +0200

    add override logic tests and mock data

--- editor.planx.uk/src/@planx/components/PlanningConstraints/utils.ts
+++ editor.planx.uk/src/@planx/components/PlanningConstraints/utils.ts
@@ -10,25 +10,28 @@ export function handleOverrides(
   nots: IntersectingConstraints,
 ): { intersectingConstraints: IntersectingConstraints, nots: IntersectingConstraints } {
   if (inaccurateConstraints) {
+    // If a previously marked inaccurate constraint key isn't in the latest GIS response, just skip it
     Object.entries(inaccurateConstraints).forEach(([inaccurateKey, inaccurateConstraint]) => {
       // Check if the whole constraint category (including any of its granular children) is now inapplicable
       const allEntitiesInaccurate = inaccurateConstraint.entities.length === constraints[inaccurateKey]["data"].length;
       if (allEntitiesInaccurate) {
-        intersectingConstraints[fn] = intersectingConstraints[fn].filter((intersectingKey) => !intersectingKey.startsWith(inaccurateKey));
-        nots[fn].push(inaccurateKey);
+        const inaccurateGranularKeys = intersectingConstraints[fn].filter((intersectingKey) => intersectingKey.startsWith(inaccurateKey));
+        inaccurateGranularKeys.forEach((inaccurateGranularKey) => {
+          nots[fn].push(inaccurateGranularKey);
+        });
+        intersectingConstraints[fn] = intersectingConstraints[fn].filter((intersectingKey) => !inaccurateGranularKeys.includes(intersectingKey));
       }
 
       // If less than all listed buildings or flood zones have been marked as inaccurate, ensure their granular children variables are correct
       if (!allEntitiesInaccurate && ["listed", "flood"].includes(inaccurateKey)) {
         // For each entity in this category marked as inaccurate, determine it's granular key
-        const granularInaccurateKeys: string[] = [];
+        const inaccurateGranularKeys: string[] = [];
         inaccurateConstraint["entities"].forEach((entityId) => {
           const inaccurateEntity = constraints[inaccurateKey]["data"].find((d: any) => d?.entity === parseInt(entityId));
-
           if (inaccurateEntity["listed-building-grade"]) {
-            granularInaccurateKeys.push(`listed.grade.${inaccurateEntity["listed-building-grade"]}`);
+            inaccurateGranularKeys.push(`listed.grade.${inaccurateEntity["listed-building-grade"]}`);
           } else if (inaccurateEntity["flood-risk-level"]) {
-            granularInaccurateKeys.push(`flood.zone.${inaccurateEntity["flood-risk-level"]}`);
+            inaccurateGranularKeys.push(`flood.zone.${inaccurateEntity["flood-risk-level"]}`);
           }
         });
 
@@ -44,19 +47,17 @@ export function handleOverrides(
         });
 
         // Remove any inaccurate keys that do NOT still apply to any remaining constraints
-        const granularKeysToRemove = granularInaccurateKeys.filter((k) => !granularAccurateKeys.includes(k));
-        if (granularKeysToRemove.length > 0) {
-          granularKeysToRemove.forEach((k) => {
-            intersectingConstraints[fn] = intersectingConstraints[fn].filter((intersectingKey) => intersectingKey !== k);
-            nots[fn].push(k);
-          });
-        }
+        const granularKeysToRemove = inaccurateGranularKeys.filter((k) => !granularAccurateKeys.includes(k));
+        granularKeysToRemove?.forEach((k) => {
+          intersectingConstraints[fn] = intersectingConstraints[fn].filter((intersectingKey) => intersectingKey !== k);
+          nots[fn].push(k);
+        });
       }
     });
 
-    // Ensure designated land variable still has at least one applicable granular child, else remove it
-    const orphanedDesignatedKey = intersectingConstraints[fn].includes("designated") && intersectingConstraints[fn].filter((intersectingKey) => intersectingKey.startsWith("designated.")).length === 0;
-    if (orphanedDesignatedKey) {
+    // Ensure "designated" land variable still has at least one applicable granular child, else remove it
+    const hasOrphanedDesignatedKey = intersectingConstraints[fn].includes("designated") && intersectingConstraints[fn].filter((intersectingKey) => intersectingKey.startsWith("designated.")).length === 0;
+    if (hasOrphanedDesignatedKey) {
       intersectingConstraints[fn] = intersectingConstraints[fn].filter((intersectingKey) => intersectingKey !== "designated");
       nots[fn].push("designated");
     }
