5cae484538ce6455087dfe153569bf6eb934f23b
Dan Goss <freemvmt@posteo.net>
2024-07-26 16:16:25 +0100

    [api] adapt configs for ESM, run tests with --experimental-vm-modules enabled, bump ts-jest

--- api.planx.uk/jest.config.ts
+++ api.planx.uk/jest.config.ts
@@ -1,16 +1,30 @@
-export default {
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  // we don't use a preset, preferring to give an explicit manual config
   testEnvironment: "node",
-  preset: "ts-jest",
   transform: {
-    "^.+\\.js$": [
-      "esbuild-jest",
+    // esbuild-jest is unmaintained and can't handle TypeScript with ESM, so we stick to ts-jest
+    // TODO: if tests are slow, consider swapping out for @swc/jest
+    "^.+\\.[jt]s$": [
+      "ts-jest",
       {
-        sourcemap: true,
+        useESM: true,
+        target: "esnext",
+        // we need a separate moduleResolutuion for tests (jest v30 may fix this)
+        tsconfig: "tsconfig.test.json",
       },
     ],
   },
   testPathIgnorePatterns: ["dist/*"],
   setupFilesAfterEnv: ["./jest.setup.js"],
+  // handle .ts files first, as ESM modules, and remove .js from imports for jest
+  moduleFileExtensions: ["ts", "js", "json"],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  // set up coverage collection
   collectCoverage: true,
   coverageThreshold: {
     global: {
@@ -20,3 +34,5 @@ export default {
   coverageReporters: ["lcov", "text-summary"],
   coverageDirectory: "./coverage",
 };
+
+export default config;
