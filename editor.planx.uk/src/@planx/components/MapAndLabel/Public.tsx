85ba7cd44fc0866a5f02030680da08aa1a140fde
Rory Doak <rory@opensystemslab.io>
2024-07-29 12:15:47 +0100

    refine logic in my-map clipGeojsonData prop and remove default geoJSON

--- editor.planx.uk/src/@planx/components/MapAndLabel/Public.tsx
+++ editor.planx.uk/src/@planx/components/MapAndLabel/Public.tsx
@@ -12,24 +12,6 @@ type Props = PublicProps<MapAndLabel>;
 
 function MapAndLabelComponent(props: Props) {
   const teamSettings = useStore.getState().teamSettings;
-
-  const defaultBboxJSON = {
-    type: "Feature",
-    geometry: {
-      type: "Polygon",
-      coordinates: [
-        [
-          [1.9134116, 49.528423],
-          [1.9134116, 61.331151],
-          [1.9134116, 61.331151],
-          [-10.76418, 61.331151],
-          [-10.76418, 49.528423],
-        ],
-      ],
-    },
-    properties: {},
-  };
-
   return (
     <Card handleSubmit={props.handleSubmit} isValid>
       <CardHeader
@@ -49,9 +31,8 @@ function MapAndLabelComponent(props: Props) {
           drawPointColor={props.drawColor}
           drawType={props.drawType}
           clipGeojsonData={
-            teamSettings !== undefined
-              ? JSON.stringify(teamSettings.boundaryBBox)
-              : JSON.stringify(defaultBboxJSON)
+            teamSettings?.boundaryBBox &&
+            JSON.stringify(teamSettings?.boundaryBBox)
           }
         />
       </MapContainer>
