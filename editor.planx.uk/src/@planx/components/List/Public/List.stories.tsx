22253ddbf65945a30e8aa2ad4fe656a56d6bbd9c
Rory Doak <rory@opensystemslab.io>
2024-08-13 16:56:23 +0100

    refactor schema selector

--- editor.planx.uk/src/@planx/components/List/Public/List.stories.tsx
+++ editor.planx.uk/src/@planx/components/List/Public/List.stories.tsx
@@ -16,13 +16,18 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const schemaFinder = (name: string) => {
+  const schemaObj = SCHEMAS.find((schema) => schema.name === name)?.schema;
+  return schemaObj;
+};
+
 export const Basic: Story = {
   args: {
     title: "List",
     description: "Add information on the removed residential units",
     schemaName: "Residential units (GLA) - Removed",
     fn: "MockFn",
-    schema: SCHEMAS[4].schema,
+    schema: schemaFinder("Residential units (GLA) - Removed"),
   },
 };
 
@@ -32,9 +37,7 @@ export const SingularCard: Story = {
     description: "List component with a min / max value set to 1",
     schemaName: "Proposed advertisements",
     fn: "MockFn",
-    schema:
-      SCHEMAS.find((schema) => schema.name === "Proposed advertisements")
-        ?.schema || SCHEMAS[0].schema,
+    schema: schemaFinder("Proposed advertisements"),
   },
 };
 
