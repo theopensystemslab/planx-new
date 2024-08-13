ee48a56e144807c5e4b8368783b05783e6433e71
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-08-13 12:26:11 +0200

    update swagger doc with missing zip param

--- api.planx.uk/modules/admin/session/zip.ts
+++ api.planx.uk/modules/admin/session/zip.ts
@@ -5,8 +5,8 @@ import { buildSubmissionExportZip } from "../../send/utils/exportZip.js";
  * @swagger
  * /admin/session/{sessionId}/zip:
  *  get:
- *    summary: Generates and downloads a zip file for Send to Email, or Uniform when XML is included
- *    description: Generates and downloads a zip file for Send to Email, or Uniform when XML is included
+ *    summary: Generates and downloads a zip file for integrations
+ *    description: Generates and downloads a zip file for integrations
  *    tags:
  *      - admin
  *    parameters:
@@ -21,6 +21,11 @@ import { buildSubmissionExportZip } from "../../send/utils/exportZip.js";
  *        type: boolean
  *        required: false
  *        description: If the Digital Planning JSON file should be included in the zip (only generated for supported application types)
+ *      - in: query
+ *        name: onlyDigitalPlanningJSON
+ *        type: boolean
+ *        required: false
+ *        description: If the Digital Planning JSON file should be the ONLY file included in the zip (only generated for supported application types)
  *    security:
  *      - bearerAuth: []
  */
@@ -35,6 +40,7 @@ export async function generateZip(
       includeOneAppXML: req.query.includeOneAppXML === "true",
       includeDigitalPlanningJSON:
         req.query.includeDigitalPlanningJSON === "false",
+      onlyDigitalPlanningJSON: req.query.onlyDigitalPlanningJSON === "false",
     });
     res.download(zip.filename, () => {
       zip.remove();
