36712a4bf9a4051005134e759c59d5ebc9900fdf
Rory Doak <rory@opensystemslab.io>
2024-08-13 12:59:34 +0100

    import SCHEMAS form editor

--- editor.planx.uk/src/@planx/components/List/Public/List.stories.tsx
+++ editor.planx.uk/src/@planx/components/List/Public/List.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 
+import { SCHEMAS } from "../Editor";
 import ListComponent from "../Public";
 
 const meta = {
@@ -14,22 +15,14 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+console.log(SCHEMAS);
+
 export const Basic: Story = {
   args: {
     fn: "",
-    title: "string",
-    description: "string",
-    schemaName: "string",
-    schema: {
-      type: "",
-      fields: [
-        { type: "text", data: { title: "", fn: "" } },
-        { type: "text", data: { title: "", fn: "" } },
-        { type: "text", data: { title: "", fn: "" } },
-        { type: "text", data: { title: "", fn: "" } },
-      ],
-      min: 1,
-      max: 3,
-    },
+    title: "This is a new Title for a List",
+    description: "This new list has a description which is what is here",
+    schemaName: "TestSchemaName",
+    schema: SCHEMAS[1].schema,
   },
 };
