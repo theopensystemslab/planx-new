04b09978e8c5d026dd6865e5672af4affa06b198
Rory Doak <138574807+RODO94@users.noreply.github.com>
2024-08-20 15:51:34 +0100

    feat: Shareable Public Link in Service Settings (#3525)

---
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/tests/helpers/setupServiceSettingsScreen.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ServiceSettings from "../../ServiceSettings";
+import { setup } from "testUtils";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { screen } from "@testing-library/react";
+
+export default async function setupServiceSettingsScreen() {
+  const { user } = setup(
+    <DndProvider backend={HTML5Backend}>
+      <ServiceSettings />
+    </DndProvider>
+  );
+
+  await screen.findByText("Your public link");
+  return user;
+}
+
+export const mockWindowLocationObject = {
+  origin: "https://mocked-origin.com",
+  hash: "",
+  host: "dummy.com",
+  port: "80",
+  protocol: "http:",
+  hostname: "dummy.com",
+  href: "http://dummy.com?page=1&name=testing",
+  pathname: "/mockTeam/mock-planning-permish",
+  search: "",
+  assign: jest.fn(),
+  reload: jest.fn(),
+  replace: jest.fn(),
+  ancestorOrigins: {
+    length: 0,
+    contains: () => true,
+    item: () => null,
+    [Symbol.iterator]: jest.fn(),
+  },
+};
