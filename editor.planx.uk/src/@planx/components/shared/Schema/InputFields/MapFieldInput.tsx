d0bdced2dbf1bc0a269024ae97a767db7527743f
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-08-22 17:43:30 +0100

    refactor: Split into multiple files

---
+++ editor.planx.uk/src/@planx/components/shared/Schema/InputFields/MapFieldInput.tsx
@@ -0,0 +1,75 @@
+import { MapContainer } from "@planx/components/shared/Preview/MapContainer";
+import type { MapField } from "@planx/components/shared/Schema/model";
+import { Feature } from "geojson";
+import { useStore } from "pages/FlowEditor/lib/store";
+import React, { useEffect, useState } from "react";
+import InputLabel from "ui/public/InputLabel";
+import ErrorWrapper from "ui/shared/ErrorWrapper";
+
+import { getFieldProps, Props } from ".";
+
+export const MapFieldInput: React.FC<Props<MapField>> = (props) => {
+  const {
+    formik,
+    data: { title, mapOptions },
+  } = props;
+  const { id, errorMessage, name } = getFieldProps(props);
+
+  const teamSettings = useStore.getState().teamSettings;
+  const passport = useStore((state) => state.computePassport());
+
+  const [_features, setFeatures] = useState<Feature[] | undefined>(undefined);
+
+  useEffect(() => {
+    const geojsonChangeHandler = async ({ detail: geojson }: any) => {
+      if (geojson["EPSG:3857"]?.features) {
+        setFeatures(geojson["EPSG:3857"].features);
+        formik.setFieldValue(name, geojson["EPSG:3857"].features);
+      } else {
+        // if the user clicks 'reset' on the map, geojson will be empty object, so set features to undefined
+        setFeatures(undefined);
+        formik.setFieldValue(name, undefined);
+      }
+    };
+
+    const map: any = document.getElementById(id);
+
+    map?.addEventListener("geojsonChange", geojsonChangeHandler);
+
+    return function cleanup() {
+      map?.removeEventListener("geojsonChange", geojsonChangeHandler);
+    };
+  }, [setFeatures]);
+
+  return (
+    <InputLabel label={title} id={`map-label-${id}`} htmlFor={id}>
+      <ErrorWrapper error={errorMessage} id={id}>
+        <MapContainer environment="standalone">
+          {/* @ts-ignore */}
+          <my-map
+            id={id}
+            // TODO
+            // ariaLabelOlFixedOverlay={`An interactive map for plotting and describing ${schema.type.toLocaleLowerCase()}`}
+            height={400}
+            basemap={mapOptions?.basemap}
+            drawMode
+            drawMany={mapOptions?.drawMany}
+            drawColor={mapOptions?.drawColor}
+            drawType={mapOptions?.drawType}
+            drawPointer="crosshair"
+            zoom={20}
+            maxZoom={23}
+            latitude={Number(passport?.data?._address?.latitude)}
+            longitude={Number(passport?.data?._address?.longitude)}
+            osProxyEndpoint={`${process.env.REACT_APP_API_URL}/proxy/ordnance-survey`}
+            osCopyright={`Basemap subject to Crown copyright and database rights ${new Date().getFullYear()} OS (0)100024857`}
+            clipGeojsonData={
+              teamSettings?.boundaryBBox &&
+              JSON.stringify(teamSettings?.boundaryBBox)
+            }
+          />
+        </MapContainer>
+      </ErrorWrapper>
+    </InputLabel>
+  );
+};
--- editor.planx.uk/src/@planx/components/shared/Schema/SchemaFields.tsx
+++ editor.planx.uk/src/@planx/components/shared/Schema/SchemaFields.tsx
@@ -2,45 +2,15 @@ import { FormikProps } from "formik";
 import React from "react";
 import InputRow from "ui/shared/InputRow";
 
-import { ChecklistFieldInput, DateFieldInput, MapFieldInput, NumberFieldInput, RadioFieldInput, SelectFieldInput, TextFieldInput } from "./Fields";
-import { Field, Schema, SchemaData } from "./model";
+import { InputFields } from "./InputFields";
+import { Schema, SchemaData } from "./model";
 
-type InputFieldProps = {
-  activeIndex: number;
-  formik: FormikProps<SchemaData>;
-} & Field;
-
-/**
-* Controller to return correct user input for field in schema
-*/
-const InputField: React.FC<InputFieldProps> = (props) => {
-  switch (props.type) {
-    case "text":
-      return <TextFieldInput {...props} />;
-    case "number":
-      return <NumberFieldInput {...props} />;
-    case "question":
-      if (props.data.options.length === 2) {
-        return <RadioFieldInput {...props} />;
-      }
-      return <SelectFieldInput {...props} />;
-    case "checklist":
-      return <ChecklistFieldInput {...props} />;
-    case "date":
-      return <DateFieldInput {...props} />;
-    case "map":
-      return <MapFieldInput {...props} />;
-    default:
-      return null;
-  }
-};
-
-interface SchemaFieldsProps { 
-  /** 
-   * Optional index of currently active schema instance. 
-   * Only required if multiple user responses are allowed, e.g. in the List component. 
+interface SchemaFieldsProps {
+  /**
+   * Optional index of currently active schema instance.
+   * Only required if multiple user responses are allowed, e.g. in the List component.
    * Defaults to 0 as `SchemaData` always holds an array of responses
-  */
+   */
   activeIndex?: number;
   /** Formik instance generated from config provided by useSchema hook */
   formik: FormikProps<SchemaData>;
@@ -50,10 +20,13 @@ interface SchemaFieldsProps {
 /**
  * Display a set of fields for the provided schema
  */
-export const SchemaFields: React.FC<SchemaFieldsProps> = ({ schema, formik, activeIndex = 0 }) => (
+export const SchemaFields: React.FC<SchemaFieldsProps> = ({
+  schema,
+  formik,
+  activeIndex = 0,
+}) =>
   schema.fields.map((field, i) => (
     <InputRow key={i}>
-      <InputField {...field} activeIndex={activeIndex} formik={formik} />
+      <InputFields {...field} activeIndex={activeIndex} formik={formik} />
     </InputRow>
-  ))
-)
+  ));
