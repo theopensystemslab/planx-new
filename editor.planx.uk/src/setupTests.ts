3598e8b351e8d0b75b987338a4c2c251716e878c
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-08-22 13:07:37 +0200

    add comment re Chai bug

--- editor.planx.uk/src/setupTests.ts
+++ editor.planx.uk/src/setupTests.ts
@@ -1,2 +1,3 @@
 import "@testing-library/jest-dom";
+// import "@testing-library/jest-dom/vitest"; << https://stackoverflow.com/questions/77611978/invalid-chai-property-in-vitest
 import "vitest-axe/extend-expect";
