dede76cac863b12218bbef0b285777561920adea
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-08-26 14:01:37 +0100

    test: Cleanup testUtils

--- editor.planx.uk/src/testUtils.tsx
+++ editor.planx.uk/src/testUtils.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable no-restricted-imports */
-import { FadeProps, ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import { render, RenderResult } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
 import React from "react";
-import { vi } from "vitest";
 
 import { defaultTheme } from "./theme";
 
@@ -18,22 +17,3 @@ export const setup = (
   user: userEvent.setup(),
   ...render(<ThemeProvider theme={defaultTheme}>{jsx}</ThemeProvider>),
 });
-
-// export const axe = configureAxe({
-//   rules: {
-//     // Currently, jest-axe does not correctly evaluate this rule due to an issue with jsdom
-//     // https://github.com/dequelabs/axe-core/issues/2587
-//     // To pass this test, non-decorative MUI icons should always use the 'titleAccess' prop
-//     "svg-img-alt": { enabled: false },
-//   },
-// });
-
-/**
- * Mock the MUI Fade component used in @planx/components/shared/Preview/Card.tsx
- * Required as this frequently updates following the final "expect()" call of a test,
- * leading to multiple "an update was not wrapped in act(...)" warnings
- * Docs: https://testing-library.com/docs/example-react-transition-group/
- */
-// export const mockFade = vi.mock("@mui/material/Fade", () =>
-//   vi.fn(({ children }: FadeProps) => <div>{children}</div>),
-// );
--- editor.planx.uk/vitest.config.ts
+++ editor.planx.uk/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./src/setupTests.ts"],
+    setupFiles: ["./src/setupTests.tsx"],
   },
   plugins: [tsconfigPaths()],
 });
