0c8926d5239d3517e42e5f07eccbf8b27a924095
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-08-26 21:19:34 +0100

    test: NodeSearchResults

--- editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Search/index.test.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Search/index.test.tsx
@@ -8,49 +8,15 @@ import { vi } from "vitest";
 import { axe } from "vitest-axe";
 
 import Search from ".";
+import { flow } from "./mocks/simple";
 
 const { setState, getState } = useStore;
-
-const mockFlow = {
-  _root: {
-    edges: ["Ej0xpn4l8u"],
-  },
-  Ej0xpn4l8u: {
-    type: 100,
-    data: {
-      fn: "country",
-      text: "Pick a country",
-    },
-    edges: ["VhSydY2fTe", "tR9tdaWOvF", "tvUxd2IoPo"],
-  },
-  VhSydY2fTe: {
-    type: 200,
-    data: {
-      text: "Spain",
-      val: "spain",
-    },
-  },
-  tR9tdaWOvF: {
-    type: 200,
-    data: {
-      text: "India",
-      val: "india",
-    },
-  },
-  tvUxd2IoPo: {
-    type: 200,
-    data: {
-      text: "Indonesia",
-      val: "indonesia",
-    },
-  },
-};
 
 let initialState: FullStore;
 
 beforeAll(() => (initialState = getState()));
 
-beforeEach(() => setState({ flow: mockFlow }));
+beforeEach(() => setState({ flow }));
 afterEach(() => act(() => setState(initialState)));
 
 vi.mock("@opensystemslab/planx-core", async (originalModule) => {
