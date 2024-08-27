3cc4a424055a86bfbec23005b27f8cd6e559a173
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-08-27 14:32:43 +0100

    feat: Setup types for data facets

---
+++ editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Search/facets.ts
@@ -0,0 +1,29 @@
+import { IndexedNode } from "@opensystemslab/planx-core/types";
+import { FuseOptionKey } from "fuse.js";
+
+type SearchFacets = Array<FuseOptionKey<IndexedNode>>;
+
+const generalData: SearchFacets = ["data.fn", "data.val"];
+
+const fileUploadAndLabelData: SearchFacets = ["data.fileTypes.fn"];
+
+const calculateData: SearchFacets = [
+  "data.output",
+  {
+    name: "formula",
+    getFn: (node: IndexedNode) => Object.keys(node.data?.defaults || {}),
+  },
+];
+
+const listData: SearchFacets = [
+  "data.schema.fields.data.fn",
+  "data.schema.fields.data.options.data.val",
+];
+
+/** Data fields used across PlanX components */
+export const DATA_FACETS = [
+  ...generalData,
+  ...fileUploadAndLabelData,
+  ...calculateData,
+  ...listData,
+];
