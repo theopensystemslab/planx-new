70d5679b51d69ffdf98e072d23f71632f316b0b9
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-09-04 14:52:50 +0200

    tidy up

--- editor.planx.uk/src/@planx/components/shared/Schema/InputFields/shared.tsx
+++ editor.planx.uk/src/@planx/components/shared/Schema/InputFields/shared.tsx
@@ -1,6 +1,5 @@
 import Typography from "@mui/material/Typography";
 import React from "react";
-import ReactMarkdownOrHtml from "ui/shared/ReactMarkdownOrHtml";
 
 interface FieldInputDescriptionProps {
   description: string;
@@ -10,10 +9,6 @@ export const FieldInputDescription: React.FC<FieldInputDescriptionProps> = ({
   description,
 }) => (
   <Typography variant="body2" mb={1.5}>
-    <ReactMarkdownOrHtml
-      source={description}
-      openLinksOnNewTab
-      // TODO a11y props? eg `id={DESCRIPTION_TEXT}` but unique ??
-    />
+    {description}
   </Typography>
 );
