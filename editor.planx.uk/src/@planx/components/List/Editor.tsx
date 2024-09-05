42b77b6ae92716e2587af8b51176b50930da8c91
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2024-09-05 13:06:55 +0100

    feat: add Lost/Gained list schema to GLA residential units (#3588)

--- editor.planx.uk/src/@planx/components/List/Editor.tsx
+++ editor.planx.uk/src/@planx/components/List/Editor.tsx
@@ -29,6 +29,8 @@ import { ResidentialUnitsGLANew } from "./schemas/ResidentialUnits/GLA/New";
 import { ResidentialUnitsGLARebuilt } from "./schemas/ResidentialUnits/GLA/Rebuilt";
 import { ResidentialUnitsGLARemoved } from "./schemas/ResidentialUnits/GLA/Removed";
 import { ResidentialUnitsGLARetained } from "./schemas/ResidentialUnits/GLA/Retained";
+import { ResidentialUnitsGLALost } from "./schemas/ResidentialUnits/GLA/Lost";
+import { ResidentialUnitsGLAGained } from "./schemas/ResidentialUnits/GLA/Gained";
 import { ResidentialUnitsProposed } from "./schemas/ResidentialUnits/Proposed";
 import { Trees } from "./schemas/Trees";
 import { TreesMapFirst } from "./schemas/TreesMapFirst";
@@ -53,6 +55,14 @@ export const SCHEMAS = [
   {
     name: "Residential units (GLA) - Retained",
     schema: ResidentialUnitsGLARetained,
+  },
+  {
+    name: "Residential units (GLA) - Lost",
+    schema: ResidentialUnitsGLALost,
+  },
+  {
+    name: "Residential units (GLA) - Gained",
+    schema: ResidentialUnitsGLAGained,
   },
   { name: "Non-residential floorspace", schema: NonResidentialFloorspace },
   {
