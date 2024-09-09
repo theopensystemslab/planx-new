b049d3145be021d7947cbcb282be1337a80ad8fb
Rory Doak <rory@opensystemslab.io>
2024-09-09 10:17:19 +0100

    drop column and change sql for team_summary view

---
+++ hasura.planx.uk/migrations/1725873202035_alter_table_public_teams_drop_column_submission_email/down.sql
@@ -0,0 +1,31 @@
+alter table "public"."teams" add column "submission_email" text;
+alter table "public"."teams" alter column "submission_email" drop not null;
+comment on column "public"."teams"."submission_email" is E'Teams are core way we organise `flows` and configure settings like theme colour, logo, and contact info; name usually reflects a council but not exclusively';
+
+
+ CREATE OR REPLACE VIEW "public"."teams_summary" AS
+  SELECT t.id,
+     t.name,
+     t.slug,
+     t.reference_code,
+     (t.settings ->> 'homepage'::text) AS homepage,
+     t.domain AS subdomain,
+     ti.has_planning_data AS planning_data_enabled,
+     '@todo'::text AS article_4s_enabled,
+     jsonb_build_object('helpEmail', ts.help_email, 'helpPhone', ts.help_phone, 'emailReplyToId', ts.email_reply_to_id, 'helpOpeningHours', ts.help_opening_hours) AS govnotify_personalisation,
+         CASE
+             WHEN (COALESCE(ti.production_govpay_secret, ti.staging_govpay_secret) IS NOT NULL) THEN true
+             ELSE false
+         END AS govpay_enabled,
+     t.submission_email AS send_to_email_address,
+     COALESCE(ti.production_bops_submission_url, ti.staging_bops_submission_url) AS bops_submission_url,
+     tt.logo,
+     tt.favicon,
+     tt.primary_colour,
+     tt.link_colour,
+     tt.action_colour
+    FROM (((teams t
+      JOIN team_integrations ti ON ((ti.team_id = t.id)))
+      JOIN team_themes tt ON ((tt.team_id = t.id)))
+      JOIN team_settings ts ON ((ts.team_id = t.id)))
+   ORDER BY t.name;
