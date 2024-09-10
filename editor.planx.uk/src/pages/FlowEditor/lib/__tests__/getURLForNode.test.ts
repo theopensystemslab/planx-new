7ecc19889735a09e9ea8ba14ed858fe5c76d50bc
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-09-10 16:52:53 +0100

    test: Setup test.todos()

---
+++ editor.planx.uk/src/pages/FlowEditor/lib/__tests__/getURLForNode.test.ts
@@ -0,0 +1,11 @@
+describe("constructing URLs for nodes", () => {
+  test.todo("a question node on the root");
+  test.todo("an answer node, of a question on the root");
+  test.todo("a question node, deeply branched");
+  test.todo("an answer node, of a deeply branched question");
+  test.todo("a question node, within an internal portal");
+  test.todo("an answer node, within an internal portal");
+  test.todo("a question node, deeply within internal portals");
+  test.todo("an answer node, deeply within internal portals");
+  test.todo("a cloned node");
+});
