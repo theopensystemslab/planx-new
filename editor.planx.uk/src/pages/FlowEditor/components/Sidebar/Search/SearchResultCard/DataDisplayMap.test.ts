20f45723b1be500900d3b42bf0b85cd5643ee4c6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-09-10 07:46:38 +0100

    test: Setup todos

---
+++ editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Search/SearchResultCard/DataDisplayMap.test.ts
@@ -0,0 +1,22 @@
+describe("Question component", () => {
+  it.todo("returns the expected display values");
+});
+
+describe("Answer component", () => {
+  it.todo("returns the expected display values");
+});
+
+describe("List component", () => {
+  it.todo("handles the default (root) data value");
+  it.todo("handles nested data variables");
+  it.todo("handles nested data variables in Answers");
+});
+
+describe("Calculate component", () => {
+  it.todo("handles the output data variables");
+  it.todo("handles the formula data variables");
+});
+
+describe("FileUploadAndLabel component", () => {
+  it.todo("handles the data variables nested in FileTypes");
+});
