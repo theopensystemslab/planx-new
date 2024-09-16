343ee878d6a2370782c6f71b89f571500665fd93
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-09-16 17:29:49 +0100

    feat(page): Add Storybook file (#3681)

---
+++ editor.planx.uk/src/@planx/components/Page/Page.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+
+import Wrapper from "../fixtures/Wrapper";
+import Editor, { PAGE_SCHEMAS } from "./Editor";
+import Public  from "./Public";
+
+const meta = {
+  title: "PlanX Components/Page",
+  component: Public,
+} satisfies Meta<typeof Public>;
+
+type SchemaItem = (typeof PAGE_SCHEMAS)[number];
+type SchemaNames = SchemaItem["name"];
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const schemaFinder = (name: SchemaNames) => {
+  const schemaObj = PAGE_SCHEMAS.find((schema) => schema.name === name)?.schema;
+  return schemaObj || PAGE_SCHEMAS[0].schema;
+};
+
+export const Basic: Story = {
+  args: {
+    title:
+      "Tell us about your proposed advertisements",
+    description: "Please add your details below",
+    schemaName: "Advert consent",
+    fn: "MockFn",
+    schema: schemaFinder("Advert consent"),
+  },
+};
+
+export const WithEditor = () => {
+  return <Wrapper Editor={Editor} Public={Public} />;
+};
