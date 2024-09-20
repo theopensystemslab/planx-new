6fcfe863111b7b0da97d56b01a538d99525304e4
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-09-20 07:20:39 +0100

    fix: Add loading indicator to public pages

---
+++ editor.planx.uk/src/pages/layout/LoadingLayout.tsx
@@ -0,0 +1,10 @@
+import DelayedLoadingIndicator from "components/DelayedLoadingIndicator";
+import React from "react";
+import { useLoadingRoute, View } from "react-navi";
+
+export const loadingView = () => <LoadingLayout />;
+
+export const LoadingLayout = () => {
+  const isLoading = useLoadingRoute();
+  return isLoading ? <DelayedLoadingIndicator /> : <View />;
+};
