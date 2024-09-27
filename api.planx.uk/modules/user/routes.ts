be6317bad3e26ec3f2aa26556c14f8a074a6a9fc
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-09-27 16:46:33 +0100

    chore: Remove deprecated \/user endpoints

--- api.planx.uk/modules/user/routes.ts
+++ api.planx.uk/modules/user/routes.ts
@@ -1,28 +1,9 @@
 import { Router } from "express";
-import { useLoginAuth, usePlatformAdminAuth } from "../auth/middleware.js";
-import { validate } from "../../shared/middleware/validate.js";
-import {
-  createUserSchema,
-  createUser,
-  deleteUserSchema,
-  deleteUser,
-  getLoggedInUserDetails,
-} from "./controller.js";
+import { useLoginAuth } from "../auth/middleware.js";
+import { getLoggedInUserDetails } from "./controller.js";
 
 const router = Router();
 
-router.put(
-  "/user",
-  usePlatformAdminAuth,
-  validate(createUserSchema),
-  createUser,
-);
-router.delete(
-  "/user/:email",
-  usePlatformAdminAuth,
-  validate(deleteUserSchema),
-  deleteUser,
-);
 router.get("/user/me", useLoginAuth, getLoggedInUserDetails);
 
 export default router;
