f2b3fd5c5c92038d3106910d3673a311c5dbef6c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-10-07 17:23:11 +0100

    chore: Temporarily hide tag from graph

--- editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Tag.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Tag.tsx
@@ -1,17 +1,24 @@
 import Box from "@mui/material/Box";
+import { visuallyHidden } from "@mui/utils";
 import { NodeTag } from "@opensystemslab/planx-core/types";
 import React from "react";
 import { FONT_WEIGHT_SEMI_BOLD } from "theme";
 
-export const TAG_DISPLAY_VALUES: Record<NodeTag, { color: string, displayName: string }> = {
+export const TAG_DISPLAY_VALUES: Record<
+  NodeTag,
+  { color: string; displayName: string }
+> = {
   placeholder: {
     color: "#FAE1B7",
-    displayName: "Placeholder"
-  }
+    displayName: "Placeholder",
+  },
 } as const;
 
 export const Tag: React.FC<{ tag: NodeTag }> = ({ tag }) => (
   <Box
+    // Temporarily hidden until we decide how this should appear in the graph
+    // Please see https://github.com/theopensystemslab/planx-new/pull/3762
+    style={visuallyHidden}
     sx={(theme) => ({
       bgcolor: TAG_DISPLAY_VALUES[tag].color,
       borderColor: theme.palette.common.black,
@@ -20,9 +27,9 @@ export const Tag: React.FC<{ tag: NodeTag }> = ({ tag }) => (
       width: "100%",
       p: 0.5,
       textAlign: "center",
-      fontWeight: FONT_WEIGHT_SEMI_BOLD
+      fontWeight: FONT_WEIGHT_SEMI_BOLD,
     })}
   >
-    { TAG_DISPLAY_VALUES[tag].displayName }
+    {TAG_DISPLAY_VALUES[tag].displayName}
   </Box>
 );
