988a2c8aa361de58cdf22b1e91aecf441a237c2c
Ian Jones <hello@ianjo.uk>
2024-10-10 10:53:47 +0100

    Revert "fix: Remove redundant tooltip on URL copy"
    This reverts commit 31fec51d259338ffa6af529fb7b966488db26f17.

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/FlowStatus/PublicLink.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/FlowStatus/PublicLink.tsx
@@ -2,6 +2,7 @@ import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
+import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import { FlowStatus } from "@opensystemslab/planx-core/types";
 import React, { useState } from "react";
@@ -10,25 +11,27 @@ import SettingsDescription from "ui/editor/SettingsDescription";
 const CopyButton = (props: { link: string; isActive: boolean }) => {
   const [copyMessage, setCopyMessage] = useState<"copy" | "copied">("copy");
   return (
-    <Button
-      disabled={!props.isActive}
-      variant="help"
-      onMouseLeave={() => {
-        setTimeout(() => {
-          setCopyMessage("copy");
-        }, 500);
-      }}
-      onClick={() => {
-        setCopyMessage("copied");
-        navigator.clipboard.writeText(props.link);
-      }}
-      sx={{ marginLeft: 0.5 }}
-    >
-      <ContentCopyIcon style={{ width: "18px", height: "18px" }} />
-      <Typography ml={0.5} variant="body3">
-        {copyMessage}
-      </Typography>
-    </Button>
+    <Tooltip title={copyMessage}>
+      <Button
+        disabled={!props.isActive}
+        variant="help"
+        onMouseLeave={() => {
+          setTimeout(() => {
+            setCopyMessage("copy");
+          }, 500);
+        }}
+        onClick={() => {
+          setCopyMessage("copied");
+          navigator.clipboard.writeText(props.link);
+        }}
+        sx={{ marginLeft: 0.5 }}
+      >
+        <ContentCopyIcon style={{ width: "18px", height: "18px" }} />
+        <Typography ml={0.5} variant="body3">
+          {copyMessage}
+        </Typography>
+      </Button>
+    </Tooltip>
   );
 };
 
