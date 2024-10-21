19465b156322ef7f6fc93babc12eecf1d15dea74
Jessica McInchak <jessica.mcinchak@gmail.com>
2024-10-21 23:31:58 +0200

    more tests

--- editor.planx.uk/src/pages/FlowEditor/lib/__tests__/filters.test.ts
+++ editor.planx.uk/src/pages/FlowEditor/lib/__tests__/filters.test.ts
@@ -1,13 +1,8 @@
 import { Store, useStore } from "../store";
-import flowWithAutoAnsweredFilterPaths from "./mocks/flowWithAutoAnsweredFilterPaths.json";
+import { clickContinue, visitedNodes } from "./utils";
 
 const { getState, setState } = useStore;
-const {
-  upcomingCardIds,
-  resetPreview,
-  record,
-  getCurrentCard,
-} = getState();
+const { upcomingCardIds, resetPreview, autoAnswerableOptions } = getState();
 
 describe("A filter on the root of the graph", () => {
   beforeEach(() => {
@@ -15,11 +10,32 @@ describe("A filter on the root of the graph", () => {
     setState({ flow: flowWithFilters });
   });
 
-  test.todo("Filter options are auto-answered correctly when a higher order flag is picked up first");
-  
-  test.todo("Filter options are auto-answered correctly when a lower order flag is picked up first");
-  
-  test.todo("Filter 'No flag result' option is auto-answered correctly when no flags in this category have been picked up");
+  test("Filter options are auto-answered correctly when a higher order flag is collected first", () => {
+    expect(upcomingCardIds()).toEqual(["FirstQuestion", "SecondQuestion", "RootFilter", "BranchingQuestion", "EndNotice"]);
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionYesAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionNoAnswer"], auto: false });
+
+    expect(upcomingCardIds()?.[0]).toEqual("RootFilter");
+    expect(autoAnswerableOptions("RootFilter")).toEqual(["RootFilterYes"]);
+  });
+
+  test("Filter options are auto-answered correctly when a lower order flag is collected first", () => {
+    expect(upcomingCardIds()).toEqual(["FirstQuestion", "SecondQuestion", "RootFilter", "BranchingQuestion", "EndNotice"]);
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionNoAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionYesAnswer"], auto: false });
+
+    expect(upcomingCardIds()?.[0]).toEqual("RootFilter");
+    expect(autoAnswerableOptions("RootFilter")).toEqual(["RootFilterYes"]);
+  });
+
+  test("Filter 'No flag result' option is auto-answered correctly when no flags in this category have been collected", () => {
+    expect(upcomingCardIds()).toEqual(["FirstQuestion", "SecondQuestion", "RootFilter", "BranchingQuestion", "EndNotice"]);
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionIdkAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionIdkAnswer"], auto: false });
+
+    expect(upcomingCardIds()?.[0]).toEqual("RootFilter");
+    expect(autoAnswerableOptions("RootFilter")).toEqual(["RootFilterNoFlagResult"]);
+  });
 });
 
 describe("A filter on a branch", () => {
@@ -28,79 +44,281 @@ describe("A filter on a branch", () => {
     setState({ flow: flowWithFilters });
   });
 
-  test.todo("Filter options are auto-answered correctly when a higher order flag is picked up first");
-  
-  test.todo("Filter options are auto-answered correctly when a lower order flag is picked up first");
-  
-  test.todo("Filter 'No flag result' option is auto-answered correctly when no flags in this category have been picked up");
+  test("Filter options are auto-answered correctly when a higher order flag is collected first", () => {
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionYesAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionNoAnswer"], auto: false });
+    clickContinue("RootFilter", { answers: ["RootFilterYes"], auto: true });
+
+    expect(upcomingCardIds()?.[0]).toEqual("AutoAnswerableChecklist");
+    expect(autoAnswerableOptions("AutoAnswerableChecklist")).toEqual(["ChecklistFacadeAnswer"]);
+    clickContinue("AutoAnswerableChecklist", { answers: ["ChecklistFacadeAnswer"], auto: true });
+
+    clickContinue("BranchingQuestion", { answers: ["GoToBranchAnswer"], auto: false });
+
+    expect(upcomingCardIds()?.[0]).toEqual("BranchFilter");
+    expect(autoAnswerableOptions("BranchFilter")).toEqual(["BranchFilterYes"]);
+  });
+
+  test("Filter options are auto-answered correctly when a lower order flag is collected first", () => {
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionNoAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionYesAnswer"], auto: false });
+    clickContinue("RootFilter", { answers: ["RootFilterYes"], auto: true });
+
+    expect(upcomingCardIds()?.[0]).toEqual("AutoAnswerableChecklist");
+    expect(autoAnswerableOptions("AutoAnswerableChecklist")).toEqual(["ChecklistLandscapingAnswer"]);
+    clickContinue("AutoAnswerableChecklist", { answers: ["ChecklistLandscapingAnswer"], auto: true });
+
+    clickContinue("BranchingQuestion", { answers: ["GoToBranchAnswer"], auto: false });
+
+    expect(upcomingCardIds()?.[0]).toEqual("BranchFilter");
+    expect(autoAnswerableOptions("BranchFilter")).toEqual(["BranchFilterYes"]);
+  });
+
+  test("Filter 'No flag result' option is auto-answered correctly when no flags in this category have been collected", () => {
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionIdkAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionIdkAnswer"], auto: false });
+    clickContinue("RootFilter", { answers: ["RootFilterNoFlagResult"], auto: true });
+    clickContinue("BranchingQuestion", { answers: ["GoToBranchAnswer"], auto: false });
+
+    expect(upcomingCardIds()?.[0]).toEqual("BranchFilter");
+    expect(autoAnswerableOptions("BranchFilter")).toEqual(["BranchFilterNoFlagResult"]);
+  });
 });
 
-describe("Nodes on a filter path should only be auto-answered when the path matches the result", () => {
+describe("Auto-answerable Questions or Checklists on filter paths", () => {
   beforeEach(() => {
     resetPreview();
-    setState({ flow: flowWithAutoAnsweredFilterPaths }); // https://editor.planx.uk/testing/flag-order-test-with-autoanswer
+    setState({ flow: flowWithFilters });
   });
 
-  test.skip("Filter path nodes are auto-answered correctly when the highest order flag is picked up first", () => {
-    const visitedNodes = () => Object.keys(getState().breadcrumbs);
+  test("Are only auto-answered when they are reached and when the path matches the result", () => {
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionYesAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionYesAnswer"], auto: false });
+    expect(visitedNodes()).not.toContain("AutoAnswerableChecklist");
 
-    // go forward manually: select not listed and select an answer with permission needed (higher order) flag
-    record("zlKQyPuKsl", { answers: ["qW1jzS1qPy"], auto: false });
-    record("Ve90wVIXsV", { answers: ["d98AoVIXsV"], auto: false }); // as we pick up this flag, later nodes in matching filter path are auto-answered immediately
+    expect(upcomingCardIds()?.[0]).toEqual("RootFilter");
+    expect(autoAnswerableOptions("RootFilter")).toEqual(["RootFilterYes"]);
+    clickContinue("RootFilter", { answers: ["RootFilterYes"], auto: true });
 
-    // continue forward manually: select an answer with permitted development (lower order) flag
-    record("TiIuAVIXsV", { answers: ["hdaeOVIXsV"], auto: false });
-
-    // land on the correct result component
-    expect(getCurrentCard()?.id).toBe("seN42VIXsV");
-    expect(getState().resultData()["Planning permission"]).toHaveProperty(
-      "flag.value",
-      "PLANNING_PERMISSION_REQUIRED",
-    );
-
-    // expect the auto-answered question on the permission needed filter path to be in our breadcrumbs
-    expect(visitedNodes()).toContain("1ShlhWrXPl");
-    expect(getState().breadcrumbs["1ShlhWrXPl"]).toEqual({
-      answers: ["tesCNavKYo"],
-      auto: true,
-    });
-
-    // make sure the auto-answerable question and its child from the permitted development filter path is not in our breadcrumbs nor upcoming card ids
-    expect(visitedNodes()).not.toContain("AaEuHnVUb4");
-    expect(upcomingCardIds).not.toContain("xjcujhpzjs");
+    expect(upcomingCardIds()?.[0]).toEqual("AutoAnswerableChecklist");
+    expect(autoAnswerableOptions("AutoAnswerableChecklist")).toEqual(["ChecklistFacadeAnswer", "ChecklistLandscapingAnswer"]);
   });
 
-  test.skip("Filter path nodes are auto-answered correctly when a lower order flag is picked up first", () => {
-    const visitedNodes = () => Object.keys(getState().breadcrumbs);
+  test("Are never auto-answered if the path does not match the result", () => {
+    clickContinue("FirstQuestion", { answers: ["FirstQuestionNoAnswer"], auto: false });
+    clickContinue("SecondQuestion", { answers: ["SecondQuestionIdkAnswer"], auto: false });
+    expect(visitedNodes()).not.toContain("AutoAnswerableChecklist");
 
-    // go forward manually: select not listed and select an answer with permitted dev (lower order) flag
-    record("zlKQyPuKsl", { answers: ["qW1jzS1qPy"], auto: false });
-    record("Ve90wVIXsV", { answers: ["pghecgQLgs"], auto: false });
-    upcomingCardIds(); // mimic "continue" and properly set visitedNodes()
+    expect(upcomingCardIds()?.[0]).toEqual("RootFilter");
+    expect(autoAnswerableOptions("RootFilter")).toEqual(["RootFilterNo"]);
+    clickContinue("RootFilter", { answers: ["RootFilterNo"], auto: true });
 
-    // TODO ensure that the auto-answerable question in the permitted dev filter path has not been immediately auto-answered before reaching the filter node
-    expect(visitedNodes()).not.toContain("AaEuHnVUb4");
-
-    // continue forward manually: select an answer with permission needed (higher order) flag
-    record("TiIuAVIXsV", { answers: ["OPOWoVIXsV"], auto: false });
-    upcomingCardIds();
-
-    // land on the correct result component
-    expect(getCurrentCard()?.id).toBe("seN42VIXsV");
-    expect(getState().resultData()["Planning permission"]).toHaveProperty(
-      "flag.value",
-      "PLANNING_PERMISSION_REQUIRED",
-    );
-
-    // expect the auto-answered question on the permission needed filter path to be in our breadcrumbs
-    expect(visitedNodes()).toContain("1ShlhWrXPl");
-    expect(getState().breadcrumbs["1ShlhWrXPl"]).toEqual({
-      answers: ["tesCNavKYo"],
-      auto: true,
-    });
+    expect(upcomingCardIds()).not.toContain("AutoAnswerableChecklist");
   });
 });
 
 const flowWithFilters: Store.Flow = {
-  // TODO 
+  "_root": {
+    "edges": [
+      "FirstQuestion",
+      "SecondQuestion",
+      "RootFilter",
+      "BranchingQuestion",
+      "EndNotice"
+    ]
+  },
+  "SecondQuestionYesAnswer": {
+    "data": {
+      "val": "alter.landscaping",
+      "flag": "MCOU_TRUE",
+      "text": "Yes"
+    },
+    "type": 200
+  },
+  "FirstQuestionNoAnswer": {
+    "data": {
+      "flag": "MCOU_FALSE",
+      "text": "No"
+    },
+    "type": 200
+  },
+  "AutoAnswerableChecklist": {
+    "data": {
+      "fn": "proposal.projectType",
+      "text": "What are you altering?",
+      "allRequired": false
+    },
+    "type": 105,
+    "edges": [
+      "ChecklistFacadeAnswer",
+      "ChecklistLandscapingAnswer"
+    ]
+  },
+  "RootFilterYes": {
+    "data": {
+      "val": "MCOU_TRUE",
+      "text": "Material change of use"
+    },
+    "type": 200,
+    "edges": [
+      "AutoAnswerableChecklist"
+    ]
+  },
+  "BranchingQuestion": {
+    "data": {
+      "text": "What about a filter on a branch?"
+    },
+    "type": 100,
+    "edges": [
+      "GoToBranchAnswer",
+      "SkipBranchAnswer"
+    ]
+  },
+  "EndNotice": {
+    "data": {
+      "color": "#EFEFEF",
+      "title": "End of test",
+      "resetButton": true
+    },
+    "type": 8
+  },
+  "RootFilterNoFlagResult": {
+    "data": {
+      "text": "No flag result"
+    },
+    "type": 200
+  },
+  "BranchFilter": {
+    "data": {
+      "fn": "flag",
+      "category": "Material change of use"
+    },
+    "type": 500,
+    "edges": [
+      "BranchFilterYes",
+      "BranchFilterNo",
+      "BranchFilterNoFlagResult"
+    ],
+  },
+  "BranchFilterNo": {
+    "data": {
+      "val": "MCOU_FALSE",
+      "text": "Not material change of use"
+    },
+    "type": 200
+  },
+  "ChecklistLandscapingAnswer": {
+    "data": {
+      "val": "alter.landscaping",
+      "text": "Landscaping"
+    },
+    "type": 200
+  },
+  "RootFilterNo": {
+    "data": {
+      "val": "MCOU_FALSE",
+      "text": "Not material change of use"
+    },
+    "type": 200
+  },
+  "ChecklistFacadeAnswer": {
+    "data": {
+      "val": "alter.facade",
+      "text": "Facade"
+    },
+    "type": 200
+  },
+  "FirstQuestion": {
+    "data": {
+      "fn": "proposal.projectType",
+      "tags": [],
+      "text": "Are you changing the building facade?"
+    },
+    "type": 100,
+    "edges": [
+      "FirstQuestionYesAnswer",
+      "FirstQuestionNoAnswer",
+      "FirstQuestionIdkAnswer"
+    ]
+  },
+  "BranchFilterNoFlagResult": {
+    "data": {
+      "text": "No flag result"
+    },
+    "type": 200
+  },
+  "FirstQuestionYesAnswer": {
+    "data": {
+      "val": "alter.facade",
+      "flag": "MCOU_TRUE",
+      "text": "Yes"
+    },
+    "type": 200
+  },
+  "BranchFilterYes": {
+    "data": {
+      "val": "MCOU_TRUE",
+      "text": "Material change of use"
+    },
+    "type": 200
+  },
+  "FirstQuestionIdkAnswer": {
+    "data": {
+      "text": "I don't know"
+    },
+    "type": 200
+  },
+  "GoToBranchAnswer": {
+    "data": {
+      "text": "Let's go"
+    },
+    "type": 200,
+    "edges": [
+      "BranchFilter"
+    ]
+  },
+  "RootFilter": {
+    "data": {
+      "fn": "flag",
+      "category": "Material change of use"
+    },
+    "type": 500,
+    "edges": [
+      "RootFilterYes",
+      "RootFilterNo",
+      "RootFilterNoFlagResult"
+    ]
+  },
+  "SecondQuestionNoAnswer": {
+    "data": {
+      "flag": "MCOU_FALSE",
+      "text": "No"
+    },
+    "type": 200
+  },
+  "SecondQuestionIdkAnswer": {
+    "data": {
+      "text": "I don't know"
+    },
+    "type": 200
+  },
+  "SkipBranchAnswer": {
+    "data": {
+      "text": "Skip it"
+    },
+    "type": 200
+  },
+  "SecondQuestion": {
+    "data": {
+      "fn": "proposal.projectType",
+      "tags": [],
+      "text": "Are you changing the landscaping materials?"
+    },
+    "type": 100,
+    "edges": [
+      "SecondQuestionYesAnswer",
+      "SecondQuestionNoAnswer",
+      "SecondQuestionIdkAnswer"
+    ]
+  }
 };
---
+++ editor.planx.uk/src/pages/FlowEditor/lib/__tests__/preview/autoAnswerableOptions.test.ts
@@ -0,0 +1,32 @@
+import { useStore } from "../../store";
+
+const { getState } = useStore;
+const { resetPreview } = getState();
+
+beforeEach(() => {
+  resetPreview();
+});
+
+describe("Questions", () => {
+  test.todo("Correctly auto-answers the option that exactly matches a passport value");
+
+  test.todo("Correctly auto-answers the less granular option when there's a single more granular passport value");
+
+  test.todo("Correctly auto-answers the single left-most option when there are many matching passport values");
+
+  test.todo("Correctly auto-answers through the blank path when there are no matching passport values but we've seen this passport fn before");
+});
+
+describe("Checklists", () => {
+  test.todo("Correctly auto-answers the options that exactly match passport values");
+
+  test.todo("Correctly auto-answers the less granular options when there are more granular passport values");
+
+  test.todo("Correctly auto-answers through the blank path when there are no matching passport values but we've seen this passport fn before");
+});
+
+describe("Filters", () => {
+  test.todo("Correctly auto-answers the single highest order flag path when many flags are collected");
+
+  test.todo("Correctly auto-answers the through the blank path (no flag result) when no matching flags have been collected");
+});
