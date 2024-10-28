9458594345e7b3ceb8767aba4a39d57ff5c981e7
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-10-28 12:29:33 +0000

    Fix hover padding

--- editor.planx.uk/src/@planx/components/Feedback/styled.ts
+++ editor.planx.uk/src/@planx/components/Feedback/styled.ts
@@ -7,7 +7,9 @@ export const StyledToggleButtonGroup = styled(ToggleButtonGroup)(
   ({ theme }) => ({
     [`& .${toggleButtonGroupClasses.grouped}`]: {
       border: 0,
+      padding: 0,
+      marginTop: theme.spacing(1),
     },
-    paddingBottom: theme.spacing(3.2),
+    paddingBottom: theme.spacing(3.5),
   }),
 );
