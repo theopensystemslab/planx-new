14e23973dd402227003c2cd62a5d2a5ad6c530c5
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-10-28 17:19:12 +0000

    Reorganise feedback component into folders

---
+++ editor.planx.uk/src/@planx/components/Feedback/Editor/Editor.tsx
@@ -0,0 +1,44 @@
+import { ComponentType as TYPES } from "@opensystemslab/planx-core/types";
+import { ICONS } from "@planx/components/shared/icons";
+import { useFormik } from "formik";
+import React from "react";
+import ModalSection from "ui/editor/ModalSection";
+import ModalSectionContent from "ui/editor/ModalSectionContent";
+import Input from "ui/shared/Input/Input";
+import InputRow from "ui/shared/InputRow";
+
+import { EditorProps } from "../../sharedTypes";
+import { Feedback, parseFeedback } from "../model";
+
+type Props = EditorProps<TYPES.SetValue, Feedback>; // TODO: use Feedback type
+
+export const FeedbackEditor = (props: Props) => {
+  const formik = useFormik({
+    initialValues: parseFeedback(props.node?.data),
+    onSubmit: (newValues) => {
+      props.handleSubmit?.({
+        type: TYPES.SetValue, // TODO: use Feedback
+        data: newValues,
+      });
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit} id="modal">
+      <ModalSection>
+        <ModalSectionContent title="Feedback" Icon={ICONS[TYPES.Notice]}>
+          <InputRow>
+            <Input
+              format="large"
+              placeholder="Feedback"
+              value={undefined}
+              onChange={() => {
+                console.log("change!");
+              }}
+            />
+          </InputRow>
+        </ModalSectionContent>
+      </ModalSection>
+    </form>
+  );
+};
