7cae337bb9981db586759f4e3f30582bfbfd39ab
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-10-29 15:45:36 +0000

    Fix import

--- editor.planx.uk/src/@planx/components/Filter/Public.tsx
+++ editor.planx.uk/src/@planx/components/Filter/Public.tsx
@@ -1,16 +1,14 @@
-import type { PublicProps } from "@planx/components/ui";
 import { useStore } from "pages/FlowEditor/lib/store";
 import { useEffect } from "react";
 
+import { PublicProps } from "../shared/types";
 import type { Props as Filter } from "./Editor";
 
 export type Props = PublicProps<Filter>;
 
 // Filters are always auto-answered and never seen by a user, but should still leave a breadcrumb
 export default function Component(props: Props) {
-  const autoAnswerableFlag = useStore(
-    (state) => state.autoAnswerableFlag,
-  );
+  const autoAnswerableFlag = useStore((state) => state.autoAnswerableFlag);
 
   let idThatCanBeAutoAnswered: string | undefined;
   if (props.id) idThatCanBeAutoAnswered = autoAnswerableFlag(props.id);
