5ddc1c28d2822da66935d39043c56cf43c4db863
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-10-30 09:42:38 +0000

    feat: Add check for `demoUser` role on login (#3872)

---
+++ api.planx.uk/modules/auth/types.ts
@@ -0,0 +1,14 @@
+import type { Role } from "@opensystemslab/planx-core/types";
+
+export type HasuraNamespace = "https://hasura.io/jwt/claims";
+export type HasuraClaims = {
+  "x-hasura-allowed-roles": Role[];
+  "x-hasura-default-role": Role;
+  "x-hasura-user-id": string;
+};
+export type HasuraJWT = Record<HasuraNamespace, HasuraClaims>;
+
+export type JWTData = HasuraJWT & {
+  sub: string;
+  email: string;
+};
