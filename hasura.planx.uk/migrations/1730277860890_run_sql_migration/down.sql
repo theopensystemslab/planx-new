22c8d6484b961f30ba0a11d1fc7dcd4f4c32e6aa
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-10-30 11:40:48 +0000

    feat: Update trigger

---
+++ hasura.planx.uk/migrations/1730277860890_run_sql_migration/down.sql
@@ -0,0 +1,29 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION grant_new_user_template_access() RETURNS trigger AS $$
+-- DECLARE
+--     templates_team_id INT;
+--     is_demo_user BOOLEAN;
+-- BEGIN
+--     SELECT EXISTS (
+--         SELECT 1
+--         FROM team_members
+--         WHERE user_id = NEW.id
+--         AND role = 'demoUser'
+--     ) INTO is_demo_user;
+--
+--     -- Demo user should not get access as a teamEditor for the templates team...
+--     IF is_demo_user THEN
+--         RETURN NULL;
+--     END IF;
+--
+--     -- ...but all other users should
+--     SELECT id INTO templates_team_id FROM teams WHERE slug = 'templates';
+--     IF templates_team_id IS NOT NULL THEN
+--         INSERT INTO team_members (user_id, team_id, role)
+--         VALUES (NEW.id, templates_team_id, 'teamEditor');
+--     END IF;
+--
+--     RETURN NULL;
+-- END;
+-- $$ LANGUAGE plpgsql;
