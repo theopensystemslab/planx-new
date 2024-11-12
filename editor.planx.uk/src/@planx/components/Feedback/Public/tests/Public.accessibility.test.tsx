5ea67fc1e3b72d57b876c996798c786fdd2639ac
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-11-12 12:53:17 +0000

    Add keyboard navigation test

--- editor.planx.uk/src/@planx/components/Feedback/Public/tests/Public.accessibility.test.tsx
+++ editor.planx.uk/src/@planx/components/Feedback/Public/tests/Public.accessibility.test.tsx
@@ -1,8 +1,19 @@
+import { screen } from "@testing-library/react";
+import {
+  getInternalFeedbackMetadata,
+  insertFeedbackMutation,
+} from "lib/feedback";
 import React from "react";
 import { setup } from "testUtils";
+import { vi } from "vitest";
 import { axe } from "vitest-axe";
 
 import FeedbackComponent from "../Public";
+
+vi.mock("lib/feedback", () => ({
+  getInternalFeedbackMetadata: vi.fn(),
+  insertFeedbackMutation: vi.fn(),
+}));
 
 describe("when the Feedback component is rendered", async () => {
   it("should not have any accessibility violations", async () => {
@@ -10,8 +21,36 @@ describe("when the Feedback component is rendered", async () => {
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
+  it("should be navigable by keyboard", async () => {
+    const { user } = setup(<FeedbackComponent feedbackRequired={false} />);
+
+    const ratingButtons = screen.getAllByRole("button");
+
+    // user tabs through all rating buttons and selects the last one
+    await user.tab();
+    expect(ratingButtons[0]).toHaveFocus();
+    await user.tab();
+    expect(ratingButtons[1]).toHaveFocus();
+    await user.tab();
+    expect(ratingButtons[2]).toHaveFocus();
+    await user.tab();
+    expect(ratingButtons[3]).toHaveFocus();
+    await user.tab();
+    expect(ratingButtons[4]).toHaveFocus();
+    await user.keyboard("[Space]"); // select 'Excellent' button
+
+    await user.tab();
+    expect(screen.getByRole("textbox")).toHaveFocus();
+
+    await user.tab();
+    expect(screen.getByTestId("continue-button")).toHaveFocus();
+    await user.keyboard("[Space]"); // submits
+
+    expect(getInternalFeedbackMetadata).toBeCalled();
+    expect(insertFeedbackMutation).toBeCalledWith({
+      feedbackScore: 5,
+      feedbackType: "component",
+      userComment: "",
+    });
+  });
 });
-
-// somethhing about recovering state when pressing the back button
-
-// should be navigable by keyboard
