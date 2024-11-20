f751e18b748bf75cb2692263a84bb01049553d8f
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-11-20 13:14:47 +0000

    chore: Invite to pay section

--- editor.planx.uk/src/@planx/components/Pay/Editor/Editor.tsx
+++ editor.planx.uk/src/@planx/components/Pay/Editor/Editor.tsx
@@ -1,4 +1,3 @@
-import Box from "@mui/material/Box";
 import {
   ComponentType as TYPES,
 } from "@opensystemslab/planx-core/types";
@@ -23,6 +22,7 @@ import { Switch } from "ui/shared/Switch";
 import { ICONS } from "../../shared/icons";
 import { EditorProps } from "../../shared/types";
 import { GovPayMetadataSection } from "./GovPayMetadataSection";
+import { InviteToPaySection } from "./InviteToPaySection";
 
 export type Props = EditorProps<TYPES.Pay, Pay>;
 
@@ -159,86 +159,7 @@ const Component: React.FC<Props> = (props: Props) => {
             </ModalSectionContent>
           </ModalSection>
           <GovPayMetadataSection/>
-          <ModalSection>
-            <ModalSectionContent title="Invite to Pay" Icon={ICONS[TYPES.Pay]}>
-              <InputRow>
-                <Switch
-                  checked={values.allowInviteToPay}
-                  onChange={() =>
-                    setFieldValue("allowInviteToPay", !values.allowInviteToPay)
-                  }
-                  label="Allow applicants to invite someone else to pay"
-                />
-              </InputRow>
-              {values.allowInviteToPay ? (
-                <>
-                  <Box>
-                    <InputRow>
-                      <Input
-                        required
-                        format="large"
-                        name="secondaryPageTitle"
-                        placeholder="Card title"
-                        value={values.secondaryPageTitle}
-                        onChange={handleChange}
-                      />
-                    </InputRow>
-                  </Box>
-                  <Box pt={4}>
-                    <InputRow>
-                      <Input
-                        required
-                        format="large"
-                        name="nomineeTitle"
-                        placeholder="Title"
-                        value={values.nomineeTitle}
-                        onChange={handleChange}
-                      />
-                    </InputRow>
-                    <InputRow>
-                      <RichTextInput
-                        name="nomineeDescription"
-                        placeholder="Description"
-                        value={values.nomineeDescription}
-                        onChange={handleChange}
-                      />
-                    </InputRow>
-                  </Box>
-                  <Box pt={4}>
-                    <InputRow>
-                      <Input
-                        required
-                        format="large"
-                        name="yourDetailsTitle"
-                        placeholder="Title"
-                        value={values.yourDetailsTitle}
-                        onChange={handleChange}
-                      />
-                    </InputRow>
-                    <InputRow>
-                      <RichTextInput
-                        name="yourDetailsDescription"
-                        placeholder="Description"
-                        value={values.yourDetailsDescription}
-                        onChange={handleChange}
-                      />
-                    </InputRow>
-                    <InputRow>
-                      <Input
-                        required
-                        name="yourDetailsLabel"
-                        placeholder="Label"
-                        value={values.yourDetailsLabel}
-                        onChange={handleChange}
-                      />
-                    </InputRow>
-                  </Box>
-                </>
-              ) : (
-                <></>
-              )}
-            </ModalSectionContent>
-          </ModalSection>
+          <InviteToPaySection/>
           <MoreInformation
             changeField={handleChange}
             definitionImg={values.definitionImg}
