541424f7f1e42cefbeda8d7e1a6acd3e77ad777e
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-11-25 16:11:14 +0000

    Use existing endpoint to check email address

--- editor.planx.uk/src/pages/VerifyEmail/VerifyEmail.tsx
+++ editor.planx.uk/src/pages/VerifyEmail/VerifyEmail.tsx
@@ -1,23 +1,54 @@
-import { gql, useQuery } from "@apollo/client";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Card from "@planx/components/shared/Preview/Card";
 import { CardHeader } from "@planx/components/shared/Preview/CardHeader/CardHeader";
+import axios from "axios";
 import { useFormik } from "formik";
-import { publicClient } from "lib/graphql";
-import React, { useMemo } from "react";
+import isEmpty from "lodash/isEmpty";
+import omitBy from "lodash/omitBy";
+import React from "react";
 import InputLabel from "ui/public/InputLabel";
 import Input from "ui/shared/Input/Input";
 import InputRow from "ui/shared/InputRow";
 import { object, string } from "yup";
 
-import { TEAM_EMAIL_SETTINGS_QUERY } from "./queries/TEAM_EMAIL_SETTINGS_QUERY";
-
 const verifyEmailSchema = object({
   email: string().email("Invalid email").required("Email address required"),
 });
 
+const downloadApplication = async (
+  sessionId: string,
+  email: string,
+  team: string
+) => {
+  const urlWithParams = (url: string, params: any) =>
+    [url, new URLSearchParams(omitBy(params, isEmpty))]
+      .filter(Boolean)
+      .join("?");
+
+  const url = urlWithParams(
+    `${
+      import.meta.env.VITE_APP_API_URL
+    }/download-application-files/${sessionId}`,
+    { email, localAuthority: team }
+  );
+
+  await axios({
+    url,
+    method: "GET",
+    responseType: "blob",
+  }).then((response) => {
+    const href = URL.createObjectURL(response.data);
+    const link = document.createElement("a");
+    link.href = href;
+    link.setAttribute("download", "application.zip");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+  });
+};
 interface VerifyEmailProps {
   params: Record<string, string>;
 }
@@ -25,29 +56,11 @@ interface VerifyEmailProps {
 export const VerifyEmail = ({ params }: VerifyEmailProps): JSX.Element => {
   const { sessionId, team } = params;
 
-  const { data, error, loading } = useQuery(
-    gql`
-      ${TEAM_EMAIL_SETTINGS_QUERY}
-    `,
-    {
-      variables: { slug: team },
-      client: publicClient,
-      skip: !team,
-    }
-  );
-
-  const teamEmail = useMemo(
-    () => data?.teamSettings.submissionEmail || null,
-    [data]
-  );
-
-  const handleSubmit = (email: string) => {
-    if (email === teamEmail) {
-      const url = `${process.env.API_URL_EXT}/download-application-files/${sessionId}?email=${teamEmail}&localAuthority=${team}`;
-      window.open(url, "_blank");
-    } else {
-      // handle error
-      console.error("wrong email!");
+  const handleSubmit = async (email: string) => {
+    try {
+      await downloadApplication(sessionId, email, team);
+    } catch (err) {
+      console.error(err);
     }
   };
 
@@ -66,36 +79,36 @@ export const VerifyEmail = ({ params }: VerifyEmailProps): JSX.Element => {
       <Typography maxWidth="formWrap" variant="h1" pt={5} gutterBottom>
         Download your application
       </Typography>
-      {loading && <Typography>Loading!</Typography>}
+      {/* {loading && <Typography>Loading!</Typography>}
 
-      {!loading && (
-        <Box width="100%">
-          <Card handleSubmit={formik.handleSubmit}>
-            <CardHeader
-              title="Verify your email address"
-              description="We will use this to verify that you can download your application. Entering the correct email address will start the file download automatically."
-            />
-            <InputRow>
-              <InputLabel label={"Email address"} htmlFor={"email"}>
-                <Input
-                  bordered
-                  errorMessage={
-                    formik.touched.email && formik.errors.email
-                      ? formik.errors.email
-                      : undefined
-                  }
-                  id="email"
-                  name="email"
-                  onBlur={formik.handleBlur}
-                  onChange={formik.handleChange}
-                  type="email"
-                  value={formik.values.email}
-                />
-              </InputLabel>
-            </InputRow>
-          </Card>
-        </Box>
-      )}
+      {!loading && ( */}
+      <Box width="100%">
+        <Card handleSubmit={formik.handleSubmit}>
+          <CardHeader
+            title="Verify your email address"
+            description="We will use this to verify that you can download your application. Entering the correct email address will start the file download automatically."
+          />
+          <InputRow>
+            <InputLabel label={"Email address"} htmlFor={"email"}>
+              <Input
+                bordered
+                errorMessage={
+                  formik.touched.email && formik.errors.email
+                    ? formik.errors.email
+                    : undefined
+                }
+                id="email"
+                name="email"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="email"
+                value={formik.values.email}
+              />
+            </InputLabel>
+          </InputRow>
+        </Card>
+      </Box>
+      {/* )} */}
     </Container>
   );
 };
