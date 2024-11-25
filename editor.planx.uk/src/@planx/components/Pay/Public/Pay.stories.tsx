6c693dc2f677ca7c07837bbcaf8b6e49cc85342d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-11-25 13:46:58 +0000

    feat(storybook): Initial setup

--- editor.planx.uk/src/@planx/components/Pay/Public/Pay.stories.tsx
+++ editor.planx.uk/src/@planx/components/Pay/Public/Pay.stories.tsx
@@ -71,3 +71,20 @@ export const ForInformationOnly = {
     onConfirm: () => {},
   },
 } satisfies Story;
+
+// TODO: Setup fee breakdown amounts
+export const WithFeeBreakdown = {
+  args: {
+    title: "Pay for your application",
+    bannerTitle: "The fee is",
+    description: "The fee covers the cost of processing your application",
+    fee: 103,
+    instructionsTitle: "How to pay",
+    instructionsDescription: "Pay via GOV.UK Pay",
+    buttonTitle: "Pay",
+    onConfirm: () => {},
+    error: undefined,
+    showInviteToPay: false,
+    showFeeBreakdown: true,
+  },
+} satisfies Story;
