547b20695d635c80b9226a4d4cecf8d162060433
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-11-26 21:05:20 +0000

    Fix typing

--- editor.planx.uk/src/pages/VerifyEmail/queries/useQueryApplicationFileDownload.ts
+++ editor.planx.uk/src/pages/VerifyEmail/queries/useQueryApplicationFileDownload.ts
@@ -5,25 +5,25 @@ export const DOWNLOAD_APPLICATION_FILE_URL = `${
   import.meta.env.VITE_APP_API_URL
 }/download-application-files`;
 
-// TODO: fix the typing
-// type DownloadApplicationFileResponse = Blob;
-// interface DownloadApplicationFileResponse {
-//   data: Blob;
-// }
-
+export interface DownloadApplicationFileResponse {
+  data: Blob;
+}
 const DOWNLOAD_APPLICATION_FILE_QUERY_KEY = "DOWNLOAD_APPLICATION_FILE/GET";
 
 const useQueryApplicationFileDownload = (
   sessionId: string,
   email: string,
   team: string,
-  options?: Readonly<Omit<UseQueryOptions, "queryKey" | "queryFn">>
+  options?: Readonly<
+    Omit<
+      UseQueryOptions<DownloadApplicationFileResponse>,
+      "queryKey" | "queryFn"
+    >
+  >,
 ) => {
-  //   return useQuery<DownloadApplicationFileResponse>({
   return useQuery({
     queryKey: [DOWNLOAD_APPLICATION_FILE_QUERY_KEY, sessionId],
     queryFn: () =>
-      //   axios<DownloadApplicationFileResponse>({
       axios({
         url: `${DOWNLOAD_APPLICATION_FILE_URL}/${sessionId}`,
         method: "GET",
