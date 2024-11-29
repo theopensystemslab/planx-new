c408fc9b25f35cff3a06069185f0e36547b7be3e
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-11-29 14:32:32 +0000

    feat: Add reductions and exemptions

--- editor.planx.uk/src/@planx/components/Pay/Public/FeeBreakdown/utils.ts
+++ editor.planx.uk/src/@planx/components/Pay/Public/FeeBreakdown/utils.ts
@@ -2,6 +2,9 @@ import { z } from "zod";
 
 import { FeeBreakdown, PassportFeeFields } from "./types";
 
+const reductionKeys = /^application\.fee\.reduction\..+$/;
+const exemptionKeys = /^application\.fee\.exemption\..+$/;
+
 export const toNumber = (input: number | [number]) =>
   Array.isArray(input) ? input[0] : input;
 
@@ -20,11 +23,18 @@ export const calculateReduction = ({ amount }: PassportFeeFields) =>
 export const toFeeBreakdown = (data: PassportFeeFields): FeeBreakdown => ({
   amount: {
     applicationFee:
-      data.amount["application.fee.calculated"] || data.amount["application.fee.payable"],
+      data.amount["application.fee.calculated"] ||
+      data.amount["application.fee.payable"],
     total: data.amount["application.fee.payable"],
     vat: data.amount["application.fee.payable.vat"],
     reduction: calculateReduction(data),
-  }
+  },
+  reductions: Object.entries(data.reductions || {})
+    .filter(([_key, value]) => value)
+    .map(([key, _value]) => key.replace("application.fee.reduction.", "")),
+  exemptions: Object.entries(data.exemptions || {})
+    .filter(([_key, value]) => value)
+    .map(([key, _value]) => key.replace("application.fee.exemption.", "")),
 });
 
 const filterByKey = (data: Record<string, unknown>, key: string) => 
@@ -32,6 +42,8 @@ const filterByKey = (data: Record<string, unknown>, key: string) =>
 
 export const preProcessPassport = (data: Record<string, unknown>) => ({
   amount: filterByKey(data, "application.fee"),
+  reductions: filterByKey(data, "application.fee.reduction"),
+  exemptions: filterByKey(data, "application.fee.exemption"),
 });
 
 export const createPassportSchema = () => {
@@ -48,10 +60,27 @@ export const createPassportSchema = () => {
       "application.fee.payable.vat": feeSchema.optional().default(0),
     });
 
+  const reductionsSchema = z.record(
+    z.string().regex(reductionKeys),
+    z.array(z.string())
+      .max(1)
+      .transform((val) => val[0].toLowerCase() === "true"),
+  ).optional();
+
+  const exemptionsSchema = z
+    .record(
+      z.string().regex(exemptionKeys),
+      z.array(z.string())
+        .max(1)
+        .transform((val) => val[0].toLowerCase() === "true")
+    )
+    .optional();
+
   const schema = z
     .object({
       amount: amountsSchema,
-
+      reductions: reductionsSchema,
+      exemptions: exemptionsSchema,
     }).transform(toFeeBreakdown);
 
   return schema;
