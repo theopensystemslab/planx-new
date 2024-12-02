c948b8fdd5b331e8f6f4c5b04e2d1e1c7a4d1585
Rory Doak <rory@opensystemslab.io>
2024-12-02 14:25:23 +0000

    simplify geoJson check

--- e2e/tests/ui-driven/src/helpers/geospatialChecks.ts
+++ e2e/tests/ui-driven/src/helpers/geospatialChecks.ts
@@ -2,10 +2,8 @@ import { expect, Page } from "@playwright/test";
 import { Feature } from "geojson";
 
 export const checkGeoJsonContent = async (page: Page, geoJson: Feature) => {
-  // Wait for the map component to be present using the id
-  const mapComponent = await page.waitForSelector(
-    "my-map#property-information-map",
-  );
+  // Wait for the map component to be present
+  const mapComponent = await page.waitForSelector("my-map",);
 
   // Get the geojsonData attribute
   const geojsonData = await mapComponent.getAttribute("geojsondata");
