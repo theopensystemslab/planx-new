570df3d960737b2ea79649aaf584935f97429ced
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-12-03 18:18:45 +0000

    Remove rq from test utils

--- editor.planx.uk/src/testUtils.tsx
+++ editor.planx.uk/src/testUtils.tsx
@@ -1,14 +1,11 @@
 /* eslint-disable no-restricted-imports */
 import { ThemeProvider } from "@mui/material";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, RenderResult } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
 import React from "react";
 
 import { defaultTheme } from "./theme";
-
-const testQueryClient = new QueryClient();
 
 /**
  * Setup @testing-library/react environment with userEvent
@@ -18,9 +15,5 @@ export const setup = (
   jsx: JSX.Element,
 ): Record<"user", UserEvent> & RenderResult => ({
   user: userEvent.setup(),
-  ...render(
-    <QueryClientProvider client={testQueryClient}>
-      <ThemeProvider theme={defaultTheme}>{jsx}</ThemeProvider>
-    </QueryClientProvider>,
-  ),
+  ...render(<ThemeProvider theme={defaultTheme}>{jsx}</ThemeProvider>),
 });
