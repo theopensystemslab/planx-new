dc6692c0318b8141b46768fa40e6eea021ac6a53
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-12-03 17:39:44 +0000

    Readd loading state

--- editor.planx.uk/src/pages/SubmissionDownload/VerifySubmissionEmail.tsx
+++ editor.planx.uk/src/pages/SubmissionDownload/VerifySubmissionEmail.tsx
@@ -4,6 +4,7 @@ import Typography from "@mui/material/Typography";
 import Card from "@planx/components/shared/Preview/Card";
 import { CardHeader } from "@planx/components/shared/Preview/CardHeader/CardHeader";
 import axios, { isAxiosError } from "axios";
+import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
 import { useFormik } from "formik";
 import React, { useState } from "react";
 import InputLabel from "ui/public/InputLabel";
@@ -27,6 +28,7 @@ export const VerifySubmissionEmail = ({
 }: VerifySubmissionEmailProps): JSX.Element => {
   const { sessionId, team } = params;
   const [downloadApplicationError, setDownloadApplicationError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -34,6 +36,7 @@ export const VerifySubmissionEmail = ({
     },
     onSubmit: async (values, { resetForm }) => {
       setDownloadApplicationError("");
+      setLoading(true);
       const url = `${DOWNLOAD_APPLICATION_FILE_URL}/${sessionId}/?email=${encodeURIComponent(
         values.email,
       )}&localAuthority=${team}`;
@@ -42,12 +45,15 @@ export const VerifySubmissionEmail = ({
           responseType: "arraybuffer",
         });
         downloadZipFile(data);
-        resetForm({ values });
+        resetForm();
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
         if (isAxiosError(error)) {
           setDownloadApplicationError(
             "Sorry, something went wrong. Please try again.",
           );
+          resetForm();
         }
         console.error(error);
       }
@@ -61,40 +67,40 @@ export const VerifySubmissionEmail = ({
       <Typography maxWidth="formWrap" variant="h1" pt={5} gutterBottom>
         Download application
       </Typography>
-      {/* {isLoading ? (
+      {loading ? (
         <DelayedLoadingIndicator />
-      ) : ( */}
-      <Box width="100%">
-        <Card handleSubmit={formik.handleSubmit}>
-          <ErrorWrapper error={downloadApplicationError}>
-            <>
-              <CardHeader
-                title="Verify your submission email address"
-                description="We will use this to confirm that you have access to the submission email inbox that is set up for your team. Entering the correct email address will start the file download automatically."
-              />
-              <InputRow>
-                <InputLabel label={"Email address"} htmlFor={"email"}>
-                  <Input
-                    bordered
-                    errorMessage={
-                      formik.touched.email && formik.errors.email
-                        ? formik.errors.email
-                        : undefined
-                    }
-                    id="email"
-                    name="email"
-                    onBlur={formik.handleBlur}
-                    onChange={formik.handleChange}
-                    type="email"
-                    value={formik.values.email}
-                  />
-                </InputLabel>
-              </InputRow>
-            </>
-          </ErrorWrapper>
-        </Card>
-      </Box>
-      {/* )} */}
+      ) : (
+        <Box width="100%">
+          <Card handleSubmit={formik.handleSubmit}>
+            <ErrorWrapper error={downloadApplicationError}>
+              <>
+                <CardHeader
+                  title="Verify your submission email address"
+                  description="We will use this to confirm that you have access to the submission email inbox that is set up for your team. Entering the correct email address will start the file download automatically."
+                />
+                <InputRow>
+                  <InputLabel label={"Email address"} htmlFor={"email"}>
+                    <Input
+                      bordered
+                      errorMessage={
+                        formik.touched.email && formik.errors.email
+                          ? formik.errors.email
+                          : undefined
+                      }
+                      id="email"
+                      name="email"
+                      onBlur={formik.handleBlur}
+                      onChange={formik.handleChange}
+                      type="email"
+                      value={formik.values.email}
+                    />
+                  </InputLabel>
+                </InputRow>
+              </>
+            </ErrorWrapper>
+          </Card>
+        </Box>
+      )}
     </Container>
   );
 };
