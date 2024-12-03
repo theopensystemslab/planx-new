6c183e4f08c5c04c25a699bb549463adb5f48626
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-12-03 18:04:59 +0000

    Add application summary

--- editor.planx.uk/src/pages/SubmissionDownload/VerifySubmissionEmail.stories.tsx
+++ editor.planx.uk/src/pages/SubmissionDownload/VerifySubmissionEmail.stories.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { VerifySubmissionEmail } from "./VerifySubmissionEmail";
 
 const meta = {
-  title: "Design System/Pages/VerifyEmail",
+  title: "Design System/Pages/VerifySubmissionEmail",
   component: VerifySubmissionEmail,
 } satisfies Meta<typeof VerifySubmissionEmail>;
 
@@ -13,5 +13,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic = {
-  render: () => <VerifySubmissionEmail params={{ sessionId: "1" }} />,
+  render: () => (
+    <VerifySubmissionEmail params={{ sessionId: "1", team: "PlanX" }} />
+  ),
 };
