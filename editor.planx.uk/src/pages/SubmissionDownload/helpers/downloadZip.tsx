868c82011bf78773ea6e2fc324fbb78507f408ab
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-12-04 17:00:08 +0000

    Use correct zip filename

--- editor.planx.uk/src/pages/SubmissionDownload/helpers/downloadZip.tsx
+++ editor.planx.uk/src/pages/SubmissionDownload/helpers/downloadZip.tsx
@@ -1,4 +1,9 @@
-export const downloadZipFile = (data: string) => {
+type ZipFileName = `${string}.zip`;
+
+export const downloadZipFile = (
+  data: string,
+  options: { filename: ZipFileName },
+) => {
   if (!data) {
     console.error("No data to download");
     return;
@@ -8,7 +13,7 @@ export const downloadZipFile = (data: string) => {
     const href = URL.createObjectURL(blobData);
     const link = document.createElement("a");
     link.href = href;
-    link.setAttribute("download", "application.zip");
+    link.setAttribute("download", options.filename);
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
