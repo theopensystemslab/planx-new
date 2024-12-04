0679d9bd7e82655bd03106d90d34b4169c86166c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-12-04 15:17:47 +0000

    feat: Remove toggle for FeeBreakdown

--- editor.planx.uk/src/@planx/components/Pay/Editor/FeeBreakdownSection.tsx
+++ editor.planx.uk/src/@planx/components/Pay/Editor/FeeBreakdownSection.tsx
@@ -1,31 +1,17 @@
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
-import { useFormikContext } from "formik";
 import { hasFeatureFlag } from "lib/featureFlags";
 import React from "react";
+import { FeaturePlaceholder } from "ui/editor/FeaturePlaceholder";
 import ModalSection from "ui/editor/ModalSection";
 import ModalSectionContent from "ui/editor/ModalSectionContent";
-import InputRow from "ui/shared/InputRow";
-import { Switch } from "ui/shared/Switch";
-
-import { Pay } from "../model";
 
 export const FeeBreakdownSection: React.FC = () => {
-  const { values, setFieldValue } = useFormikContext<Pay>();
-
   if (!hasFeatureFlag("FEE_BREAKDOWN")) return null;
 
   return (
     <ModalSection>
       <ModalSectionContent title="Fee breakdown" Icon={ReceiptLongIcon}>
-        <InputRow>
-          <Switch
-            checked={values.showFeeBreakdown}
-            onChange={() =>
-              setFieldValue("showFeeBreakdown", !values.showFeeBreakdown)
-            }
-            label="Display a breakdown of the fee to the applicant"
-          />
-        </InputRow>
+      <FeaturePlaceholder title="TODO: Fee breakdown explanation"/>
       </ModalSectionContent>
     </ModalSection>
   );
