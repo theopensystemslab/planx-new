af43cbc6bdef1f071c99f2ba390ab640edeea5ee
Rory Doak <rory@opensystemslab.io>
2024-12-10 18:57:37 +0000

    lint:fix

--- e2e/tests/ui-driven/src/create-flow-with-geospatial.spec.ts
+++ e2e/tests/ui-driven/src/create-flow-with-geospatial.spec.ts
@@ -185,7 +185,9 @@ test.describe("Flow creation, publish and preview", () => {
       ),
     ).toBeVisible();
 
-    await expect(page.getByRole('button', { name: 'is, or is within, a Listed' })).toBeVisible()
+    await expect(
+      page.getByRole("button", { name: "is, or is within, a Listed" }),
+    ).toBeVisible();
 
     // TODO: answer uploadAndLabel
     // TODO: answerPropertyInfo, answerDrawBoundary, answerPlanningConstraints
