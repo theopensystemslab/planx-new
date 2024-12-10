b4d18258b7b4dda812743650998241b542cbdfb7
Rory Doak <rory@opensystemslab.io>
2024-12-10 16:20:56 +0000

    refine comments

--- e2e/tests/ui-driven/src/helpers/geospatialChecks.ts
+++ e2e/tests/ui-driven/src/helpers/geospatialChecks.ts
@@ -1,37 +1,38 @@
-import {  expect, Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 import { Feature } from "geojson";
 
 export const waitForMapComponent = async (page: Page) => {
   await page.waitForFunction(() => {
-    const map = document.getElementById('draw-boundary-map')
-    return map
+    const map = document.getElementById("draw-boundary-map");
+    return map;
   });
+};
 
-}
-
-export const getMapProperties = async (page: Page, attribute:"geojsondata" | "drawgeojsondata") => {
+export const getMapProperties = async (
+  page: Page,
+  attribute: "geojsondata" | "drawgeojsondata",
+) => {
   const mapComponent = await page.waitForSelector("my-map");
- return await mapComponent.getAttribute(attribute);
-}
+  return await mapComponent.getAttribute(attribute);
+};
 
 export const alterDrawGeoJson = async (page: Page) => {
-const map = page.getByTestId('map-test-id')
+  const map = page.getByTestId("map-test-id");
 
-await map.click({button:"left", position:{x:100, y:200}})
-await map.click({button:"left", position:{x:150, y:250}})
-await map.click({button:"left", position:{x:200, y:250}})
-await map.click({button:"left", position:{x:100, y:200}})
+  await map.click({ button: "left", position: { x: 100, y: 200 } });
+  await map.click({ button: "left", position: { x: 150, y: 250 } });
+  await map.click({ button: "left", position: { x: 200, y: 250 } });
+  await map.click({ button: "left", position: { x: 100, y: 200 } });
+};
 
- }
+export const resetMapBoundary = async (page: Page) => {
+  const resetButton = page.getByLabel("Reset map view");
+  await resetButton.click();
 
- export const resetMapBoundary = async (page: Page)=>{
-     const resetButton = page.getByLabel('Reset map view')
-   await resetButton.click()
+  const resetGeoJson = await getMapProperties(page, "drawgeojsondata");
 
-  const resetGeoJson = await getMapProperties(page, "drawgeojsondata") 
-
-  expect(resetGeoJson, "drawGeoJsonData should be reset").toEqual(null)
- }
+  expect(resetGeoJson, "drawGeoJsonData should be reset").toEqual(null);
+};
 
 export const checkGeoJsonContent = async (
   page: Page,
@@ -41,7 +42,7 @@ export const checkGeoJsonContent = async (
   // Wait for the map component to be present
   const mapComponent = await page.waitForSelector("my-map");
 
-  await page.waitForFunction(() => customElements.get('my-map'));
+  await page.waitForFunction(() => customElements.get("my-map"));
   await expect(
     page.getByTestId("map-test-id"),
     "Check we can see the map",
@@ -50,7 +51,10 @@ export const checkGeoJsonContent = async (
   // Get the geojsonData attribute
   const geojsonData = await mapComponent.getAttribute(attribute);
 
-  expect(JSON.parse(geojsonData!), "map attribute matches expected mock attribute").toEqual(geoJson);
+  expect(
+    JSON.parse(geojsonData!),
+    "map attribute matches expected mock attribute",
+  ).toEqual(geoJson);
 };
 
 export const checkUploadFileAltRoute = async (page: Page) => {
