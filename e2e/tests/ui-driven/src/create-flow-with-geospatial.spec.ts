26a3a80a49e5beeb4f0f82b3701170d1349b38e9
Rory Doak <rory@opensystemslab.io>
2024-12-10 18:57:23 +0000

    add checks up to the first constraint displaying for listed building

--- e2e/tests/ui-driven/src/create-flow-with-geospatial.spec.ts
+++ e2e/tests/ui-driven/src/create-flow-with-geospatial.spec.ts
@@ -242,6 +242,8 @@ test.describe("Flow creation, publish and preview", () => {
       ),
     ).toBeVisible();
 
+    await expect(page.getByRole('button', { name: 'is, or is within, a Listed' })).toBeVisible()
+
     // TODO: answer uploadAndLabel
     // TODO: answerPropertyInfo, answerPlanningConstraints
   });
