72ec6c4da429cd20f6b00b90247ab0910befd5a0
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2024-12-11 15:29:37 +0000

    test(e2e): Setup `FEE_BREAKDOWN` feature flag (#4065)

--- e2e/tests/ui-driven/src/invite-to-pay/nominee.spec.ts
+++ e2e/tests/ui-driven/src/invite-to-pay/nominee.spec.ts
@@ -8,7 +8,7 @@ import {
   setUpTestContext,
   tearDownTestContext,
 } from "../helpers/context";
-import { cards } from "../helpers/globalHelpers";
+import { cards, setFeatureFlag } from "../helpers/globalHelpers";
 import { fillGovUkCardDetails } from "../helpers/userActions";
 import inviteToPayFlow from "../mocks/flows/invite-to-pay-flow";
 import { getPaymentRequestBySessionId } from "./helpers";
@@ -40,6 +40,10 @@ test.describe("Nominee journey @regression", async () => {
     }
   });
 
+  test.beforeEach(async ({ page }) => {
+    await setFeatureFlag(page, "FEE_BREAKDOWN");
+  });
+
   test.afterAll(async () => {
     await tearDownTestContext();
   });
@@ -60,7 +64,10 @@ test.describe("Nominee journey @regression", async () => {
       "Add a verandah or deck and changes to internal walls or layout";
     await expect(page.getByText(formattedProjectType)).toBeVisible();
 
-    await expect(page.getByText("Fee breakdown")).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Fee", level: 3 }),
+    ).toBeVisible();
+    await expect(page.getByTestId("fee-breakdown-table")).toBeVisible();
 
     const payButton = page.getByRole("button", {
       name: "Pay using GOV.UK Pay",
