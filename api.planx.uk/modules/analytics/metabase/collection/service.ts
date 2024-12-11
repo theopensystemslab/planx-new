50f0c1821ffa069612fb624c808ec6998e5036a2
zz-hh-aa <zzhhaa@protonmail.com>
2024-12-11 17:24:55 +0000

    feat: createCollectionIfDoesNotExist function

---
+++ api.planx.uk/modules/analytics/metabase/collection/service.ts
@@ -0,0 +1,36 @@
+import { createMetabaseClient } from "../shared/client.js";
+import { updateMetabaseId } from "./updateMetabaseId.js";
+import type { NewCollectionParams } from "./types.js";
+import { getTeamIdAndMetabaseId } from "./getTeamIdAndMetabaseId.js";
+import { createCollection } from "./createCollection.js";
+
+const client = createMetabaseClient();
+
+/**
+ * First uses name to get teams.id and .metabase_id, if present.
+ * If metabase_id is null, return an object that includes its id. If not, create the collection.
+ * @params `name` is required, but `description` and `parent_id` are optional.
+ * @returns `response.data`, so use dot notation to access `id` or `parent_id`.
+ */
+export async function createCollectionIfDoesNotExist(
+  params: NewCollectionParams,
+): Promise<number> {
+  try {
+    const { metabaseId, id: teamId } = await getTeamIdAndMetabaseId(
+      params.name,
+    );
+
+    if (metabaseId) {
+      return metabaseId;
+    }
+
+    // Create new Metabase collection if !metabaseId
+    const newMetabaseId = await createCollection(params);
+    await updateMetabaseId(teamId, newMetabaseId);
+    console.log({ newMetabaseId });
+    return newMetabaseId;
+  } catch (error) {
+    console.error("Error in createCollectionIfDoesNotExist:", error);
+    throw error;
+  }
+}
