79b9f86c9abc0e7b764d4b64ad4a55f56762ac1d
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-12-18 15:36:15 +0000

    sort imports

--- editor.planx.uk/src/@planx/components/Checklist/Public/helpers.ts
+++ editor.planx.uk/src/@planx/components/Checklist/Public/helpers.ts
@@ -2,7 +2,6 @@ import { type Group } from "@planx/components/Checklist/model";
 import { Store } from "pages/FlowEditor/lib/store";
 
 import { Option } from "../../shared";
-import { FormikProps } from "formik";
 
 export function toggleInArray<T>(value: T, arr: Array<T>): Array<T> {
   return arr.includes(value)
