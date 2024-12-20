a4b53433d9e9874e2b3cc90ca3d838fabe7c49e5
Rory Doak <rory@opensystemslab.io>
2024-12-20 11:32:26 +0000

    refine logic in Card component

--- editor.planx.uk/src/@planx/components/shared/Preview/NavigateToPublishedButton.tsx
+++ editor.planx.uk/src/@planx/components/shared/Preview/NavigateToPublishedButton.tsx
@@ -2,34 +2,19 @@ import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import React from "react";
 import { useNavigation } from "react-navi";
-
-import { InnerContainer } from "./SaveResumeButton";
-
 const NavigateToPublishedButton: React.FC = () => {
-  const navigation = useNavigation();
-
-  const testEnvironment = window.location.pathname.endsWith("/draft")
-    ? "/draft"
-    : "/preview";
-
-  const editorLink = window.location.pathname.replace(
-    testEnvironment,
-    "/published",
-  ) as `/${string}`;
+  const { navigate } = useNavigation();
 
   const handleClick = () => {
-    navigation.navigate(editorLink);
+    navigate("published");
   };
 
   return (
-    <InnerContainer>
-      <Typography variant="body1">or</Typography>
-      <Link onClick={handleClick} component="button">
-        <Typography variant="body1" textAlign="left">
-          Go to the live service
-        </Typography>
-      </Link>
-    </InnerContainer>
+    <Link onClick={handleClick} component="button">
+      <Typography variant="body1" textAlign="left">
+        Go to the live service
+      </Typography>
+    </Link>
   );
 };
 
