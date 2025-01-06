de7130b0638c159fc01c6031724caf2ab32a726f
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-01-06 17:39:52 +0000

    Test that download zip file is called when correct information supplied

--- editor.planx.uk/src/pages/SubmissionDownload/tests/VerifySubmissionEmail.test.tsx
+++ editor.planx.uk/src/pages/SubmissionDownload/tests/VerifySubmissionEmail.test.tsx
@@ -1,9 +1,15 @@
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
 import React from "react";
 import { setup } from "testUtils";
+import { vi } from "vitest";
 import { axe } from "vitest-axe";
 
+import { downloadZipFile } from "../helpers/downloadZip";
 import { VerifySubmissionEmail } from "../VerifySubmissionEmail";
+
+vi.mock("./../helpers/downloadZip.tsx");
 
 describe("when the VerifySubmissionEmail component renders", () => {
   it("displays the email address input", () => {
@@ -50,8 +56,40 @@ describe("when the VerifySubmissionEmail component renders", () => {
 });
 
 describe("when the user submits a correct email address", () => {
+  const mockData = new ArrayBuffer();
+  const server = setupServer(
+    http.get(
+      "http://localhost:7002/download-application-files/a-session-id/?email=submission%40council.com&localAuthority=barking-and-dagenham",
+      () => {
+        return new HttpResponse(mockData, { status: 200 });
+      },
+    ),
+  );
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
   it.todo("displays visual feedback to the user");
-  it.todo("downloads the application file");
+
+  it("downloads the application file", async () => {
+    const { user } = setup(
+      <VerifySubmissionEmail
+        params={{ sessionId: "a-session-id", team: "barking-and-dagenham" }}
+      />,
+    );
+
+    const emailInput = screen.getByLabelText("Submission email address");
+    await user.type(emailInput, "submission@council.com");
+    await user.click(screen.getByRole("button", { name: "Continue" }));
+
+    await waitFor(() =>
+      expect(downloadZipFile).toHaveBeenCalledWith(mockData, {
+        // the filename should be in the form `${flow}-${sessionId}.zip`
+        filename: "undefined-a-session-id.zip", // undefined as we have not mocked a flow
+      }),
+    );
+  });
 });
 
 describe("when the user submits an incorrect email address", () => {
