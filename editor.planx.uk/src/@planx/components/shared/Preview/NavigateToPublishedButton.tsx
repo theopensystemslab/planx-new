d47c96b2dd5dbfb0bbcdc0691e5d971f8a27842f
Rory Doak <rory@opensystemslab.io>
2025-01-07 11:22:59 +0000

    reset flow on navigation to published

--- editor.planx.uk/src/@planx/components/shared/Preview/NavigateToPublishedButton.tsx
+++ editor.planx.uk/src/@planx/components/shared/Preview/NavigateToPublishedButton.tsx
@@ -1,18 +1,24 @@
 import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
+import { clearLocalFlow } from "lib/local";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 import { useNavigation } from "react-navi";
 
 const NavigateToPublishedButton: React.FC = () => {
   const { navigate } = useNavigation();
+  const id = useStore().id;
+
   const handleClick = () => {
-    navigate("published");
+    clearLocalFlow(id);
+    navigate("published?analytics=false");
+    window.location.reload();
   };
 
   return (
     <Link onClick={handleClick} component="button">
       <Typography variant="body1" textAlign="left">
-        Go to the live service
+        Go to the published version of this service
       </Typography>
     </Link>
   );
