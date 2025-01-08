052305f60935f5bbcf28da998f40df118cc5904b
Ian Jones <hello@ianjo.uk>
2025-01-08 10:57:11 +0000

    import inactive colour for icon

--- editor.planx.uk/src/ui/icons/ToggleOverlay.tsx
+++ editor.planx.uk/src/ui/icons/ToggleOverlay.tsx
@@ -5,6 +5,7 @@ import * as React from "react";
 export default function EditorIcon(props: SvgIconProps) {
   const theme = useTheme();
   const inactiveColor = theme.palette.text.disabled;
+  const overlayColor = theme.palette.background.paper;
 
   return (
     <SvgIcon {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -12,7 +13,10 @@ export default function EditorIcon(props: SvgIconProps) {
         points="0 0 1.45 0 24 22.5 24 24 22.46 24 0 1.52 0 0"
         fill={inactiveColor}
       />
-      <polygon points="1.45 0 4.48 0 24 19.55 24 22.5 1.45 0" fill="#fff" />
+      <polygon
+        points="1.45 0 4.48 0 24 19.55 24 22.5 1.45 0"
+        fill={overlayColor}
+      />
     </SvgIcon>
   );
 }
