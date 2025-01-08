b3454f5a17119eca4802a9fa513d613f1d4d6a7a
Ian Jones <hello@ianjo.uk>
2025-01-08 10:16:07 +0000

    import inactive colour for icon

--- editor.planx.uk/src/ui/icons/ToggleOverlay.tsx
+++ editor.planx.uk/src/ui/icons/ToggleOverlay.tsx
@@ -1,12 +1,16 @@
+import { useTheme } from "@mui/material/styles";
 import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
 import * as React from "react";
 
 export default function EditorIcon(props: SvgIconProps) {
+  const theme = useTheme();
+  const inactiveColor = theme.palette.text.disabled;
+
   return (
     <SvgIcon {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <polygon
         points="0 0 1.45 0 24 22.5 24 24 22.46 24 0 1.52 0 0"
-        fill="#68787D"
+        fill={inactiveColor}
       />
       <polygon points="1.45 0 4.48 0 24 19.55 24 22.5 1.45 0" fill="#fff" />
     </SvgIcon>
