a1a94119e50e585b2440904d633bb9b675ac2e7c
Rory Doak <rory@opensystemslab.io>
2025-01-09 16:06:58 +0000

    add in has_send_component

--- scripts/seed-database/write/published_flows.sql
+++ scripts/seed-database/write/published_flows.sql
@@ -5,10 +5,11 @@ CREATE TEMPORARY TABLE sync_published_flows (
   flow_id uuid,
   summary text,
   publisher_id int,
-  created_at timestamptz
+  created_at timestamptz,
+  has_send_component boolean
   );
 
-\copy sync_published_flows (id, data, flow_id, summary, publisher_id, created_at) FROM '/tmp/published_flows.csv' (FORMAT csv, DELIMITER ';');
+\copy sync_published_flows (id, data, flow_id, summary, publisher_id, created_at, has_send_component) FROM '/tmp/published_flows.csv' (FORMAT csv, DELIMITER ';');
 
 INSERT INTO published_flows (
   id,
@@ -16,7 +17,8 @@ INSERT INTO published_flows (
   flow_id,
   summary,
   publisher_id,
-  created_at
+  created_at,
+  has_send_component
   )
 SELECT
   id,
@@ -24,7 +26,8 @@ SELECT
   flow_id,
   summary,
   publisher_id,
-  created_at
+  created_at,
+  has_send_component
 FROM sync_published_flows
 ON CONFLICT (id) DO UPDATE
 SET
@@ -32,4 +35,5 @@ SET
   flow_id = EXCLUDED.flow_id,
   summary = EXCLUDED.summary,
   publisher_id = EXCLUDED.publisher_id,
-  created_at = EXCLUDED.created_at;
+  created_at = EXCLUDED.created_at,
+  has_send_component = EXCLUDED.has_send_component;
