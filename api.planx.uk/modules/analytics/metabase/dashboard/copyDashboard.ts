6bdf10baacaaf261c39bb7590a2a9e053a9aea55
zz-hh-aa <zzhhaa@protonmail.com>
2025-01-14 10:29:28 +0000

    fix: handle camelCase snake_case conversion

--- api.planx.uk/modules/analytics/metabase/dashboard/copyDashboard.ts
+++ api.planx.uk/modules/analytics/metabase/dashboard/copyDashboard.ts
@@ -1,13 +1,16 @@
 import type { CopyDashboardParams } from "./types.js";
 import { $metabase } from "../shared/client.js";
+import { toMetabaseParams } from "./types.js";
 
 /** Returns the ID of the copied dashboard. */
 export async function copyDashboard(
   params: CopyDashboardParams,
 ): Promise<number> {
+  const metabaseParams = toMetabaseParams(params);
+  console.log({ metabaseParams });
   const response = await $metabase.post(
     `/api/dashboard/${params.templateId}/copy`,
-    params,
+    metabaseParams,
   );
   return response.data.id;
 }
