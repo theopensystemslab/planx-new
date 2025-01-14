05e622e94d1868bc2cead9f0a1de7430adcc22dd
zz-hh-aa <zzhhaa@protonmail.com>
2025-01-14 16:53:39 +0000

    fix: dashboard service following tests

--- api.planx.uk/modules/analytics/metabase/dashboard/getDashboard.ts
+++ api.planx.uk/modules/analytics/metabase/dashboard/getDashboard.ts
@@ -1,11 +1,10 @@
 import { $metabase } from "../shared/client.js";
 
-/** Takes dashboard ID and returns dashboard name. */
+/** Takes dashboard ID and returns dashboard with name as param; it's useful to return `response.data` to access other properties in testing. */
 export async function getDashboard(dashboardId: number): Promise<any> {
   try {
     const response = await $metabase.get(`/api/dashboard/${dashboardId}`);
-    console.log("Original dashboard name: ", response.data.name);
-    return response.data.name;
+    return response.data;
   } catch (error) {
     console.error("Error in getDashboard:", error);
     throw error;
