4b57fd6397d6b3f5d69816a70d772088d3613ceb
Rory Doak <138574807+RODO94@users.noreply.github.com>
2025-01-20 16:20:01 +0000

    feat: populate database for `has_send_comp` and add to api mutation (#4156)

--- api.planx.uk/modules/flows/publish/service.ts
+++ api.planx.uk/modules/flows/publish/service.ts
@@ -1,9 +1,14 @@
 import * as jsondiffpatch from "jsondiffpatch";
 import { dataMerged, getMostRecentPublishedFlow } from "../../../helpers.js";
 import { gql } from "graphql-request";
-import type { FlowGraph, Node } from "@opensystemslab/planx-core/types";
+import {
+  ComponentType,
+  type FlowGraph,
+  type Node,
+} from "@opensystemslab/planx-core/types";
 import { userContext } from "../../auth/middleware.js";
 import { getClient } from "../../../client/index.js";
+import { hasComponentType } from "../validate/helpers.js";
 
 interface PublishFlow {
   publishedFlow: {
@@ -21,6 +26,7 @@ export const publishFlow = async (flowId: string, summary?: string) => {
 
   const flattenedFlow = await dataMerged(flowId);
   const mostRecent = await getMostRecentPublishedFlow(flowId);
+  const hasSendComponent = hasComponentType(flattenedFlow, ComponentType.Send);
   const delta = jsondiffpatch.diff(mostRecent, flattenedFlow);
 
   if (!delta) return null;
@@ -33,6 +39,7 @@ export const publishFlow = async (flowId: string, summary?: string) => {
         $flow_id: uuid
         $publisher_id: Int
         $summary: String
+        $has_send_component: Boolean
       ) {
         publishedFlow: insert_published_flows_one(
           object: {
@@ -40,6 +47,7 @@ export const publishFlow = async (flowId: string, summary?: string) => {
             flow_id: $flow_id
             publisher_id: $publisher_id
             summary: $summary
+            has_send_component: $has_send_component
           }
         ) {
           id
@@ -55,6 +63,7 @@ export const publishFlow = async (flowId: string, summary?: string) => {
       flow_id: flowId,
       publisher_id: parseInt(userId),
       summary: summary ?? null,
+      has_send_component: hasSendComponent,
     },
   );
 
