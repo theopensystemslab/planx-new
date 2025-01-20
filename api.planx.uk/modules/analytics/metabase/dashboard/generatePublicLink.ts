014b0a4784ac7f1215c7f276c731e67f855adf07
zz-hh-aa <zzhhaa@protonmail.com>
2025-01-20 16:03:00 +0000

    nit: tidy comments and imports

--- api.planx.uk/modules/analytics/metabase/dashboard/generatePublicLink.ts
+++ api.planx.uk/modules/analytics/metabase/dashboard/generatePublicLink.ts
@@ -1,6 +1,5 @@
 import { $metabase } from "../shared/client.js";
 
-/** Returns the ID of the copied dashboard. */
 export async function generatePublicLink(params: number): Promise<string> {
   const response = await $metabase.post(`/api/dashboard/${params}/public_link`);
   const url = `${process.env.METABASE_URL_EXT}/public/dashboard/${response.data.uuid}`;
--- api.planx.uk/modules/analytics/metabase/dashboard/types.ts
+++ api.planx.uk/modules/analytics/metabase/dashboard/types.ts
@@ -5,10 +5,9 @@ export interface CreateNewDashboardParams {
   teamName: string;
   /** Original / template Metabase Dashboard ID, it is the number that follows /dashboard/ in the URL */
   templateId: number;
-  /** What the copied dashboard should be named;
-   * should be the original dashboard name
+  /** What the copied dashboard should be named; should be the original dashboard name
    * but with 'Template' replaced with council name */
-  // name?: string;
+  name?: string;
   /** Optional text to be displayed as the dashboard description */
   description?: string;
   /** Number for the copied dashboard's parent collection */
