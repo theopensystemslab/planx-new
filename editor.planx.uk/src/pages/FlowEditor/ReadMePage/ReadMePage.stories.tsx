ee956c08d56970f7c2cdf2504f3f722a4b9df3d2
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-01-21 22:05:08 +0000

    Add storybook file for ReadMePage

---
+++ editor.planx.uk/src/pages/FlowEditor/ReadMePage/ReadMePage.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { ReadMePage } from "./ReadMePage";
+
+const meta = {
+  title: "Design System/Pages/ReadMe",
+  component: ReadMePage,
+} satisfies Meta<typeof ReadMePage>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic = {
+  args: {
+    flowSlug: "find-out-if-you-need-planning-permission",
+    flowInformation: {
+      status: "online",
+      description: "A long description of a service",
+      summary: "A short blurb",
+      limitations: "",
+      settings: {},
+    },
+  },
+} satisfies Story;
