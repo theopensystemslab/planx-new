f3ab39b22889a8f0fe7ed4bce0a758623aeff072
Rory Doak <rory@opensystemslab.io>
2025-01-23 15:22:57 +0000

    fix script formatting

--- scripts/seed-database/write/published_flows.sql
+++ scripts/seed-database/write/published_flows.sql
@@ -6,7 +6,8 @@ CREATE TEMPORARY TABLE sync_published_flows (
   summary text,
   publisher_id int,
   created_at timestamptz,
-  has_send_component boolean  );
+  has_send_component boolean  
+  );
 
 /* Ensure columns here are kept in sync with container.sh */
 \copy sync_published_flows (id, data, flow_id, summary, publisher_id, created_at, has_send_component) FROM '/tmp/published_flows.csv' (FORMAT csv, DELIMITER ';');
@@ -18,7 +19,8 @@ INSERT INTO published_flows (
   summary,
   publisher_id,
   created_at,
-  has_send_component  )
+  has_send_component  
+  )
 SELECT
   id,
   data,
