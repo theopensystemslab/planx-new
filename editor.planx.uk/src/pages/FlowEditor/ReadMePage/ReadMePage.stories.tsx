7679a4c9f817d889cb272518ab65e3299a215520
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-01-23 16:18:40 +0000

    Fix story error

--- editor.planx.uk/src/pages/FlowEditor/ReadMePage/ReadMePage.stories.tsx
+++ editor.planx.uk/src/pages/FlowEditor/ReadMePage/ReadMePage.stories.tsx
@@ -14,6 +14,7 @@ type Story = StoryObj<typeof meta>;
 export const Basic = {
   args: {
     flowSlug: "find-out-if-you-need-planning-permission",
+    teamSlug: "barnet",
     flowInformation: {
       status: "online",
       description: "A long description of a service",
