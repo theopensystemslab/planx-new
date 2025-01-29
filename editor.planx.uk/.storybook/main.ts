8cb4dc968c516ad1ff9f5bca69338708bdf92879
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-01-29 11:35:34 +0000

    fix: Update Storybook config for Vite v6

--- editor.planx.uk/.storybook/main.ts
+++ editor.planx.uk/.storybook/main.ts
@@ -1,18 +1,27 @@
-module.exports = {
+import { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     "@storybook/addon-a11y",
   ],
   staticDirs: ["../public"],
   framework: {
     name: "@storybook/react-vite",
-    options: {}
+    options: {
+      builder: {
+        viteConfigPath: "vite.config.ts",
+      },
+    },
   },
-  docs: {},
   typescript: {
-    reactDocgen: "react-docgen-typescript"
-  }
+    reactDocgen: "react-docgen-typescript",
+  },
+  docs: {
+    autodocs: true,
+  },
 };
+
+export default config;
