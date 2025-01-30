52575561369ceb75f0a99eca68da6c34dec5fdef
zz-hh-aa <zzhhaa@protonmail.com>
2025-01-30 13:34:02 +0000

    nit: tidy comments, types and var names

--- api.planx.uk/modules/analytics/metabase/dashboard/copyDashboard.ts
+++ api.planx.uk/modules/analytics/metabase/dashboard/copyDashboard.ts
@@ -1,13 +1,11 @@
-import type { CopyDashboardParams } from "./types.js";
+import type { CopyDashboardParams, GetDashboardResponse } from "./types.js";
 import { $metabase } from "../shared/client.js";
 import { toMetabaseParams } from "./types.js";
 
-/** Returns the ID of the copied dashboard. */
 export async function copyDashboard(
   params: CopyDashboardParams,
-): Promise<number> {
+): Promise<GetDashboardResponse["id"]> {
   const metabaseParams = toMetabaseParams(params);
-  console.log({ metabaseParams });
   const response = await $metabase.post(
     `/api/dashboard/${params.templateId}/copy`,
     metabaseParams,
