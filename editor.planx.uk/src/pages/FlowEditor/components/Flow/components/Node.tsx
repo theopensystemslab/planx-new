bf5343f5fcf65ed99b2ee414d29d335c89be0189
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-02-04 10:14:19 +0100

    account for undefined node

--- editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Node.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Flow/components/Node.tsx
@@ -24,7 +24,7 @@ const Node: React.FC<any> = (props) => {
   const allProps = {
     ...props,
     wasVisited,
-    tags: node.data?.tags,
+    tags: node?.data?.tags,
   };
 
   const type = props.type as TYPES;
