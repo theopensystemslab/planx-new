40e4e312c27b4fbcda2ad81d1148de0228bd96dc
Ian Jones <hello@ianjo.uk>
2025-02-05 15:41:42 +0000

    remove replicated add service button

--- editor.planx.uk/src/pages/Team.tsx
+++ editor.planx.uk/src/pages/Team.tsx
@@ -231,7 +231,6 @@ const Team: React.FC = () => {
             alignItems: "flex-end",
           }}
         >
-          {showAddFlowButton && <AddFlowButton flows={flows} />}
           {showAddFlowButton && hasFeatureFlag("TEMPLATES") && (
             <StartFromTemplateButton />
           )}
