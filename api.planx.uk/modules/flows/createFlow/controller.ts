bfa34d94b8b6bcb1e71a914dea2baf23959f122a
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-02-06 10:03:53 +0100

    feat[api]: create flows from templates (#4254)

---
+++ api.planx.uk/modules/flows/createFlow/controller.ts
@@ -0,0 +1,49 @@
+import type { FlowGraph } from "@opensystemslab/planx-core/types";
+import { z } from "zod";
+import { ServerError } from "../../../errors/index.js";
+import { createFlow } from "../../../helpers.js";
+import type { ValidatedRequestHandler } from "../../../shared/middleware/validate.js";
+import type { Flow } from "../../../types.js";
+
+interface CreateFlowResponse {
+  message: string;
+  id: Flow["id"];
+  slug: Flow["slug"];
+}
+
+export const createFlowSchema = z.object({
+  body: z.object({
+    teamId: z.number(),
+    slug: z.string(),
+    name: z.string(),
+  }),
+});
+
+export type CreateFlowController = ValidatedRequestHandler<
+  typeof createFlowSchema,
+  CreateFlowResponse
+>;
+
+export const createFlowController: CreateFlowController = async (
+  _req,
+  res,
+  next,
+) => {
+  try {
+    const { teamId, slug, name } = res.locals.parsedReq.body;
+    const initialFlowData: FlowGraph = { _root: { edges: [] } };
+
+    // createFlow automatically handles the associated operation and initial publish
+    const { id } = await createFlow(teamId, slug, name, initialFlowData);
+
+    res.status(200).send({
+      message: `Successfully created flow ${slug}`,
+      id,
+      slug,
+    });
+  } catch (error) {
+    return next(
+      new ServerError({ message: `Failed to create flow. Error: ${error}` }),
+    );
+  }
+};
