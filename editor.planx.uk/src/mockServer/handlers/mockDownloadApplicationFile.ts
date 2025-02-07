9eeb933540e3ec327863423277acf5cc6ac57a5e
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-02-07 00:42:33 +0000

    Fix slashes

--- editor.planx.uk/src/mockServer/handlers/mockDownloadApplicationFile.ts
+++ editor.planx.uk/src/mockServer/handlers/mockDownloadApplicationFile.ts
@@ -4,21 +4,21 @@ import { DOWNLOAD_APPLICATION_FILE_URL } from "pages/SubmissionDownload/VerifySu
 export const mockData = new ArrayBuffer(100); // a byte number is needed
 
 export const getWithData = http.get(
-  `${DOWNLOAD_APPLICATION_FILE_URL}/a-session-id/?email=submission%40council.com&localAuthority=barking-and-dagenham`,
+  `${DOWNLOAD_APPLICATION_FILE_URL}/a-session-id?email=submission%40council.com&localAuthority=barking-and-dagenham`,
   () => {
     return new HttpResponse(mockData, { status: 200 });
   },
 );
 
 export const getWithServerSideError = http.get(
-  `${DOWNLOAD_APPLICATION_FILE_URL}/a-session-id/?email=submission%40council.com&localAuthority=barking-and-dagenham`,
+  `${DOWNLOAD_APPLICATION_FILE_URL}/a-session-id?email=submission%40council.com&localAuthority=barking-and-dagenham`,
   () => {
     return new HttpResponse(null, { status: 500 });
   },
 );
 
 export const getWith403 = http.get(
-  `${DOWNLOAD_APPLICATION_FILE_URL}/a-session-id/?email=wrong_email%40council.com&localAuthority=barking-and-dagenham`,
+  `${DOWNLOAD_APPLICATION_FILE_URL}/a-session-id?email=wrong_email%40council.com&localAuthority=barking-and-dagenham`,
   () => {
     return new HttpResponse(null, { status: 403 });
   },
