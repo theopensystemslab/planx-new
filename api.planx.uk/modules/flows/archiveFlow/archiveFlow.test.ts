300db02f71a019367dd918af098cbb1fd22dd398
Rory Doak <rory.doak@gmail.com>
2025-02-14 12:01:27 +0000

    add api tests for archiving flow

--- api.planx.uk/modules/flows/archiveFlow/archiveFlow.test.ts
+++ api.planx.uk/modules/flows/archiveFlow/archiveFlow.test.ts
@@ -1,64 +1,49 @@
 import { queryMock } from "../../../tests/graphqlQueryMock.js";
+import supertest from "supertest";
+import app from "../../../server.js";
+import { authHeader } from "../../../tests/mockJWT.js";
 
 beforeEach(() => {
   queryMock.mockQuery({
-    name: "GetTeamBySlug",
-    variables: {
-      slug: "new-team",
-    },
-    data: {
-      teams: [
-        {
-          id: "1",
-        },
-      ],
-    },
-  });
-
-  queryMock.mockQuery({
-    name: "UpdateFlow",
+    name: "ArchiveFlow",
     variables: {
       id: "1",
-      team_id: "1",
     },
     data: {
       flow: {
         id: "1",
+        name: "ArchiveFlow",
       },
     },
   });
 });
 
-test.todo("returns an error if authorization headers are not set", async () => {
-  return;
+it("returns an error if authorization headers are not set", async () => {
+  await supertest(app)
+    .post("/flows/1/archive")
+    .expect(401)
+    .then((res) => {
+      expect(res.body).toEqual({
+        error: "No authorization token was found",
+      });
+    });
 });
 
-test.todo(
-  "returns an error if the user does not have the 'teamEditor' role",
-  async () => {
-    return;
-  },
-);
-
-test.todo("archives a flow", async () => {
-  return;
+it("returns an error if the user does not have the 'teamEditor' role", async () => {
+  await supertest(app)
+    .post("/flows/1/archive")
+    .set(authHeader({ role: "teamViewer" }))
+    .expect(403);
 });
 
-test.todo("returns an error when the service errors", async () => {
-  queryMock.reset();
-  queryMock.mockQuery({
-    name: "GetTeamBySlug",
-    variables: {
-      slug: "new-team",
-    },
-    data: {
-      teams: null,
-    },
-    graphqlErrors: [
-      {
-        message: "Something went wrong",
-      },
-    ],
-  });
-  return;
+it("archives a flow", async () => {
+  await supertest(app)
+    .post(`/flows/1/archive`)
+    .set(authHeader({ role: "teamEditor" }))
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toEqual({
+        message: `Successfully archived ArchiveFlow with id 1`,
+      });
+    });
 });
