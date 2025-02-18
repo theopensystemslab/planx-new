52f6311f5e970fd530a4cc63b26029011468b395
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-02-18 17:59:33 +0000

    Tidy up

--- editor.planx.uk/src/ui/shared/DataTable/components/CustomSingleSelectInput.tsx
+++ editor.planx.uk/src/ui/shared/DataTable/components/CustomSingleSelectInput.tsx
@@ -1,50 +1,34 @@
 // import SyncIcon from "@mui/icons-material/Sync";
 import Box from "@mui/material/Box";
 import TextField, { TextFieldProps } from "@mui/material/TextField";
-import { useGridRootProps } from "@mui/x-data-grid";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const CustomSingleSelectInput = (
   props: Record<string, any>,
 ): React.JSX.Element | null => {
-  const rootProps = useGridRootProps();
   const { item, applyValue, type, focusElementRef, valueOptions } = props;
 
-  // const filterTimeout = useRef<ReturnType<typeof setTimeout>>(undefined);
   const [filterValueState, setFilterValueState] = useState<[string, string]>(
     item.value ?? "",
   );
-  const [applying, setIsApplying] = useState(false);
-
-  // useEffect(() => {
-  //   return () => {
-  //     clearTimeout(filterTimeout.current);
-  //   };
-  // }, []);
 
   useEffect(() => {
     const itemValue = item.value ?? [undefined, undefined];
     setFilterValueState(itemValue);
   }, [item.value]);
 
-  const updateFilterValue = (lowerBound: string, upperBound: string) => {
-    // clearTimeout(filterTimeout.current);
-    setFilterValueState([lowerBound, upperBound]);
-
-    setIsApplying(true);
-    // filterTimeout.current = setTimeout(() => {
-    setIsApplying(false);
-    applyValue({ ...item, value: [lowerBound, upperBound] });
-    // }, rootProps.filterDebounceMs);
+  const updateFilterValue = (firstInput: string, secondInput: string) => {
+    setFilterValueState([firstInput, secondInput]);
+    applyValue({ ...item, value: [firstInput, secondInput] });
   };
 
-  const handleUpperFilterChange: TextFieldProps["onChange"] = (event) => {
-    const newUpperBound = event.target.value;
-    updateFilterValue(filterValueState[0], newUpperBound);
+  const handleFirstFilterChange: TextFieldProps["onChange"] = (event) => {
+    const newSecondInput = event.target.value;
+    updateFilterValue(filterValueState[0], newSecondInput);
   };
-  const handleLowerFilterChange: TextFieldProps["onChange"] = (event) => {
-    const newLowerBound = event.target.value;
-    updateFilterValue(newLowerBound, filterValueState[1]);
+  const handleSecondFilterChange: TextFieldProps["onChange"] = (event) => {
+    const newFirstInput = event.target.value;
+    updateFilterValue(newFirstInput, filterValueState[1]);
   };
 
   return (
@@ -53,22 +37,19 @@ export const CustomSingleSelectInput = (
         display: "inline-flex",
         flexDirection: "row",
         alignItems: "end",
-        pl: "20px",
+        pl: "10px",
       }}
     >
-      {/* <FormLabel className={muiFormLabelClasses}>Service name</FormLabel> need dynamic name! */}
       <TextField
         id={`contains-input-${item.id}`}
-        // value={item.value}
         value={filterValueState[0]}
-        // onChange={(event) => applyValue({ ...item, value: event.target.value })}
-        onChange={handleLowerFilterChange}
+        onChange={handleSecondFilterChange}
         type={type || "text"}
         variant="standard"
         InputLabelProps={{
           shrink: true,
         }}
-        label="bla"
+        label="Option 1"
         inputRef={focusElementRef}
         select
         SelectProps={{
@@ -82,23 +63,19 @@ export const CustomSingleSelectInput = (
         ))}
       </TextField>
       <TextField
-        // sx={{ marginTop: "-10px" }}
         id={`contains-input-${item.id}`}
-        // value={item.value}
-        // onChange={(event) => applyValue({ ...item, value: event.target.value })}
         value={filterValueState[1]}
-        onChange={handleUpperFilterChange}
+        onChange={handleFirstFilterChange}
         type={type || "text"}
         variant="standard"
         InputLabelProps={{
           shrink: true,
         }}
-        label="bla2"
+        label="Option 2"
         select
         SelectProps={{
           native: true,
         }}
-        // InputProps={applying ? { endAdornment: <SyncIcon /> } : {}}
       >
         {valueOptions.map((option: string) => (
           <option key={option} value={option}>
