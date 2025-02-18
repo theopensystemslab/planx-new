12101aca3b46f640eaabc52e277de24d24c8db49
Rory Doak <rory.doak@gmail.com>
2025-02-18 12:21:09 +0000

    remove unused imports and run prettier

--- editor.planx.uk/src/ui/editor/Filter/tests/helpers.ts
+++ editor.planx.uk/src/ui/editor/Filter/tests/helpers.ts
@@ -2,22 +2,27 @@ import { Screen } from "@testing-library/react";
 import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
 import { expect } from "vitest";
 
-export const openFilterAccordion = async (screen: Screen, user:UserEvent) => {
-    const showAccordionTitle = screen.getByText("Show filters")
-    await user.click(showAccordionTitle);
+export const openFilterAccordion = async (screen: Screen, user: UserEvent) => {
+  const showAccordionTitle = screen.getByText("Show filters");
+  await user.click(showAccordionTitle);
 
-    const hideAccordionTitle = screen.getByText("Hide filters")
-    expect(hideAccordionTitle).toBeVisible()
-}
-export const closeFilterAccordion = async (screen: Screen, user:UserEvent) => {
-    const hideAccordionTitle = screen.getByText("Hide filters")
-    await user.click(hideAccordionTitle);
+  const hideAccordionTitle = screen.getByText("Hide filters");
+  expect(hideAccordionTitle).toBeVisible();
+};
+export const closeFilterAccordion = async (screen: Screen, user: UserEvent) => {
+  const hideAccordionTitle = screen.getByText("Hide filters");
+  await user.click(hideAccordionTitle);
 
-    const showAccordionTitle = screen.getByText("Show filters")
-    expect(showAccordionTitle).toBeVisible()}
+  const showAccordionTitle = screen.getByText("Show filters");
+  expect(showAccordionTitle).toBeVisible();
+};
 
-    export const selectCheckbox = async (screen: Screen, user: UserEvent, name:string) => {
-        const checkbox = screen.getByRole('checkbox', { name: name })
-        await user.click(checkbox)
-        expect(checkbox).toHaveAttribute("selected", true)
-    }
+export const selectCheckbox = async (
+  screen: Screen,
+  user: UserEvent,
+  name: string,
+) => {
+  const checkbox = screen.getByRole("checkbox", { name: name });
+  await user.click(checkbox);
+  expect(checkbox).toHaveAttribute("selected", true);
+};
