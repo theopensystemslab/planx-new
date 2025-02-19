69e323072024808022e6c5a254b5fefdc426a4d0
Ian Jones <hello@ianjo.uk>
2025-02-19 15:03:19 +0000

    extend to component: Map and label

--- editor.planx.uk/src/theme.ts
+++ editor.planx.uk/src/theme.ts
@@ -650,6 +650,12 @@ const getThemeOptions = ({
               outlineOffset: "1px",
               boxShadow: `0 0 0 4px ${palette.action.focus}`,
             },
+            [`&.${radioClasses.disabled}::before`]: {
+              borderColor: palette.text.disabled,
+            },
+            [`&.${radioClasses.disabled}::after`]: {
+              color: palette.text.disabled,
+            },
           },
         },
         variants: [
