511ab3b3455586d5de1e44b8a4bc77bb997321cf
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-02-20 12:38:14 +0000

    feat: Create '*' role to denote all users can access route

--- editor.planx.uk/src/components/EditorNavMenu/types.ts
+++ editor.planx.uk/src/components/EditorNavMenu/types.ts
@@ -1,11 +1,13 @@
 import { Role } from "@opensystemslab/planx-core/types";
 import React from "react";
 
+type AllUsers = "*";
+
 export interface Route {
   title: string;
   Icon: React.ElementType;
   route: string;
-  accessibleBy: Role[];
+  accessibleBy: Role[] | AllUsers;
   disabled?: boolean;
 }
 export interface RoutesForURL {
