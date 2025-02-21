02b7618f55a13834d02bd684b927bd74e31c68d4
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-02-21 07:57:53 +0000

    fix: Filter submission by flowId

--- editor.planx.uk/src/routes/submissionsLog.tsx
+++ editor.planx.uk/src/routes/submissionsLog.tsx
@@ -7,17 +7,16 @@ import { makeTitle } from "./utils";
 const submissionsLogRoutes = compose(
   withData((req) => ({
     mountpath: req.mountpath,
-    flow: req.params.flow?.split(",")[0],
   })),
 
   mount({
     "/": compose(
       route(async (req) => {
-        const { team: teamSlug, flow: flowSlug } = req.params;
+        const { team: teamSlug, flow: flowId } = req.params;
 
         return {
-          title: makeTitle([teamSlug, "submissions-log", flowSlug].join("/")),
-          view: <Submissions flowSlug={flowSlug} />,
+          title: makeTitle([teamSlug, "submissions-log"].join("/")),
+          view: <Submissions flowId={flowId} />,
         };
       }),
     ),
