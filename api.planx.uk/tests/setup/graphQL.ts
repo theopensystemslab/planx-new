789e26ff431539d566b4ec608690a12bb9212b29
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-02-21 12:18:57 +0000

    test: Setup passing IsTokenRevoked query before all tests

--- api.planx.uk/tests/setup/graphQL.ts
+++ api.planx.uk/tests/setup/graphQL.ts
@@ -1,5 +1,19 @@
 import { queryMock } from "../graphqlQueryMock.js";
+import { type MockGraphQLConfig } from "graphql-query-test-mock";
+
+// Ensure that all API test run on the default assumption that tokens are not revoked
+// This avoid needing to manually mock this before each test
+export const mockIsTokenRevokedQuery: MockGraphQLConfig = {
+  name: "IsTokenRevoked",
+  matchOnVariables: false,
+  data: {
+    revokedToken: {
+      revokedAt: null,
+    },
+  },
+};
 
 beforeEach(() => {
   queryMock.setup(process.env.HASURA_GRAPHQL_URL!);
+  queryMock.mockQuery(mockIsTokenRevokedQuery);
 });
