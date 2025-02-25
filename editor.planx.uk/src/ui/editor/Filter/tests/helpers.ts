5b43fbf6818fceb8c8cd247e7c40d7a358ae8222
Rory Doak <138574807+RODO94@users.noreply.github.com>
2025-02-25 14:37:50 +0000

    feat: unit tests for Filters (#4375)

---
+++ editor.planx.uk/src/ui/editor/Filter/tests/helpers.ts
@@ -0,0 +1,25 @@
+import { screen } from "@testing-library/react";
+import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
+import { expect } from "vitest";
+
+export const expandFilterAccordion = async (user: UserEvent) => {
+  const showAccordionTitle = screen.getByText("Show filters");
+  await user.click(showAccordionTitle);
+
+  const hideAccordionTitle = screen.getByText("Hide filters");
+  expect(hideAccordionTitle).toBeVisible();
+};
+
+export const addFilter = async (user: UserEvent, name: string) => {
+  const checkbox = screen.getByRole("checkbox", { name: name });
+  await user.click(checkbox);
+  const filterChip = screen.getByRole("button", { name: name });
+  expect(filterChip).toBeVisible();
+};
+
+export const removeFilter = async (user: UserEvent, name: string) => {
+  const checkbox = screen.getByRole("checkbox", { name: name });
+  await user.click(checkbox);
+  const filterChip = screen.queryByRole("button", { name: name });
+  expect(filterChip).not.toBeInTheDocument();
+};
---
+++ editor.planx.uk/src/ui/editor/Filter/tests/mocks.ts
@@ -0,0 +1,47 @@
+import { vi } from "vitest";
+import { FilterOptions } from "../Filter";
+
+export type MockRecordType = {
+  name: string;
+  status: "online" | "offline";
+};
+
+export const mockRecords: MockRecordType[] = [
+  {
+    name: "offline-mock",
+    status: "offline",
+  },
+  {
+    name: "online-mock",
+    status: "online",
+  },
+  {
+    name: "online-mock-2",
+    status: "online",
+  },
+  {
+    name: "offline-mock-2",
+    status: "online",
+  },
+];
+
+export const mockSetFilteredRecords = vi.fn() as React.Dispatch<
+  React.SetStateAction<MockRecordType[] | null>
+>;
+
+export const mockFilterOptions: FilterOptions<MockRecordType>[] = [
+  {
+    displayName: "Online status",
+    optionKey: "status",
+    optionValue: ["online", "offline"],
+    validationFn: (option: MockRecordType, value: string | undefined) =>
+      option.status === value,
+  },
+  {
+    displayName: "Name",
+    optionKey: "name",
+    optionValue: ["online-mock-2", "offline-mock-2"],
+    validationFn: (option: MockRecordType, value: string | undefined) =>
+      option.name === value,
+  },
+];
