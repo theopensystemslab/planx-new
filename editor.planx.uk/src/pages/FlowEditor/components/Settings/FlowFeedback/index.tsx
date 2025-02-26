65ffc9768e49672107562acc5460e58a32cd0c19
Rory Doak <rory.doak@gmail.com>
2025-02-26 14:08:48 +0000

    add in feedback to table and wire up sidebar icon

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/FlowFeedback/index.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/FlowFeedback/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import { FeedbackLog } from "../../Flow/FeedbackLog/FeedbackLog";
 
-export const FlowFeedback: React.FC<any> = () => {
-  return <FeedbackLog feedback={[]} />;
+export const FlowFeedback: React.FC<any> = ({ feedback }) => {
+  return <FeedbackLog feedback={feedback} />;
 };
