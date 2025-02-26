9f27f7a3534ef64cb9cf9b6a1e1f9215bc776e60
Rory Doak <rory.doak@gmail.com>
2025-02-26 15:07:00 +0000

    refine the use of components in serviceFeedback

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/FlowFeedback/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { FeedbackLog } from "../../Flow/FeedbackLog/FeedbackLog";
-
-export const FlowFeedback: React.FC<any> = ({ feedback }) => {
-  return <FeedbackLog feedback={feedback} />;
-};
