0da016e32ce438e8339e8c4d188d7be6224ed5a3
Rory Doak <rory.doak@gmail.com>
2025-02-26 14:00:19 +0000

    made initial component and route

---
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/FlowFeedback/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { FeedbackLog } from "../../Flow/FeedbackLog/FeedbackLog";
+
+export const FlowFeedback: React.FC<any> = () => {
+  return <FeedbackLog feedback={[]} />;
+};
