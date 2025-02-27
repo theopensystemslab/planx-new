c0e141d4357ebeeeb85cad5c2fafb220090bf562
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2025-02-27 17:53:46 +0000

    Fill out tick/cross test and general render test

--- editor.planx.uk/src/pages/PlatformAdminPanel/PlatformAdminPanel.test.tsx
+++ editor.planx.uk/src/pages/PlatformAdminPanel/PlatformAdminPanel.test.tsx
@@ -1,7 +1,11 @@
-import { act } from "@testing-library/react";
+import { act, screen, within } from "@testing-library/react";
 import { FullStore, useStore } from "pages/FlowEditor/lib/store";
+import React from "react";
+import { setup } from "testUtils";
 import { mockTeams } from "ui/shared/DataTable/mockTeams";
 import { it } from "vitest";
+
+import { PlatformAdminPanel } from "./PlatformAdminPanel";
 
 const { getState, setState } = useStore;
 
@@ -17,9 +21,59 @@ describe("Platform admin panel", () => {
     act(() => setState(initialState));
   });
 
-  it.todo("renders without an error");
+  it("renders without an error", () => {
+    setup(<PlatformAdminPanel />);
+    const headers = [
+      "Team",
+      "Reference code",
+      "Live services",
+      "Planning constraints",
+      "Article 4s (API)",
+      "GOV.UK Pay",
+      "GOV.UK Notify",
+      "Send to email",
+      "BOPS",
+      "Power automate",
+      "Subdomain",
+      "Logo",
+      "Favicon",
+    ];
+    headers.map((header) => expect(screen.getByText(header)).toBeVisible());
+
+    // test for a selection of row values
+    expect(
+      screen.getByRole("gridcell", { name: "Barking and Dagenham" }),
+    ).toBeVisible();
+
+    expect(screen.getByRole("gridcell", { name: "DON" })).toBeVisible();
+
+    expect(
+      screen.getAllByText("Apply for a lawful development certificate"),
+    ).toHaveLength(2); // Two teams in the mock data have an LDC flow
+  });
+
+  it.todo("does not show rows for internal teams");
 
   it.todo("is only viewable with the correct role");
 
-  it.todo("renders a tick / cross for boolean values");
+  it("renders a tick / cross for boolean values", () => {
+    const { container } = setup(<PlatformAdminPanel />);
+
+    // get all cells in the 'Planning Constraints' column
+    const planningContraintCells = container.querySelectorAll(
+      '[data-field="planningDataEnabled"]',
+    );
+
+    const firstRow = planningContraintCells[1]; // first non-header row
+
+    expect(
+      within(firstRow as HTMLElement).getByTestId("CloseIcon"), // cross icon
+    ).toBeVisible();
+
+    const secondRow = planningContraintCells[2];
+
+    expect(
+      within(secondRow as HTMLElement).getByTestId("DoneIcon"), // tick icon
+    ).toBeVisible();
+  });
 });
