64c79fe07907089e3fb6db931fe7666051685cf3
zz-hh-aa <zzhhaa@protonmail.com>
2025-03-04 10:03:20 +0000

    remove unnecessary parentId and description params

--- api.planx.uk/modules/analytics/metabase/collection/createCollection.ts
+++ api.planx.uk/modules/analytics/metabase/collection/createCollection.ts
@@ -4,13 +4,12 @@ import type {
 } from "./types.js";
 import { $metabase } from "../shared/client.js";
 
+
 export async function createCollection(
   params: CreateCollectionParams,
 ): Promise<number> {
   const metabaseCreateCollectionParams: MetabaseCreateCollectionParams = {
-    name: params.name,
-    description: params.description,
-    parent_id: params.parentId,
+    name: params.name
   };
 
   const response = await $metabase.post(
