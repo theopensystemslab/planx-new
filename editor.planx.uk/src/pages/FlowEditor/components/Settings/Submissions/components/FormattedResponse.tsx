8153769b25ce2fa5b74e9c89abcdf298b27d2e39
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-03-05 09:18:00 +0000

    fix: ErrorBoundary setup

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/Submissions/components/FormattedResponse.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/Submissions/components/FormattedResponse.tsx
@@ -1,12 +1,11 @@
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
 import React from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 import { Submission } from "../types";
 
-const Response = styled(Box)(({ theme }) => ({
+const Root = styled(Box)(({ theme }) => ({
   fontSize: "1em",
   margin: 1,
   overflowWrap: "break-word",
@@ -16,23 +15,28 @@ const Response = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
 }));
 
-const InvalidJSONError = () => <Typography>Error parsing response</Typography>
+const InvalidJSONError = () => "Error parsing response";
+
+const Response: React.FC<{ response: string }> = ({ response }) => {
+  const parsedResponse = JSON.parse(response);
+  return JSON.stringify(parsedResponse, null, 2);
+};
 
 export const FormattedResponse: React.FC<Submission> = (submission) => {
   const getResponse = ({ eventType, status, response }: Submission) => {
     if (eventType === "Pay") return response;
     if (status === "Success") return response?.data?.body;
 
-    return response.data;
-  }
+    return response.data?.message;
+  };
 
   const response = getResponse(submission);
-  
+
   return (
-    <Response component="pre">
+    <Root component="pre">
       <ErrorBoundary FallbackComponent={InvalidJSONError}>
-        {JSON.stringify(response, null, 2)}
+        <Response response={response} />
       </ErrorBoundary>
-    </Response>
+    </Root>
   );
 };
