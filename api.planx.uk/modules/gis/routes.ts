e0b671c6b083db2efb77fd0ad9e4b9d7163bae49
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-03-06 14:09:56 +0000

    chore(api): Drop redundant a4 route

--- api.planx.uk/modules/gis/routes.ts
+++ api.planx.uk/modules/gis/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
 import { locationSearch } from "./service/index.js";
-import { hasArticle4Schema } from "./service/article4Schema.js";
 import { classifiedRoadsSearch } from "./service/classifiedRoads.js";
 
 const router = Router();
 
 router.get("/gis/:localAuthority", locationSearch);
-router.get("/gis/:localAuthority/article4-schema", hasArticle4Schema);
 router.get("/roads", classifiedRoadsSearch);
 
 export default router;
