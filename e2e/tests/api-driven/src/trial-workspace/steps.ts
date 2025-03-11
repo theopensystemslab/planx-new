f1207a37654307a98cb97c9a3a0f41e3b0b06fae
Rory Doak <rory.doak@gmail.com>
2025-03-11 14:55:09 +0000

    add feature and steps file for trial team tests

---
+++ e2e/tests/api-driven/src/trial-workspace/steps.ts
@@ -0,0 +1,49 @@
+Given("the following teams exist:", async function (dataTable) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("the following users exist:", async function (dataTable) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Given("users are members of teams:", async function (dataTable) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("I am in my own team", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("I am on a flow in my team", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("my team has full access rights", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When("my team has trial access rights", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("I can create a flow", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("I should be able to update the status", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then("I should not be able to update the status", async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
