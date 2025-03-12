72637d0c4f403e5bb1959160ff1ff4bf2b3391d8
Rory Doak <rory.doak@gmail.com>
2025-03-12 13:38:36 +0000

    add semi colon to drop column sql

--- hasura.planx.uk/migrations/1741771336243_alter_table_public_teams_add_column_access_rights/down.sql
+++ hasura.planx.uk/migrations/1741771336243_alter_table_public_teams_add_column_access_rights/down.sql
@@ -1,5 +1,5 @@
 alter table "public"."teams" drop constraint "teams_access_rights_fkey";
 
-alter table "public"."teams" drop column "access_rights"
+alter table "public"."teams" drop column "access_rights";
 
 DROP TABLE "public"."team_access_rights_enum";
