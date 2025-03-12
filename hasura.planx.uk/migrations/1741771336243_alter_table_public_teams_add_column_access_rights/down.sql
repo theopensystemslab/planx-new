0552ee5cd23d4f719cac3455c57c0d8084926fa0
Rory Doak <rory.doak@gmail.com>
2025-03-12 13:37:45 +0000

    change order of drop file and add space

--- hasura.planx.uk/migrations/1741771336243_alter_table_public_teams_add_column_access_rights/down.sql
+++ hasura.planx.uk/migrations/1741771336243_alter_table_public_teams_add_column_access_rights/down.sql
@@ -1,3 +1,5 @@
-DROP TABLE "public"."team_access_rights_enum";
 alter table "public"."teams" drop constraint "teams_access_rights_fkey";
+
 alter table "public"."teams" drop column "access_rights"
+
+DROP TABLE "public"."team_access_rights_enum";
