6d287c295686d85cd4a5aa9128c4e5607eb8e6ef
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-03-24 15:04:31 +0000

    refactor(editor): Move types and popup icon to own files

---
+++ editor.planx.uk/src/ui/shared/PopUpIcon.tsx
@@ -0,0 +1,9 @@
+import ArrowIcon from "@mui/icons-material/KeyboardArrowDown";
+import React from "react";
+
+export const PopupIcon = (
+  <ArrowIcon
+    sx={(theme) => ({ color: theme.palette.primary.main })}
+    fontSize="large"
+  />
+);
