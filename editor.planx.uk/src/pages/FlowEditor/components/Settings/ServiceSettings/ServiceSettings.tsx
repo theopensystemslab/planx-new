deac7d3df8af0875c54debb0cdc8249f0beebf2c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-03-24 15:08:55 +0000

    refactor: Break up components, add subfolders

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/ServiceSettings.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/ServiceSettings.tsx
@@ -1,8 +1,8 @@
 import Container from "@mui/material/Container";
 import React from "react";
 
+import { FooterLinksAndLegalDisclaimer } from "./FlowElements/FooterLinksAndLegalDisclaimer";
 import FlowStatus from "./FlowStatus";
-import { FooterLinksAndLegalDisclaimer } from "./FooterLinksAndLegalDisclaimer";
 
 const ServiceSettings: React.FC = () => (
   <Container
