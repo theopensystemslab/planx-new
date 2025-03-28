0845a3aabb525a0ca0a089b2d8e81f55e04a6d07
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-03-28 09:22:21 +0000

    fix: Use correct env var for e2e test env

--- api.planx.uk/shared/middleware/useEnvGuard.ts
+++ api.planx.uk/shared/middleware/useEnvGuard.ts
@@ -8,7 +8,7 @@ type UseEnvGuard = (envs: Environment[]) => RequestHandler;
  * Only allow access to the listed environments
  */
 export const useEnvGuard: UseEnvGuard = (envs) => async (_req, res, next) => {
-  const currentEnv = process.env.NODE_ENV as Environment;
+  const currentEnv = process.env.APP_ENVIRONMENT as Environment;
   const isBlocked = !envs.includes(currentEnv);
   if (isBlocked) return res.status(403).send();
 
