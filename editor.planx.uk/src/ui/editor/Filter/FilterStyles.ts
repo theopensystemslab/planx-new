8e419da7c00eb3003169de951a5eb999b3c94ea5
Ian Jones <hello@ianjo.uk>
2025-04-02 20:11:33 +0100

    move sort control to filters bar

--- editor.planx.uk/src/ui/editor/Filter/FilterStyles.ts
+++ editor.planx.uk/src/ui/editor/Filter/FilterStyles.ts
@@ -3,32 +3,26 @@ import Chip from "@mui/material/Chip";
 import { styled } from "@mui/material/styles";
 
 export const StyledChip = styled(Chip)(({ theme }) => ({
-  background: theme.palette.common.white,
+  background: theme.palette.background.dark,
+  color: theme.palette.common.white,
   cursor: "default",
   textTransform: "capitalize",
   display: "flex",
   justifyContent: "space-between",
   borderRadius: "50px",
+  height: "36px",
   "& > svg": {
-    fill: theme.palette.text.secondary,
-  },
-  "&:hover": {
-    background: theme.palette.common.white,
-    "& > svg": {
-      fill: theme.palette.text.primary,
+    fill: theme.palette.secondary.dark,
+    "&:hover": {
+      fill: theme.palette.common.white,
     },
   },
 }));
 
 export const FiltersContent = styled(Box)(({ theme }) => ({
-  width: "100%",
-  margin: theme.spacing(1, 0, 3),
-  padding: theme.spacing(1.5, 2),
   display: "flex",
   flexDirection: "row",
-  flexWrap: "wrap",
   alignItems: "center",
   gap: theme.spacing(1),
-  border: `1px solid ${theme.palette.border.main}`,
-  background: theme.palette.secondary.dark,
+  flexShrink: 0,
 }));
