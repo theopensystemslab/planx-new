e0bead310db0342fedbdef0fc96df50aeb867349
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-04-02 09:09:31 +0100

    chore: Remove old placeholder modal

--- editor.planx.uk/src/pages/Team/index.tsx
+++ editor.planx.uk/src/pages/Team/index.tsx
@@ -1,10 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
-import { hasFeatureFlag } from "lib/featureFlags";
 import { isEmpty, orderBy } from "lodash";
 import React, { useCallback, useEffect, useState } from "react";
 import { useCurrentRoute } from "react-navi";
@@ -19,10 +17,6 @@ import { AddFlow } from "./components/AddFlow";
 import FlowCard, { Card, CardContent } from "./components/FlowCard";
 import { ShowingServicesHeader } from "./components/ShowingServicesHeader";
 import { filterOptions, sortOptions } from "./helpers/sortAndFilterOptions";
-import {
-  StartFromTemplateButton,
-  TemplateOption,
-} from "./StartFromTemplateButton";
 
 const DashboardList = styled("ul")(({ theme }) => ({
   padding: theme.spacing(3, 0),
