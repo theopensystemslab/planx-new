aac98a2a4c3381272648205c37b258ac17c6cec3
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-04-04 15:54:50 +0100

    chore: Tidy up

--- editor.planx.uk/src/pages/Team/components/AddFlow/index.tsx
+++ editor.planx.uk/src/pages/Team/components/AddFlow/index.tsx
@@ -40,7 +40,6 @@ export const AddFlow: React.FC = () => {
     { mode, flow },
     { setFieldError },
   ) => {
-    console.log({ flow, mode });
     try {
       switch (mode) {
         case "new":
