5aa1ee7e55771dc7f667b3f123dbe0583821ba8c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-04-10 21:38:47 +0100

    chore: Share URLPrefix component

--- editor.planx.uk/src/pages/Team/components/AddFlow/BaseFormSection.tsx
+++ editor.planx.uk/src/pages/Team/components/AddFlow/BaseFormSection.tsx
@@ -1,39 +1,15 @@
-import InputAdornment from "@mui/material/InputAdornment";
 import MenuItem from "@mui/material/MenuItem";
-import { typographyClasses } from "@mui/material/Typography";
 import { useFormikContext } from "formik";
 import { hasFeatureFlag } from "lib/featureFlags";
-import React, { useState } from "react";
+import React from "react";
 import SelectInput from "ui/editor/SelectInput/SelectInput";
+import { URLPrefix } from "ui/editor/URLPrefix";
 import InputLabel from "ui/public/InputLabel";
 import Input from "ui/shared/Input/Input";
 import { slugify } from "utils";
 
 import { CreateFromTemplateFormSection } from "./CreateFromTemplateFormSection";
 import { CREATE_FLOW_MODES, CreateFlow } from "./types";
-
-const URLPrefix: React.FC = () => {
-  // Store in component state so this does not update when user submits form
-  const [urlPrefix] = useState(() => {
-    const { origin, pathname } = window.location;
-    return `${origin}${pathname}/`;
-  });
-
-  return (
-    <InputAdornment
-      position="start"
-      sx={(theme) => ({
-        mr: 0,
-        [`& .${typographyClasses.root}`]: {
-          color: theme.palette.text.disabled,
-        },
-      })}
-    >
-      {urlPrefix}
-    </InputAdornment>
-  );
-};
-
 export const BaseFormSection: React.FC = () => {
   const { values, setFieldValue, getFieldProps, errors } =
     useFormikContext<CreateFlow>();
