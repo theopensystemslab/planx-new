6645b7eece0ec5b66ebfd12ce7cba6b2a463d5ac
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-04-18 15:37:06 +0200

    wip

--- editor.planx.uk/src/@planx/components/Send/Editor.tsx
+++ editor.planx.uk/src/@planx/components/Send/Editor.tsx
@@ -1,4 +1,4 @@
-import Warning from "@mui/icons-material/Warning";
+import FactCheckIcon from "@mui/icons-material/FactCheck";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
@@ -7,7 +7,9 @@ import {
   SendIntegration,
 } from "@opensystemslab/planx-core/types";
 import { getIn, useFormik } from "formik";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
+import { ModalFooter } from "ui/editor/ModalFooter";
 import ModalSection from "ui/editor/ModalSection";
 import ModalSectionContent from "ui/editor/ModalSectionContent";
 import ChecklistItem from "ui/shared/ChecklistItem/ChecklistItem";
@@ -17,6 +19,7 @@ import InputRow from "ui/shared/InputRow";
 import { array, object } from "yup";
 
 import { ICONS } from "../shared/icons";
+import { WarningContainer } from "../shared/Preview/WarningContainer";
 import { EditorProps } from "../shared/types";
 import { parseContent, Send } from "./model";
 
@@ -45,26 +48,45 @@ const SendComponent: React.FC<Props> = (props) => {
     }),
   });
 
-  const options: { value: SendIntegration; label: string }[] = [
+  const options: {
+    value: SendIntegration;
+    label: string;
+    description?: string;
+    disabled?: boolean;
+  }[] = [
     {
       value: "bops",
-      label: "BOPS",
-    },
-    {
-      value: "uniform",
-      label: "Uniform",
-    },
-    {
-      value: "idox",
-      label: "Idox Nexus (TESTING ONLY)",
+      label: `BOPS ${
+        import.meta.env.VITE_APP_ENV === "production" ? "production" : "staging"
+      }`,
+      // TODO add BOPS to state.teamIntegrations type and disable accordingly
     },
     {
       value: "email",
-      label: "Email to planning office",
+      label: `Email to ${useStore(
+        (state) => state.teamSettings.submissionEmail || "planning office",
+      )}`,
+      description:
+        "Configure one submission email per account under Team Settings. Please setup redirect rules in your inbox if different services require different emails.",
     },
     {
       value: "s3",
-      label: "Upload to AWS S3 bucket",
+      label: "Microsoft Sharepoint",
+      description:
+        "Receive submissions direct in your internal files system using a Power Automate workflow",
+      // TODO add Webhook URL to state.teamIntegrations type and disable accordingly
+    },
+    {
+      value: "uniform",
+      label: `Uniform ${
+        import.meta.env.VITE_APP_ENV === "production" ? "production" : "staging"
+      }`,
+      description:
+        "Warning: Legacy integration with very limited support! Only suitable for use with LDC-E and LDC-P application types for select councils.",
+      disabled: useStore(
+        (state) =>
+          !["buckinghamshire", "lambeth", "southwark"].includes(state.teamSlug),
+      ),
     },
   ];
 
@@ -88,37 +110,6 @@ const SendComponent: React.FC<Props> = (props) => {
           return originalValues.indexOf(a) - originalValues.indexOf(b);
         }),
       );
-
-      // Show warnings on selection of BOPS or Uniform for likely unsupported services
-      //   Don't actually restrict selection because flowSlug matching is imperfect for some valid test cases
-      const teamSlug = window.location.pathname?.split("/")?.[1];
-      const flowSlug = window.location.pathname?.split("/")?.[2];
-      if (value === "bops" && newCheckedValues.includes(value)) {
-        alert(
-          "BOPS only accepts Lawful Development Certificate, Prior Approval, and Planning Permission submissions. Please do not select if you're building another type of submission service!",
-        );
-      }
-
-      if (
-        value === "uniform" &&
-        newCheckedValues.includes(value) &&
-        flowSlug !== "apply-for-a-lawful-development-certificate" &&
-        !["buckinghamshire", "lambeth", "southwark"].includes(teamSlug)
-      ) {
-        alert(
-          "Uniform is only enabled for Bucks, Lambeth and Southwark to accept Lawful Development Certificate submissions. Please do not select if you're building another type of submission service!",
-        );
-      }
-
-      if (
-        value === "s3" &&
-        newCheckedValues.includes(value) &&
-        !["barnet", "southwark"].includes(teamSlug)
-      ) {
-        alert(
-          "AWS S3 uploads require API tokens and are currently being prototyped with select councils. Reach out on Slack #planx-alternatives-to-integrations before selecting this option please.",
-        );
-      }
     };
 
   return (
@@ -143,27 +134,29 @@ const SendComponent: React.FC<Props> = (props) => {
                     <ChecklistItem
                       id={option.value}
                       label={option.label}
+                      description={option.description}
                       onChange={changeCheckbox(option.value)}
                       checked={formik.values.destinations.includes(
                         option.value,
                       )}
-                      disabled={props.disabled}
+                      disabled={option.disabled || props.disabled}
                     />
                   </Grid>
                 ))}
               </Grid>
             </ErrorWrapper>
           </Box>
-          <Box display="flex" mt={2}>
-            <Warning titleAccess="Warning" color="primary" fontSize="large" />
-            <Typography variant="body2" ml={1}>
-              API tokens may be required to submit successfully. Check in with
-              PlanX developers before launching your service to make sure that
-              submissions are configured for your environment.
+          <WarningContainer>
+            <FactCheckIcon />
+            <Typography variant="body2" ml={2}>
+              View submissions activity for this service directly in the Plan✕
+              editor from the left menu. Successful submissions received less
+              than 28 days ago will be available to download by team editors.
             </Typography>
-          </Box>
+          </WarningContainer>
         </ModalSectionContent>
       </ModalSection>
+      <ModalFooter formik={formik} showMoreInformation={false} />
     </form>
   );
 };
