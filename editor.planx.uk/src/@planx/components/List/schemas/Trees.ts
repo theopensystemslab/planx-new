ef6d404502fa5d09158e8eec9af8fe91f548dd78
Ian Jones <hello@ianjo.uk>
2025-05-01 12:05:08 +0100

    feat: Add sketch plan schema to list component

---
+++ editor.planx.uk/src/@planx/components/List/schemas/Trees.ts
@@ -0,0 +1,49 @@
+import { Schema } from "@planx/components/shared/Schema/model";
+import { TextInputType } from "@planx/components/TextInput/model";
+
+export const Trees: Schema = {
+  type: "Tree type",
+  fields: [
+    {
+      type: "map",
+      data: {
+        title:
+          "Click the map to mark the position of a tree. Multiple trees can be added if they are of the same species, proposed work and reason. Markers can be removed by clicking the delete icon.",
+        fn: "features",
+        mapOptions: {
+          basemap: "MapboxSatellite",
+          drawType: "Point",
+          drawColor: "#66ff00",
+          drawMany: true,
+        },
+      },
+    },
+    {
+      type: "text",
+      data: {
+        title: "Species",
+        fn: "species",
+        type: TextInputType.Short,
+      },
+    },
+    {
+      type: "text",
+      required: false,
+      data: {
+        title: "Proposed work",
+        fn: "work",
+        type: TextInputType.Short,
+      },
+    },
+    {
+      type: "text",
+      required: false,
+      data: {
+        title: "Reasons for the proposed work",
+        fn: "reason",
+        type: TextInputType.Short,
+      },
+    },
+  ],
+  min: 1,
+} as const;
