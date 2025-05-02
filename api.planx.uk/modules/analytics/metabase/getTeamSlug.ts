c615c696049d14424c1b146ed4965326a4bc76ea
zz-hh-aa <zzhhaa@protonmail.com>
2025-05-02 10:40:39 +0100

    feat: updated metabase dashboard link service

---
+++ api.planx.uk/modules/analytics/metabase/getTeamSlug.ts
@@ -0,0 +1,36 @@
+import { gql } from "graphql-request";
+import { $api } from "../../../client/index.js";
+
+interface GetTeamSlug {
+  teams: {
+    id: number;
+    teamSlug: string;
+  }[];
+}
+
+export const getTeamSlug = async (id: number) => {
+  try {
+    const response = await $api.client.request<GetTeamSlug>(
+      gql`
+        query GetTeamNameAndSlug($id: Int!) {
+          teams(where: { id: { _eq: $id } }) {
+            id
+            teamSlug: slug
+          }
+        }
+      `,
+      {
+        id,
+      },
+    );
+
+    const result = response.teams[0];
+    return result;
+  } catch (e) {
+    console.error(
+      "Error fetching team name and slug from PlanX DB:",
+      (e as Error).stack,
+    );
+    throw e;
+  }
+};
