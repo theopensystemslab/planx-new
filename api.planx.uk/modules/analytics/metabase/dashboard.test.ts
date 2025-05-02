b3cf4ee266333098c7606595664a3b24f9a7d3a5
zz-hh-aa <zzhhaa@protonmail.com>
2025-05-02 13:58:38 +0100

    test(fix): include mock planx api calls

--- api.planx.uk/modules/analytics/metabase/dashboard.test.ts
+++ api.planx.uk/modules/analytics/metabase/dashboard.test.ts
@@ -2,7 +2,7 @@ import { findDashboardPublicLink } from "./findDashboardPublicLink.js";
 import { getTeamSlug } from "./getTeamSlug.js";
 import { filterPublicLink } from "./filterPublicLink.js";
 import { updatePublicAnalyticsLink } from "./updatePublicAnalyticsLink.js";
-import nock from "nock";
+import { $api } from "../../../client/index.js";
 
 describe("Metabase dashboard link module", () => {
   describe("findDashboardPublicLink", () => {
@@ -16,45 +16,24 @@ describe("Metabase dashboard link module", () => {
       expect(linkFOI).toBe(publicLinkFOI);
       expect(linkCIY).toBe(publicLinkFOI);
     });
-
-    test("returns correct public link for a range of submission services", () => {
-      const serviceSlugAFPP = "apply-for-planning-permission";
-      const serviceSlugLDC = "apply-for-a-lawful-development-certificate";
-      const serviceSlugPreApp = "apply-for-pre-application-advice";
-
-      const linkAFPP = findDashboardPublicLink(serviceSlugAFPP);
-      const linkLDC = findDashboardPublicLink(serviceSlugLDC);
-      const linkPreApp = findDashboardPublicLink(serviceSlugPreApp);
-
-      const publicLinkSubmission =
-        "https://metabase.editor.planx.dev/public/dashboard/363fd552-8c2b-40d9-8b7a-21634ec182cc?service_slug={service-slug}&team_slug={team-slug}";
-      expect(linkAFPP).toBe(publicLinkSubmission);
-      expect(linkLDC).toBe(publicLinkSubmission);
-      expect(linkPreApp).toBe(publicLinkSubmission);
-    });
-
-    test("returns null for an invalid service slug", () => {
-      const serviceSlug = "invalid-slug";
-      const link = findDashboardPublicLink(serviceSlug);
-      expect(link).toBeNull();
-    });
   });
 
   describe("getTeamSlug", () => {
     test("fetches team slug for a valid team ID", async () => {
-      const mockResponse = {
-        data: { teams: [{ id: 1, teamSlug: "team-slug" }] },
-      };
-      nock("http://localhost").post("/graphql").reply(200, mockResponse);
+      // Mock API response
+      vi.spyOn($api.client, "request").mockResolvedValueOnce({
+        teams: [{ id: 1, teamSlug: "team-slug" }],
+      });
 
       const result = await getTeamSlug(1);
       expect(result.teamSlug).toBe("team-slug");
     });
 
     test("throws an error for an invalid team ID", async () => {
-      nock("http://localhost")
-        .post("/graphql")
-        .reply(400, { errors: [{ message: "Error" }] });
+      // Mock API error response
+      vi.spyOn($api.client, "request").mockRejectedValueOnce(
+        new Error("Error"),
+      );
 
       await expect(getTeamSlug(999)).rejects.toThrow("Error");
     });
@@ -73,25 +52,25 @@ describe("Metabase dashboard link module", () => {
 
   describe("updatePublicAnalyticsLink", () => {
     test("updates analytics link for a valid flow ID", async () => {
-      const mockResponse = {
-        data: {
-          flow: { id: "flow-id", analytics_link: "https://test.com" },
-        },
-      };
-      nock("http://localhost").post("/graphql").reply(200, mockResponse);
+      // Mock API response
+      vi.spyOn($api.client, "request").mockResolvedValueOnce({
+        flow: { id: "flow-id", publicLink: "https://test.com" },
+      });
 
       const result = await updatePublicAnalyticsLink(
         "flow-id",
         "https://test.com",
       );
+      console.log({ result });
       expect(result.flow.id).toBe("flow-id");
       expect(result.flow.publicLink).toBe("https://test.com");
     });
 
     test("throws an error for an invalid flow ID", async () => {
-      nock("http://localhost")
-        .post("/graphql")
-        .reply(400, { errors: [{ message: "Error" }] });
+      // Mock API error response
+      vi.spyOn($api.client, "request").mockRejectedValueOnce(
+        new Error("Error"),
+      );
 
       await expect(
         updatePublicAnalyticsLink("invalid-id", "https://test.com"),
