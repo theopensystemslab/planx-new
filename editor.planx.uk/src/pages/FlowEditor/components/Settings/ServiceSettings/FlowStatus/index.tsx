3b653b8285968ab23a520439d849365646a96391
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2025-05-20 15:27:20 +0100

    feat: add confirmation dialogue to service status changes

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/FlowStatus/index.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/FlowStatus/index.tsx
@@ -1,17 +1,21 @@
 import PendingActionsIcon from "@mui/icons-material/PendingActions";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import type { FlowStatus } from "@opensystemslab/planx-core/types";
 import { WarningContainer } from "@planx/components/shared/Preview/WarningContainer";
 import axios from "axios";
+import { ConfirmationDialog } from "components/ConfirmationDialog";
 import { useFormik } from "formik";
 import { useToast } from "hooks/useToast";
-import React from "react";
+import React, { useState } from "react";
 import { rootFlowPath } from "routes/utils";
+import { FONT_WEIGHT_BOLD } from "theme";
 import SettingsDescription from "ui/editor/SettingsDescription";
 import SettingsSection from "ui/editor/SettingsSection";
-import { Switch } from "ui/shared/Switch";
+import ChecklistItem from "ui/shared/ChecklistItem/ChecklistItem";
+import ErrorWrapper from "ui/shared/ErrorWrapper";
 
 import { useStore } from "../../../../lib/store";
 import { PublicLink } from "./PublicLink";
@@ -34,7 +38,7 @@ const FlowStatus = () => {
     state.flowSlug,
     state.teamDomain,
     state.isFlowPublished,
-    state.teamSettings.isTrial
+    state.teamSettings.isTrial,
   ]);
   const toast = useToast();
 
@@ -53,6 +57,9 @@ const FlowStatus = () => {
       resetForm({ values });
     },
   });
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [completed, setCompleted] = useState(false);
+  const [showError, setShowError] = useState(false);
 
   const publishedLink = `${window.location.origin}${rootFlowPath(
     false,
@@ -104,64 +111,104 @@ const FlowStatus = () => {
         </Typography>
       </SettingsSection>
       <SettingsSection background>
-        {isTrial &&
+        {isTrial && (
           <WarningContainer>
             <PendingActionsIcon sx={{ mr: 1 }} />
             <Typography variant="body2">
-              Trial accounts cannot turn flows online.
+              Trial accounts cannot set flows online.
             </Typography>
           </WarningContainer>
-        }
-        <Switch
+        )}
+        <Typography
+          variant="body1"
+          fontWeight={FONT_WEIGHT_BOLD}
+          sx={{ mb: 2 }}
+        >
+          {statusForm.values.status === "online"
+            ? "Your service is currently online"
+            : "Your service is currently offline"}
+        </Typography>
+        <Button
+          sx={{ mb: 2 }}
           disabled={isTrial}
-          label={statusForm.values.status as string}
-          name={"service.status"}
-          variant="editorPage"
-          capitalize
-          checked={statusForm.values.status === "online"}
-          onChange={() =>
-            statusForm.setFieldValue(
+          variant="contained"
+          onClick={() => setDialogOpen(true)}
+        >
+          {statusForm.values.status === "online"
+            ? "Set your service offline"
+            : "Set your service online"}
+        </Button>
+        <ConfirmationDialog
+          open={dialogOpen}
+          onClose={async (confirmed) => {
+            if (!confirmed) {
+              setDialogOpen(false);
+              setCompleted(false);
+              setShowError(false);
+              return;
+            }
+
+            if (!completed) {
+              setShowError(true);
+              return;
+            }
+
+            setDialogOpen(false);
+            setCompleted(false);
+            setShowError(false);
+            await statusForm.setFieldValue(
               "status",
               statusForm.values.status === "online" ? "offline" : "online",
-            )
+            );
+            await statusForm.submitForm();
+          }}
+          title="Confirm status change"
+          confirmText={
+            statusForm.values.status === "online" ? "Set offline" : "Set online"
           }
-        />
+          cancelText="Cancel"
+        >
+          <Box marginTop={2}>
+            <Typography gutterBottom>
+              {statusForm.values.status === "online"
+                ? "Services that are set offline are no longer publicly discoverable and cannot accept responses from users."
+                : "Only services that are ready to be publicly discoverable and accept responses from users should be set online."}
+            </Typography>
+            <ErrorWrapper error={showError ? `Confirm before continuing` : ``}>
+              <Grid container component="fieldset" sx={{ margin: 0 }}>
+                <Grid item xs={12} sx={{ pointerEvents: "auto" }}>
+                  <ChecklistItem
+                    label={
+                      statusForm.values.status === "online"
+                        ? "This service will no longer be able to receive public responses"
+                        : "This service is ready to accept public responses"
+                    }
+                    checked={completed}
+                    onChange={() => {
+                      setCompleted(!completed);
+                      setShowError(false);
+                    }}
+                  />
+                </Grid>
+              </Grid>
+            </ErrorWrapper>
+          </Box>
+        </ConfirmationDialog>
         <SettingsDescription>
-          <p>Toggle your service between "offline" and "online".</p>
           <p>
             A service must be online to be accessed by the public, and to enable
             analytics gathering.
           </p>
           <p>Offline services can still be edited and published as normal.</p>
         </SettingsDescription>
-        {!isTrial &&
+        {!isTrial && (
           <PublicLink
             isFlowPublished={isFlowPublished}
             status={flowStatus || "offline"}
             subdomain={subdomainLink}
             publishedLink={publishedLink}
           />
-        }
-
-        <Box>
-          <Button
-            type="submit"
-            variant="contained"
-            disabled={!statusForm.dirty}
-          >
-            Save
-          </Button>
-          <Button
-            onClick={() => statusForm.resetForm()}
-            type="reset"
-            variant="contained"
-            disabled={!statusForm.dirty}
-            color="secondary"
-            sx={{ ml: 1.5 }}
-          >
-            Reset changes
-          </Button>
-        </Box>
+        )}{" "}
       </SettingsSection>
     </Box>
   );
