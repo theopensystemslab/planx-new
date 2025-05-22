8118d1a53d4b29e0115ba16656a94669cc4a7e67
Ian Jones <hello@ianjo.uk>
2025-05-22 20:43:48 +0100

    fix filters

--- editor.planx.uk/src/ui/editor/Filter/tests/helpers.ts
+++ editor.planx.uk/src/ui/editor/Filter/tests/helpers.ts
@@ -2,15 +2,23 @@ import { screen } from "@testing-library/react";
 import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
 import { expect } from "vitest";
 
-export const addFilter = async (user: UserEvent, name: string) => {
-  const combobox = screen.getByRole("combobox", { name: "Online status" });
+export const addFilter = async (
+  user: UserEvent,
+  selectLabel: string,
+  optionLabel: string,
+) => {
+  const combobox = screen.getByRole("combobox", { name: selectLabel });
   await user.click(combobox);
-  const filterChip = screen.getByRole("button", { name });
+
+  const option = screen.getByRole("option", { name: optionLabel });
+  await user.click(option);
+
+  const filterChip = screen.getByRole("button", { name: optionLabel });
   expect(filterChip).toBeVisible();
 };
 
-export const removeFilter = async (user: UserEvent, name: string) => {
-  const filterChip = screen.getByRole("button", { name });
+export const removeFilter = async (user: UserEvent, optionLabel: string) => {
+  const filterChip = screen.getByRole("button", { name: optionLabel });
   await user.click(filterChip);
   expect(filterChip).not.toBeInTheDocument();
 };
