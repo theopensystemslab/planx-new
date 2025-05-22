0d95a2d25fff034ec604c5857261bad6b60c6f5e
Ian Jones <hello@ianjo.uk>
2025-05-22 15:21:44 +0100

    update filter tests for combobox

--- editor.planx.uk/src/ui/editor/Filter/tests/helpers.ts
+++ editor.planx.uk/src/ui/editor/Filter/tests/helpers.ts
@@ -3,15 +3,14 @@ import { UserEvent } from "@testing-library/user-event/dist/types/setup/setup";
 import { expect } from "vitest";
 
 export const addFilter = async (user: UserEvent, name: string) => {
-  const checkbox = screen.getByRole("checkbox", { name: name });
-  await user.click(checkbox);
-  const filterChip = screen.getByRole("button", { name: name });
+  const combobox = screen.getByRole("combobox", { name: "Online status" });
+  await user.click(combobox);
+  const filterChip = screen.getByRole("button", { name });
   expect(filterChip).toBeVisible();
 };
 
 export const removeFilter = async (user: UserEvent, name: string) => {
-  const checkbox = screen.getByRole("checkbox", { name: name });
-  await user.click(checkbox);
-  const filterChip = screen.queryByRole("button", { name: name });
+  const filterChip = screen.getByRole("button", { name });
+  await user.click(filterChip);
   expect(filterChip).not.toBeInTheDocument();
 };
