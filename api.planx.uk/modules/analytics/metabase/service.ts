e809a6a90252c4791c489022ce12b89377a1ec94
zz-hh-aa <zzhhaa@protonmail.com>
2025-05-30 16:16:42 +0100

    chore: remove unused getTeamSlug

--- api.planx.uk/modules/analytics/metabase/service.ts
+++ api.planx.uk/modules/analytics/metabase/service.ts
@@ -2,7 +2,6 @@ import { updatePublicAnalyticsLink } from "./updatePublicAnalyticsLink.js";
 import type { CreateNewDashboardLinkParams } from "./types.js";
 import { ServerError } from "../../../errors/serverError.js";
 import { generateDashboardLink } from "./generateDashboardLink.js";
-import { getTeamSlug } from "./getTeamSlug.js";
 
 /**
  * @returns The dashboard name (the Metabase API performs GETs with the dashboard ID, so we have to have that locally already--no need to return it here)
