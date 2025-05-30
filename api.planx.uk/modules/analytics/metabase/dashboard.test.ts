e7987d326d4e1a7046e65feaefdc97274d59bba8
zz-hh-aa <zzhhaa@protonmail.com>
2025-05-30 16:31:38 +0100

    test: update tests

--- api.planx.uk/modules/analytics/metabase/dashboard.test.ts
+++ api.planx.uk/modules/analytics/metabase/dashboard.test.ts
@@ -1,6 +1,4 @@
 import { generateDashboardLink } from "./generateDashboardLink.js";
-import { getTeamSlug } from "./getTeamSlug.js";
-import { filterPublicLink } from "./filterPublicLink.js";
 import { updatePublicAnalyticsLink } from "./updatePublicAnalyticsLink.js";
 import { $api } from "../../../client/index.js";
 
@@ -8,47 +6,15 @@ describe("Metabase dashboard link module", () => {
   describe("generateDashboardLink", () => {
     test("returns correct public link for a valid service slug", () => {
       const serviceSlug = "find-out-if-you-need-planning-permission";
-      const teamSlug = "test-council";
+      const flowId = "aaaa-bbbb-cccc-dddd";
       const linkFOI = generateDashboardLink({
         environment: "staging",
         serviceSlug,
-        teamSlug,
+        flowId,
       });
       const publicLink =
-        "https://metabase.editor.planx.dev/public/dashboard/d6303f0b-d6e8-4169-93c0-f988a93e19bc?service=find-out-if-you-need-planning-permission&team=test-council";
+        "https://metabase.editor.planx.dev/public/dashboard/d6303f0b-d6e8-4169-93c0-f988a93e19bc?flow_id=aaaa-bbbb-cccc-dddd";
       expect(linkFOI).toBe(publicLink);
-    });
-  });
-
-  describe("getTeamSlug", () => {
-    test("fetches team slug for a valid team ID", async () => {
-      // Mock API response
-      vi.spyOn($api.client, "request").mockResolvedValueOnce({
-        teams: [{ id: 1, teamSlug: "team-slug" }],
-      });
-
-      const result = await getTeamSlug(1);
-      expect(result.teamSlug).toBe("team-slug");
-    });
-
-    test("throws an error for an invalid team ID", async () => {
-      // Mock API error response
-      vi.spyOn($api.client, "request").mockRejectedValueOnce(
-        new Error("Error"),
-      );
-
-      await expect(getTeamSlug(999)).rejects.toThrow("Error");
-    });
-  });
-
-  describe("filterPublicLink", () => {
-    test("replaces placeholders in the public link", async () => {
-      const publicLink = "https://test.com/{team-slug}/{service-slug}";
-      const teamSlug = "team-slug";
-      const serviceSlug = "service-slug";
-
-      const result = await filterPublicLink(publicLink, serviceSlug, teamSlug);
-      expect(result).toBe("https://test.com/team-slug/service-slug");
     });
   });
 
@@ -63,7 +29,6 @@ describe("Metabase dashboard link module", () => {
         "flow-id",
         "https://test.com",
       );
-      console.log({ result });
       expect(result.flow.id).toBe("flow-id");
       expect(result.flow.publicLink).toBe("https://test.com");
     });
