2a9cc03d2667eca392008a1391ec0f8286793e9c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-06-02 10:13:49 +0100

    feat(lps): Configure env vars (#4714)

--- localplanning.services/astro.config.mjs
+++ localplanning.services/astro.config.mjs
@@ -1,12 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, envField } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+  site: "https://localplanning.services",
+  env: {
+    schema: {
+      PUBLIC_PLANX_EDITOR_URL: envField.string({ context: "client", access: "public", optional: false }),
+      PUBLIC_PLANX_GRAPHQL_API_URL: envField.string({ context: "client", access: "public", optional: false }),
+    }
+  },
   vite: {
     plugins: [tailwindcss()]
   }
