3911e3074ea9b26b0995cbae69623bd5ba5d194b
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2025-06-03 16:48:01 +0100

    add rich text input to result descriptions

--- editor.planx.uk/src/@planx/components/Result/Public/ResultSummary.tsx
+++ editor.planx.uk/src/@planx/components/Result/Public/ResultSummary.tsx
@@ -1,18 +1,13 @@
 import Typography from "@mui/material/Typography";
 import React from "react";
 import Banner from "ui/public/Banner";
+import ReactMarkdownOrHtml from "ui/shared/ReactMarkdownOrHtml/ReactMarkdownOrHtml";
 
 const ResultSummary = ({ heading, description, color }: any) => (
   <Banner heading={heading} color={color}>
     {description && (
-      <Typography
-        variant="body1"
-        align="left"
-        gutterBottom
-        mt={2}
-        maxWidth="formWrap"
-      >
-        {description}
+      <Typography variant="body1" align="left" gutterBottom maxWidth="formWrap">
+        <ReactMarkdownOrHtml source={description} openLinksOnNewTab />
       </Typography>
     )}
   </Banner>
