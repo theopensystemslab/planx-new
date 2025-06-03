e08d67443f041a13de4aac577d86f3516be0f6e6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-06-03 19:36:22 +0100

    wip

--- editor.planx.uk/src/@planx/components/Review/Editor.tsx
+++ editor.planx.uk/src/@planx/components/Review/Editor.tsx
@@ -1,4 +1,5 @@
 import { ComponentType as TYPES } from "@opensystemslab/planx-core/types";
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form";
 import { useFormik } from "formik";
 import React from "react";
 import { ModalFooter } from "ui/editor/ModalFooter";
@@ -7,6 +8,7 @@ import ModalSectionContent from "ui/editor/ModalSectionContent";
 import RichTextInput from "ui/editor/RichTextInput/RichTextInput";
 import Input from "ui/shared/Input/Input";
 import InputRow from "ui/shared/InputRow";
+import { z } from "zod";
 
 import { ICONS } from "../shared/icons";
 import { EditorProps } from "../shared/types";
@@ -14,58 +16,99 @@ import { parseContent, Review } from "./model";
 
 type Props = EditorProps<TYPES.Review, Review>;
 
+const { fieldContext, formContext } = createFormHookContexts();
+
+const { useAppForm } = createFormHook({
+  fieldComponents: {
+    TextField: Input,
+    NumberField: Input,
+    RichTextInput,
+  },
+  formComponents: {},
+  fieldContext,
+  formContext,
+});
+
 function Component(props: Props) {
-  const formik = useFormik<Review>({
-    initialValues: parseContent(props.node?.data),
-    onSubmit: (newValues) => {
+  const form = useAppForm({
+    defaultValues: parseContent(props.node?.data),
+    validators: {
+      onChange: z.object({
+        title: z.string(),
+        description: z.string(),
+        disclaimer: z.string(),
+      }),
+    },
+    onSubmit: ({ value }) => {
+      console.log({ value });
       props.handleSubmit?.({
         type: TYPES.Review,
-        data: newValues,
+        data: value,
       });
     },
   });
 
   return (
-    <form onSubmit={formik.handleSubmit} id="modal">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        form.handleSubmit();
+      }}
+      id="modal"
+    >
       <ModalSection>
         <ModalSectionContent title="Review" Icon={ICONS[TYPES.Review]}>
           <InputRow>
-            <Input
-              format="large"
+            <form.AppField
               name="title"
-              placeholder={formik.values.title}
-              value={formik.values.title}
-              onChange={formik.handleChange}
-              disabled={props.disabled}
+              children={(field) => (
+                <field.TextField
+                  format="large"
+                  placeholder={form.state.values.title}
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  onBlur={field.handleBlur}
+                />
+              )}
             />
           </InputRow>
           <InputRow>
-            <RichTextInput
+            <form.AppField
               name="description"
-              placeholder="Description"
-              value={formik.values.description}
-              onChange={formik.handleChange}
-              disabled={props.disabled}
+              children={(field) => (
+                <field.RichTextInput
+                  name="description"
+                  placeholder={form.state.values.description}
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  onBlur={field.handleBlur}
+                />
+              )}
             />
           </InputRow>
         </ModalSectionContent>
         <ModalSectionContent subtitle="Disclaimer">
           <InputRow>
-            <RichTextInput
+            <form.AppField
               name="disclaimer"
-              placeholder="Disclaimer"
-              value={formik.values.disclaimer}
-              onChange={formik.handleChange}
-              disabled={props.disabled}
+              children={(field) => (
+                <field.RichTextInput
+                  name="disclaimer"
+                  placeholder={form.state.values.disclaimer}
+                  value={field.state.value}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                  onBlur={field.handleBlur}
+                />
+              )}
             />
           </InputRow>
         </ModalSectionContent>
       </ModalSection>
-      <ModalFooter
+      {/* <ModalFooter
         formik={formik}
         showMoreInformation={false}
         disabled={props.disabled}
-      />
+      /> */}
     </form>
   );
 }
