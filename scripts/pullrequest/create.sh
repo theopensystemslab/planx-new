5abcaccb6cd25b38aafe2a31a804ca8fe02633a3
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-06-04 16:45:45 +0100

    chore(ci): Conditionally run non-essential services on Pizza environments

--- scripts/pullrequest/create.sh
+++ scripts/pullrequest/create.sh
@@ -16,9 +16,23 @@ source .env.pizza
 DOCKER_BUILDKIT=1
 set +o allexport
 
+# Conditionally run non-essential services
+declare -a profile_args
+
+if [[ $RUN_STORYBOOK == "true" ]]; then
+  profile_args+=("--profile" "storybook")
+fi
+
+if [[ $RUN_LOCALPLANNING == "true" ]]; then
+  profile_args+=("--profile" "localplanning")
+fi
+
+echo "Starting Docker Compose with profiles: ${COMPOSE_PROFILES}"
+
 # start services
 docker compose \
   -f docker-compose.yml \
   -f docker-compose.pizza.yml \
   -f docker-compose.seed.yml \
+  "${profile_args[@]}" \
   up --build  --wait
--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -10,8 +10,22 @@ source .env.pizza
 DOCKER_BUILDKIT=1
 set +o allexport
 
+# Conditionally run non-essential services
+declare -a profile_args
+
+if [[ $RUN_STORYBOOK == "true" ]]; then
+  profile_args+=("--profile" "storybook")
+fi
+
+if [[ $RUN_LOCALPLANNING == "true" ]]; then
+  profile_args+=("--profile" "localplanning")
+fi
+
+echo "Starting Docker Compose with profiles: ${COMPOSE_PROFILES}"
+
 docker compose \
   -f docker-compose.yml \
   -f docker-compose.pizza.yml \
   -f docker-compose.seed.yml \
+  "${profile_args[@]}" \
   up --build --renew-anon-volumes --force-recreate --remove-orphans --wait
