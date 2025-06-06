862b9e47bdd3c30a54a743d697d2c68079bdbd60
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-06-06 09:17:52 +0100

    wip: Wire up cards to node URLs

--- editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Customisations/CustomisationCard.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Customisations/CustomisationCard.tsx
@@ -1,9 +1,10 @@
 import Done from "@mui/icons-material/Done";
-import ListItem, { ListItemProps } from "@mui/material/ListItem";
+import ListItemButton, { ListItemButtonProps } from "@mui/material/ListItemButton";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
 import { useStore } from "pages/FlowEditor/lib/store";
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigation } from "react-navi";
 
 import { NodeEdits } from "./types";
 
@@ -12,9 +13,9 @@ interface Props {
   nodeEdits?: NodeEdits
 };
 
-const Root = styled(ListItem, {
+const Root = styled(ListItemButton, {
   shouldForwardProp: (prop) => prop !== "isComplete",
-})<ListItemProps & { isComplete: boolean }>(({ theme, isComplete }) => ({
+})<ListItemButtonProps & { isComplete: boolean }>(({ theme, isComplete }) => ({
   display: "list-item",
   backgroundColor:
     isComplete
@@ -27,14 +28,35 @@ const Root = styled(ListItem, {
 }));
 
 export const CustomisationCard: React.FC<Props> = ({ nodeId, nodeEdits }) => {
-  const [flow] = useStore((state) => [state.flow]);
+
+  // Get ordered flow of indexed nodes from store
+  const [flow, orderedFlow, setOrderedFlow, getURLForNode] = useStore((state) => [
+    state.flow,
+    state.orderedFlow,
+    state.setOrderedFlow,
+    state.getURLForNode,
+  ]);
+
+  // Ensure we have an indexed version of the flow ready to generate links
+  // TODO: Should we move this responsibility to the store?
+  useEffect(() => {
+    if (!orderedFlow) setOrderedFlow();
+  }, [orderedFlow, setOrderedFlow]);
+  
   const cardTitle = 
     flow[nodeId].data?.title ||
     flow[nodeId].data?.text ||
     flow[nodeId].type
 
+  const { navigate } = useNavigation();
+
+  const handleClick = () => {
+    const url = getURLForNode(nodeId);
+    navigate(url);
+  };
+
   return (
-    <Root isComplete={Boolean(nodeEdits)}>
+    <Root isComplete={Boolean(nodeEdits)} onClick={handleClick}>
       <Typography variant="h5" alignContent="center" alignItems="center">
         {nodeEdits && <Done color="success" />}
         {cardTitle}
