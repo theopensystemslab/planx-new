dee48fa854b0f6a6a5d8cd13769baeca5ce0419a
ollie <107874766+zz-hh-aa@users.noreply.github.com>
2025-06-06 17:10:21 +0100

    feat: automate analytics link creation in frontend (#4763)

--- api.planx.uk/modules/analytics/routes.ts
+++ api.planx.uk/modules/analytics/routes.ts
@@ -5,9 +5,6 @@ import {
   logUserExitController,
   logUserResumeController,
 } from "./analyticsLog/controller.js";
-import { metabaseDashboardsController } from "./metabase/controller.js";
-import { createNewDashboardLinkSchema } from "./metabase/types.js";
-import { useEnvGuard } from "../../shared/middleware/useEnvGuard.js";
 
 const router = Router();
 
@@ -20,18 +17,6 @@ router.post(
   "/analytics/log-user-resume",
   validate(logAnalyticsSchema),
   logUserResumeController,
-);
-
-router.use(
-  "/metabase",
-  // "test" is removed because the api tests mock metabase calls, and the e2e tests don't need to actually hit Metabase either
-  useEnvGuard(["staging", "production"]), // "development" is removed to avoid Metabase staging noise, but when working on Metabase features locally it's useful to include it in the allow list (to hook local Hasura / editor up with Metabase staging)
-);
-
-router.post(
-  "/metabase/dashboard",
-  validate(createNewDashboardLinkSchema),
-  metabaseDashboardsController,
 );
 
 export default router;
