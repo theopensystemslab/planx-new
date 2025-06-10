550bd0e0c29fccd22ad6a6e1e78b1de322cb03db
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-06-10 12:37:37 +0200

    get isTemplate from state in ModalFooter component, fix FileUploadAndLabel schema

--- editor.planx.uk/src/@planx/components/Pay/Editor/Editor.tsx
+++ editor.planx.uk/src/@planx/components/Pay/Editor/Editor.tsx
@@ -127,6 +127,13 @@ const Component: React.FC<Props> = (props: Props) => {
             value={values.tags}
             disabled={props.disabled}
           />
+          {/* <TemplatedNodeConfiguration
+            formik={formik}
+            isTemplatedNode={values.isTemplatedNode}
+            templatedNodeInstructions={values.templatedNodeInstructions}
+            areTemplatedNodeInstructionsRequired={values.areTemplatedNodeInstructionsRequired}
+            disabled={props.disabled}
+          /> */}
         </Form>
       )}
     </Formik>
--- editor.planx.uk/src/ui/editor/ModalFooter.tsx
+++ editor.planx.uk/src/ui/editor/ModalFooter.tsx
@@ -1,5 +1,6 @@
 import { BaseNodeData } from "@planx/components/shared";
 import { useFormik } from "formik";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 import { InternalNotes } from "ui/editor/InternalNotes";
 import { MoreInformation } from "ui/editor/MoreInformation/MoreInformation";
@@ -12,7 +13,6 @@ interface Props<T extends BaseNodeData> {
   showMoreInformation?: boolean;
   showInternalNotes?: boolean;
   showTags?: boolean;
-  isTemplate?: boolean;
   disabled?: boolean;
 }
 
@@ -21,45 +21,48 @@ export const ModalFooter = <T extends BaseNodeData>({
   showMoreInformation = true,
   showInternalNotes = true,
   showTags = true,
-  isTemplate = false,
   disabled,
-}: Props<T>) => (
-  <>
-    {showMoreInformation && (
-      <MoreInformation
-        changeField={formik.handleChange}
-        definitionImg={formik.values.definitionImg}
-        howMeasured={formik.values.howMeasured}
-        policyRef={formik.values.policyRef}
-        info={formik.values.info}
-        disabled={disabled}
-      />
-    )}
-    {showInternalNotes && (
-      <InternalNotes
-        name="notes"
-        onChange={formik.handleChange}
-        value={formik.values.notes}
-        disabled={disabled}
-      />
-    )}
-    {showTags && (
-      <ComponentTagSelect
-        value={formik.values.tags}
-        onChange={(value) => formik.setFieldValue("tags", value)}
-        disabled={disabled}
-      />
-    )}
-    {isTemplate && (
-      <TemplatedNodeConfiguration
-        formik={formik}
-        isTemplatedNode={formik.values.isTemplatedNode}
-        templatedNodeInstructions={formik.values.templatedNodeInstructions}
-        areTemplatedNodeInstructionsRequired={
-          formik.values.areTemplatedNodeInstructionsRequired
-        }
-        disabled={disabled}
-      />
-    )}
-  </>
-);
+}: Props<T>) => {
+  const isTemplate = useStore.getState().isTemplate;
+
+  return (
+    <>
+      {showMoreInformation && (
+        <MoreInformation
+          changeField={formik.handleChange}
+          definitionImg={formik.values.definitionImg}
+          howMeasured={formik.values.howMeasured}
+          policyRef={formik.values.policyRef}
+          info={formik.values.info}
+          disabled={disabled}
+        />
+      )}
+      {showInternalNotes && (
+        <InternalNotes
+          name="notes"
+          onChange={formik.handleChange}
+          value={formik.values.notes}
+          disabled={disabled}
+        />
+      )}
+      {showTags && (
+        <ComponentTagSelect
+          value={formik.values.tags}
+          onChange={(value) => formik.setFieldValue("tags", value)}
+          disabled={disabled}
+        />
+      )}
+      {isTemplate && (
+        <TemplatedNodeConfiguration
+          formik={formik}
+          isTemplatedNode={formik.values.isTemplatedNode}
+          templatedNodeInstructions={formik.values.templatedNodeInstructions}
+          areTemplatedNodeInstructionsRequired={
+            formik.values.areTemplatedNodeInstructionsRequired
+          }
+          disabled={disabled}
+        />
+      )}
+    </>
+  );
+};
