be536190e5e4a0f6512a247e6a8d555480169b4e
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-06-19 13:28:18 +0200

    copy and insert summary

--- api.planx.uk/helpers.ts
+++ api.planx.uk/helpers.ts
@@ -12,6 +12,7 @@ export interface GetFlowDataResponse {
   slug: string;
   name: string;
   data: Flow["data"];
+  summary: string | null;
   team_id: number;
   team: { slug: string };
   publishedFlows:
@@ -36,6 +37,7 @@ const getFlowData = async (id: string): Promise<GetFlowDataResponse> => {
           slug
           data
           name
+          summary
           team_id
           team {
             slug
@@ -75,6 +77,7 @@ const createFlow = async ({
   flowData,
   copiedFrom,
   templatedFrom,
+  summary,
 }: {
   teamId: number;
   slug: string;
@@ -83,6 +86,7 @@ const createFlow = async ({
   flowData: Flow["data"];
   copiedFrom?: Flow["id"];
   templatedFrom?: Flow["id"];
+  summary?: string;
 }) => {
   const { client: $client } = getClient();
   const userId = userContext.getStore()?.user?.sub;
@@ -100,6 +104,7 @@ const createFlow = async ({
           $copied_from: uuid
           $templated_from: uuid
           $is_template: Boolean
+          $summary: String
         ) {
           flow: insert_flows_one(
             object: {
@@ -111,6 +116,7 @@ const createFlow = async ({
               copied_from: $copied_from
               templated_from: $templated_from
               is_template: $is_template
+              summary: $summary
             }
           ) {
             id
@@ -125,6 +131,7 @@ const createFlow = async ({
         copied_from: copiedFrom,
         templated_from: templatedFrom,
         is_template: isTemplate,
+        summary: summary,
       },
     );
 
