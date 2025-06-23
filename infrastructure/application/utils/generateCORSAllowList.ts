35ad1e826c98a5638d7c9e551e98636cacbb08db
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-06-23 10:37:40 +0100

    chore(api): Setup CORS to allow LPS to make requests to PlanX API [skip pizza]

--- infrastructure/application/utils/generateCORSAllowList.ts
+++ infrastructure/application/utils/generateCORSAllowList.ts
@@ -7,6 +7,8 @@ export const generateCORSAllowList = (customDomains: CustomDomains, domain: stri
   const editorURL = `https://${domain}`;
   const apiURL = `https://api.${domain}`; // Required for requests from API docs
   const hasuraURL = `https://hasura.${domain}`; // Required for proxied auth requests
+  // TODO: Configure when staging / production infra for LPS is put in place
+  // const lpsURL = "https://localplanning.services"
   const microsoftLoginURLs = ["https://login.live.com, https://login.microsoftonline.com"];
   const corsAllowList = [
     ...customDomainURLs,
