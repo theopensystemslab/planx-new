9700dd146afe7a8fe47ede8dec99ddbefe30d16d
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-06-23 12:43:57 +0200

    update copy

--- editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/TemplatedFlowStatus/TemplatedFlowStatusSection.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Settings/ServiceSettings/TemplatedFlowStatus/TemplatedFlowStatusSection.tsx
@@ -1,15 +1,22 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import Typography from "@mui/material/Typography";
 import { useFormik } from "formik";
 import { useToast } from "hooks/useToast";
-import React from "react";
+import React, { useState } from "react";
 import SettingsDescription from "ui/editor/SettingsDescription";
 import SettingsSection from "ui/editor/SettingsSection";
 
 import { useStore } from "../../../../lib/store";
 
 export const TemplatedFlowStatus = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const [sourceTemplateTeamName, flowId] = useStore((state) => [
     state.template?.team?.name,
     state.id,
@@ -21,7 +28,7 @@ export const TemplatedFlowStatus = () => {
     onSubmit: async () => {
       const isSuccess = true; // await ejectTemplatedFlow(flowId);
       if (isSuccess) {
-        toast.success("Successfully ejected from template features");
+        toast.success("Successfully opted-out of templated flow updates");
       }
     },
   });
@@ -30,41 +37,69 @@ export const TemplatedFlowStatus = () => {
     <Box component="form" onSubmit={form.handleSubmit} mb={2}>
       <SettingsSection>
         <Typography variant="h2" component="h3" gutterBottom>
-          Templated flow status
+          Templated flow updates
         </Typography>
         <Typography variant="body1">
-          Manage the status of your templated flow.
+          Manage how your templated flow receives updates.
         </Typography>
       </SettingsSection>
       <SettingsSection background>
         <SettingsDescription>
           <p>
             <strong>
-              {`Your templated flow is currently receiving updates from ${sourceTemplateTeamName}.`}
+              {`This service is templated from ${sourceTemplateTeamName}.`}
             </strong>
           </p>
           <p>
-            If you no longer wish to receive updates and instead make changes
-            independently, use the "Eject" button below to opt-out of templated
-            flow features.
+            This means this service will update whenever the source template is
+            published. Updates are made to reflect legislative changes,
+            introduce additional functionality and improve user experience.
           </p>
           <p>
-            Please beware that once you eject, your service will behave like a
-            "copy" and you'll be fully responsible for managing it. Opting back
-            into templated flow features is not currently supported.
+            If you no longer wish to receive updates and instead manage the
+            content of this service manually you can do this by opting out of
+            updates below.
+          </p>
+          <p>
+            Please note that once you opt out of updates, you will be fully
+            responsible for managing all content in this service. Opting back
+            into templated flow updates is not currently supported.
           </p>
         </SettingsDescription>
         <Box>
           <Button
-            onClick={() => form.submitForm()}
-            type="submit"
+            onClick={() => setIsOpen(true)}
             variant="contained"
             color="warning"
           >
-            Eject from templated flow features
+            Opt-out of updates
           </Button>
         </Box>
       </SettingsSection>
+      {isOpen && (
+        <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+          <DialogTitle>Opt-out of templated flow updates</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to opt-out of templated flow updates and
+              manage the content of this service manually?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setIsOpen(false)} color="secondary">
+              Cancel
+            </Button>
+            <Button
+              onClick={() => form.submitForm()}
+              type="submit"
+              color="warning"
+              variant="contained"
+            >
+              Opt-out of updates
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </Box>
   );
 };
