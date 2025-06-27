5bb72259b97e16def23c97ee4ccb9fb9bd4e5815
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2025-06-27 14:15:34 +0100

    hardcode privacy disclaimer

--- editor.planx.uk/src/@planx/components/Feedback/components/defaultContent.tsx
+++ editor.planx.uk/src/@planx/components/Feedback/components/defaultContent.tsx
@@ -8,10 +8,8 @@ export const defaultContent: Feedback = {
   ratingQuestion:
     "<strong>How would you rate your experience with this service?</strong>",
 
-  description: `This service is a work in progress, any feedback you share about your experience will help us to improve it.
-<br>
-<br>
-Don't share any personal or financial information in your feedback. If you do we will act according to our <a href="">privacy policy</a>.`,
+  description:
+    "This service is a work in progress, any feedback you share about your experience will help us to improve it.",
 
   disclaimer:
     "The information collected here isn't monitored by planning officers. Don't use it to give extra information about your project or submission. If you do, it cannot be used to assess your project.",
