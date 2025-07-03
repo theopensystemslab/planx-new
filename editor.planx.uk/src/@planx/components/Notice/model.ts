d4fb9ea265c41c75b394a0576fd0776a4eb797ed
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-07-03 17:16:31 +0100

    feat: Validation for Notice (#4891)

--- editor.planx.uk/src/@planx/components/Notice/model.ts
+++ editor.planx.uk/src/@planx/components/Notice/model.ts
@@ -1,7 +1,10 @@
-import { BaseNodeData, parseBaseNodeData } from "../shared";
+import { richText } from "lib/yupExtensions";
+import { boolean, object, SchemaOf, string } from "yup";
+
+import { BaseNodeData, baseNodeDataValidationSchema, parseBaseNodeData } from "../shared";
 
 export interface Notice extends BaseNodeData {
-  title: string;
+  title?: string;
   description: string;
   color: string;
   resetButton?: boolean;
@@ -14,3 +17,10 @@ export const parseNotice = (data: Record<string, any> | undefined) => ({
   resetButton: data?.resetButton || false,
   ...parseBaseNodeData(data),
 });
+
+export const validationSchema: SchemaOf<Notice> = baseNodeDataValidationSchema.concat(object({
+  title: string(),
+  description: richText(),
+  color: string().required(),
+  resetButton: boolean(),
+}));
