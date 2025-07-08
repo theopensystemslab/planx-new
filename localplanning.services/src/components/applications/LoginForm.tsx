8789b141fed3c84eefb49ecd312de21664e8efd1
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2025-07-08 13:34:23 +0100

    feat(lps): Universal input styles (#4910)

--- localplanning.services/src/components/applications/LoginForm.tsx
+++ localplanning.services/src/components/applications/LoginForm.tsx
@@ -21,7 +21,6 @@ const LoginForm: React.FC = () => {
           Enter your email address to log in
         </label>
         <input
-          className="pt-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="email"
           type="email"
           name="email"
