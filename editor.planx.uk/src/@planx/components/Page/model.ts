37f180bbfc64349e45d514bb7e8aab8b69a02159
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-07-08 08:14:40 +0100

    feat: Validation for Page (#4893)

--- editor.planx.uk/src/@planx/components/Page/model.ts
+++ editor.planx.uk/src/@planx/components/Page/model.ts
@@ -1,6 +1,12 @@
+import { richText } from "lib/yupExtensions";
 import { cloneDeep } from "lodash";
+import { object, string } from "yup";
 
-import { BaseNodeData, parseBaseNodeData } from "../shared";
+import {
+  BaseNodeData,
+  baseNodeDataValidationSchema,
+  parseBaseNodeData,
+} from "../shared";
 import { Schema } from "../shared/Schema/model";
 import { PAGE_SCHEMAS } from "./Editor";
 
@@ -25,3 +31,12 @@ export const parsePage = (data: Record<string, any> | undefined): Page => ({
   schema: cloneDeep(data?.schema) || PAGE_SCHEMAS[0].schema,
   ...parseBaseNodeData(data),
 });
+
+export const validationSchema = baseNodeDataValidationSchema.concat(
+  object({
+    fn: string().required(),
+    title: string().required(),
+    description: richText(),
+    schemaName: string().required(),
+  })
+);
