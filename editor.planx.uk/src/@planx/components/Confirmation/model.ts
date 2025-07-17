7dd2c743730ab842382eb707aa78e01f0c6da83a
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-07-17 13:52:05 +0200

    fix: ensure Confirmation retains initial base node data (#4957)

--- editor.planx.uk/src/@planx/components/Confirmation/model.ts
+++ editor.planx.uk/src/@planx/components/Confirmation/model.ts
@@ -2,7 +2,7 @@ import { richText } from "lib/yupExtensions";
 import { string } from "mathjs";
 import { array, object, SchemaOf } from "yup";
 
-import { BaseNodeData } from "../shared";
+import { BaseNodeData, parseBaseNodeData } from "../shared";
 import { baseNodeDataValidationSchema } from "./../shared/index";
 
 export interface Step {
@@ -18,10 +18,28 @@ export interface Confirmation extends BaseNodeData {
   contactInfo?: string;
 }
 
-export const parseNextSteps = (
-  data: { [key: string]: any } | undefined,
-): { nextSteps: Step[] } => ({
+export const parseConfirmation = (
+  data: Record<string, any> | undefined,
+): Confirmation => ({
+  heading: data?.heading || "Application sent",
+  description:
+    data?.description ||
+    `<p>A payment receipt has been emailed to you. You will also receive an email to confirm when your application has been received.</p>`,
+  moreInfo:
+    data?.moreInfo ||
+    `<h2>You will be contacted</h2>
+        <ul>
+        <li>if there is anything missing from the information you have provided so far</li>
+        <li>if any additional information is required</li>
+        <li>to arrange a site visit, if required</li>
+        </ul>`,
+  contactInfo:
+    data?.contactInfo ||
+    `You can contact us at <em>ADD YOUR COUNCIL CONTACT</em>
+          <br><br>
+          <p><strong>What did you think of this service? Please give us your feedback on the next page.</strong></p>`,
   nextSteps: data?.nextSteps || [],
+  ...parseBaseNodeData(data),
 });
 
 export const validationSchema: SchemaOf<Confirmation> =
