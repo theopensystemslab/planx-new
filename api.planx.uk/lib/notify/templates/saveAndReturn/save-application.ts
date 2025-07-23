205961da5c273db3214d67337cd7a8cfee485078
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-07-23 14:05:57 +0100

    chore(lps): Point to new email templates for UR (#4976)
    Co-authored-by: Ian Jones <hello@ianjo.uk>

--- api.planx.uk/lib/notify/templates/saveAndReturn/save-application.ts
+++ api.planx.uk/lib/notify/templates/saveAndReturn/save-application.ts
@@ -1,10 +1,11 @@
 import type { EmailFooter, NotifyConfig, NotifyTemplate } from "../index.js";
 
 /**
- * GovNotify template: https://www.notifications.service.gov.uk/services/012e65af-0eb2-45d5-87bd-4248354c4c22/templates/428c4dfd-a70b-44d6-9f81-b4f833d80405
+ * GovNotify template: https://www.notifications.service.gov.uk/services/012e65af-0eb2-45d5-87bd-4248354c4c22/templates/72e211e3-22d8-4a4a-8226-712a82768d86
+ * @deprecated Temp testing template - do not merge to `main` or `production`
  */
 export const saveTemplate: NotifyTemplate<Config> = {
-  id: "428c4dfd-a70b-44d6-9f81-b4f833d80405",
+  id: "72e211e3-22d8-4a4a-8226-712a82768d86",
   access: "public",
   config: {} as Config,
 };
