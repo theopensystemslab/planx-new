6aba970d188a2c8698957ba6d4f92432b1fbafd8
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-07-28 09:11:16 +0100

    fix: Drop validate property, additional test, and fix condition

--- editor.planx.uk/src/@planx/components/Question/Editor.tsx
+++ editor.planx.uk/src/@planx/components/Question/Editor.tsx
@@ -71,25 +71,6 @@ export const Question: React.FC<Props> = (props) => {
         alert(JSON.stringify({ type, ...values, children }, null, 2));
       }
     },
-    validate: ({ options, ...values }) => {
-      const errors: FormikErrors<FormikValues> = {};
-      if (values.fn && !options.some((option) => option.data.val)) {
-        errors.fn = "At least one option must also set a data field";
-      }
-      if (values.alwaysAutoAnswerBlank && !values.fn) {
-        errors.alwaysAutoAnswerBlank =
-          "Set a data field for the Question and all options but one when never putting to user";
-      }
-      if (
-        values.alwaysAutoAnswerBlank &&
-        values.fn &&
-        options.filter((option) => !option.data.val).length !== 1
-      ) {
-        errors.alwaysAutoAnswerBlank =
-          "Exactly one option should have a blank data field when never putting to user";
-      }
-      return errors;
-    },
     validationSchema,
     validateOnChange: false,
     validateOnBlur: false,
