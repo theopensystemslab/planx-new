ab776ef089c19f68509236f67a222650a2dd7239
zz-hh-aa <zzhhaa@protonmail.com>
2025-08-07 14:12:00 +0100

    chore: update metabase [skip pizza]

--- infrastructure/application/index.ts
+++ infrastructure/application/index.ts
@@ -211,7 +211,7 @@ export = async () => {
       }),
       container: {
         // if changing, also check docker-compose.yml
-        image: "metabase/metabase:v0.53.7",
+        image: "metabase/metabase:v0.55.10",
         portMappings: [metabaseListenerHttp],
         // When changing `memory`, also update `JAVA_OPTS` below
         memory: 4096 /*MB*/,
