368483862671300e8e144dc8b00ffaa579def235
Jimmy McBroom <james.mcbroom@gmail.com>
2025-08-08 15:24:44 -0400

    update AddressSearch component

--- localplanning.services/src/components/AddressSearch.tsx
+++ localplanning.services/src/components/AddressSearch.tsx
@@ -1,13 +1,20 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { navigate } from "astro:transitions/client";
 import type { Action } from "@content/actions";
 
 interface Props { 
   action: Action | undefined;
 };
 
+interface Address {
+  LPI: {
+    LAT: number;
+    LNG: number;
+  };
+}
 
 interface AddressSelectionEventDetail {
-  address: string;
+  address: Address
 }
 
 // @ts-expect-error
@@ -23,9 +30,11 @@ interface AddressSearchElement extends HTMLElement {
 }
 
 
-const AddressSearch: React.FC<Props> = () => {
+const AddressSearch: React.FC<Props> = ({ action }) => {
 
   const addressSearchRef = useRef<AddressSearchElement>(null);
+
+  const [address, setAddress] = useState<Address|null>(null)
 
   useEffect(() => {
     const inputElement = addressSearchRef.current;
@@ -38,6 +47,7 @@ const AddressSearch: React.FC<Props> = () => {
     const handleAddressSelection = (event: CustomEvent<AddressSelectionEventDetail>) => {
       const { detail } = event;
       console.debug("Address selected:", detail)
+      setAddress(detail.address)
     }
 
     inputElement.addEventListener("addressSelection", handleAddressSelection)
@@ -49,10 +59,31 @@ const AddressSearch: React.FC<Props> = () => {
   }, []);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+
     event.preventDefault();
 
     // address coords => LPA lookup
+    if (!address) {
+      return;
+    }
 
+    const { LAT, LNG } = address.LPI;
+
+    // remove in favor of some better way.. just not sure what that is
+    await fetch(`http://localhost:7002/lpa?lat=${LAT}&lon=${LNG}`)
+      .then(res => res.json())
+      .then(data => {
+        if(data.matchingLpa) {
+          navigate(`/${data.matchingLpa}`)
+          action 
+                ? navigate(`/${data.matchingLpa}?action=${action}`)
+                : navigate(`/${data.matchingLpa}`)
+        }
+        else {
+          // TODO: what to display to user?
+          console.debug("There was no matching LPA")
+        }
+      })
   };
 
   return (
