efbb51686f1dac8f8e68110fb8ed5e98d7fd3ede
Jimmy McBroom <james.mcbroom@gmail.com>
2025-08-14 09:52:27 -0400

    show rough end user msg

--- localplanning.services/src/components/AddressSearch.tsx
+++ localplanning.services/src/components/AddressSearch.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { navigate } from "astro:transitions/client";
 import type { Action } from "@content/actions";
+import { PUBLIC_PLANX_REST_API_URL } from "astro:env/client";
 
 interface Props { 
   action: Action | undefined;
@@ -16,6 +17,30 @@ interface Address {
 interface AddressSelectionEventDetail {
   address: Address
 }
+
+interface LocalPlanningAuthorityFeature {
+  name: string
+  reference: string
+  entity: number
+}
+
+// storing this here for now, but perhaps this should be elsewhere..?
+const lpaReferenceLookup: Record<string, string> = {
+  E60000203: "barnet",
+  E60000331: "buckinghamshire",
+  E60000188: "camden",
+  E60000065: "doncaster",
+  E60000271: "epsom-and-ewell",
+  E60000008: "gateshead",
+  E60000311: "gloucester",
+  E60000195: "lambeth",
+  E60000224: "medway",
+  E60000009: "newcastle",
+  E60000198: "southwark",
+  E60000171: "st-albans",
+  E60000313: "tewkesbury",
+  E60000230: "west-berkshire",
+};
 
 // @ts-expect-error
 interface AddressSearchElement extends HTMLElement {
@@ -36,6 +61,8 @@ const AddressSearch: React.FC<Props> = ({ action }) => {
 
   const [address, setAddress] = useState<Address|null>(null)
 
+  const [matchingLpas, setMatchingLpas] = useState<LocalPlanningAuthorityFeature[]|null>(null)
+
   useEffect(() => {
     const inputElement = addressSearchRef.current;
 
@@ -48,6 +75,7 @@ const AddressSearch: React.FC<Props> = ({ action }) => {
       const { detail } = event;
       console.debug("Address selected:", detail)
       setAddress(detail.address)
+      setMatchingLpas(null)
     }
 
     inputElement.addEventListener("addressSelection", handleAddressSelection)
@@ -70,18 +98,21 @@ const AddressSearch: React.FC<Props> = ({ action }) => {
     const { LAT, LNG } = address.LPI;
 
     // remove in favor of some better way.. just not sure what that is
-    await fetch(`http://localhost:7002/lpa?lat=${LAT}&lon=${LNG}`)
+    await fetch(`${PUBLIC_PLANX_REST_API_URL}/lpa?lat=${LAT}&lon=${LNG}`)
       .then(res => res.json())
       .then(data => {
-        if(data.matchingLpa) {
-          navigate(`/${data.matchingLpa}`)
-          action 
-                ? navigate(`/${data.matchingLpa}?action=${action}`)
-                : navigate(`/${data.matchingLpa}`)
-        }
-        else {
-          // TODO: what to display to user?
-          console.debug("There was no matching LPA")
+
+        setMatchingLpas(data.entities)
+
+        // sometimes there are multiple intersecting LPAs
+        // do we need more sophisticated match logic here?
+        for (const entity of data.entities) {
+          if (Object.keys(lpaReferenceLookup).indexOf(entity.reference) > -1) {
+            let matchingLpaRoute = lpaReferenceLookup[entity.reference]
+            action 
+                  ? navigate(`/${matchingLpaRoute}?action=${action}`)
+                  : navigate(`/${matchingLpaRoute}`)
+          }
         }
       })
   };
@@ -96,6 +127,8 @@ const AddressSearch: React.FC<Props> = ({ action }) => {
       <button type="submit" className="button button--primary button--medium">
         Find the local planning authority
       </button>
+      {matchingLpas?.length === 0 && <p>We couldn't find a local planning authority for this address.</p>}
+      {matchingLpas && matchingLpas?.length > 0 && <p>Your local planning authorities are: {matchingLpas?.map(e => e.name)}</p>}
     </form>
   );
 };
