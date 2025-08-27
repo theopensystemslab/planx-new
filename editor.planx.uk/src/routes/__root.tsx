fad96833f217d056e45645cf3cb1554c33dbd59e
nichgalzin <dropout@Nichs-MacBook-Air.local>
2025-08-27 17:51:50 +0100

    Revert "feat: adds context to root route and adds before load function to check auth status redirects"
    This reverts commit 6b961033ceafdd7855b655aafad03a50d3c42bd9.

--- editor.planx.uk/src/routes/__root.tsx
+++ editor.planx.uk/src/routes/__root.tsx
@@ -1,27 +1,8 @@
-import {
-  createRootRouteWithContext,
-  Outlet,
-  redirect,
-} from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import React from "react";
 
-interface RouterContext {
-  currentUser?: boolean;
-}
-
-export const Route = createRootRouteWithContext<RouterContext>()({
-  beforeLoad: async ({ context, location }) => {
-    // Allow login page and other public routes
-    if (location.pathname === "/login") return {};
-    if (!context.currentUser) {
-      throw redirect({
-        to: "/login",
-        search: { redirectTo: location.pathname },
-      });
-    }
-    return {};
-  },
+export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
