7e42839f1b55d18efce275bc608ce7fb013bb72d
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-08-27 10:07:44 +0200

    drop flow doc templates

--- scripts/seed-database/write/main.sql
+++ scripts/seed-database/write/main.sql
@@ -2,7 +2,6 @@
 \include write/users.sql
 \include write/teams.sql
 \include write/flows.sql
-\include write/flow_document_templates.sql
 \include write/published_flows.sql
 \include write/team_members.sql
 \include write/team_integrations.sql
