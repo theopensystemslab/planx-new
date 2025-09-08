3339f96d2f50204e6a1278be03453bd16ffd1d0c
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-09-08 10:27:02 +0100

    feat: Don't prompt for additional email when redirecting from LPS (#5194)

--- api.planx.uk/modules/lps/service/getApplications/index.test.ts
+++ api.planx.uk/modules/lps/service/getApplications/index.test.ts
@@ -4,33 +4,39 @@ import { generateResumeLink } from "./index.js";
 describe("generateResumeLink() helper function", () => {
   it("generates a valid URL for teams with a custom subdomain", () => {
     const expected =
-      "https://planningservices.mycouncil.gov/my-flow?sessionId=123";
-    const actual = generateResumeLink({
-      id: "123",
-      service: {
-        slug: "my-flow",
-        team: {
-          domain: "planningservices.mycouncil.gov",
+      "https://planningservices.mycouncil.gov/my-flow?sessionId=123&email=test%40example.com";
+    const actual = generateResumeLink(
+      {
+        id: "123",
+        service: {
+          slug: "my-flow",
+          team: {
+            domain: "planningservices.mycouncil.gov",
+          },
         },
-      },
-    } as Application);
+      } as Application,
+      "test@example.com",
+    );
 
     expect(actual).toEqual(expected);
   });
 
   it("generates a valid URL for teams without a custom subdomain", () => {
     const expected =
-      "https://www.example.com/my-team/my-flow/published?sessionId=123";
-    const actual = generateResumeLink({
-      id: "123",
-      service: {
-        slug: "my-flow",
-        team: {
-          slug: "my-team",
-          domain: null,
+      "https://www.example.com/my-team/my-flow/published?sessionId=123&email=test%40example.com";
+    const actual = generateResumeLink(
+      {
+        id: "123",
+        service: {
+          slug: "my-flow",
+          team: {
+            slug: "my-team",
+            domain: null,
+          },
         },
-      },
-    } as Application);
+      } as Application,
+      "test@example.com",
+    );
 
     expect(actual).toEqual(expected);
   });
