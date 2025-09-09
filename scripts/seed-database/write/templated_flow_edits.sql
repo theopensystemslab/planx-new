c337ddb7e52a3be6998b01b567e51b3426a719dd
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-09-09 13:19:06 +0200

    wip

---
+++ scripts/seed-database/write/templated_flow_edits.sql
@@ -0,0 +1,26 @@
+CREATE TEMPORARY TABLE sync_templated_flow_edits (
+  id uuid,
+  flow_id uuid,
+  data jsonb,
+  created_at timestamptz,
+  updated_at timestamptz,
+);
+
+\copy sync_templated_flow_edits from '/tmp/templated_flow_edits.csv' WITH (FORMAT csv, DELIMITER ';');
+
+INSERT INTO 
+  templated_flow_edits (
+    id,
+    flow_id,
+    data,
+    created_at,
+    updated_at
+  )
+SELECT 
+  id,
+  flow_id,
+  data,
+  created_at,
+  updated_at
+FROM 
+  sync_templated_flow_edits ON CONFLICT (id) DO NOTHING;
