92e7dbdcd4f376340dab187387b2ce7c53c27e39
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-09-10 14:12:06 +0100

    feat(lps): Get list of applications, with `user_status` property, instead of grouped applications (#5176)
    Merging in Dafydd's absence

--- api.planx.uk/modules/lps/types.ts
+++ api.planx.uk/modules/lps/types.ts
@@ -26,21 +26,29 @@ interface LPSApplication {
   };
   address: string | null;
   createdAt: string;
+  updatedAt: string;
+  /** Only services which use Section components will have values for progress */
+  progress?: {
+    completed: number;
+  };
+  expiresAt: string;
 }
 
 export type DraftLPSApplication = LPSApplication & {
-  expiresAt: string;
   serviceUrl: string;
+};
+
+export type AwaitingPaymentLPSApplication = LPSApplication & {
+  paymentUrl: string;
 };
 
 export type SubmittedLPSApplication = LPSApplication & {
   submittedAt: string;
 };
 
-export interface Success {
-  drafts: DraftLPSApplication[];
-  submitted: SubmittedLPSApplication[];
-}
+export type Success = Array<
+  DraftLPSApplication | AwaitingPaymentLPSApplication | SubmittedLPSApplication
+>;
 
 interface Failure {
   error: "LINK_INVALID" | "LINK_CONSUMED" | "LINK_EXPIRED";
--- localplanning.services/src/components/applications/ApplicationsList.tsx
+++ localplanning.services/src/components/applications/ApplicationsList.tsx
@@ -1,14 +1,12 @@
 import type React from "react";
 import { useFetchApplications } from "./hooks/useFetchApplications";
-import { ApplicationCard } from "./ApplicationCard";
 import { InvalidLink } from "./errors/InvalidLink";
 import { ExpiredLink } from "./errors/ExpiredLink";
 import { ConsumedLink } from "./errors/ConsumedLink";
 import { UnhandledError } from "./errors/UnhandledError";
-import { NoApplications } from "./errors/NoApplications";
 
 export const ApplicationsList: React.FC = () => {
-  const { applications: { drafts, submitted }, isLoading, error } = useFetchApplications();
+  const { applications, isLoading, error } = useFetchApplications();
 
   // TODO: Better UI - skeleton or spinner?
   if (isLoading) return <p>Loading your applications...</p>;
@@ -26,32 +24,34 @@ export const ApplicationsList: React.FC = () => {
     }
   }
 
-  if (!drafts.length && !submitted.length) {
-    return <NoApplications />;
-  }
+  return <pre>{ JSON.stringify(applications, null, 4) }</pre>
 
-  return (
-    <div className="flex flex-col gap-8 max-w-3xl">
-      {Boolean(drafts.length) && (
-        <>
-          <h2 className="text-heading-lg">Draft applications</h2>
-          <ul className="flex flex-col gap-8">
-            {drafts.map((draft) =>
-              <ApplicationCard application={draft} variant="draft" key={draft.id} />
-            )}
-          </ul>
-        </>
-      )}
-      {Boolean(submitted.length) && (
-        <>
-          <h2 className="text-heading-lg">Submitted applications</h2>
-          <ul className="flex flex-col gap-8">
-            {submitted.map((submitted) =>
-              <ApplicationCard application={submitted} variant="submitted" key={submitted.id} />
-            )}
-          </ul>
-        </>
-      )}
-    </div>
-  );
+  // if (!drafts.length && !submitted.length) {
+  //   return <NoApplications />;
+  // }
+
+  // return (
+  //   <div className="flex flex-col gap-8 max-w-3xl">
+  //     {Boolean(drafts.length) && (
+  //       <>
+  //         <h2 className="text-heading-lg">Draft applications</h2>
+  //         <ul className="flex flex-col gap-8">
+  //           {drafts.map((draft) =>
+  //             <ApplicationCard application={draft} variant="draft" key={draft.id} />
+  //           )}
+  //         </ul>
+  //       </>
+  //     )}
+  //     {Boolean(submitted.length) && (
+  //       <>
+  //         <h2 className="text-heading-lg">Submitted applications</h2>
+  //         <ul className="flex flex-col gap-8">
+  //           {submitted.map((submitted) =>
+  //             <ApplicationCard application={submitted} variant="submitted" key={submitted.id} />
+  //           )}
+  //         </ul>
+  //       </>
+  //     )}
+  //   </div>
+  // );
 };
