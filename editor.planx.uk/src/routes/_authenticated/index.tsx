1176943bd351371aa77e6db31ec0e5235a813187
nichgalzin <dropout@Nichs-MacBook-Air.local>
2025-09-10 12:45:05 +0100

    feat: authenticate home route with data fetching for home/teams page

---
+++ editor.planx.uk/src/routes/_authenticated/index.tsx
@@ -0,0 +1,58 @@
+import {
+  Team,
+  TeamSettings,
+  TeamTheme,
+} from "@opensystemslab/planx-core/types";
+import { createFileRoute } from "@tanstack/react-router";
+import gql from "graphql-tag";
+import { useStore } from "pages/FlowEditor/lib/store";
+import Teams from "pages/Teams";
+import React from "react";
+import { makeTitle } from "routes-navi/utils";
+
+import { client } from "../../lib/graphql";
+
+export type TeamSummary = Pick<Team, "id" | "name" | "slug"> & {
+  settings: Pick<TeamSettings, "isTrial">;
+} & { theme: Pick<TeamTheme, "primaryColour" | "logo"> };
+
+const AuthenticatedHomeRoute = () => {
+  const data = Route.useLoaderData();
+  return (
+    <>
+      <Teams teams={data.teams} />
+    </>
+  );
+};
+
+export const Route = createFileRoute("/_authenticated/")({
+  loader: async () => {
+    await useStore.getState().initUserStore();
+    const { data } = await client.query<{ teams: TeamSummary[] }>({
+      query: gql`
+        query GetTeamSummaries {
+          teams(order_by: { name: asc }) {
+            id
+            name
+            slug
+            settings: team_settings {
+              isTrial: is_trial
+            }
+            theme {
+              primaryColour: primary_colour
+              logo
+            }
+          }
+        }
+      `,
+    });
+
+    useStore.getState().clearTeamStore();
+
+    return {
+      title: makeTitle("Teams"),
+      teams: data.teams,
+    };
+  },
+  component: AuthenticatedHomeRoute,
+});
