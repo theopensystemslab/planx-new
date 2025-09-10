7b56ce69fe61df83247864f288db5579134abacb
nichgalzin <dropout@Nichs-MacBook-Air.local>
2025-09-10 16:56:13 +0100

    fix: adds auth check to all routes

--- editor.planx.uk/src/routes/__root.tsx
+++ editor.planx.uk/src/routes/__root.tsx
@@ -11,6 +11,21 @@ interface RouterContext {
 }
 
 export const Route = createRootRouteWithContext<RouterContext>()({
+  beforeLoad: ({ location, context }) => {
+    if (location.pathname === "/login") {
+      return {};
+    }
+    if (!context.currentUser) {
+      throw redirect({
+        to: "/login",
+        search: {
+          redirectTo: location.pathname !== "/" ? location.pathname : undefined,
+        },
+      });
+    }
+
+    return {};
+  },
   component: () => (
     <>
       <Outlet />
