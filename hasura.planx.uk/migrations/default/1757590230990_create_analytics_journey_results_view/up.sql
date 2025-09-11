35f8bd8d3d81096c972ef65df251dcd5f074bba2
zz-hh-aa <zzhhaa@protonmail.com>
2025-09-11 13:06:16 +0100

    fix: show separate results

--- hasura.planx.uk/migrations/default/1757590230990_create_analytics_journey_results_view/up.sql
+++ hasura.planx.uk/migrations/default/1757590230990_create_analytics_journey_results_view/up.sql
@@ -2,14 +2,15 @@ DROP MATERIALIZED VIEW IF EXISTS "public"."analytics_journey_results";
 CREATE MATERIALIZED VIEW "public"."analytics_journey_results" AS 
 SELECT a.id AS analytics_id,
     a.flow_id AS flow_id,
-    max(a.created_at) AS analytics_created_at,
-    string_agg(rf.result_text, ', '::text ORDER BY al.created_at) AS results
+    a.created_at AS analytics_created_at,
+    al.created_at AS analytics_log_created_at,
+    rf.result_text
 FROM analytics a
     LEFT JOIN analytics_logs al ON (a.id = al.analytics_id)
     LEFT JOIN LATERAL (
         SELECT (((al.metadata ->> 'flag'::text))::jsonb ->> 'text'::text) AS result_text
     ) rf ON ((al.metadata ->> 'flag'::text) IS NOT NULL)
-GROUP BY a.id, a.flow_id
-ORDER BY a.id;
+WHERE rf.result_text IS NOT NULL
+ORDER BY a.id, al.created_at;
 
 GRANT SELECT ON "public"."analytics_journey_results" TO metabase_read_only;
