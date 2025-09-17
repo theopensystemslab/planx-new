42e60b4fb750266fe7d7965fb303aced89a132dd
nichgalzin <dropout@Nichs-MacBook-Air.local>
2025-09-17 16:52:55 +0100

    feat: placeholder routes for $team menu pages

---
+++ editor.planx.uk/src/routes/_authenticated/$team/general-settings.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+import React from "react";
+
+export const Route = createFileRoute("/_authenticated/$team/general-settings")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <div>Hello "/_authenticated/$team/general-settings"!</div>;
+}
