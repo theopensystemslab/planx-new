67090ee1507a030cb6e3f45f9ce0bdec130de519
nichgalzin <dropout@Nichs-MacBook-Air.local>
2025-09-17 13:06:54 +0100

    feat: data fetching and component loading for admin-panel

--- editor.planx.uk/src/routes/_authenticated/admin-panel.tsx
+++ editor.planx.uk/src/routes/_authenticated/admin-panel.tsx
@@ -1,10 +1,60 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
+import { AdminPanelData } from "types";
+
+import { client } from "../../lib/graphql";
+import { PlatformAdminPanel } from "../../pages/PlatformAdminPanel/PlatformAdminPanel";
+import {
+  PRODUCTION_ADMIN_PANEL_QUERY,
+  STAGING_ADMIN_PANEL_QUERY,
+} from "../utils";
+
+const AdminPanelRoute = () => {
+  return <PlatformAdminPanel />;
+};
 
 export const Route = createFileRoute("/_authenticated/admin-panel")({
-  component: RouteComponent,
-});
+  beforeLoad: async ({ context }) => {
+    const { user } = context;
 
-function RouteComponent() {
-  return <div>Hello "/_authenticated/admin-settings"!</div>;
-}
+    if (!user) {
+      throw notFound();
+    }
+    const { isPlatformAdmin, isAnalyst } = user;
+    const isAuthorised = isPlatformAdmin || isAnalyst;
+
+    if (!isAuthorised) {
+      throw notFound();
+    }
+
+    return { user };
+  },
+  loader: async ({ context }) => {
+    const { user } = context;
+    const { isPlatformAdmin } = user;
+
+    // Select query based on environment
+    const query =
+      import.meta.env.VITE_APP_ENV === "production"
+        ? PRODUCTION_ADMIN_PANEL_QUERY
+        : STAGING_ADMIN_PANEL_QUERY;
+
+    // Fetch admin panel data
+    const { data } = await client.query<{ adminPanel: AdminPanelData[] }>({
+      query,
+      context: {
+        headers: {
+          "x-hasura-role": isPlatformAdmin ? "platformAdmin" : "analyst",
+        },
+      },
+    });
+
+    useStore.getState().setAdminPanelData(data.adminPanel);
+
+    return {
+      adminPanel: data.adminPanel,
+    };
+  },
+  component: AdminPanelRoute,
+});
