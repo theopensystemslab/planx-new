72069169afdad70f3527f04715affb85756f9315
nichgalzin <nichgalzin@gmail.com>
2025-09-25 12:16:56 +0100

    fix: updates before load layout component with correct preview env role value

--- editor.planx.uk/src/routes/_authenticated/route.tsx
+++ editor.planx.uk/src/routes/_authenticated/route.tsx
@@ -1,9 +1,13 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import React from "react";
 
+import { useStore } from "../../pages/FlowEditor/lib/store";
 import AuthenticatedLayout from "../../pages/layout/AuthenticatedLayout";
 
 export const Route = createFileRoute("/_authenticated")({
+  beforeLoad: async () => {
+    useStore.getState().setPreviewEnvironment("editor");
+  },
   component: () => (
     <AuthenticatedLayout>
       <Outlet />
