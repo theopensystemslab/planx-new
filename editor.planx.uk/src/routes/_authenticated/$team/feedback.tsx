b882b67aa681f0daeac4b40779fcbd79f2f3f7d4
nichgalzin <nichgalzin@gmail.com>
2025-10-01 16:33:34 +0100

    feat: loader functions and route components for feedback and subscription pages

--- editor.planx.uk/src/routes/_authenticated/$team/feedback.tsx
+++ editor.planx.uk/src/routes/_authenticated/$team/feedback.tsx
@@ -1,10 +1,46 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import gql from "graphql-tag";
+import { FEEDBACK_SUMMARY_FIELDS } from "lib/feedback";
+import { client } from "lib/graphql";
+import { FeedbackLog } from "pages/FlowEditor/components/FeedbackLog/FeedbackLog";
+import { Feedback } from "pages/FlowEditor/components/FeedbackLog/types";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 
 export const Route = createFileRoute("/_authenticated/$team/feedback")({
-  component: RouteComponent,
+  loader: async ({ params }) => {
+    const isAuthorised = useStore.getState().canUserEditTeam(params.team);
+    if (!isAuthorised) {
+      throw notFound();
+    }
+
+    const {
+      data: { feedback },
+    } = await client.query<{ feedback: Feedback[] }>({
+      query: gql`
+        query GetFeedbackForTeam($teamSlug: String!) {
+          feedback: feedback_summary(
+            order_by: { created_at: desc }
+            where: { team_slug: { _eq: $teamSlug } }
+          ) {
+            ...FeedbackSummaryFields
+          }
+        }
+
+        ${FEEDBACK_SUMMARY_FIELDS}
+      `,
+      variables: { teamSlug: params.team },
+      fetchPolicy: "no-cache",
+    });
+
+    return {
+      feedback,
+    };
+  },
+  component: FeedbackRoute,
 });
 
-function RouteComponent() {
-  return <div>Hello "/_authenticated/$team/feedback"!</div>;
+function FeedbackRoute() {
+  const { feedback } = Route.useLoaderData();
+  return <FeedbackLog feedback={feedback} />;
 }
