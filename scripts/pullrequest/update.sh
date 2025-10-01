331157a659f259a88e1fdbf90c9762c8aef8aea7
Dan G <freemvmt@posteo.net>
2025-10-01 08:31:53 +0100

    fix: use pizza failover config to build caddy when pull fails (#5316)

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -10,8 +10,14 @@ source .env.pizza
 DOCKER_BUILDKIT=1
 set +o allexport
 
+PIZZA_FAILOVER=""
+if ! docker pull "$VULTR_CR_URN/caddy-vultr:latest"; then
+  echo "Failed to pull caddy image, building from source..."
+  PIZZA_FAILOVER="-f docker-compose.pizza.failover.yml"
+fi
+
 docker compose \
   -f docker-compose.yml \
-  -f docker-compose.pizza.yml \
+  -f docker-compose.pizza.yml $PIZZA_FAILOVER \
   -f docker-compose.seed.yml \
   up --build --renew-anon-volumes --force-recreate --remove-orphans --wait
