9ee424201fd99f4ceef420e56e3301ea1734ab8b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-10-02 16:35:15 +0100

    chore(lps): Update copy for "notify" card [skip pizza]

--- localplanning.services/src/content/actions/index.ts
+++ localplanning.services/src/content/actions/index.ts
@@ -1,6 +1,6 @@
 import type { Action } from "@stores/action";
 
-export const actions: Record<Action, { title: string, description: string }> = {
+export const actions: Record<Action, { title: string; description: string }> = {
   apply: {
     title: "Start a planning application",
     description:
@@ -9,11 +9,11 @@ export const actions: Record<Action, { title: string, description: string }> = {
   notify: {
     title: "Notify your authority",
     description:
-      "Inform your local planning authority about changes you think may be a planning breach.",
+      "Submit reports, complaints, requests, and notifications directly to your local planning authority.",
   },
-  guidance:  {
+  guidance: {
     title: "Get planning guidance",
     description:
       "Find out if you need planning permission, and other guidance services.",
-  }
+  },
 } as const;
