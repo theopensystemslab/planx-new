bd5f06bfa40653c61eeb27b5362a32a10a815484
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2025-10-06 17:44:52 +0100

    update and clean up review tab

--- editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Review/ReviewCard.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Review/ReviewCard.tsx
@@ -1,17 +1,19 @@
 import ListItem from "@mui/material/ListItem";
 import Typography from "@mui/material/Typography";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 import { NodeCard } from "ui/editor/NodeCard";
 
-export const ReviewCard: React.FC<{ nodeId: string; notes?: string }> = ({
-  nodeId,
-  notes,
-}) => (
-  <ListItem>
-    <NodeCard nodeId={nodeId} backgroundColor="#fff">
-      {notes && (
-        <Typography variant="body2">Internal notes - {notes}</Typography>
-      )}
-    </NodeCard>
-  </ListItem>
-);
+export const ReviewCard: React.FC<{ nodeId: string }> = ({ nodeId }) => {
+  const node = useStore((state) => state.getNode(nodeId));
+  const notes = node?.data?.notes;
+  return (
+    <ListItem>
+      <NodeCard nodeId={nodeId} backgroundColor="#fff">
+        {notes && (
+          <Typography variant="body2">Internal notes - {notes}</Typography>
+        )}
+      </NodeCard>
+    </ListItem>
+  );
+};
--- editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Review/index.tsx
+++ editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Review/index.tsx
@@ -1,24 +1,19 @@
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
-import { sortIdsDepthFirst } from "@planx/graph";
 import { useStore } from "pages/FlowEditor/lib/store";
-import React from "react";
+import React, { useMemo } from "react";
 
 import { ReviewCard } from "./ReviewCard";
 
 const Reviews = () => {
   const flow = useStore((state) => state.flow);
+  const filterFlowByTag = useStore((state) => state.filterFlowByTag);
 
-  // Get the nodes within this flow that are tagged as 'To review' and  sort them top-down to match graph
-  const reviewNodeIds = new Set(
-    Object.entries(flow)
-      .filter(([_nodeId, nodeData]) =>
-        Boolean(nodeData.data?.tags?.includes("toReview")),
-      )
-      .map(([nodeId]) => nodeId),
+  const sortedReviewNodeIds = useMemo(
+    () => Object.keys(filterFlowByTag("toReview")),
+    [flow, filterFlowByTag],
   );
-  const sortedReviewNodeIds = sortIdsDepthFirst(flow)(reviewNodeIds);
 
   return (
     <Box p={2} sx={{ backgroundColor: "background.paper", minHeight: "100%" }}>
@@ -26,11 +21,9 @@ const Reviews = () => {
         {`Nodes with a 'To review' tag`}
       </Typography>
       <List sx={{ mt: 1 }}>
-        {sortedReviewNodeIds.map((nodeId) => {
-          const node = flow[nodeId];
-          const notes = node?.data?.notes;
-          return <ReviewCard key={nodeId} nodeId={nodeId} notes={notes} />;
-        })}
+        {sortedReviewNodeIds.map((nodeId) => (
+          <ReviewCard key={nodeId} nodeId={nodeId} />
+        ))}
       </List>
     </Box>
   );
