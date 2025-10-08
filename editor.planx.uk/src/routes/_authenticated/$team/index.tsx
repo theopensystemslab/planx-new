6e22e3d9890823d5579bd3f077da125667a333d4
nichgalzin <nichgalzin@gmail.com>
2025-10-08 16:35:41 +0100

    feat: router for main team page including data loading and setting of search params

---
+++ editor.planx.uk/src/routes/_authenticated/$team/index.tsx
@@ -0,0 +1,60 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { fallback, zodValidator } from "@tanstack/zod-adapter";
+import Team from "pages/Team";
+import React from "react";
+import { z } from "zod";
+
+import { getTeamFromDomain } from "../../../lib/utils";
+import { useStore } from "../../../pages/FlowEditor/lib/store";
+
+const teamSearchSchema = z.object({
+  sort: fallback(
+    z.enum(["last-edited", "last-published", "name"]),
+    "last-edited",
+  ).default("last-edited"),
+  sortDirection: fallback(z.enum(["asc", "desc"]), "desc").default("desc"),
+});
+
+export type TeamSearch = z.infer<typeof teamSearchSchema>;
+
+export const Route = createFileRoute("/_authenticated/$team/")({
+  validateSearch: zodValidator(teamSearchSchema),
+  beforeLoad: async ({ params }) => {
+    const { initTeamStore, teamSlug: currentSlug } = useStore.getState();
+    const routeSlug =
+      params.team || (await getTeamFromDomain(window.location.hostname));
+
+    if (!routeSlug) {
+      throw new Error("Team not found");
+    }
+
+    if (currentSlug !== routeSlug) {
+      try {
+        await initTeamStore(routeSlug);
+      } catch (error) {
+        throw new Error(`Team not found: ${error}`);
+      }
+    }
+  },
+  loader: async () => {
+    const { getTeam, getFlows } = useStore.getState();
+    const team = getTeam();
+
+    try {
+      const flows = await getFlows(team.id);
+      return {
+        team,
+        flows,
+      };
+    } catch (error) {
+      throw new Error(`Failed to load flows: ${error}`);
+    }
+  },
+  component: TeamComponent,
+});
+
+function TeamComponent() {
+  const { flows } = Route.useLoaderData();
+
+  return <Team flows={flows} />;
+}
