8a882ab8e60081fb10c493b9cb1dccdf42f215fb
nichgalzin <nichgalzin@gmail.com>
2025-10-09 14:20:03 +0100

    feat: flow editor base route with data fetching and store initiation and update

--- editor.planx.uk/src/routes/_authenticated/$team/$flow/index.tsx
+++ editor.planx.uk/src/routes/_authenticated/$team/$flow/index.tsx
@@ -1,10 +1,70 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, stripSearchParams } from "@tanstack/react-router";
+import FlowEditorLayout from "pages/layout/FlowEditorLayout";
 import React from "react";
+import { z } from "zod";
 
-export const Route = createFileRoute("/_authenticated/$team/$flow/")({
-  component: RouteComponent,
+import { useStore } from "../../../../pages/FlowEditor/lib/store";
+import { getBasicFlowData, getFlowEditorData } from "./_utils";
+
+const flowParamsSchema = z.object({
+  team: z.string(),
+  flow: z.string(),
 });
 
-function RouteComponent() {
-  return <div>Hello "/_authenticated/$team/$flow/"!</div>;
-}
+// Empty search schema to override inherited search params from team route
+const flowSearchSchema = z.object({});
+
+export const Route = createFileRoute("/_authenticated/$team/$flow/")({
+  params: {
+    parse: flowParamsSchema.parse,
+    stringify: ({ team, flow }) => ({ team, flow }),
+  },
+  validateSearch: flowSearchSchema,
+  search: {
+    middlewares: [
+      stripSearchParams({ sort: "last-edited", sortDirection: "desc" }),
+    ],
+  },
+  beforeLoad: async ({ params }) => {
+    const { team: teamSlug, flow: flowSlug } = params;
+
+    const actualFlowSlug = flowSlug.split(",")[0];
+
+    try {
+      const flow = await getBasicFlowData(actualFlowSlug, teamSlug);
+
+      const store = useStore.getState();
+      store.setFlowName(flow.name);
+      store.setFlowSlug(actualFlowSlug);
+      await store.connectTo(flow.id);
+
+      const flowEditorData = await getFlowEditorData(actualFlowSlug, teamSlug);
+
+      useStore.setState({
+        id: flowEditorData.id,
+        flowStatus: flowEditorData.flowStatus,
+        isFlowPublished: flowEditorData.isFlowPublished,
+        isTemplate: flowEditorData.isTemplate,
+        isTemplatedFrom: Boolean(flowEditorData.templatedFrom),
+        template: flowEditorData.template,
+      });
+
+      if (flowEditorData.templatedFrom) {
+        store.setOrderedFlow();
+      }
+
+      store.getFlowInformation(actualFlowSlug, teamSlug);
+    } catch (error) {
+      throw new Error(
+        `Failed to load flow: ${error instanceof Error ? error.message : "Unknown error"}`,
+      );
+    }
+  },
+  component: FlowEditorLayout,
+  errorComponent: ({ error }) => (
+    <div>
+      <h1>Flow Error</h1>
+      <p>{error.message}</p>
+    </div>
+  ),
+});
