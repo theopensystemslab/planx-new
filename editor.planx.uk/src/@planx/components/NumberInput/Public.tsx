d086f3288c0fcdf0ce4241df0bb9144e2686dd23
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-10-12 12:47:33 -0400

    type fix again

--- editor.planx.uk/src/@planx/components/NumberInput/Public.tsx
+++ editor.planx.uk/src/@planx/components/NumberInput/Public.tsx
@@ -11,10 +11,10 @@ import InputRowLabel from "ui/shared/InputRowLabel";
 
 import { DESCRIPTION_TEXT, ERROR_MESSAGE } from "../shared/constants";
 import { getPreviouslySubmittedData, makeData } from "../shared/utils";
-import type { NumberInput } from "./model";
+import type { NumberInput, UserData } from "./model";
 import { parseNumber, publicValidationSchema } from "./model";
 
-export type Props = PublicProps<NumberInput> & { autoAnswer?: string };
+export type Props = PublicProps<NumberInput> & { autoAnswer?: UserData };
 
 export default function NumberInputComponent(props: Props): FCReturn {
   const formik = useFormik({
@@ -43,10 +43,9 @@ export default function NumberInputComponent(props: Props): FCReturn {
   // Auto-answered NumberInputs still set a breadcrumb even though they render null
   useEffect(() => {
     if (props.autoAnswer) {
-      const parsed = parseNumber(props.autoAnswer);
-      if (!isNil(parsed)) {
+      if (!isNil(props.autoAnswer)) {
         props.handleSubmit?.({
-          ...makeData(props, parsed),
+          ...makeData(props, props.autoAnswer),
           auto: true,
         });
       }
