31d61af9c78482355321685eb9babf475b15ce9e
Ian Jones <hello@ianjo.uk>
2025-10-16 16:37:19 +0100

    add contentWide breakpoint to sidebar

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Sidebar/PreviewBrowser/index.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Sidebar/PreviewBrowser/index.tsx
@@ -5,10 +5,8 @@ import {
   ThemeProvider,
   useTheme,
 } from "@mui/material/styles";
-import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
-import { isEmpty } from "lodash";
 import { useStore } from "pages/FlowEditor/lib/store";
-import React, { useMemo } from "react";
+import React from "react";
 import Reset from "ui/icons/Reset";
 
 import Questions from "../../../../Preview/Questions";
@@ -23,33 +21,25 @@ const ResetToggle = styled(Button)(({ theme }) => ({
 }));
 
 export const PreviewBrowser: React.FC = () => {
-  const [resetPreview, flow, currentCard] = useStore((state) => [
-    state.resetPreview,
-    state.flow,
-    state.currentCard,
-  ]);
-  const isLoading = isEmpty(flow);
+  const [resetPreview] = useStore((state) => [state.resetPreview]);
 
   const theme = useTheme();
-  const mobileTheme = useMemo(
-    () =>
-      createTheme({
-        ...theme,
-        breakpoints: {
-          values: {
-            xs: 0,
-            // Force mobile breakpoints as sidebar is pinned at 500px
-            sm: 9999,
-            md: 9999,
-            lg: 9999,
-            xl: 9999,
-            formWrap: 9999,
-            contentWrap: 9999,
-          },
-        },
-      }),
-    [theme],
-  );
+  const mobileTheme = createTheme({
+    ...theme,
+    breakpoints: {
+      values: {
+        xs: 0,
+        // Force mobile breakpoints as sidebar is pinned at 500px
+        sm: 9999,
+        md: 9999,
+        lg: 9999,
+        xl: 9999,
+        formWrap: 9999,
+        contentWrap: 9999,
+        contentWide: 9999,
+      },
+    },
+  });
 
   return (
     <ThemeProvider theme={mobileTheme}>
@@ -57,14 +47,7 @@ export const PreviewBrowser: React.FC = () => {
         <Reset fontSize="small" />
         Restart
       </ResetToggle>
-      {isLoading ? (
-        <DelayedLoadingIndicator
-          msDelayBeforeVisible={50}
-          text="Loading flow data..."
-        />
-      ) : (
-        <Questions previewEnvironment="editor" key={currentCard?.id} />
-      )}
+      <Questions previewEnvironment="editor" />
     </ThemeProvider>
   );
 };
