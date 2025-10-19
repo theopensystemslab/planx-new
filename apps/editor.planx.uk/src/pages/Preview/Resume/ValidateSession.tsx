5db62dc6ac80b7f7b012cb8810e143ecc467236a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-10-19 13:42:15 +0100

    refactor: First pass at splitting up ResumePage

---
+++ apps/editor.planx.uk/src/pages/Preview/Resume/ValidateSession.tsx
@@ -0,0 +1,173 @@
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import { PaymentRequest } from "@opensystemslab/planx-core/types";
+import { useMutation } from "@tanstack/react-query";
+import { validateSession } from "api/saveAndReturn/requests";
+import { ReconciliationResponse } from "api/saveAndReturn/types";
+import { isAxiosError } from "axios";
+import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
+import { useStore } from "pages/FlowEditor/lib/store";
+import React, { useCallback, useEffect } from "react";
+import { Link as ReactNaviLink } from "react-navi";
+import { ApplicationPath } from "types";
+
+import ReconciliationPage from "../ReconciliationPage";
+import StatusPage from "../StatusPage";
+import { EmailRequired } from ".";
+
+// A minimal representation of a PaymentRequest for display purposes
+type MinPaymentRequest = Pick<
+  PaymentRequest,
+  "id" | "payeeEmail" | "payeeName"
+>;
+
+export interface LockedSessionResponse {
+  paymentRequest: MinPaymentRequest;
+  status: number;
+  message: string;
+}
+
+export const InvalidSession: React.FC<{
+  retry: () => void;
+}> = ({ retry }) => (
+  <StatusPage
+    bannerHeading="We can't find your application"
+    buttonText="Try again"
+    onButtonClick={retry}
+    additionalOption="startNewApplication"
+  >
+    <Typography variant="h3" component="h2">
+      Reasons
+    </Typography>
+    <Typography variant="body1">
+      <br />
+      This may be because your application has expired or there was a mistake in
+      your email address.
+      <br />
+      <br />
+      Try entering your email address again.
+    </Typography>
+  </StatusPage>
+);
+
+export const LockedSession: React.FC<{
+  paymentRequest?: MinPaymentRequest;
+}> = ({ paymentRequest }) => (
+  <StatusPage
+    bannerHeading="Sorry, you can't make changes to this application"
+    additionalOption="startNewApplication"
+  >
+    <Typography variant="body1">
+      This is because you've invited {paymentRequest?.payeeName} (
+      <Link href={`mailto:${paymentRequest?.payeeEmail}`}>
+        {paymentRequest?.payeeEmail}
+      </Link>
+      ) to pay for this application and changes might affect the fee.
+      <br />
+      <br />
+      You can{" "}
+      <Link
+        component={ReactNaviLink}
+        href={`../pay?paymentRequestId=${paymentRequest?.id}`}
+      >
+        pay for this application yourself on the payment page
+      </Link>
+    </Typography>
+  </StatusPage>
+);
+
+export const ValidationSuccess: React.FC<{
+  reconciliationResponse: ReconciliationResponse;
+  continueApplication: () => void;
+}> = ({ reconciliationResponse, continueApplication }) => {
+  return (
+    <ReconciliationPage
+      bannerHeading="Resume your application"
+      reconciliationResponse={reconciliationResponse}
+      buttonText="Continue"
+      onButtonClick={() => continueApplication()}
+    ></ReconciliationPage>
+  );
+};
+
+const ValidateSession: React.FC<{
+  sessionId: string;
+  initialEmail: string;
+}> = ({ sessionId, initialEmail }) => {
+  const resumeSession = useStore((state) => state.resumeSession);
+
+  /**
+   * Continue application following successful validation & reconciliation
+   * Updating path will navigate user away
+   */
+  const continueApplication = (email: string): void => {
+    useStore.setState({
+      saveToEmail: email,
+      path: ApplicationPath.SaveAndReturn,
+    });
+  };
+
+  const {
+    mutate,
+    data,
+    reset,
+    isPending,
+    isSuccess,
+    isError,
+    error,
+    variables,
+  } = useMutation({
+    mutationFn: validateSession,
+    onSuccess: (data) => resumeSession(data.reconciledSessionData),
+    onError: console.debug,
+  });
+
+  const handleSubmit = useCallback(
+    (submittedEmail: string) => {
+      mutate({
+        payload: { email: submittedEmail, sessionId },
+      });
+    },
+    [sessionId, mutate],
+  );
+
+  useEffect(() => {
+    if (initialEmail) handleSubmit(initialEmail);
+  }, [initialEmail, handleSubmit]);
+
+  const retryWithNewEmailAddress = () => reset();
+
+  if (isPending) return <DelayedLoadingIndicator text="Validating..." />;
+
+  if (isSuccess) {
+    const shouldSkipReconciliation = data.message.match(
+      /Payment process initiated, skipping reconciliation/,
+    );
+    if (shouldSkipReconciliation) continueApplication(variables.payload.email!);
+
+    return (
+      <ValidationSuccess
+        continueApplication={() =>
+          continueApplication(variables.payload.email!)
+        }
+        reconciliationResponse={data}
+      />
+    );
+  }
+
+  if (isError) {
+    if (isAxiosError(error) && error.response?.status === 403) {
+      const lockedSessionResponse = error.response
+        .data as LockedSessionResponse;
+      return (
+        <LockedSession paymentRequest={lockedSessionResponse.paymentRequest} />
+      );
+    }
+
+    return <InvalidSession retry={retryWithNewEmailAddress} />;
+  }
+
+  return <EmailRequired handleSubmit={handleSubmit} />;
+};
+
+export default ValidateSession;
---
+++ apps/editor.planx.uk/src/pages/Preview/Resume/index.tsx
@@ -0,0 +1,116 @@
+import Box from "@mui/material/Box";
+import Card from "@planx/components/shared/Preview/Card";
+import { CardHeader } from "@planx/components/shared/Preview/CardHeader/CardHeader";
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import { useCurrentRoute } from "react-navi";
+import InputLabel from "ui/public/InputLabel";
+import Input from "ui/shared/Input/Input";
+import InputRow from "ui/shared/InputRow";
+import { object, string } from "yup";
+
+import SendResumeEmail from "./SendResumeEmail";
+import ValidateSession from "./ValidateSession";
+
+export const EmailRequired: React.FC<{
+  handleSubmit: (email: string) => void;
+}> = ({ handleSubmit }) => {
+  const emailSchema = object({
+    email: string().email("Invalid email").required("Email address required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+    },
+    onSubmit: ({ email }) => handleSubmit(email),
+    validateOnChange: false,
+    validateOnBlur: false,
+    validationSchema: emailSchema,
+  });
+
+  return (
+    <Box width="100%">
+      <Card handleSubmit={formik.handleSubmit}>
+        <CardHeader
+          title="Resume your application"
+          description="Enter your email to resume your application."
+        ></CardHeader>
+        <InputRow>
+          <InputLabel label={"Email address"} htmlFor={"email"}>
+            <Input
+              bordered
+              errorMessage={
+                formik.touched.email && formik.errors.email
+                  ? formik.errors.email
+                  : undefined
+              }
+              id="email"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              autoComplete="email"
+              value={formik.values.email}
+            ></Input>
+          </InputLabel>
+        </InputRow>
+      </Card>
+    </Box>
+  );
+};
+
+/**
+ * If an email is passed in as a query param, do not prompt a user for this
+ *
+ * This means there's not an additional step of friction from logging into LPS,
+ * or redirecting back from GOV.UK Pay
+ *
+ * XXX: Won't work locally as referrer is stripped from the browser when navigating from HTTPS to HTTP (localhost)
+ */
+const getInitialEmailValue = (emailQueryParam?: string) => {
+  const trustedAddresses = [
+    "https://www.payments.service.gov.uk/",
+    "https://card.payments.service.gov.uk/",
+    "https://localplanning.editor.planx.dev/",
+    "https://www.localplanning.services/",
+  ];
+
+  const trustedPatterns = [
+    /^https:\/\/localplanning\.\d{4,5}\.planx\.pizza\/$/,
+  ];
+
+  const isRedirectFromTrustedSource =
+    trustedAddresses.includes(document.referrer) ||
+    trustedPatterns.some((pattern) => pattern.test(document.referrer));
+
+  if (isRedirectFromTrustedSource && emailQueryParam) return emailQueryParam;
+  return "";
+};
+
+/**
+ * Component which handles the "Resume" page used for Save & Return
+ * The user can access this page via four "paths"
+ * 1. Directly via PlanX, user enters email to trigger "dashboard" email with resume magic links
+ * 2. Magic link in email with a sessionId, user enters email to continue application
+ * 3. Redirect back from GovPay - sessionId and email come from query params
+ * 4. Redirect from localplanning.services - sessionId and email come from query params
+ */
+const ResumePage: React.FC = () => {
+  const sessionId = useCurrentRoute().url.query.sessionId;
+  const route = useCurrentRoute();
+
+  const [initialEmail] = useState(
+    getInitialEmailValue(decodeURIComponent(route.url.query.email)),
+  );
+
+  if (sessionId) {
+    return (
+      <ValidateSession sessionId={sessionId} initialEmail={initialEmail} />
+    );
+  }
+
+  return <SendResumeEmail initialEmail={initialEmail} />;
+};
+
+export default ResumePage;
