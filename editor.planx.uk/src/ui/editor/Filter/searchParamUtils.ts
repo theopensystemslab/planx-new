dbdb49aa18be3c8c3719cc66d71c6895ef376444
nichgalzin <nichgalzin@gmail.com>
2025-10-22 12:01:23 +0100

    refactor: updates filter logic to use tanstack setting and clearing hook and simplifies logic

--- editor.planx.uk/src/ui/editor/Filter/searchParamUtils.ts
+++ editor.planx.uk/src/ui/editor/Filter/searchParamUtils.ts
@@ -1,77 +1,54 @@
-import { Navigation } from "navi";
+import { NavigateOptions } from "@tanstack/react-router";
 import { slugify } from "utils";
 
 import Filters, { FilterOptions } from "./Filter";
-import { mapFilters, MappedFilters } from "./helpers";
+import { mapFilters } from "./helpers";
 
-export const updateUrl = (
-  navigation: Navigation<any>,
-  searchParams: URLSearchParams,
-) => {
-  navigation.navigate(
-    {
-      pathname: window.location.pathname,
-      search: searchParams.toString(),
-    },
-    {
-      replace: true,
-    },
-  );
-};
-
-export const addFilterSearchParam = (
-  searchParams: URLSearchParams,
-  mappedFilters: MappedFilters[],
-) => {
-  mappedFilters.forEach((filter) =>
-    searchParams.set(filter.displayName, `${filter.filterValue}`),
-  );
-};
-
-export const removeUnusedFilterSearchParam = <T extends object>(
-  filterOptions: FilterOptions<T>[],
-  searchParams: URLSearchParams,
-  mappedFilters: MappedFilters[] | [],
-) => {
-  const displayNames = filterOptions.map((option) => option.displayName);
-  displayNames.map((name) => {
-    const paramToDelete = mappedFilters.find(
-      (filter) => filter.displayName === slugify(name),
-    );
-    !paramToDelete && searchParams.delete(slugify(name));
-  });
-};
-
-export const addToSearchParams = <T extends object>(
-  params: Filters<T>,
-  url: string,
+export const updateSearchParams = <T extends object>(
+  filters: Filters<T> | null,
   optionsToFilter: FilterOptions<T>[],
-  navigation: Navigation<any>,
+  navigate: (opts: NavigateOptions) => void,
 ) => {
-  const searchParams = new URLSearchParams(url);
-  const mappedFilters = mapFilters(params, optionsToFilter);
-
-  mappedFilters && addFilterSearchParam(searchParams, mappedFilters);
-
-  removeUnusedFilterSearchParam<T>(
-    optionsToFilter,
-    searchParams,
-    mappedFilters || [],
-  );
-
-  updateUrl(navigation, searchParams);
-};
-
-export const clearSearchParams = <T extends object>(
-  url: string,
-  optionsToFilter: FilterOptions<T>[],
-  navigation: Navigation<any>,
-) => {
-  const searchParams = new URLSearchParams(url);
   const displayNames = optionsToFilter.map((option) => option.displayName);
+
+  if (!filters) {
+    // Clear all filter params
+    const updates = Object.fromEntries(
+      displayNames.map((name) => [slugify(name), undefined]),
+    );
+
+    navigate({
+      search: (prev) => ({ ...prev, ...updates }),
+      replace: true,
+    });
+    return;
+  }
+
+  // Map filters to search params
+  const mappedFilters = mapFilters(filters, optionsToFilter);
+  const updates: Record<string, string | undefined> = {};
+
+  // Set active filter values
+  if (mappedFilters) {
+    mappedFilters.forEach((filter) => {
+      updates[filter.displayName] =
+        filter.filterValue === "" ? undefined : filter.filterValue;
+    });
+  }
+
+  // Clear unused filter params
   displayNames.forEach((name) => {
-    searchParams.delete(slugify(name));
+    const slugifiedName = slugify(name);
+    const hasValue = mappedFilters?.some(
+      (filter) => filter.displayName === slugifiedName,
+    );
+    if (!hasValue) {
+      updates[slugifiedName] = undefined;
+    }
   });
 
-  updateUrl(navigation, searchParams);
+  navigate({
+    search: (prev) => ({ ...prev, ...updates }),
+    replace: true,
+  });
 };
