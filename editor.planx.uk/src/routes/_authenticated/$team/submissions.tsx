b7655e738dac9877f8afb51976562dbd5a297268
nichgalzin <nichgalzin@gmail.com>
2025-10-22 15:39:08 +0100

    refactor: removes unused loader function

--- editor.planx.uk/src/routes/_authenticated/$team/submissions.tsx
+++ editor.planx.uk/src/routes/_authenticated/$team/submissions.tsx
@@ -2,6 +2,5 @@ import { createFileRoute } from "@tanstack/react-router";
 import Submissions from "pages/FlowEditor/components/Submissions/Submissions";
 
 export const Route = createFileRoute("/_authenticated/$team/submissions")({
-  loader: ({ params }) => {},
   component: Submissions,
 });
