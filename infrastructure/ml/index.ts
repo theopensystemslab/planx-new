111c10aa1cd55703c08262c7f8378104cb96ec49
Dan G <freemvmt@posteo.net>
2025-10-28 13:55:18 +0000

    provision minimal ML infra layer in Pulumi (#5375)

---
+++ infrastructure/ml/index.ts
@@ -0,0 +1,130 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as fs from "fs";
+import * as path from "path";
+
+const config = new pulumi.Config();
+
+// get the existing VPC from the networking stack
+const env = pulumi.getStack();
+const networking = new pulumi.StackReference(`planx/networking/${env}`);
+const vpcId = networking.requireOutput("vpcId");
+const publicSubnetIds = networking.requireOutput("publicSubnetIds");
+
+// create security group for ML instances that allows SSH access only
+const mlSecurityGroup = new aws.ec2.SecurityGroup("ml-ssh-sg", {
+  description: "Security group for ML EC2 instances - SSH access only",
+  vpcId: vpcId,
+});
+
+new aws.vpc.SecurityGroupIngressRule("ml-allow-ssh-in", {
+    securityGroupId: mlSecurityGroup.id,
+    cidrIpv4: "0.0.0.0/0", // allow SSH from anywhere
+    fromPort: 22,
+    toPort: 22,
+    ipProtocol: "tcp",
+});
+
+new aws.vpc.SecurityGroupEgressRule("ml-allow-ipv4-traffic-out", {
+    securityGroupId: mlSecurityGroup.id,
+    cidrIpv4: "0.0.0.0/0",
+    ipProtocol: "-1",
+});
+
+// load existing public key into the instance so we can SSH in
+const publicKey = config.require("ssh-devops-public-key");
+const devopsKeyPair = new aws.ec2.KeyPair("ssh-devops-key-pair", {
+    keyName: "ssh-devops-key-pair",
+    publicKey: publicKey,
+})
+
+// see https://docs.aws.amazon.com/dlami/latest/devguide/appendix-ami-release-notes.html
+const amazonLinuxAmi = aws.ec2.getAmi({
+  mostRecent: true,
+  owners: ["amazon"],
+  filters: [
+    {
+      name: "name",
+      values: ["Deep Learning Base OSS Nvidia Driver GPU AMI (Ubuntu 24.04)*"],
+    },
+    {
+      name: "state",
+      values: ["available"],
+    },
+  ],
+});
+
+// create the g6 EC2 instance for ML training and experiment workloads
+const g6Instance = new aws.ec2.Instance("ml-training-instance", {
+  instanceType: aws.ec2.InstanceType.G6_XLarge,
+  ami: amazonLinuxAmi.then(ami => ami.id),
+  keyName: devopsKeyPair.keyName,
+  
+  // use first public subnet in vpc (you can make this configurable)
+  subnetId: publicSubnetIds.apply(subnets => subnets[0]),
+  vpcSecurityGroupIds: [mlSecurityGroup.id],
+  
+  // assign a public IP for SSH access
+  associatePublicIpAddress: true,
+  
+  // don't terminate on shutdown
+  instanceInitiatedShutdownBehavior: "stop",
+  
+  // persistent EBS volume configuration for ML workloads (survives instance stop/starts)
+  rootBlockDevice: {
+    volumeType: "gp3",
+    volumeSize: 256,
+    deleteOnTermination: true,
+    encrypted: true,
+  },
+
+  // script for basic setup/dependencies - runs only on first launch
+  userData: fs.readFileSync(path.join(__dirname, "user_data.sh"), "utf8"),
+});
+
+// create a cost budget for ML resources (£200/month)
+const mlBudget = new aws.budgets.Budget("ml-infra-budget-monthly", {
+  name: "ml-infra-budget-monthly",
+  budgetType: "COST",
+  limitAmount: "250", // ~ £200
+  limitUnit: "USD",
+  timeUnit: "MONTHLY",
+  timePeriodStart: "2025-10-01_00:00",
+  
+  // cost filters to track resources with project=ml tag (applied by default via yaml config)
+  costFilters: [{
+    name: "TagKeyValue",
+    values: ["project$ml"],
+  }],
+  
+  // XXX: could add SNS/Lambda combo to stop instances when budget exceeded
+  notifications: [
+    {
+      comparisonOperator: "GREATER_THAN",
+      threshold: 80, // alert at 80% of budget
+      thresholdType: "PERCENTAGE", 
+      notificationType: "ACTUAL",
+      subscriberEmailAddresses: [
+        "devops@opensystemslab.io",
+      ],
+    },
+    {
+      comparisonOperator: "GREATER_THAN",
+      threshold: 100, // alert again when budget is exceeded
+      thresholdType: "PERCENTAGE",
+      notificationType: "ACTUAL",
+      subscriberEmailAddresses: [
+        "devops@opensystemslab.io", 
+      ],
+    },
+  ],
+});
+
+// export important values
+export const g6InstanceId = g6Instance.id;
+export const g6InstancePublicIp = g6Instance.publicIp;
+export const g6InstancePublicDns = g6Instance.publicDns;
+export const mlSecurityGroupId = mlSecurityGroup.id;
+export const devopsKeyPairName = devopsKeyPair.keyName;
+export const mlBudgetName = mlBudget.name;
+export const sshCommand = pulumi.interpolate`ssh -i ~/.ssh/${devopsKeyPairName} ubuntu@${g6InstancePublicIp}`;
