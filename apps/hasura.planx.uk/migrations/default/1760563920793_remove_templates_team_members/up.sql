c3f2498b5ad88e9c609e4ebaa2f154987f7e966f
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-10-28 07:56:50 -0400

    swap drop order

--- apps/hasura.planx.uk/migrations/default/1760563920793_remove_templates_team_members/up.sql
+++ apps/hasura.planx.uk/migrations/default/1760563920793_remove_templates_team_members/up.sql
@@ -1,5 +1,6 @@
-DELETE FROM team_members
+DELETE FROM "public"."team_members"
 WHERE team_id = 29;
 
-DROP FUNCTION IF EXISTS grant_new_user_template_team_access;
-DROP TRIGGER IF EXISTS grant_new_user_template_team_access on users;
+-- function relies on trigger and trigger has associated pg comment, so drop order and 'CASCADE' matters
+DROP TRIGGER IF EXISTS "public"."grant_new_user_template_team_access on users" CASCADE;
+DROP FUNCTION IF EXISTS "public"."grant_new_user_template_team_access";
