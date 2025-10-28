79baa76edd4456cc0fd99e6697ed35506b1d7712
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-10-28 06:54:58 +0000

    feat(poc): Use `@tanstack/react-query` for sending ITP requests (#5453)

--- apps/editor.planx.uk/src/setupTests.tsx
+++ apps/editor.planx.uk/src/setupTests.tsx
@@ -28,3 +28,19 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 
 afterAll(() => server.close());
+
+// Polyfill for ProgressEvent - missing from JSDOM
+if (typeof ProgressEvent === 'undefined') {
+  global.ProgressEvent = class ProgressEvent extends Event {
+    lengthComputable: boolean;
+    loaded: number;
+    total: number;
+
+    constructor(type: string, eventInitDict: ProgressEventInit = {}) {
+      super(type, eventInitDict);
+      this.lengthComputable = eventInitDict.lengthComputable || false;
+      this.loaded = eventInitDict.loaded || 0;
+      this.total = eventInitDict.total || 0;
+    }
+  };
+}
