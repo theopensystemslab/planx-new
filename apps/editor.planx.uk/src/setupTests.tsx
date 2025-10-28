ec78a659deef00f8b2a67a9121edf2466aac3800
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-10-28 07:55:09 +0000

    fix: Ensure polyfill is available before server setup

--- apps/editor.planx.uk/src/setupTests.tsx
+++ apps/editor.planx.uk/src/setupTests.tsx
@@ -7,6 +7,22 @@ import React from "react";
 import server from "test/mockServer";
 import { vi } from "vitest";
 
+// Polyfill for ProgressEvent - missing from JSDOM
+if (typeof ProgressEvent === 'undefined') {
+  global.ProgressEvent = class ProgressEvent extends Event {
+    lengthComputable: boolean;
+    loaded: number;
+    total: number;
+
+    constructor(type: string, eventInitDict: ProgressEventInit = {}) {
+      super(type, eventInitDict);
+      this.lengthComputable = eventInitDict.lengthComputable || false;
+      this.loaded = eventInitDict.loaded || 0;
+      this.total = eventInitDict.total || 0;
+    }
+  };
+}
+
 /**
  * Mock the MUI Fade component used in @planx/components/shared/Preview/Card.tsx
  * Required as this frequently updates following the final "expect()" call of a test,
@@ -28,19 +44,3 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 
 afterAll(() => server.close());
-
-// Polyfill for ProgressEvent - missing from JSDOM
-if (typeof ProgressEvent === 'undefined') {
-  global.ProgressEvent = class ProgressEvent extends Event {
-    lengthComputable: boolean;
-    loaded: number;
-    total: number;
-
-    constructor(type: string, eventInitDict: ProgressEventInit = {}) {
-      super(type, eventInitDict);
-      this.lengthComputable = eventInitDict.lengthComputable || false;
-      this.loaded = eventInitDict.loaded || 0;
-      this.total = eventInitDict.total || 0;
-    }
-  };
-}
