b559ded033ae62854b408b1e7180e66c05985061
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-10-31 09:52:14 -0400

    chore: remove users from Templates team (#5454)

---
+++ apps/hasura.planx.uk/migrations/default/1760563920793_remove_templates_team_members/up.sql
@@ -0,0 +1,6 @@
+DELETE FROM team_members
+WHERE team_id = 29;
+
+-- function relies on trigger and trigger has associated pg comment, so drop order and 'CASCADE' matters
+DROP TRIGGER IF EXISTS grant_new_user_template_team_access on users CASCADE;
+DROP FUNCTION IF EXISTS grant_new_user_template_team_access;
