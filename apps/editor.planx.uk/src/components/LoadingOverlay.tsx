5497846918dbd2a36f559a5d4442674b95698892
Ian Jones <hello@ianjo.uk>
2025-10-31 10:37:31 +0000

    use MUI portal

--- apps/editor.planx.uk/src/components/LoadingOverlay.tsx
+++ apps/editor.planx.uk/src/components/LoadingOverlay.tsx
@@ -1,9 +1,9 @@
 import Alert, { alertClasses } from "@mui/material/Alert";
 import Backdrop from "@mui/material/Backdrop";
 import Box from "@mui/material/Box";
+import Portal from "@mui/material/Portal";
 import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
 import React from "react";
-import { createPortal } from "react-dom";
 
 interface LoadingOverlayProps {
   open: boolean;
@@ -16,44 +16,45 @@ const LoadingOverlay: React.FC<LoadingOverlayProps> = ({
   message = "Loading...",
   msDelayBeforeVisible = 300,
 }) => {
-  return createPortal(
-    <Backdrop
-      sx={(theme) => ({
-        zIndex: theme.zIndex.modal + 1,
-        alignItems: "flex-start",
-        paddingTop: theme.spacing(2),
-      })}
-      open={open}
-      transitionDuration={300}
-    >
-      <Box>
-        <Alert
-          severity="info"
-          sx={(theme) => ({
-            fontSize: "1rem",
-            padding: theme.spacing(1, 2),
-            borderRadius: "4px",
-            [`& .${alertClasses.icon}`]: {
-              display: "none",
-            },
-            [`& .${alertClasses.message}`]: {
-              display: "flex",
-              alignItems: "center",
-              gap: theme.spacing(2),
-              overflow: "hidden",
-            },
-          })}
-        >
-          <DelayedLoadingIndicator
-            inline
-            text={message}
-            msDelayBeforeVisible={msDelayBeforeVisible}
-            variant="spinner"
-          />
-        </Alert>
-      </Box>
-    </Backdrop>,
-    document.body,
+  return (
+    <Portal>
+      <Backdrop
+        sx={(theme) => ({
+          zIndex: theme.zIndex.modal + 1,
+          alignItems: "flex-start",
+          paddingTop: theme.spacing(2),
+        })}
+        open={open}
+        transitionDuration={300}
+      >
+        <Box>
+          <Alert
+            severity="info"
+            sx={(theme) => ({
+              fontSize: "1rem",
+              padding: theme.spacing(1, 2),
+              borderRadius: "4px",
+              [`& .${alertClasses.icon}`]: {
+                display: "none",
+              },
+              [`& .${alertClasses.message}`]: {
+                display: "flex",
+                alignItems: "center",
+                gap: theme.spacing(2),
+                overflow: "hidden",
+              },
+            })}
+          >
+            <DelayedLoadingIndicator
+              inline
+              text={message}
+              msDelayBeforeVisible={msDelayBeforeVisible}
+              variant="spinner"
+            />
+          </Alert>
+        </Box>
+      </Backdrop>
+    </Portal>
   );
 };
 
