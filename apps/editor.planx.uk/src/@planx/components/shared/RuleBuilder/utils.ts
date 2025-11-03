d886d54c4981ff03c47b7c74bf7376cda0c14033
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-03 14:16:07 +0000

    feat: Extract `<RuleBuilder/>` from FileUploadAndLabel (#5555)

---
+++ apps/editor.planx.uk/src/@planx/components/shared/RuleBuilder/utils.ts
@@ -0,0 +1,40 @@
+import { Store } from "pages/FlowEditor/lib/store";
+
+import { Condition, ConditionalRule, Operator, Rule } from "./types";
+
+export const isRuleMet = (
+  passport: Store.Passport,
+  rule: ConditionalRule<Condition.RequiredIf | Condition.RecommendedIf>,
+): boolean => {
+  const passportVal = passport.data?.[rule.fn];
+  if (!passportVal) return false;
+
+  const isExactMatch = passportVal === rule.val;
+  const isArray = Array.isArray(passportVal);
+  const isExactMatchInArray = isArray && passportVal.includes(rule.val);
+  const re = new RegExp(`^${rule.val}(\\..+| $)`);
+  const isGranularMatchInArray =
+    isArray && passportVal.some((value: string) => re.test(value));
+
+  return isExactMatch || isExactMatchInArray || isGranularMatchInArray;
+};
+
+export const checkIfConditionalRule = (condition: Condition) =>
+  [Condition.RecommendedIf, Condition.RequiredIf].includes(condition);
+
+export const formatRule = (
+  newCondition: Condition,
+  { fn, val }: Rule,
+): Rule => {
+  const isConditionalRule = checkIfConditionalRule(newCondition);
+
+  // Drop fields which are only required for ConditionalRules
+  const updatedRule = {
+    condition: newCondition,
+    operator: isConditionalRule ? Operator.Equals : undefined,
+    fn: isConditionalRule ? fn : undefined,
+    val: isConditionalRule ? val : undefined,
+  } as Rule;
+
+  return updatedRule;
+};
