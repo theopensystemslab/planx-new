d77f12ff1490c127dc2422893fae8be1e4baea5c
zz-hh-aa <zzhhaa@protonmail.com>
2025-11-04 12:04:59 +0000

    chore: update down migration

--- apps/hasura.planx.uk/migrations/default/1762256028089_create_submission_integrations_table/down.sql
+++ apps/hasura.planx.uk/migrations/default/1762256028089_create_submission_integrations_table/down.sql
@@ -1,12 +1,1 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- CREATE TABLE public.submission_integrations (
---     team_id INTEGER NOT NULL,
---     email_id UUID NOT NULL,
---     submission_email TEXT NOT NULL,
---     default_email BOOLEAN NOT NULL,
---     PRIMARY KEY (email_id),
---     CONSTRAINT fk_submission_integrations_team_settings
---         FOREIGN KEY (team_id)
---         REFERENCES public.team_settings(team_id)
--- );
+DROP TABLE public.submission_integrations;
