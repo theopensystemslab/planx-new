bf3b7530b890ab18298ca2c02674ef62fd4b7103
ollie <107874766+zz-hh-aa@users.noreply.github.com>
2025-11-05 09:00:16 +0000

    chore: add cols to analytics_action_nodes [skip pizza] (#5596)

---
+++ apps/hasura.planx.uk/migrations/default/1762277379292_additional_cols_to_analytics_action_nodes/up.sql
@@ -0,0 +1,24 @@
+DROP MATERIALIZED VIEW "public"."analytics_action_nodes";
+CREATE MATERIALIZED VIEW "public"."analytics_action_nodes" AS 
+ SELECT a.flow_id,
+    a.id AS analytics_id,
+    ((al.allow_list_answers -> 'drawBoundary.action'::text))::text AS draw_boundary_action,
+    ((al.allow_list_answers -> 'proposal.projectType'::text))::text AS proposal_project_type,
+    ((al.allow_list_answers -> 'rab.exitReason'::text))::text AS rab_exit_reason,
+    ((al.allow_list_answers -> 'usedFOIYNPP'::text))::text AS used_foiynpp,
+    al.node_type,
+    al.node_title,
+    al.input_errors,
+    al.has_clicked_save,
+    al.has_clicked_help
+   FROM (analytics a
+     LEFT JOIN analytics_logs al ON ((a.id = al.analytics_id)))
+  WHERE (((al.allow_list_answers -> 'drawBoundary.action'::text) IS NOT NULL) 
+    OR ((al.allow_list_answers -> 'proposal.projectType'::text) IS NOT NULL) 
+    OR ((al.allow_list_answers -> 'rab.exitReason'::text) IS NOT NULL) 
+    OR ((al.allow_list_answers -> 'usedFOIYNPP'::text) IS NOT NULL) 
+    OR (al.has_clicked_save = true) 
+    OR (al.has_clicked_help = true) 
+    OR (al.input_errors::text != '[]'));
+
+GRANT SELECT ON TABLE "public"."analytics_action_nodes" TO metabase_read_only;
