c43d10e3aebb4e4d42172853d556891ea5835685
ollie <107874766+zz-hh-aa@users.noreply.github.com>
2025-11-06 19:17:12 +0100

    Revert "chore: copy data into flow_integrations and submission_integrations t…"
    This reverts commit 3ff202eeafa711a70f5d14e73958a6217319e099.

--- apps/hasura.planx.uk/migrations/default/1762352102061_populate_submission_integrations/up.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO submission_integrations (team_id, submission_email, default_email)
-SELECT 
-    ts.team_id,
-    ts.submission_email,
-    true AS default_email
-FROM team_settings ts
-WHERE ts.submission_email IS NOT NULL;
