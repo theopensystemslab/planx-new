21fda49f62984903773ee36d40b8c6b31f430960
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-06 19:37:08 +0000

    chore: Refactor shared Editor UI

---
+++ apps/editor.planx.uk/src/@planx/components/shared/BaseChecklist/Editor/components/ExclusiveOrOptionManager.tsx
@@ -0,0 +1,105 @@
+import Box from "@mui/material/Box";
+import { ComponentType } from "@opensystemslab/planx-core/types";
+import { useCurrentOptions } from "@planx/components/Checklist/Public/hooks/useInitialOptions";
+import { AnyOption, AnyOptions } from "@planx/components/Option/model";
+import {
+  AnyChecklist,
+  OptionGroup,
+} from "@planx/components/shared/BaseChecklist/model";
+import { getOptionsSchemaByFn } from "@planx/components/shared/utils";
+import { getIn } from "formik";
+import React from "react";
+import { FormikHookReturn } from "types";
+import ListManager from "ui/editor/ListManager/ListManager";
+import ErrorWrapper from "ui/shared/ErrorWrapper";
+
+import ChecklistOptionsEditor from "./OptionsEditor";
+
+interface Props<T extends AnyChecklist> {
+  formik: FormikHookReturn<T>;
+  exclusiveOptions: AnyOptions;
+  nonExclusiveOptions: AnyOptions | OptionGroup[];
+  groupIndex?: number;
+  grouped?: true;
+  disabled?: boolean;
+  isTemplatedNode?: boolean;
+}
+
+export const ExclusiveOrOptionManager = <T extends AnyChecklist>({
+  formik,
+  exclusiveOptions,
+  nonExclusiveOptions,
+  groupIndex,
+  grouped,
+  disabled,
+  isTemplatedNode,
+}: Props<T>) => {
+  const { schema, currentOptionVals } = useCurrentOptions(formik);
+
+  return (
+    <Box mt={2}>
+      <ErrorWrapper error={getIn(formik.errors, "allRequired")}>
+        <Box mt={1}>
+          <ListManager
+            values={exclusiveOptions || []}
+            onChange={(newExclusiveOptions) => {
+              if (grouped) {
+                const updateGroupedOptions = () => {
+                  if (newExclusiveOptions.length > 0) {
+                    const exclusiveOptionGroup = {
+                      title: "Or",
+                      exclusive: true,
+                      children: newExclusiveOptions,
+                    };
+
+                    return [...nonExclusiveOptions, exclusiveOptionGroup];
+                  } else {
+                    return [...nonExclusiveOptions];
+                  }
+                };
+
+                formik.setFieldValue("groupedOptions", updateGroupedOptions());
+                return;
+              }
+              const newCombinedOptions = [
+                ...nonExclusiveOptions,
+                ...newExclusiveOptions,
+              ];
+              formik.setFieldValue("options", newCombinedOptions);
+            }}
+            newValueLabel='add "or" option'
+            maxItems={1}
+            noDragAndDrop
+            newValue={() =>
+              ({
+                id: "",
+                data: {
+                  text: "",
+                  description: "",
+                  val: "",
+                  flags: [],
+                  exclusive: true,
+                  // TODO: Rule
+                },
+              }) satisfies AnyOption
+            }
+            Editor={ChecklistOptionsEditor}
+            disabled={disabled}
+            editorExtraProps={{
+              type: ComponentType.Checklist,
+              showValueField: !!formik.values.fn,
+              groupIndex,
+              optionPlaceholder: "Exclusive 'or' option",
+              schema: getOptionsSchemaByFn(
+                formik.values.fn,
+                schema,
+                currentOptionVals,
+              ),
+            }}
+            isTemplatedNode={isTemplatedNode}
+          />
+        </Box>
+      </ErrorWrapper>
+    </Box>
+  );
+};
