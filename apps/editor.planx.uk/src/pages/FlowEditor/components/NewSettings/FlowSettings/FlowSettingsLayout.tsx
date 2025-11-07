cfcb77ccc9cab857fc81a3187c43f99c66271174
Ian Jones <hello@ianjo.uk>
2025-11-07 10:53:55 +0000

    use combined settings layout

--- apps/editor.planx.uk/src/pages/FlowEditor/components/NewSettings/FlowSettings/FlowSettingsLayout.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/NewSettings/FlowSettings/FlowSettingsLayout.tsx
@@ -1,79 +1,37 @@
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
-import { useCurrentRoute, useNavigation } from "react-navi";
 
-interface SettingsLink {
-  label: string;
-  path: string;
-  condition?: boolean;
-}
+import SettingsLayout from "../SettingsLayout";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const FlowSettingsLayout: React.FC<Props> = ({ children }) => {
-  const { navigate } = useNavigation();
-  const { url } = useCurrentRoute();
   const [teamSlug, flowSlug, isTemplatedFrom] = useStore((state) => [
     state.teamSlug,
     state.flowSlug,
     state.isTemplatedFrom,
-    state.isTemplate,
   ]);
 
-  const settingsLinks: SettingsLink[] = [
+  const settingsLinks = [
     { label: "Visibility", path: "/visibility" },
     { label: "Legal disclaimer", path: "/legal-disclaimer" },
     { label: "Help page", path: "/pages/help" },
     { label: "Privacy page", path: "/pages/privacy" },
     { label: "Templates", path: "/templates", condition: isTemplatedFrom },
-  ].filter((link) => link.condition === undefined || link.condition);
-
-  const isActive = (path: string) => {
-    return url.pathname.includes(`/new-settings${path}`);
-  };
-
-  const handleNavClick = (path: string) => {
-    navigate(`/${teamSlug}/${flowSlug}/new-settings${path}`);
-  };
+  ];
 
   return (
-    <Container maxWidth="contentWrap">
-      <Typography variant="h2" component="h1" gutterBottom>
-        Flow settings
-      </Typography>
-
-      <Box component="ul" sx={{ listStyle: "none", padding: 0, margin: 0 }}>
-        {settingsLinks.map(({ label, path }) => (
-          <Box component="li" key={path} sx={{ display: "inline", mr: 2 }}>
-            {isActive(path) ? (
-              <Typography component="span" variant="body1">
-                {label}
-              </Typography>
-            ) : (
-              <Typography
-                component="a"
-                variant="body1"
-                // Roughly style as a link for now. TODO: bring in tabs
-                sx={{
-                  textDecoration: "underline",
-                  cursor: "pointer",
-                }}
-                onClick={() => handleNavClick(path)}
-              >
-                {label}
-              </Typography>
-            )}
-          </Box>
-        ))}
-      </Box>
-
-      <Box sx={{ py: 4 }}>{children}</Box>
-    </Container>
+    <SettingsLayout
+      title="Flow settings"
+      settingsLinks={settingsLinks}
+      getNavigationPath={(path) =>
+        `/${teamSlug}/${flowSlug}/new-settings${path}`
+      }
+    >
+      {children}
+    </SettingsLayout>
   );
 };
 
--- apps/editor.planx.uk/src/pages/FlowEditor/components/NewSettings/TeamSettings/TeamSettingsLayout.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/NewSettings/TeamSettings/TeamSettingsLayout.tsx
@@ -1,72 +1,30 @@
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
-import { useCurrentRoute, useNavigation } from "react-navi";
 
-interface SettingsLink {
-  label: string;
-  path: string;
-}
+import SettingsLayout from "../SettingsLayout";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const TeamSettingsLayout: React.FC<Props> = ({ children }) => {
-  const { navigate } = useNavigation();
-  const { url } = useCurrentRoute();
   const [teamSlug] = useStore((state) => [state.teamSlug]);
 
-  const settingsLinks: SettingsLink[] = [
+  const settingsLinks = [
     { label: "Contact information", path: "/contact" },
     { label: "Integrations", path: "/integrations" },
     { label: "GIS data", path: "/gis-data" },
     { label: "Advanced", path: "/advanced" },
   ];
 
-  const isActive = (path: string) => {
-    return url.pathname.includes(`/new-settings${path}`);
-  };
-
-  const handleNavClick = (path: string) => {
-    navigate(`/${teamSlug}/new-settings${path}`);
-  };
-
   return (
-    <Container maxWidth="contentWrap">
-      <Typography variant="h2" component="h1" gutterBottom>
-        Team settings
-      </Typography>
-
-      <Box component="ul" sx={{ listStyle: "none", padding: 0, margin: 0 }}>
-        {settingsLinks.map(({ label, path }) => (
-          <Box component="li" key={path} sx={{ display: "inline", mr: 2 }}>
-            {isActive(path) ? (
-              <Typography component="span" variant="body1">
-                {label}
-              </Typography>
-            ) : (
-              <Typography
-                component="a"
-                variant="body1"
-                // Roughly style as a link for now. TODO: bring in tabs
-                sx={{
-                  textDecoration: "underline",
-                  cursor: "pointer",
-                }}
-                onClick={() => handleNavClick(path)}
-              >
-                {label}
-              </Typography>
-            )}
-          </Box>
-        ))}
-      </Box>
-
-      <Box sx={{ py: 4 }}>{children}</Box>
-    </Container>
+    <SettingsLayout
+      title="Team settings"
+      settingsLinks={settingsLinks}
+      getNavigationPath={(path) => `/${teamSlug}/new-settings${path}`}
+    >
+      {children}
+    </SettingsLayout>
   );
 };
 
