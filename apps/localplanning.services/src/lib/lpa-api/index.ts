43574aab19038b7b9e135fa4735ca7c3b8012eff
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-11 17:37:20 +0000

    chore(lps): Add `heritage-constraints` to the allow list [skip pizza]

--- apps/localplanning.services/src/lib/lpa-api/index.ts
+++ apps/localplanning.services/src/lib/lpa-api/index.ts
@@ -52,10 +52,12 @@ const NOTIFY_SERVICE_SLUGS = [
 ];
 
 const GUIDANCE_SERVICE_SLUGS = [
-  "general-enquiries",
   "check-if-you-need-planning-permission",
+  "check-your-planning-constraints",
+  "find-out-if-you-need-planning-permission-energy-efficiency",
   "find-out-if-you-need-planning-permission",
-  "find-out-if-you-need-planning-permission-energy-efficiency"
+  "general-enquiries",
+  "heritage-constraints",
 ];
 
 export async function fetchAllLPAs(): Promise<LPA[]> {
