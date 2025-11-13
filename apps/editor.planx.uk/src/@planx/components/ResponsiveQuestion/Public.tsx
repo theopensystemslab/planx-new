1c086ce504dc5b76611a0b8b15ca7fb0f784367e
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-13 09:31:49 +0000

    feat: Update `useConditionalOptions()` to handle grouped options (#5658)

--- apps/editor.planx.uk/src/@planx/components/ResponsiveQuestion/Public.tsx
+++ apps/editor.planx.uk/src/@planx/components/ResponsiveQuestion/Public.tsx
@@ -2,13 +2,13 @@ import type { PublicProps } from "@planx/components/shared/types";
 import React from "react";
 
 import BaseQuestionComponent from "../shared/BaseQuestion/Public";
-import { useConditionalResponses } from "../shared/RuleBuilder/hooks/useConditionalResponses";
+import { useConditionalOptions } from "../shared/RuleBuilder/hooks/useConditionalResponses";
 import type { ResponsiveQuestionWithOptions } from "./model";
 
 export type Props = PublicProps<ResponsiveQuestionWithOptions>;
 
 const ResponsiveQuestion = (props: Props) => {
-  const conditionalOptions = useConditionalResponses(props.options);
+  const { conditionalOptions } = useConditionalOptions(props.options);
   if (!conditionalOptions) return null;
 
   return <BaseQuestionComponent {...props} options={conditionalOptions} />;
