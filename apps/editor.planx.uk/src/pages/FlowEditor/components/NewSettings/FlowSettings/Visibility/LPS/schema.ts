7fc8aba9b32083e70c6fd89be89cd62cbd43215e
Ian Jones <hello@ianjo.uk>
2025-11-14 13:51:28 +0000

    feat: Migrate LPS visibility to new settings

---
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/NewSettings/FlowSettings/Visibility/LPS/schema.ts
@@ -0,0 +1,21 @@
+import { boolean, object, type SchemaOf, string } from "yup";
+
+import { LPSListingFormValues } from "./types";
+
+export const validationSchema: SchemaOf<LPSListingFormValues> = object({
+  isListedOnLPS: boolean()
+    .required()
+    .test(
+      "requires-summary",
+      'Service summary required - please set via the "About this flow" tab',
+      function (value) {
+        const { summary } = this.parent;
+        if (value && !summary) {
+          return false;
+        }
+        return true;
+      },
+    )
+    .default(false),
+  summary: string().nullable(),
+});
