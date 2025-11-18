2d388ada4d9bc5fb38f15285c115a656d7977df0
nichgalzin <nichgalzin@gmail.com>
2025-11-18 10:58:44 +0000

    refactor: updates back navigation to tanstack hook

--- apps/editor.planx.uk/src/pages/Preview/ContentPage.tsx
+++ apps/editor.planx.uk/src/pages/Preview/ContentPage.tsx
@@ -4,10 +4,10 @@ import Container from "@mui/material/Container";
 import IconButton from "@mui/material/IconButton";
 import { styled } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "@tanstack/react-router";
 import { NotFoundError } from "navi";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
-import { useNavigation } from "react-navi";
 import { FOOTER_ITEMS } from "types";
 import ReactMarkdownOrHtml from "ui/shared/ReactMarkdownOrHtml/ReactMarkdownOrHtml";
 
@@ -50,7 +50,7 @@ function Layout(props: {
 }
 
 function ContentPage(props: { page: string }) {
-  const navigation = useNavigation();
+  const navigate = useNavigate();
   const { flowSettings, globalSettings } = useStore();
   const isFooterItem = FOOTER_ITEMS.includes(props.page);
   // Determine if the content is a flow setting or a global setting, and only show it if it isn't hidden
@@ -74,7 +74,7 @@ function ContentPage(props: { page: string }) {
   return (
     <Layout
       {...content}
-      onClose={() => navigation.goBack()}
+      onClose={() => navigate({ to: ".." })}
       openLinksOnNewTab={!isFooterItem}
     />
   );
