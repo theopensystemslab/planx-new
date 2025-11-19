df4117d52d0559c8d9a404c33c63181950280465
nichgalzin <nichgalzin@gmail.com>
2025-11-19 11:43:38 +0000

    fix: adds search params validator to route

--- apps/editor.planx.uk/src/routes/$team/$flow/draft/route.tsx
+++ apps/editor.planx.uk/src/routes/$team/$flow/draft/route.tsx
@@ -1,13 +1,16 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { zodValidator } from "@tanstack/zod-adapter";
 import React from "react";
 import {
   createPublicRouteBeforeLoad,
   createPublicRouteErrorComponent,
   createPublicRouteHead,
   PublicRouteLayout,
+  publicRouteSearchSchemas,
 } from "utils/routeUtils/publicRouteHelpers";
 
 export const Route = createFileRoute("/$team/$flow/draft")({
+  validateSearch: zodValidator(publicRouteSearchSchemas.draft),
   beforeLoad: createPublicRouteBeforeLoad("draft"),
   head: createPublicRouteHead("draft"),
   errorComponent: createPublicRouteErrorComponent("draft"),
