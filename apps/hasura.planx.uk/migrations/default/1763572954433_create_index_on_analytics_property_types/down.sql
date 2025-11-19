5255b98dee86b32692e94f86b1d340c887edecf8
zz-hh-aa <zzhhaa@protonmail.com>
2025-11-19 17:24:20 +0000

    create index

---
+++ apps/hasura.planx.uk/migrations/default/1763572954433_create_index_on_analytics_property_types/down.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF NOT EXISTS analytics_property_types_analytics_id_idx
+  ON analytics_property_types (analytics_id);
---
+++ apps/hasura.planx.uk/migrations/default/1763572954433_create_index_on_analytics_property_types/up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS analytics_property_types_analytics_id_idx
+  ON analytics_property_types (analytics_id);
