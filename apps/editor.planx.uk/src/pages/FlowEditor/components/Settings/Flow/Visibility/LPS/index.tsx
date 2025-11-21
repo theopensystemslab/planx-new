884251fe64a076be2125dd039a98852326c442d0
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-21 10:16:24 +0000

    refactor: Split out LPS toggle

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/Visibility/LPS/index.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/Visibility/LPS/index.tsx
@@ -2,11 +2,10 @@ import Link from "@mui/material/Link";
 import { useLPS } from "hooks/useLPS";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
-import SettingsDescription from "ui/editor/SettingsDescription";
-import { Switch } from "ui/shared/Switch";
 
 import SettingsFormContainer from "../../../shared/SettingsForm";
 import CategorySelection from "./components/CategorySelection";
+import ToggleLPS from "./components/ToggleLPS";
 import { GET_LPS_LISTING, UPDATE_LPS_LISTING } from "./queries";
 import { defaultValues, validationSchema } from "./schema";
 import {
@@ -48,34 +47,9 @@ const LPSListingSettings: React.FC = () => {
         category: values.category,
       })}
     >
-      {({ formik }) => (
+      {() => (
         <>
-          <Switch
-            error={formik.errors?.isListedOnLPS}
-            label={
-              formik.values.isListedOnLPS
-                ? "Service is listed on localplanning.services"
-                : "Service is not listed on localplanning.services"
-            }
-            name="isListedOnLPS"
-            variant="editorPage"
-            checked={formik.values.isListedOnLPS}
-            onChange={() =>
-              formik.setFieldValue(
-                "isListedOnLPS",
-                !formik.values.isListedOnLPS,
-              )
-            }
-          />
-          <SettingsDescription>
-            <p>
-              Listing your service requires a summary. This can be provided on{" "}
-              <Link style={{ whiteSpace: "nowrap" }} href="../about">
-                the "About this flow" page
-              </Link>
-              .
-            </p>
-          </SettingsDescription>
+          <ToggleLPS />
           <CategorySelection />
         </>
       )}
