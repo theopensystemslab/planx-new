c8cc0260e2c621668ca07483b7a9ca88dcb620aa
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-22 22:32:16 +0000

    chore: Tidy up

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Team/Design/shared/validationSchema.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Team/Design/shared/validationSchema.ts
@@ -1,5 +1,4 @@
 import type { TeamTheme } from "@opensystemslab/planx-core/types";
-import { object, string } from "yup";
 
 export const defaultValues: TeamTheme = {
   primaryColour: "#0010A4",
@@ -8,12 +7,3 @@ export const defaultValues: TeamTheme = {
   linkColour: "#0010A4",
   actionColour: "#0010A4",
 };
-
-// TODO: delete!
-export const validationSchema = object({
-  primaryColour: string().optional(),
-  linkColour: string().optional(),
-  actionColour: string().optional(),
-  logo: string().nullable(),
-  favicon: string().nullable(),
-});
