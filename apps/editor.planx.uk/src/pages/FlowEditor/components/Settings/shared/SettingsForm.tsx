5afede0eb540432fe7e3b959a66e31345765254e
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-23 16:49:34 +0000

    chore: Migrate tests

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/shared/SettingsForm.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/shared/SettingsForm.tsx
@@ -4,6 +4,7 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import { Form, Formik, FormikHelpers, FormikProps, FormikValues } from "formik";
 import { useToast } from "hooks/useToast";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 import InputLegend from "ui/editor/InputLegend";
 import NewSettingsSection from "ui/editor/NewSettingsSection";
@@ -78,6 +79,11 @@ const SettingsFormContainer = <
   showActionButtons = true,
 }: SettingsFormContainerProps<TData, TVariables, TFormValues>) => {
   const toast = useToast();
+  const [teamSlug, canUserEditTeam] = useStore((state) => [
+    state.teamSlug,
+    state.canUserEditTeam,
+  ]);
+  const userPermissionError = !canUserEditTeam(teamSlug);
 
   // Fetch current data
   const { data, loading, error } = useQuery<TData>(query, {
@@ -152,43 +158,41 @@ const SettingsFormContainer = <
                 <SettingsDescription>{description}</SettingsDescription>
               </Grid>
 
-              <Grid item xs={12} md={8}>
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: 2,
-                    paddingTop: 0.25,
-                  }}
+          <Grid item xs={12} md={8}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                paddingTop: 0.25,
+              }}
+            >
+              {children({ formik, data, loading })}
+            </Box>
+            {preview && <Box mt={2}>{preview(formik)}</Box>}
+            {showActionButtons && (
+              <Box mt={2} display="flex" gap={1.5}>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled={userPermissionError || !formik.dirty || updating}
                 >
-                  {children({ formik, data, loading })}
-                </Box>
-                {preview && <Box mt={2}>{preview(formik)}</Box>}
-                {showActionButtons && (
-                  <Box mt={2} display="flex" gap={1.5}>
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      disabled={!formik.dirty || updating}
-                    >
-                      Save
-                    </Button>
-                    <Button
-                      onClick={() => formik.resetForm()}
-                      type="reset"
-                      variant="contained"
-                      disabled={!formik.dirty}
-                      color="secondary"
-                    >
-                      Reset changes
-                    </Button>
-                  </Box>
-                )}
-              </Grid>
-            </Grid>
-          </Form>
-        )}
-      </Formik>
+                  Save
+                </Button>
+                <Button
+                  onClick={() => formik.resetForm()}
+                  type="reset"
+                  variant="contained"
+                  disabled={userPermissionError || !formik.dirty}
+                  color="secondary"
+                >
+                  Reset changes
+                </Button>
+              </Box>
+            )}
+          </Grid>
+        </Grid>
+      </Box>
     </NewSettingsSection>
   );
 };
