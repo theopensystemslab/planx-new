d046897832c7ddd5ed41e5af6e7f38f7922de3d9
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-23 14:23:11 +0000

    feat: Description

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/About/index.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/About/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import Description from "./Description";
 import Summary from "./Summary";
 
 const About: React.FC = () => (
   <>
     <Summary />
+    <Description />
   </>
 );
 
