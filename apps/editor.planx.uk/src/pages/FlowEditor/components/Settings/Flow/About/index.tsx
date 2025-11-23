41f45afc3cf1d660ee4986af15bc6a7a72b26af9
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-23 14:26:10 +0000

    feat: Limitations

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/About/index.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/About/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Description from "./Description";
+import Limitations from "./Limitations";
 import Summary from "./Summary";
 
 const About: React.FC = () => (
   <>
     <Summary />
     <Description />
+    <Limitations />
   </>
 );
 
