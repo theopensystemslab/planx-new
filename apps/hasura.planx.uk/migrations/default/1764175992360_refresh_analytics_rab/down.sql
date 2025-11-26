20ebff2f7f704f00964046c6edff67d3c78dac5a
zz-hh-aa <zzhhaa@protonmail.com>
2025-11-26 09:00:16 -0800

    cron job for analytics_rab

---
+++ apps/hasura.planx.uk/migrations/default/1764175992360_refresh_analytics_rab/down.sql
@@ -0,0 +1,1 @@
+SELECT cron.unschedule('refresh_analytics_rab');
---
+++ apps/hasura.planx.uk/migrations/default/1764175992360_refresh_analytics_rab/up.sql
@@ -0,0 +1,5 @@
+SELECT cron.schedule(
+  'refresh_analytics_rab',
+  '30 3 * * *',
+  $$REFRESH MATERIALIZED VIEW analytics_rab$$
+);
