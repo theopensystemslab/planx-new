b624037124df7c350bb18e99179a7a75231804e6
Ian Jones <51156018+ianjon3s@users.noreply.github.com>
2025-11-26 14:52:20 +0000

    fix(lps): Reset session for magic link errors (#5777)

--- apps/localplanning.services/src/components/applications/errors/InvalidLink.tsx
+++ apps/localplanning.services/src/components/applications/errors/InvalidLink.tsx
@@ -1,6 +1,12 @@
-export const InvalidLink: React.FC = () => (
+import { useResetSession } from "../hooks/useResetSession";
+
+export const InvalidLink: React.FC = () => {
+  const { resetAndNavigate } = useResetSession();
+
+  return (
   <section className="styled-content">
     <h2>Error: Invalid magic link</h2>
-    <p>Please <a href="/applications">try to log in again</a>.</p>
+    <p>Please <button onClick={() => resetAndNavigate()} className="button-link button-focus-style">try to log in again</button>.</p>
   </section>
-);
+  );
+};
