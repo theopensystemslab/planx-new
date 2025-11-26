05fa23a816b9b643a889932bff7ca405353fd69a
nichgalzin <nichgalzin@gmail.com>
2025-11-26 14:43:43 +0000

    test: corrects text and store setup relate to team members screen

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Team/tests/helpers/setupTeamMembersScreen.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Team/tests/helpers/setupTeamMembersScreen.tsx
@@ -1,5 +1,6 @@
 import { screen } from "@testing-library/react";
 import { ToastContextProvider } from "contexts/ToastContext";
+import { useStore } from "pages/FlowEditor/lib/store";
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -7,11 +8,16 @@ import { HTML5Backend } from "react-dnd-html5-backend";
 import { setup } from "../../../../../../testUtils";
 import { TeamMembers } from "../../TeamMembers";
 
+const TeamMembersWrapper = () => {
+  const teamMembers = useStore((state) => state.teamMembers || []);
+  return <TeamMembers teamMembers={teamMembers} />;
+};
+
 export const setupTeamMembersScreen = async () => {
   const setupResult = setup(
     <DndProvider backend={HTML5Backend}>
       <ToastContextProvider>
-        <TeamMembers />
+        <TeamMembersWrapper />
       </ToastContextProvider>
     </DndProvider>,
   );
