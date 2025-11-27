5a67d295829465630c52a380bc6c815ac04573a7
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-11-27 11:58:10 +0000

    feat: Styles and layout

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/SubmissionHTML.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/SubmissionHTML.tsx
@@ -1,9 +1,21 @@
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Skeleton from "@mui/material/Skeleton";
 import { useQuery } from "@tanstack/react-query";
-import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
 import { getSubmissionHTML } from "lib/api/submissions/requests";
 import React from "react";
-import ErrorSummary from "ui/shared/ErrorSummary/ErrorSummary";
+
+const LoadingSkeleton = () => (
+  <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+    <Skeleton variant="text" sx={{ fontSize: "4rem" }} />
+    <Skeleton variant="rectangular" width={650} height={650} />
+    <Skeleton variant="rectangular" width={1000} height={185} />
+    <Skeleton variant="text" width={200} sx={{ fontSize: "2rem" }} />
+    <Skeleton variant="rectangular" width={1000} height={185} />
+    <Skeleton variant="text" width={200} sx={{ fontSize: "2rem" }} />
+    <Skeleton variant="rectangular" width={1000} height={185} />
+  </Box>
+);
 
 const SubmissionHTML: React.FC<{ sessionId: string }> = ({ sessionId }) => {
   const { data, error, isPending } = useQuery({
@@ -12,10 +24,17 @@ const SubmissionHTML: React.FC<{ sessionId: string }> = ({ sessionId }) => {
     enabled: !!sessionId,
   });
 
-  if (isPending) return <DelayedLoadingIndicator />;
-  if (error) return <ErrorSummary message="Failed to download HTML" />;
+  if (error) throw Error(`Unable to download session ${sessionId}`);
 
-  return <Box dangerouslySetInnerHTML={{ __html: data }} />;
+  return (
+    <Container maxWidth="contentWrap">
+      {isPending ? (
+        <LoadingSkeleton />
+      ) : (
+        <Box dangerouslySetInnerHTML={{ __html: data }} />
+      )}
+    </Container>
+  );
 };
 
 export default SubmissionHTML;
