f0337478ea91dba650dae18c6ce9cb88b2e05fc4
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2025-12-08 10:19:25 +0000

    add unlimited condition description [skip pizza]

--- apps/editor.planx.uk/src/@planx/components/List/schemas/DischargeConditions.ts
+++ apps/editor.planx.uk/src/@planx/components/List/schemas/DischargeConditions.ts
@@ -21,7 +21,8 @@ export const DischargeConditions: Schema = {
         description:
           "This is the exact wording of the condition as shown on the decision notice.",
         fn: "description",
-        type: TextInputType.ExtraLong,
+        type: TextInputType.Custom,
+        customLength: 10000,
       },
     },
     {
