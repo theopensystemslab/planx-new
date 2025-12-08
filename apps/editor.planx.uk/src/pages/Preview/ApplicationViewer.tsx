f22f1bb0bfe60b82624d49c6d2c4487f087d9a88
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-08 15:36:16 +0000

    feat: Show fee breakdown

---
+++ apps/editor.planx.uk/src/pages/Preview/ApplicationViewer.tsx
@@ -0,0 +1,51 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { FeeBreakdown } from "@planx/components/Pay/Public/FeeBreakdown/FeeBreakdown";
+import SummaryListsBySections from "@planx/components/shared/Preview/SummaryList";
+import { PrintButton } from "components/PrintButton";
+import { useStore } from "pages/FlowEditor/lib/store";
+import React from "react";
+
+import StatusPage from "./StatusPage";
+
+const NoContentPage = () => (
+  <StatusPage
+    bannerHeading="Your form"
+    buttonText="Start a new form"
+  >
+    <Typography variant="body2">
+      No answers to display
+    </Typography>
+  </StatusPage>
+)
+
+const ApplicationViewer: React.FC = () => {
+  const [flow, passport, breadcrumbs, govUkPayment] = useStore(state => [state.flow, state.computePassport(), state.breadcrumbs, state.govUkPayment]);
+  const hasPaymentInfo = Boolean(govUkPayment);
+
+  // No answered questions - saving on first question
+  const noBreadcrumbs = Object.keys(breadcrumbs).length === 0;
+  if (noBreadcrumbs) return <NoContentPage />;
+
+  return (
+    <StatusPage bannerHeading="Your form" bannerText="Review your answers">
+      <SummaryListsBySections
+        changeAnswer={() => null}
+        showChangeButton={false}
+        flow={flow}
+        passport={passport}
+        breadcrumbs={breadcrumbs}
+        sectionComponent="h2"
+      />
+      {hasPaymentInfo &&
+        <Box mb={4}>
+          <Typography variant="h2">Fee breakdown</Typography>
+          <FeeBreakdown />
+        </Box>
+      }
+      <PrintButton />
+    </StatusPage>
+  );
+}
+
+export default ApplicationViewer;
