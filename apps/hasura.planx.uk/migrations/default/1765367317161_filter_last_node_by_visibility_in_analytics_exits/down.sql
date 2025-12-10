bcd088e10e456a3060d2b11a048f4e0aff0ac267
ollie <107874766+zz-hh-aa@users.noreply.github.com>
2025-12-10 12:56:49 +0000

    fix: `analytics_exits` aggregation logic (#5874)

---
+++ apps/hasura.planx.uk/migrations/default/1765367317161_filter_last_node_by_visibility_in_analytics_exits/down.sql
@@ -0,0 +1,29 @@
+DROP MATERIALIZED VIEW "public"."analytics_exits";
+CREATE MATERIALIZED VIEW "public"."analytics_exits" AS 
+ WITH exit_log AS (
+         SELECT DISTINCT ON (analytics_logs.analytics_id) analytics_logs.analytics_id,
+            analytics_logs.user_exit,
+            analytics_logs.node_title,
+            analytics_logs.node_type,
+            bool_or(analytics_logs.has_clicked_save) AS has_clicked_save,
+            max(
+                CASE
+                    WHEN (analytics_logs.user_exit = true) THEN analytics_logs.node_title
+                    ELSE NULL::text
+                END) AS exit_node_title
+           FROM analytics_logs
+          GROUP BY analytics_logs.analytics_id, analytics_logs.user_exit, analytics_logs.node_title, analytics_logs.node_type, analytics_logs.created_at
+          ORDER BY analytics_logs.analytics_id DESC, analytics_logs.created_at DESC
+        )
+ SELECT a.id AS analytics_id,
+    a.flow_id,
+    a.type AS analytics_type,
+    el.user_exit AS is_user_exit,
+    el.node_title AS last_node_title,
+    el.node_type AS last_node_type,
+    el.has_clicked_save,
+    el.exit_node_title
+   FROM (analytics a
+     JOIN exit_log el ON ((a.id = el.analytics_id)));
+
+GRANT SELECT ON "public"."analytics_exits" TO metabase_read_only; 
