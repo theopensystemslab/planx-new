fb4b8c0651092d7c8513dacb65949cadf6b411dc
Ian Jones <hello@ianjo.uk>
2025-12-10 12:45:47 +0000

    update component: responsive checklist

--- apps/editor.planx.uk/src/@planx/components/ResponsiveChecklist/Editor.tsx
+++ apps/editor.planx.uk/src/@planx/components/ResponsiveChecklist/Editor.tsx
@@ -1,5 +1,8 @@
-import { ComponentType, ComponentType as TYPES } from "@opensystemslab/planx-core/types";
-import { useFormik } from "formik";
+import {
+  ComponentType,
+  ComponentType as TYPES,
+} from "@opensystemslab/planx-core/types";
+import { useFormikWithRef } from "@planx/components/shared/useFormikWithRef";
 import React from "react";
 
 import { ConditionalOption } from "../Option/model";
@@ -10,7 +13,12 @@ import {
   GroupedOptions,
 } from "../shared/BaseChecklist/model";
 import { EditorProps } from "../shared/types";
-import { parseResponsiveChecklist, ResponsiveChecklist, type ResponsiveChecklistWithOptions, validationSchema } from "./model";
+import {
+  parseResponsiveChecklist,
+  ResponsiveChecklist,
+  type ResponsiveChecklistWithOptions,
+  validationSchema,
+} from "./model";
 
 type ExtraProps =
   | FlatOptions<ConditionalOption>
@@ -26,27 +34,32 @@ export default ResponsiveChecklistComponent;
 function ResponsiveChecklistComponent(props: Props) {
   const type = ComponentType.ResponsiveChecklist;
 
-  const formik = useFormik<ResponsiveChecklistWithOptions>({
-    initialValues: parseResponsiveChecklist({
-      ...props.node?.data,
-      options: props?.options,
-      groupedOptions: props?.groupedOptions,
-    }),
-    onSubmit: (values) => {
-      const { data, children } = generatePayload(values)
+  const formik = useFormikWithRef<ResponsiveChecklistWithOptions>(
+    {
+      initialValues: parseResponsiveChecklist({
+        ...props.node?.data,
+        options: props?.options,
+        groupedOptions: props?.groupedOptions,
+      }),
+      onSubmit: (values) => {
+        const { data, children } = generatePayload(values);
 
-      if (props.handleSubmit) {
-        props.handleSubmit({ type, data }, children);
-      } else {
-        alert(JSON.stringify({ type, ...values, options: values.options }, null, 2));
-      }
+        if (props.handleSubmit) {
+          props.handleSubmit({ type, data }, children);
+        } else {
+          alert(
+            JSON.stringify(
+              { type, ...values, options: values.options },
+              null,
+              2,
+            ),
+          );
+        }
+      },
+      validationSchema,
     },
-    validationSchema,
-    validateOnBlur: false,
-    validateOnChange: false,
-  });
+    props.formikRef,
+  );
 
-  return (
-    <BaseChecklistComponent formik={formik} type={type} {...props} />
-  )
+  return <BaseChecklistComponent formik={formik} type={type} {...props} />;
 }
