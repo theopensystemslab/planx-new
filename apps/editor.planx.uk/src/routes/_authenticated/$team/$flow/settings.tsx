507717f26ebf8cda073ac8ba9f88c917e0b8847e
nichgalzin <nichgalzin@gmail.com>
2025-12-11 16:12:29 +0000

    build: deletes route files as they have been updated to new setting routes

--- apps/editor.planx.uk/src/routes/_authenticated/$team/$flow/settings.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import ServiceSettings from "pages/FlowEditor/components/Settings/ServiceSettings/ServiceSettings";
-
-export const Route = createFileRoute("/_authenticated/$team/$flow/settings")({
-  component: ServiceSettings,
-});
--- apps/editor.planx.uk/src/routes/_authenticated/$team/general-settings.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import GeneralSettings from "pages/FlowEditor/components/Settings/GeneralSettings";
-
-export const Route = createFileRoute("/_authenticated/$team/general-settings")({
-  component: GeneralSettings,
-});
