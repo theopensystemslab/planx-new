a8d2c27718ec3f5989bcd42ea932eaa01f057de6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-13 11:23:58 +0000

    feat: Very basic public component

---
+++ apps/editor.planx.uk/src/@planx/components/EnhancedTextInput/Public/index.tsx
@@ -0,0 +1,53 @@
+import Card from "@planx/components/shared/Preview/Card";
+import { CardHeader } from "@planx/components/shared/Preview/CardHeader/CardHeader";
+import type { PublicProps } from "@planx/components/shared/types";
+import {
+  getPreviouslySubmittedData,
+  makeData,
+} from "@planx/components/shared/utils";
+import { Formik } from "formik";
+import React from "react";
+
+import { validationSchema } from "../model";
+import type { BreadcrumbData, EnhancedTextInput, TaskComponentMap } from "../types";
+import EnhanceProjectDescriptions from "./Tasks/EnhanceProjectDescription";
+
+type Props = PublicProps<EnhancedTextInput>;
+
+const taskComponents: TaskComponentMap = {
+  enhanceProjectDescription: EnhanceProjectDescriptions,
+};
+
+const EnhancedTextInputComponent = (props: Props) => {
+  const initialValues = getPreviouslySubmittedData(props);
+  const TaskComponent = taskComponents[
+    props.task
+  ] as React.ComponentType<Props> | null;
+
+  if (!TaskComponent) return null;
+
+  return (
+    <Formik<BreadcrumbData>
+      initialValues={initialValues}
+      onSubmit={(values) => props.handleSubmit?.(makeData(props, values))}
+      validateOnBlur={false}
+      validateOnChange={false}
+      validationSchema={validationSchema}
+    >
+      {(formik) => (
+        <Card handleSubmit={formik.handleSubmit}>
+          <CardHeader
+            title={props.title}
+            description={props.description}
+            info={props.info}
+            policyRef={props.policyRef}
+            howMeasured={props.howMeasured}
+          />
+          <TaskComponent {...props} />
+        </Card>
+      )}
+    </Formik>
+  );
+};
+
+export default EnhancedTextInputComponent;
