6286101e8cdaf12fd9988b908d42b6d6e1f8c63a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-15 16:14:25 +0000

    Revert "chore(metabase): Create basic views of both teams and flows (#5905)"
    This reverts commit 315ba9a19b890f6ad79abb2a8801835f11d28179.

--- apps/hasura.planx.uk/migrations/default/1765798010056_run_sql_migration/down.sql
+++
@@ -1,5 +0,0 @@
-REVOKE EXECUTE ON FUNCTION public.flow_first_online_at(flows) FROM metabase_read_only;
-REVOKE EXECUTE ON FUNCTION public.flow_production_url(flows) FROM metabase_read_only;
-REVOKE SELECT ON public.analytics_planning_data_flows FROM metabase_read_only;
-
-DROP VIEW IF EXISTS analytics_planning_data_flows;
