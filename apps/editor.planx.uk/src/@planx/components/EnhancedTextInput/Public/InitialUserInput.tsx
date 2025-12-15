6a7dbed47359a078c97b036049f93bd8425d96bc
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-15 18:16:17 +0000

    feat(wip): Very basic two card setup for public facing component

---
+++ apps/editor.planx.uk/src/@planx/components/EnhancedTextInput/Public/InitialUserInput.tsx
@@ -0,0 +1,65 @@
+import { DESCRIPTION_TEXT, ERROR_MESSAGE } from "@planx/components/shared/constants";
+import Card from "@planx/components/shared/Preview/Card";
+import { CardHeader } from "@planx/components/shared/Preview/CardHeader/CardHeader";
+import type { PublicProps } from "@planx/components/shared/types";
+import { TEXT_LIMITS, TextInputType } from "@planx/components/TextInput/model";
+import { Formik } from "formik";
+import React, { type Dispatch, type SetStateAction } from "react";
+import InputLabel from "ui/public/InputLabel";
+import { CharacterCounter } from "ui/shared/CharacterCounter";
+import Input from "ui/shared/Input/Input";
+import InputRow from "ui/shared/InputRow";
+
+import type { EnhancedTextInput } from "../types";
+
+const InitialUserInput: React.FC<PublicProps<EnhancedTextInput> & { setUserInput:Dispatch<SetStateAction<string>>}> = (props) => (
+  <Formik<{ original: string }>
+    initialValues={{ original: "" }}
+    onSubmit={({ original }) => props.setUserInput(original)}
+    validateOnBlur={false}
+    validateOnChange={false}
+  >
+    {({ values, handleChange, errors, handleSubmit }) => (
+      <Card handleSubmit={handleSubmit}>
+        <CardHeader
+          title={props.title}
+          description={props.description}
+          info={props.info}
+          policyRef={props.policyRef}
+          howMeasured={props.howMeasured}
+        />
+        <InputRow>
+          <InputLabel label={props.title} hidden htmlFor={props.id}>
+            <Input
+              type="text"
+              multiline
+              rows={5}
+              name="original"
+              value={values.original}
+              bordered
+              onChange={handleChange}
+              errorMessage={errors.original as string}
+              id={props.id}
+              inputProps={{
+                "aria-describedby": [
+                  props.description ? DESCRIPTION_TEXT : "",
+                  "character-hint",
+                  errors.original ? `${ERROR_MESSAGE}-${props.id}` : "",
+                ]
+                  .filter(Boolean)
+                  .join(" "),
+              }}
+            />
+            <CharacterCounter
+              limit={TEXT_LIMITS[TextInputType.Long]}
+              count={values.original.length}
+              error={Boolean(errors.original)}
+            />
+          </InputLabel>
+        </InputRow>
+      </Card>
+    )}
+  </Formik>
+)
+
+export default InitialUserInput;
--- apps/editor.planx.uk/src/@planx/components/EnhancedTextInput/Public/index.tsx
+++ apps/editor.planx.uk/src/@planx/components/EnhancedTextInput/Public/index.tsx
@@ -1,15 +1,8 @@
-import Card from "@planx/components/shared/Preview/Card";
-import { CardHeader } from "@planx/components/shared/Preview/CardHeader/CardHeader";
 import type { PublicProps } from "@planx/components/shared/types";
-import {
-  getPreviouslySubmittedData,
-  makeData,
-} from "@planx/components/shared/utils";
-import { Formik } from "formik";
-import React from "react";
+import React, { useState } from "react";
 
-import { validationSchema } from "../model";
-import type { BreadcrumbData, EnhancedTextInput, TaskComponentMap } from "../types";
+import type { EnhancedTextInput, TaskComponentMap } from "../types";
+import InitialUserInput from "./InitialUserInput";
 import ProjectDescription from "./Tasks/ProjectDescription";
 
 type Props = PublicProps<EnhancedTextInput>;
@@ -19,32 +12,15 @@ const taskComponents: TaskComponentMap = {
 };
 
 const EnhancedTextInputComponent = (props: Props) => {
-  const initialValues = getPreviouslySubmittedData(props);
+  const [userInput, setUserInput] = useState("");
   const TaskComponent = taskComponents[props.task];
-
   if (!TaskComponent) return null;
 
   return (
-    <Formik<BreadcrumbData>
-      initialValues={initialValues}
-      onSubmit={(values) => props.handleSubmit?.(makeData(props, values))}
-      validateOnBlur={false}
-      validateOnChange={false}
-      validationSchema={validationSchema}
-    >
-      {(formik) => (
-        <Card handleSubmit={formik.handleSubmit}>
-          <CardHeader
-            title={props.title}
-            description={props.description}
-            info={props.info}
-            policyRef={props.policyRef}
-            howMeasured={props.howMeasured}
-          />
-          <TaskComponent {...props} />
-        </Card>
-      )}
-    </Formik>
+    <>
+      {!userInput && <InitialUserInput {...props} setUserInput={setUserInput} /> }
+      {userInput && <TaskComponent {...props} userInput={userInput}/> }
+    </>
   );
 };
 
