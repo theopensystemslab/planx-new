315ba9a19b890f6ad79abb2a8801835f11d28179
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-15 15:57:04 +0000

    chore(metabase): Create basic views of both teams and flows (#5905)

---
+++ apps/hasura.planx.uk/migrations/default/1765797673747_run_sql_migration/up.sql
@@ -0,0 +1,20 @@
+REVOKE SELECT ON public.analytics_planning_data_health FROM metabase_read_only;
+
+DROP VIEW IF EXISTS analytics_planning_data_health;
+
+CREATE OR REPLACE VIEW analytics_planning_data_teams AS (
+SELECT
+    t.id AS team_id,
+    t.name AS team_name,
+    t.slug AS team_slug,
+    ti.has_planning_data AS planning_constraints_enabled_in_planx,
+    ts.reference_code AS planning_data_reference_code,
+    ts.is_trial,
+    FORMAT('https://provide.planning.data.gov.uk/organisations/local-authority:%s', ts.reference_code) AS planning_data_link
+FROM teams t
+    JOIN team_settings ts ON ts.team_id = t.id
+    JOIN team_integrations ti ON ti.team_id = t.id
+    WHERE ts.reference_code IS NOT NULL
+);
+
+GRANT SELECT ON public.analytics_planning_data_teams TO metabase_read_only;
