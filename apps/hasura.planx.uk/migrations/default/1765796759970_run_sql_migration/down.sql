be6ff4220dfc9c98fcd14e791f202e6f99fed680
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-15 11:08:23 +0000

    chore(metabase): Grant read-access to common tables for onboarding stats [skip pizza]

---
+++ apps/hasura.planx.uk/migrations/default/1765796759970_run_sql_migration/down.sql
@@ -0,0 +1,4 @@
+REVOKE SELECT ON public.flows TO metabase_read_only;
+REVOKE SELECT ON public.teams TO metabase_read_only;
+REVOKE SELECT ON public.team_settings TO metabase_read_only;
+REVOKE SELECT ON public.flow_status_history TO metabase_read_only;
---
+++ apps/hasura.planx.uk/migrations/default/1765796759970_run_sql_migration/up.sql
@@ -0,0 +1,4 @@
+GRANT SELECT ON public.flows TO metabase_read_only;
+GRANT SELECT ON public.teams TO metabase_read_only;
+GRANT SELECT ON public.team_settings TO metabase_read_only;
+GRANT SELECT ON public.flow_status_history TO metabase_read_only;
