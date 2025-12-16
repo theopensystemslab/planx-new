8571068babe167ef9a08e02885225b06071493ad
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-16 20:03:17 +0000

    chore(metabase): Add `flows.status` column [skip pizza]

---
+++ apps/hasura.planx.uk/migrations/default/1765915320420_add_flow_status_column_to_planx_flows/down.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE VIEW analytics_planx_flows AS (
+SELECT
+    f.id AS flow_id,
+    f.name AS flow_name,
+    f.slug AS flow_slug,
+    f.team_id,
+    public.flow_first_online_at(f) as first_online_at,
+    public.flow_production_url(f) as url
+FROM flows f
+    JOIN teams t ON f.team_id = t.id
+    JOIN team_settings ts ON ts.team_id = t.id
+    JOIN team_integrations ti ON ti.team_id = t.id
+);
+
+GRANT SELECT ON public.analytics_planx_flows TO metabase_read_only;
