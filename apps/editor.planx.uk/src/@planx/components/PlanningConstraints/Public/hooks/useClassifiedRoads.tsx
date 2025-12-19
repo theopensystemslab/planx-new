4d1d6d5bfc4f741d0a93c53cbea2a77d9195d122
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2025-12-19 11:38:56 +0000

    feat(editor): Pre-fetch classified roads as soon as we have a USRN

--- apps/editor.planx.uk/src/@planx/components/PlanningConstraints/Public/hooks/useClassifiedRoads.tsx
+++ apps/editor.planx.uk/src/@planx/components/PlanningConstraints/Public/hooks/useClassifiedRoads.tsx
@@ -1,7 +1,11 @@
+import { ComponentType } from "@opensystemslab/planx-core/types";
 import type { SiteAddress } from "@planx/components/FindProperty/model";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getClassifiedRoads } from "lib/api/gis/requests";
 import { useStore } from "pages/FlowEditor/lib/store";
+import { useEffect } from "react";
+
+import type { PlanningConstraints } from "../../model";
 
 export const useClassifiedRoads = (dataValues: string[]) => {
   const [hasPlanningData, { usrn }] = useStore((state) => [
@@ -19,7 +23,44 @@ export const useClassifiedRoads = (dataValues: string[]) => {
     queryFn: () => getClassifiedRoads(usrn),
     enabled: shouldFetchRoads,
     refetchOnWindowFocus: false,
+    staleTime: Infinity,
   });
 
   return query;
+};
+
+/**
+ * Prefetch the expensive "classified roads" query in order to reduce the overall latency for the user
+ * Triggered as soon as the user selects an address with USRN in useFindPropertyData()
+ */
+export const usePrefetchClassifiedRoads = (usrn?: string) => {
+  const queryClient = useQueryClient();
+
+  const [hasPlanningData, flow] = useStore((state) => [
+    state.teamIntegrations?.hasPlanningData,
+    state.flow,
+  ]);
+
+  // Validation rules enforce that each graph can only contain a single PlanningConstraints node
+  const planningConstraintsNode = Object.values(flow).find(
+    ({ type }) => type === ComponentType.PlanningConstraints,
+  );
+
+  const dataValues = (planningConstraintsNode?.data as PlanningConstraints)
+    ?.dataValues;
+
+  useEffect(() => {
+    const shouldPrefetch =
+      hasPlanningData &&
+      Boolean(usrn) &&
+      dataValues.includes("road.classified");
+
+    if (shouldPrefetch) {
+      queryClient.prefetchQuery({
+        queryKey: ["classifiedRoads", usrn],
+        queryFn: () => getClassifiedRoads(usrn),
+        staleTime: Infinity,
+      });
+    }
+  }, [usrn, queryClient]);
 };
