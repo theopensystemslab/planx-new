ceb1010adde74c2178ad384b4d63f5195986cea3
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-01-06 11:04:02 +0000

    fix(editor): Add Node events polyfill for ShareDB

--- apps/editor.planx.uk/vite.config.ts
+++ apps/editor.planx.uk/vite.config.ts
@@ -1,11 +1,18 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(), 
+    tsconfigPaths(), 
+    nodePolyfills({
+      include: ["events"],
+    }),
+  ],
   server: {
     port: 3000,
     hmr: {
