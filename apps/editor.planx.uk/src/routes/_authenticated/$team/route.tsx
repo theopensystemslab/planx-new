97ccce4959e6f4a376c61f3b27642447b476ab48
nichgalzin <nichgalzin@gmail.com>
2026-01-06 12:05:18 +0000

    fix: adds pending components and corrects delayed loading indicator to use router loading state

--- apps/editor.planx.uk/src/routes/_authenticated/$team/route.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/$team/route.tsx
@@ -1,10 +1,12 @@
 import { createFileRoute, notFound, Outlet } from "@tanstack/react-router";
+import RouteLoadingIndicator from "components/RouteLoadingIndicator";
 import React from "react";
 import { getTeamFromDomain } from "utils/routeUtils/utils";
 
 import { useStore } from "../../../pages/FlowEditor/lib/store";
 
 export const Route = createFileRoute("/_authenticated/$team")({
+  pendingComponent: RouteLoadingIndicator,
   beforeLoad: async ({ params }) => {
     const { initTeamStore, teamSlug: currentSlug } = useStore.getState();
     const routeSlug =
