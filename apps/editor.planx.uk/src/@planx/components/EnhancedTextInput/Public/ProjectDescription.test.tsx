573476aa64588f57cf1a439ed6b7599af3652a0b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-01-12 16:33:44 +0000

    test: Coverage for 'back' behaviour

--- apps/editor.planx.uk/src/@planx/components/EnhancedTextInput/Public/ProjectDescription.test.tsx
+++ apps/editor.planx.uk/src/@planx/components/EnhancedTextInput/Public/ProjectDescription.test.tsx
@@ -168,6 +168,139 @@ describe("Passport generation", () => {
   });
 });
 
+describe("navigating back to the EnhancedTextInput component", () => {
+  const previouslySubmittedData = {
+    auto: false,
+    createdAt: "2026-01-12T15:12:40.994Z",
+    seq: 1,
+    data: {
+      "project.description": "My hybrid response",
+      "project.description.action": "hybrid",
+      _enhancements: {
+        "project.description": {
+          original: "my first attempt",
+          enhanced: "our LLM-enhanced suggestion",
+        },
+      },
+    },
+  };
+
+  it("returns the user to the original input screen", async () => {
+    setup(
+      <EnhancedTextInputComponent
+        id="testId"
+        title="test"
+        task={"projectDescription"}
+        previouslySubmittedData={previouslySubmittedData}
+        {...taskDefaults.projectDescription}
+      />,
+    );
+
+    // User lands on original "input" step
+    const textbox = screen.getByRole("textbox", { name: /test/i });
+    expect(textbox).toBeVisible();
+    expect(textbox).toHaveValue(
+      previouslySubmittedData.data["project.description"],
+    );
+    expect(
+      screen.queryByRole("heading", {
+        name: "We suggest revising your project description",
+        level: 2,
+      }),
+    ).not.toBeInTheDocument();
+  });
+
+  it("pre-populates their previous answer", () => {
+    setup(
+      <EnhancedTextInputComponent
+        id="testId"
+        title="test"
+        task={"projectDescription"}
+        previouslySubmittedData={previouslySubmittedData}
+        {...taskDefaults.projectDescription}
+      />,
+    );
+
+    expect(screen.getByRole("textbox", { name: /test/i })).toHaveValue(
+      previouslySubmittedData.data["project.description"],
+    );
+  });
+
+  test("proceeding forwards again, without making a change", async () => {
+    const handleSubmit = vi.fn();
+
+    const { user } = setup(
+      <EnhancedTextInputComponent
+        id="testId"
+        title="test"
+        task={"projectDescription"}
+        previouslySubmittedData={previouslySubmittedData}
+        handleSubmit={handleSubmit}
+        {...taskDefaults.projectDescription}
+      />,
+    );
+
+    await user.click(screen.getByTestId("continue-button"));
+
+    // User is not on "task" step, but immediately goes to next node
+    expect(
+      screen.queryByText(taskDefaults.projectDescription.revisionTitle),
+    ).not.toBeInTheDocument();
+
+    // Breadcrumb not amended as user has made no changes
+    expect(handleSubmit).toHaveBeenCalledWith(
+      expect.objectContaining({ data: previouslySubmittedData.data }),
+    );
+  });
+
+  test("proceeding forwards again, with a change", async () => {
+    const handleSubmit = vi.fn();
+
+    const { user } = setup(
+      <EnhancedTextInputComponent
+        id="testId"
+        title="test"
+        task={"projectDescription"}
+        previouslySubmittedData={previouslySubmittedData}
+        handleSubmit={handleSubmit}
+        {...taskDefaults.projectDescription}
+      />,
+    );
+
+    // User navigate back and then enters a new description
+    await user.type(screen.getByRole("textbox", { name: /test/i }), ORIGINAL, {
+      initialSelectionStart: 0,
+      initialSelectionEnd:
+        previouslySubmittedData.data["project.description"].length,
+    });
+    await user.click(screen.getByTestId("continue-button"));
+
+    // Wait for next screen
+    expect(
+      screen.getByText(taskDefaults.projectDescription.revisionTitle),
+    ).toBeVisible();
+
+    // Proceed with new AI-enhanced description
+    await user.click(screen.getByTestId("continue-button"));
+
+    // Breadcrumb formatted as expected - action and values updated
+    expect(handleSubmit).toHaveBeenCalledWith(
+      expect.objectContaining({
+        data: {
+          "project.description": ENHANCED,
+          "project.description.action": "acceptedEnhanced",
+          _enhancements: {
+            "project.description": {
+              original: ORIGINAL,
+              enhanced: ENHANCED,
+            },
+          },
+        },
+      }),
+    );
+  });
+});
+
 it.todo("basic layout and behaviour");
 it.todo("should not have any accessibility violations on initial load");
 it.todo("error states");
