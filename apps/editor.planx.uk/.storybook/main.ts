c2b77d1cd63c5e64c3f49306bde9856301936022
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-01-14 11:29:19 +0000

    fix(storybook): Resolve dev mode errors

--- apps/editor.planx.uk/.storybook/main.ts
+++ apps/editor.planx.uk/.storybook/main.ts
@@ -1,11 +1,10 @@
-module.exports = {
+export default {
   stories: [
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     "@storybook/addon-a11y",
   ],
   staticDirs: ["../public"],
