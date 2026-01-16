0712566075855ea04b22ce3f38df5ba49feccd6b
zz-hh-aa <zzhhaa@protonmail.com>
2026-01-16 12:18:53 +0000

    refresh mv with cron job

---
+++ apps/hasura.planx.uk/migrations/default/1768565873766_refresh_analytics_service_overview/down.sql
@@ -0,0 +1,1 @@
+SELECT cron.unschedule('refresh_analytics_service_overview');
---
+++ apps/hasura.planx.uk/migrations/default/1768565873766_refresh_analytics_service_overview/up.sql
@@ -0,0 +1,5 @@
+SELECT cron.schedule(
+  'refresh_analytics_service_overview',
+  '35 3 * * *',
+  $$REFRESH MATERIALIZED VIEW analytics_service_overview$$
+);
