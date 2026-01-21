434e2a72d8597beb7e6614a726189f9232f37041
zz-hh-aa <zzhhaa@protonmail.com>
2026-01-21 16:33:03 +0000

    create flow_integration per flow

---
+++ apps/hasura.planx.uk/migrations/default/1769013134021_create_flow_integration_per_flow/up.sql
@@ -0,0 +1,14 @@
+INSERT INTO flow_integrations (flow_id, team_id, email_id)
+SELECT DISTINCT ON (f.id)
+    f.id AS flow_id,
+    f.team_id,
+    si.id AS email_id
+FROM flows f
+LEFT JOIN submission_integrations si 
+    ON si.team_id = f.team_id
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM flow_integrations fi 
+    WHERE fi.flow_id = f.id
+)
+ORDER BY f.id, si.default_email DESC NULLS LAST;
