a4e39bbefc3c4bf70a788520bc7f8e40a19c6f07
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-01-22 16:27:16 +0000

    feat: Add internal back button for in-node navigation

--- apps/editor.planx.uk/src/@planx/components/shared/Preview/Card.tsx
+++ apps/editor.planx.uk/src/@planx/components/shared/Preview/Card.tsx
@@ -7,13 +7,21 @@ import { useAnalyticsTracking } from "pages/FlowEditor/lib/analytics/provider";
 import { useStore } from "pages/FlowEditor/lib/store";
 import React, { useEffect } from "react";
 
+import InternalBackButton from "./InternalBackButton";
 import OrNavigationButton from "./OrNavigationButton";
 
 interface Props {
   children: React.ReactNode;
+  /**
+   * If falsey then disable Continue button, otherwise enable
+   */
   isValid?: boolean;
+  /**
+   * If included then show the Continue button
+   */
   handleSubmit?: (data?: any) => void;
   sx?: SxProps<Theme>;
+  handleBack?: () => void;
 }
 
 export const contentFlowSpacing = (theme: Theme): React.CSSProperties => ({
@@ -37,16 +45,13 @@ const InnerContainer = styled(Box)(({ theme }) => ({
 
 /**
  * Card which acts as a wrapper for public components
- * @param {object} props Component props
- * @param {bool} props.handleSubmit if included then show the Continue button
- * @param {bool} props.isValid if falsey then disable Continue button, otherwise enable
- * @param {bool} props.isTestWarningWrapper if truthy then show navigate to publish Or button
  */
 const Card: React.FC<Props> = ({
   children,
   isValid = true,
   handleSubmit,
   sx,
+  handleBack,
   ...props
 }) => {
   const theme = useTheme();
@@ -59,40 +64,43 @@ const Card: React.FC<Props> = ({
   }, []);
 
   return (
-    <Fade
-      in={true}
-      timeout={theme.transitions.duration.enteringScreen}
-      mountOnEnter={true}
-      unmountOnExit={true}
-    >
-      <Container maxWidth="contentWrap" sx={sx}>
-        <InnerContainer
-          bgcolor="background.default"
-          mt={{ xs: 4, md: 6 }}
-          mb={{ xs: 4, md: 8 }}
-          {...props}
-        >
-          {children}
+    <>
+      {handleBack && <InternalBackButton handleBack={handleBack} />}
+      <Fade
+        in={true}
+        timeout={theme.transitions.duration.enteringScreen}
+        mountOnEnter={true}
+        unmountOnExit={true}
+      >
+        <Container maxWidth="contentWrap" sx={sx}>
+          <InnerContainer
+            bgcolor="background.default"
+            mt={{ xs: 4, md: 6 }}
+            mb={{ xs: 4, md: 8 }}
+            {...props}
+          >
+            {children}
 
-          <Box pt={2}>
-            {handleSubmit && (
-              <Button
-                variant="contained"
-                color="prompt"
-                size="large"
-                type="submit"
-                disabled={!isValid}
-                onClick={async () => await handleSubmit()}
-                data-testid="continue-button"
-              >
-                Continue
-              </Button>
-            )}
-            <OrNavigationButton handleSubmit={handleSubmit} />
-          </Box>
-        </InnerContainer>
-      </Container>
-    </Fade>
+            <Box pt={2}>
+              {handleSubmit && (
+                <Button
+                  variant="contained"
+                  color="prompt"
+                  size="large"
+                  type="submit"
+                  disabled={!isValid}
+                  onClick={async () => await handleSubmit()}
+                  data-testid="continue-button"
+                >
+                  Continue
+                </Button>
+              )}
+              <OrNavigationButton handleSubmit={handleSubmit} />
+            </Box>
+          </InnerContainer>
+        </Container>
+      </Fade>
+    </>
   );
 };
 export default Card;
