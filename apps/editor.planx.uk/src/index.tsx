5eacf845af9f8b4f80ce91f376d69cce50972c93
nichgalzin <nichgalzin@gmail.com>
2026-01-28 13:14:29 +0000

    fix: sets scroll restoration during router creation to correct scroll to top of page in some navigation scenarios

--- apps/editor.planx.uk/src/index.tsx
+++ apps/editor.planx.uk/src/index.tsx
@@ -70,13 +70,12 @@ const hasJWT = (): boolean | void => {
   return true;
 };
 
-// Create a new router instance
 const tanstackRouter = createRouter({
   routeTree,
   context: { currentUser: hasJWT() },
+  scrollRestoration: true,
 });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof tanstackRouter;
