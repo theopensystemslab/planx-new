f0373d8cd98ab96f5eacbf025d4b5601ad6efca3
ollie <107874766+zz-hh-aa@users.noreply.github.com>
2026-01-29 09:46:33 +0000

    chore: optimise analytics_planx_flows view (#6036)

---
+++ apps/hasura.planx.uk/migrations/default/1769595120820_optimise_analytics_planx_flows/up.sql
@@ -0,0 +1,30 @@
+DROP VIEW IF EXISTS public.analytics_planx_flows CASCADE;
+
+CREATE VIEW public.analytics_planx_flows AS (
+SELECT
+    f.id AS flow_id,
+    f.name AS flow_name,
+    f.slug AS flow_slug,
+    f.team_id,
+    t.slug AS team_slug,
+    t.name AS team_name,
+    f.created_at,
+    f.status,
+    public.flow_first_online_at(f) AS first_online_at,
+    public.flow_production_url(f) AS url
+FROM flows f
+    JOIN teams t ON f.team_id = t.id
+    JOIN team_settings ts ON ts.team_id = t.id
+    JOIN team_integrations ti ON ti.team_id = t.id
+GROUP BY
+    f.id,
+    f.name,
+    f.slug,
+    f.team_id,
+    t.slug,
+    t.name,
+    f.created_at,
+    f.status
+);
+
+GRANT SELECT ON public.analytics_planx_flows TO metabase_read_only;
