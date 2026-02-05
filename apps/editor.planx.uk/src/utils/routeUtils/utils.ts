ed2cc71e1e8114fcc5c97b4aa787127eea4026aa
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-02-05 15:55:34 +0000

    fix: Handle internal portals with additional app param

--- apps/editor.planx.uk/src/utils/routeUtils/utils.ts
+++ apps/editor.planx.uk/src/utils/routeUtils/utils.ts
@@ -14,8 +14,9 @@ import { client } from "../../lib/graphql";
 export const makeTitle = (str: string) =>
   [str, "PlanX"].filter(Boolean).join(" | ");
 
+// TODO: Replace with type-safe version
 export const rootFlowPath = (includePortals = false) => {
-  const path = window.location.pathname.split("/").slice(0, 3).join("/");
+  const path = window.location.pathname.split("/").slice(0, 4).join("/");
   return includePortals ? path : path.split(",")[0];
 };
 
