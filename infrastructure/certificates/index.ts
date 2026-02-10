c9eb0c6f11a54b102280f6fd4fe61bebeefdd7cc
Dan G <freemvmt@posteo.net>
2026-02-10 17:42:00 +0000

    [infra] upgrade certificates layer (no significant code changes)

--- infrastructure/certificates/index.ts
+++ infrastructure/certificates/index.ts
@@ -2,22 +2,15 @@
 
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 import * as cloudflare from "@pulumi/cloudflare";
 
-import * as tldjs from "tldjs";
-
-import { Input } from "@pulumi/pulumi";
-
 const config = new pulumi.Config();
-
-const env = pulumi.getStack();
 
 // The @pulumi/cloudflare package doesn't generate errors so this is here just to create a warning in case the CloudFlare API token is missing.
 new pulumi.Config("cloudflare").requireSecret("apiToken");
 
 // https://docs.aws.amazon.com/acm/latest/userguide/setup-caa.html
-const caaRecordRoot = new cloudflare.Record(`caa-record-root`, {
+const caaRecordRoot = new cloudflare.DnsRecord("caa-record-root", {
   name: `${config.require("domain")}`,
   ttl: 600,
   type: "CAA",
@@ -28,7 +21,8 @@ const caaRecordRoot = new cloudflare.Record(`caa-record-root`, {
     value: "amazon.com",
   },
 });
-const caaRecordWildcard = new cloudflare.Record(`caa-record-wildcard`, {
+
+const caaRecordWildcard = new cloudflare.DnsRecord("caa-record-wildcard", {
   name: `${config.require("domain")}`,
   ttl: 600,
   type: "CAA",
@@ -39,8 +33,8 @@ const caaRecordWildcard = new cloudflare.Record(`caa-record-wildcard`, {
     value: "amazon.com",
   },
 });
-const sslCert = new aws.acm.Certificate(
-  `sslCert`,
+
+const sslCert = new aws.acm.Certificate("sslCert",
   {
     // XXX: For wildcards remember that *.example.com will only cover a single level subdomain such as www.example.com not secondary levels such as beta.www.example.com.
     domainName: `${config.require("domain")}`,
@@ -56,23 +50,19 @@ const sslCert = new aws.acm.Certificate(
     dependsOn: [caaRecordRoot, caaRecordWildcard],
   }
 );
-const sslCertValidationRecord = new cloudflare.Record(
-  `sslCertValidationRecord`,
-  {
+
+const sslCertValidationRecord = new cloudflare.DnsRecord("sslCertValidationRecord", {
     name: sslCert.domainValidationOptions[0].resourceRecordName,
     ttl: 3600,
     type: sslCert.domainValidationOptions[0].resourceRecordType,
-    value: sslCert.domainValidationOptions[0].resourceRecordValue,
+    content: sslCert.domainValidationOptions[0].resourceRecordValue,
     zoneId: config.require("cloudflare-zone-id"),
-  }
-);
-const sslCertValidation = new aws.acm.CertificateValidation(
-  `sslCertValidation`,
-  {
+});
+
+const sslCertValidation = new aws.acm.CertificateValidation("sslCertValidation", {
     certificateArn: sslCert.arn,
     validationRecordFqdns: [sslCertValidationRecord.name],
-  }
-);
+});
 
 export const certificateArn = sslCertValidation.certificateArn;
 export const domain = config.require("domain");
