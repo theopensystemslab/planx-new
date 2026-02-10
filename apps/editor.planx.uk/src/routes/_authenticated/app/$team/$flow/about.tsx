ff9a84c64b886d98b129bcc4adb4a2b1c7bedb8b
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-02-10 04:18:52 -0500

    fix: scope `FlowSkeleton` to graph loading only (#6080)

--- apps/editor.planx.uk/src/routes/_authenticated/app/$team/$flow/about.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/app/$team/$flow/about.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
+import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
 import About from "pages/FlowEditor/components/Settings/Flow/About";
 
 export const Route = createFileRoute("/_authenticated/app/$team/$flow/about")({
+  pendingComponent: DelayedLoadingIndicator,
   component: About,
 });
