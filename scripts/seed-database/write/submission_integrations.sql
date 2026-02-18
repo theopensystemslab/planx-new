87dc1bc7e83ed575c369779846c21797b60300e6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-02-18 12:11:27 +0000

    fix: Sync script missing column [skip pizza]

--- scripts/seed-database/write/submission_integrations.sql
+++ scripts/seed-database/write/submission_integrations.sql
@@ -3,22 +3,25 @@ CREATE TEMPORARY TABLE sync_submission_integrations (
   team_id integer,
   id uuid,
   submission_email text,
-  default_email boolean
+  default_email boolean,
+  deleted_at timestamptz
 );
 
 \COPY sync_submission_integrations FROM '/tmp/submission_integrations.csv' WITH (FORMAT csv, DELIMITER ';');
 
 INSERT INTO
-  submission_integrations (team_id, id, submission_email, default_email)
+  submission_integrations (team_id, id, submission_email, default_email, deleted_at)
 SELECT
   team_id integer,
   id uuid,
   submission_email text,
-  default_email boolean
+  default_email boolean,
+  deleted_at timestamptz
 FROM
   sync_submission_integrations ON CONFLICT (id) DO
 UPDATE
 SET
   team_id = EXCLUDED.team_id,
   submission_email = EXCLUDED.submission_email,
-  default_email = EXCLUDED.default_email;
+  default_email = EXCLUDED.default_email,
+  deleted_at = EXCLUDED.deleted_at;
