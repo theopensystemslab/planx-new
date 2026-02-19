6d8ede925eb6a57d4185d7437eae83e443e8cdca
Ian Jones <hello@ianjo.uk>
2026-02-19 10:52:35 +0000

    update notion embed routes

--- apps/editor.planx.uk/src/routes/_authenticated/app/$team/resources.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/app/$team/resources.tsx
@@ -2,6 +2,6 @@ import { createFileRoute } from "@tanstack/react-router";
 import React from "react";
 import NotionEmbed from "ui/editor/NotionEmbed";
 
-export const Route = createFileRoute("/_authenticated/app/resources")({
+export const Route = createFileRoute("/_authenticated/app/$team/resources")({
   component: () => <NotionEmbed page="resources" title="Resources" />,
 });
