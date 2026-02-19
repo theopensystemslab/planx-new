3dc27fc04faff1401bcfe53f3cd14f7583f89c25
zz-hh-aa <zzhhaa@protonmail.com>
2026-02-19 17:29:27 +0000

    cascade team_settings update and delete

---
+++ apps/hasura.planx.uk/migrations/default/1771521882317_update_submission_integrations_constraint/up.sql
@@ -0,0 +1,9 @@
+ALTER TABLE public.submission_integrations
+  DROP CONSTRAINT fk_submission_integrations_team_settings;
+
+ALTER TABLE public.submission_integrations
+  ADD CONSTRAINT fk_submission_integrations_team_settings
+    FOREIGN KEY (team_id)
+    REFERENCES public.team_settings(team_id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE;
