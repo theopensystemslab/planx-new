04734757064acd6c2fb214dae8ffdf1e85ea0db2
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-02-20 14:21:35 +0000

    fix(editor): Do not set Zustand state on beforeload (#6122)

--- apps/editor.planx.uk/src/pages/FlowEditor/lib/store/team.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/lib/store/team.ts
@@ -24,7 +24,6 @@ export interface TeamStore {
 
   setTeam: (team: Team) => void;
   getTeam: () => Team;
-  initTeamStore: (slug: string) => Promise<void>;
   clearTeamStore: () => void;
   fetchCurrentTeam: () => Promise<Team>;
   createTeam: (newTeam: CreateTeam) => Promise<number>;
@@ -78,59 +77,6 @@ export const teamStore: StateCreator<
   createTeam: async (newTeam) => {
     const { $client } = get();
     return await $client.team.create(newTeam);
-  },
-
-  initTeamStore: async (slug) => {
-    const { data } = await client.query({
-      query: gql`
-        query GetTeamBySlug($slug: String!) {
-          teams(
-            order_by: { name: asc }
-            limit: 1
-            where: { slug: { _eq: $slug } }
-          ) {
-            id
-            name
-            slug
-            domain
-            flows(order_by: { updated_at: desc }) {
-              slug
-              updated_at
-              operations(limit: 1, order_by: { id: desc }) {
-                actor {
-                  first_name
-                  last_name
-                }
-              }
-            }
-            integrations {
-              hasPlanningData: has_planning_data
-            }
-            settings: team_settings {
-              id
-              boundaryUrl: boundary_url
-              boundaryBBox: boundary_bbox
-              referenceCode: reference_code
-              helpEmail: help_email
-              helpPhone: help_phone
-              helpOpeningHours: help_opening_hours
-              emailReplyToId: email_reply_to_id
-              homepage: homepage
-              externalPlanningSiteName: external_planning_site_name
-              externalPlanningSiteUrl: external_planning_site_url
-              submissionEmail: submission_email
-              isTrial: is_trial
-            }
-          }
-        }
-      `,
-      variables: { slug },
-    });
-
-    const team = data.teams[0];
-
-    if (!team) throw new Error("Team not found");
-    get().setTeam(team);
   },
 
   clearTeamStore: () =>
--- apps/editor.planx.uk/src/routes/_authenticated/app/$team/route.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/app/$team/route.tsx
@@ -1,6 +1,9 @@
+import type { Team } from "@opensystemslab/planx-core/types";
 import { createFileRoute, notFound, Outlet } from "@tanstack/react-router";
 import RouteLoadingIndicator from "components/RouteLoadingIndicator";
-import React from "react";
+import gql from "graphql-tag";
+import { client } from "lib/graphql";
+import React, { useEffect } from "react";
 import { CatchAllComponent } from "routes/$";
 
 import { useStore } from "../../../../pages/FlowEditor/lib/store";
@@ -8,20 +11,76 @@ import { useStore } from "../../../../pages/FlowEditor/lib/store";
 export const Route = createFileRoute("/_authenticated/app/$team")({
   pendingComponent: RouteLoadingIndicator,
   beforeLoad: async ({ params }) => {
-    const { initTeamStore, teamSlug: currentSlug } = useStore.getState();
+    const { data } = await client.query<{ teams: Team[] }>({
+      query: gql`
+        query GetTeamBySlug($slug: String!) {
+          teams(
+            order_by: { name: asc }
+            limit: 1
+            where: { slug: { _eq: $slug } }
+          ) {
+            id
+            name
+            slug
+            domain
+            flows(order_by: { updated_at: desc }) {
+              slug
+              updated_at
+              operations(limit: 1, order_by: { id: desc }) {
+                actor {
+                  first_name
+                  last_name
+                }
+              }
+            }
+            integrations {
+              hasPlanningData: has_planning_data
+            }
+            settings: team_settings {
+              id
+              boundaryUrl: boundary_url
+              boundaryBBox: boundary_bbox
+              referenceCode: reference_code
+              helpEmail: help_email
+              helpPhone: help_phone
+              helpOpeningHours: help_opening_hours
+              emailReplyToId: email_reply_to_id
+              homepage: homepage
+              externalPlanningSiteName: external_planning_site_name
+              externalPlanningSiteUrl: external_planning_site_url
+              submissionEmail: submission_email
+              isTrial: is_trial
+            }
+          }
+        }
+      `,
+      variables: { slug: params.team },
+    });
 
-    if (currentSlug !== params.team) {
-      try {
-        await initTeamStore(params.team);
-      } catch (error) {
-        throw notFound();
-      }
+    const team = data.teams[0];
+
+    if (!team) {
+      throw notFound();
     }
+
+    return { team };
+  },
+  loader: ({ context }) => {
+    return context.team;
   },
   component: TeamLayout,
   notFoundComponent: CatchAllComponent,
 });
 
 function TeamLayout() {
+  const teamData = Route.useLoaderData();
+  const setTeam = useStore((state) => state.setTeam);
+
+  useEffect(() => {
+    if (!teamData) return;
+
+    setTeam(teamData);
+  }, [teamData, setTeam]);
+
   return <Outlet />;
 }
