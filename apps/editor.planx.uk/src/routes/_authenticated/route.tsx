24f5c9719a7015f9ff7efbe3e8f8abef493302fb
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-03-05 13:44:36 +0000

    fix(editor): Robots.txt information disclosure (#6270)

--- apps/editor.planx.uk/src/routes/_authenticated/route.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/route.tsx
@@ -4,4 +4,16 @@ import { validateDomain } from "./-loader";
 
 export const Route = createFileRoute("/_authenticated")({
   beforeLoad: validateDomain,
+  head: () => ({
+    meta: [
+      {
+        name: "robots",
+        content: "noindex, nofollow",
+      },
+      {
+        name: "googlebot",
+        content: "noindex, nofollow",
+      },
+    ],
+  }),
 });
