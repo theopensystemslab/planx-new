049babd38ef87f24f4599fe0d91478225d875bff
zz-hh-aa <zzhhaa@protonmail.com>
2026-03-12 15:28:21 +0000

    update queries by pk

--- apps/editor.planx.uk/src/@planx/components/Send/queries.tsx
+++ apps/editor.planx.uk/src/@planx/components/Send/queries.tsx
@@ -2,18 +2,23 @@ import { gql } from "@apollo/client";
 
 export const GET_FLOW_EMAIL_ID = gql`
   query GetFlowEmailId($flowId: uuid!) {
-    flows(where: { id: { _eq: $flowId } }) {
+    flowsByPK: flows_by_pk(id: $flowId) {
       submissionEmailId: submission_email_id
     }
   }
 `;
 
 export const UPDATE_FLOW_SUBMISSION_EMAIL_ID = gql`
-  mutation UpdateFlowSubmissionEmailId($flowId: uuid!, $submissionEmailId: uuid!) {
-    update_flows(where: {id: {_eq: $flowId}}, _set: {submission_email_id: $submissionEmailId}) {
-      returning {
-        submission_email_id
-      }
-    }
+  mutation UpdateFlowSubmissionEmailId(
+  $flowId: uuid!
+  $submissionEmailId: uuid!
+) {
+  update_flows_by_pk(
+    pk_columns: { id: $flowId }
+    _set: { submission_email_id: $submissionEmailId }
+  ) {
+    id
+    submission_email_id
   }
+}
 `;
