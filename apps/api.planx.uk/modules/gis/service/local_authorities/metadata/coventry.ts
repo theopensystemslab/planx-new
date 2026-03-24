a2a1201d7776fe7647cb14b7272320830cd73fea
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2026-03-24 10:51:01 +0000

    updated metadata

--- apps/api.planx.uk/modules/gis/service/local_authorities/metadata/coventry.ts
+++ apps/api.planx.uk/modules/gis/service/local_authorities/metadata/coventry.ts
@@ -1,16 +1,29 @@
-// Coventry A4s Metadata
-export const coventryMetadata = {
-    title: 'Coventry A4s Metadata',
-    description: 'Metadata for Coventry A4s, including attributes and data sources.',
-    attributes: [
-        'Name',
-        'Type',
-        'Location',
-        'Population',
-        'Area'
-    ],
-    dataSources: [
-        'Source1',
-        'Source2'
-    ]
+/*
+LAD20CD: E08000026
+LAD20NM: Coventry
+LAD20NMW:
+FID:
+
+https://www.planning.data.gov.uk/entity/?dataset=article-4-direction-area&geometry_curie=statistical-geography%3AE08000026&q=&entry_date_day=&entry_date_month=&entry_date_year=
+*/
+const planningConstraints = {
+  articleFour: {
+    // Planx granular values link to Digital Land entity.reference
+    records: {
+      "articleFour.coventry.stoneleighRoad": "A4/001/001",
+      "articleFour.coventry.landAtFletchampstead": "A4/002/001",
+      "articleFour.coventry.landBorderedByHawkesmill": "A4/003/001",
+      "articleFour.coventry.blackHorse": "AA4/004/001",
+      "articleFour.coventry.copsewoodGrange": "A4/005/001",
+      "articleFour.coventry.unitedReformChurch": "A4/006/001",
+      "articleFour.coventry.landAtSixTheFirs": "A4/007/001",
+      "articleFour.coventry.landAtTwoThreeBankHouse": "A4/009/001",
+      "articleFour.coventry.brownshillGreen": "A4/010/001",
+      "articleFour.coventry.earlsdon": "A4/011/001",
+      "articleFour.coventry.cheylesmore": "A4/012/001",
+      "articleFour.coventry.stNicholas": "A4/013/001",
+      "articleFour.coventry.ladyHerberts": "A4/014/001",
+    },
+  },
 };
+export { planningConstraints };
