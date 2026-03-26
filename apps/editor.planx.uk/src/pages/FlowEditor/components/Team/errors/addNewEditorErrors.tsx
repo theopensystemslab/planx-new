0c0b96c1a9643f527d3caa05f0f2df2ca70205d3
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-03-26 10:00:27 +0000

    test: Update tests to handle email field first

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Team/errors/addNewEditorErrors.tsx
+++
@@ -1,8 +0,0 @@
-export const AddNewMemberErrors = {
-  USER_ALREADY_EXISTS: {
-    regex: /violates unique constraint "users_email_key"/i,
-    errorMessage: "User already exists",
-  },
-};
-export const isUserAlreadyExistsError = (error: string) =>
-  AddNewMemberErrors.USER_ALREADY_EXISTS.regex.test(error);
