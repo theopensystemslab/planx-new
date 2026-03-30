186d6f141a05a8cd01394111139452b4397f5f28
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-03-30 14:04:15 +0100

    feat(editor): Handle draft route

--- apps/editor.planx.uk/src/routes/_public/_planXDomain/$team/$flow/draft/index.tsx
+++ apps/editor.planx.uk/src/routes/_public/_planXDomain/$team/$flow/draft/index.tsx
@@ -1,8 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 import SaveAndReturnLayout from "pages/layout/SaveAndReturnLayout";
-import Questions from "pages/Preview/Questions";
 import React from "react";
 import WatermarkBackground from "ui/shared/WatermarkBackground";
+
+import { FlattenedFlow } from "../-FlattenedFlow";
 
 export const Route = createFileRoute(
   "/_public/_planXDomain/$team/$flow/draft/",
@@ -18,7 +19,7 @@ function RouteComponent() {
         opacity={0.05}
         forceVisibility={true}
       />
-      <Questions previewEnvironment="standalone" />
+      <FlattenedFlow mode="draft"/>
     </SaveAndReturnLayout>
   );
 }
--- apps/editor.planx.uk/src/routes/_public/_planXDomain/$team/$flow/draft/route.tsx
+++ apps/editor.planx.uk/src/routes/_public/_planXDomain/$team/$flow/draft/route.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { zodValidator } from "@tanstack/zod-adapter";
-import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
+import { getFlattenedFlowData } from "lib/api/flow/requests";
+import { queryClient } from "lib/queryClient";
 import {
   createPublicRouteBeforeLoad,
   createPublicRouteErrorComponent,
@@ -13,6 +14,16 @@ export const Route = createFileRoute("/_public/_planXDomain/$team/$flow/draft")(
     validateSearch: zodValidator(publicRouteSearchSchemas.draft),
     beforeLoad: (args) =>
       createPublicRouteBeforeLoad("draft", args.context)(args),
+      loader: ({ context }) => {
+        queryClient.prefetchQuery({
+          queryKey: ["flattenedFlowData", "draft", context.flow.id],
+          queryFn: () =>
+            getFlattenedFlowData({
+              flowId: context.flow.id,
+              isDraft: true,
+            }),
+        })
+      },
     head: createPublicRouteHead("draft"),
     errorComponent: createPublicRouteErrorComponent("draft"),
   },
