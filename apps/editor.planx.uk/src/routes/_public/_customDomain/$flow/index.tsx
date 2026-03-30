b6ccff0da731b4e9c5e963b2cf6de25835434cb8
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-03-30 14:15:27 +0100

    feat: Handle custom subdomain routes

--- apps/editor.planx.uk/src/routes/_public/_customDomain/$flow/index.tsx
+++ apps/editor.planx.uk/src/routes/_public/_customDomain/$flow/index.tsx
@@ -1,17 +1,20 @@
 import { createFileRoute } from "@tanstack/react-router";
 import OfflineLayout from "pages/layout/OfflineLayout";
 import SaveAndReturnLayout from "pages/layout/SaveAndReturnLayout";
-import Questions from "pages/Preview/Questions";
 import React from "react";
+import { PublishedFlow } from "utils/routeUtils/PublishedFlow";
+
 export const Route = createFileRoute("/_public/_customDomain/$flow/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { flow } = Route.useRouteContext();
+
   return (
     <OfflineLayout>
       <SaveAndReturnLayout>
-        <Questions previewEnvironment="standalone" />
+        <PublishedFlow flowId={flow.id}/>
       </SaveAndReturnLayout>
     </OfflineLayout>
   );
