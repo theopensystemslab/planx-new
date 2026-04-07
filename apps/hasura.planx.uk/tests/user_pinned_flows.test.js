1e8ee7970384565c2230284a50dad01301928f22
Joe Carver <joecarver@live.co.uk>
2026-04-07 16:36:17 +0100

    feat: add tests for user_pinned_flows table

---
+++ apps/hasura.planx.uk/tests/user_pinned_flows.test.js
@@ -0,0 +1,148 @@
+const { introspectAs } = require("./utils");
+
+describe("user_pinned_flows", () => {
+  describe("public", () => {
+    let i;
+    beforeAll(async () => {
+      i = await introspectAs("public");
+    });
+
+    test("cannot query user_pinned_flows", () => {
+      expect(i.queries).not.toContain("user_pinned_flows");
+    });
+
+    test("cannot create, update, or delete user_pinned_flows", () => {
+      expect(i).toHaveNoMutationsFor("user_pinned_flows");
+    });
+  });
+
+  describe("admin", () => {
+    let i;
+    beforeAll(async () => {
+      i = await introspectAs("admin");
+    });
+
+    test("can query user_pinned_flows", () => {
+      expect(i.queries).toContain("user_pinned_flows");
+    });
+
+    test("can create user_pinned_flows", () => {
+      expect(i.mutations).toContain("insert_user_pinned_flows");
+    });
+
+    test("can delete user_pinned_flows", () => {
+      expect(i.mutations).toContain("delete_user_pinned_flows");
+      expect(i.mutations).toContain("delete_user_pinned_flows_by_pk");
+    });
+  });
+
+  describe("platformAdmin", () => {
+    let i;
+    beforeAll(async () => {
+      i = await introspectAs("platformAdmin");
+    });
+
+    // Row-level permissions tested in e2e/tests/api-driven
+    // platformAdmins can only query/mutate their own records
+    test("can query user_pinned_flows", () => {
+      expect(i.queries).toContain("user_pinned_flows");
+    });
+
+    test("can create user_pinned_flows", () => {
+      expect(i.mutations).toContain("insert_user_pinned_flows");
+    });
+
+    test("cannot update user_pinned_flows", () => {
+      expect(i.mutations).not.toContain("update_user_pinned_flows");
+      expect(i.mutations).not.toContain("update_user_pinned_flows_by_pk");
+    });
+
+    test("can delete user_pinned_flows", () => {
+      expect(i.mutations).toContain("delete_user_pinned_flows");
+    });
+  });
+
+  describe("teamEditor", () => {
+    let i;
+    beforeAll(async () => {
+      i = await introspectAs("teamEditor");
+    });
+
+    // Row-level permissions tested in e2e/tests/api-driven
+    // teamEditors can only query/mutate their own records
+    test("can query user_pinned_flows", () => {
+      expect(i.queries).toContain("user_pinned_flows");
+    });
+
+    test("can create user_pinned_flows", () => {
+      expect(i.mutations).toContain("insert_user_pinned_flows");
+    });
+
+    test("cannot update user_pinned_flows", () => {
+      expect(i.mutations).not.toContain("update_user_pinned_flows");
+      expect(i.mutations).not.toContain("update_user_pinned_flows_by_pk");
+    });
+
+    test("can delete user_pinned_flows", () => {
+      expect(i.mutations).toContain("delete_user_pinned_flows");
+    });
+  });
+
+  describe("demoUser", () => {
+    let i;
+    beforeAll(async () => {
+      i = await introspectAs("demoUser");
+    });
+
+    // Row-level permissions tested in e2e/tests/api-driven
+    // demoUsers can only query/mutate their own records
+    test("can query user_pinned_flows", () => {
+      expect(i.queries).toContain("user_pinned_flows");
+    });
+
+    test("can create user_pinned_flows", () => {
+      expect(i.mutations).toContain("insert_user_pinned_flows");
+    });
+
+    test("cannot update user_pinned_flows", () => {
+      expect(i.mutations).not.toContain("update_user_pinned_flows");
+      expect(i.mutations).not.toContain("update_user_pinned_flows_by_pk");
+    });
+
+    test("can delete user_pinned_flows", () => {
+      expect(i.mutations).toContain("delete_user_pinned_flows");
+    });
+  });
+
+  describe("analyst", () => {
+    let i;
+    beforeAll(async () => {
+      i = await introspectAs("analyst");
+    });
+
+    // Row-level permissions tested in e2e/tests/api-driven
+    // analysts can only query their own records
+    test("can query user_pinned_flows", () => {
+      expect(i.queries).toContain("user_pinned_flows");
+    });
+
+    test("cannot create, update, or delete user_pinned_flows", () => {
+      expect(i).toHaveNoMutationsFor("user_pinned_flows");
+    });
+  });
+
+  describe("api", () => {
+    let i;
+    beforeAll(async () => {
+      i = await introspectAs("api");
+    });
+
+    test("cannot query user_pinned_flows", () => {
+      expect(i.queries).not.toContain("user_pinned_flows");
+    });
+
+    test("cannot create, update, or delete user_pinned_flows", () => {
+      expect(i).toHaveNoMutationsFor("user_pinned_flows");
+    });
+  });
+});
