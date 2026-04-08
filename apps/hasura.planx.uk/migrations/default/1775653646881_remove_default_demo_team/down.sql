e76f07b502c851ca34b647e653eccdd407ee1ff8
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-04-08 09:10:08 -0400

    wip

---
+++ apps/hasura.planx.uk/migrations/default/1775653646881_remove_default_demo_team/down.sql
@@ -0,0 +1,5 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- UPDATE users
+-- SET default_team_id = NULL
+-- WHERE default_team_id = 32;
