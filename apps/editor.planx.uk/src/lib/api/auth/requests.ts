2509a1c8044753f06eeae5577080a23d00c7c339
Joe Carver <joecarver@live.co.uk>
2026-04-09 15:23:09 +0100

    fix: pass credentials (jwt) to api logout endpoint

--- apps/editor.planx.uk/src/lib/api/auth/requests.ts
+++ apps/editor.planx.uk/src/lib/api/auth/requests.ts
@@ -8,4 +8,7 @@ export const getUser = async () => {
   return data;
 };
 
-export const logout = async () => await apiClient.post("auth/logout");
+export const logout = async () =>
+  await apiClient.post("auth/logout", undefined, {
+    withCredentials: true,
+  });
