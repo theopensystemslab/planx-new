e3d5e8d85c57817f89b5f9605f027901adc47057
zz-hh-aa <zzhhaa@protonmail.com>
2026-04-15 11:40:10 +0100

    delete functionality

--- apps/editor.planx.uk/src/pages/Team/components/ArchivedFlowMenu.tsx
+++ apps/editor.planx.uk/src/pages/Team/components/ArchivedFlowMenu.tsx
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 import SimpleMenu from "ui/editor/SimpleMenu";
 
 import { ArchiveDialog } from "./ArchiveDialog";
+import { useDeleteFlow } from "./hooks/useDeleteFlow";
 import { useUnarchiveFlow } from "./hooks/useUnarchiveFlow";
 import { FlowMenuProps } from "./StyledSimpleMenu";
 import { StyledSimpleMenu } from "./StyledSimpleMenu";
@@ -15,12 +16,16 @@ const ArchivedFlowMenu: React.FC<FlowMenuProps> = ({
   variant = "card",
   teamId,
 }) => {
-  type OpenFlowDialog = "unarchive";
+  type OpenFlowDialog = "unarchive" | "delete";
   const [openFlowDialog, setOpenFlowDialog] = useState<OpenFlowDialog | null>(
     null,
   );
+    
   const unarchivedSlug = flow.slug.replace("-archive", "");
   const [unarchiveFlow] = useUnarchiveFlow(flow.id, unarchivedSlug, teamId);
+    
+  const deletedSlug = flow.slug.replace("-archive", "-deleted");
+  const [deleteFlow] = useDeleteFlow(flow.id, deletedSlug, teamId);
 
   const toast = useToast();
 
@@ -40,13 +45,30 @@ const ArchivedFlowMenu: React.FC<FlowMenuProps> = ({
       setOpenFlowDialog(null);
     }
   };
+    
+  const handleDelete = async () => {
+  try {
+      await deleteFlow();
+      toast.success("Deleted flow");
+  } catch (error) {
+      toast.error(
+      "We are unable to delete this flow, refesh and try again or contact an admin",
+      );
+  } finally {
+      setOpenFlowDialog(null);
+  }
+  };
 
-  const menuItems = [
-    {
+    const menuItems = [
+      {
       label: "Unarchive",
       onClick: () => setOpenFlowDialog("unarchive"),
-    },
-  ];
+      },
+      {
+      label: "Delete",
+      onClick: () => setOpenFlowDialog("delete"),
+      },
+    ];
 
   return (
     <>
@@ -58,6 +80,16 @@ const ArchivedFlowMenu: React.FC<FlowMenuProps> = ({
           handleClose={handleClose}
           onConfirm={handleUnarchive}
           submitLabel="Unarchive this flow"
+        />
+      )}
+      {openFlowDialog === "delete" && (
+        <ArchiveDialog
+          title={`Delete "${flow.name}"`}
+          open={openFlowDialog === "delete"}
+          content={`Are you sure you want to delete "${flow.name}"? You will no longer be able to restore it from the archive.`}
+          handleClose={handleClose}
+          onConfirm={handleDelete}
+          submitLabel="Delete this flow"
         />
       )}
 
