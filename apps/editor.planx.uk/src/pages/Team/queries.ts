2668ece10b414ea34f677244a17f59613a240115
zz-hh-aa <zzhhaa@protonmail.com>
2026-04-15 16:32:50 +0100

    unarchive flow query and hook

--- apps/editor.planx.uk/src/pages/Team/queries.ts
+++ apps/editor.planx.uk/src/pages/Team/queries.ts
@@ -51,3 +51,22 @@ export type GetArchivedFlowsQuery = {
 export type GetArchivedFlowsVars = {
   teamId: number;
 };
+
+export const UNARCHIVE_FLOW = gql`
+  mutation UnarchiveFlow($id: uuid!, $slug: String!) {
+    flow: update_flows_by_pk(pk_columns: {id: $id}, _set: {archived_at: null, slug: $slug}) {
+      id
+    }
+  }
+`
+
+export type FlowStatusMutation = {
+  flow: {
+    id: string;
+  }
+}
+
+export type FlowStatusMutationVars = {
+  id: string; 
+  slug: string;
+}
