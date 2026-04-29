d299bb5e367dc1a97c99163a6656be9157ee15d9
Joe Carver <joecarver@live.co.uk>
2026-04-29 11:01:30 +0100

    add an id to the Checkicon for template nodes

--- apps/editor.planx.uk/src/ui/editor/TemplatedNodeContainer.tsx
+++ apps/editor.planx.uk/src/ui/editor/TemplatedNodeContainer.tsx
@@ -124,6 +124,7 @@ export const TemplatedNodeContainer: React.FC<TemplatedNodeContainerProps> = ({
           </Typography>
           {isComplete && (
             <CheckCircleIcon
+              id="customisation-complete"
               fontSize="small"
               sx={(theme) => ({ color: theme.palette.success.main })}
             />
