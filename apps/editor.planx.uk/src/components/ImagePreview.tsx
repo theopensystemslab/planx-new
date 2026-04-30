214274a7bfd26cd1d038e5e0c8471a5550ae8f4b
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-04-30 12:43:35 +0100

    fix: src='' warning

--- apps/editor.planx.uk/src/components/ImagePreview.tsx
+++ apps/editor.planx.uk/src/components/ImagePreview.tsx
@@ -4,5 +4,7 @@ import React from "react";
 export default function ImagePreview(props: UseFileUrlProps) {
   const { fileUrl } = useFileUrl(props);
 
+  if (!fileUrl) return null;
+
   return <img src={fileUrl} alt={`Preview of uploaded file: ${fileUrl}`} />;
 }
