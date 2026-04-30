95ac5b0ddf4110d70020c1cc584783446523c15d
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2026-04-30 14:30:08 +0100

    chore: add Stockport A4 metadata (#6556)

---
+++ apps/api.planx.uk/modules/gis/service/local_authorities/metadata/stockport.ts
@@ -0,0 +1,48 @@
+/*
+LAD20CD: 
+LAD20NM: Stockport Metropolitan Borough
+LAD20NMW:
+FID:
+
+*/
+
+import type { LocalAuthorityMetadata } from "../../digitalLand.js";
+
+const planningConstraints: LocalAuthorityMetadata["planningConstraints"] = {
+  articleFour: {
+    records: {
+      "articleFour.stockport.a41": "A4-1",
+      "articleFour.stockport.a42": "A4-2",
+      "articleFour.stockport.a43": "A4-3",
+      "articleFour.stockport.a44": "A4-4",
+      "articleFour.stockport.a45": "A4-5",
+      "articleFour.stockport.a46": "A4-6",
+      "articleFour.stockport.a47": "A4-7",
+      "articleFour.stockport.a48": "A4-8",
+      "articleFour.stockport.a49": "A4-9",
+      "articleFour.stockport.a410": "A4-10",
+      "articleFour.stockport.a411": "A4-11",
+      "articleFour.stockport.a412": "A4-12",
+      "articleFour.stockport.a413": "A4-13",
+      "articleFour.stockport.a414": "A4-14",
+      "articleFour.stockport.a415": "A4-15",
+      "articleFour.stockport.a416": "A4-16",
+      "articleFour.stockport.a417": "A4-17",
+      "articleFour.stockport.a418": "A4-18",
+      "articleFour.stockport.a419": "A4-19",
+      "articleFour.stockport.a420": "A4-20",
+      "articleFour.stockport.a421": "A4-21",
+      "articleFour.stockport.a422": "A4-22",
+      "articleFour.stockport.a423": "A4-23",
+      "articleFour.stockport.a424": "A4-24",
+      "articleFour.stockport.a425": "A4-25",
+      "articleFour.stockport.a426": "A4-26",
+      "articleFour.stockport.a427": "A4-27",
+      "articleFour.stockport.a428": "A4-28",
+      "articleFour.stockport.a430": "A4-30",
+      "articleFour.stockport.a431": "A4-31",
+    },
+  },
+};
+
+export { planningConstraints };
