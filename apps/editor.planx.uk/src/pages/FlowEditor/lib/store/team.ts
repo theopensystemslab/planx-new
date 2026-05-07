8b258802d5896a210d0dea4c3b49d262c6dd01b2
Ian Jones <hello@ianjo.uk>
2026-05-07 12:06:38 +0100

    reduce favicon radius

--- apps/editor.planx.uk/src/pages/FlowEditor/lib/store/team.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/lib/store/team.ts
@@ -28,7 +28,7 @@ export interface TeamStore {
 }
 
 const generateCircleFavicon = (color: string): string => {
-  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><circle cx="16" cy="16" r="16" fill="${color}"/></svg>`;
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><circle cx="16" cy="16" r="14" fill="${color}"/></svg>`;
   return `data:image/svg+xml,${encodeURIComponent(svg)}`;
 };
 
