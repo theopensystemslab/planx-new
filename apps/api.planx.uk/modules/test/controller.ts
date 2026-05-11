2e4d39278eccc60bfea567ed49e983c5e950a3af
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-05-11 14:59:14 +0100

    test: Temp endpoint to throw unhandled error

--- apps/api.planx.uk/modules/test/controller.ts
+++ apps/api.planx.uk/modules/test/controller.ts
@@ -5,3 +5,7 @@ export const testSessionMethods: RequestHandler = (req, res) => {
   const hasSave = typeof req.session?.save === "function";
   res.status(200).json({ hasRegenerate, hasSave });
 };
+
+export const testAirbrake: RequestHandler = (_req, _res, next) => {
+  next(new Error("Test Airbrake error - checking source maps are working"));
+};
--- apps/api.planx.uk/modules/test/routes.ts
+++ apps/api.planx.uk/modules/test/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { testSessionMethods } from "./controller.js";
+import { testAirbrake, testSessionMethods } from "./controller.js";
 
 const router = Router();
 
 // in order to test the session setup, we need a dedicated test route
 router.get("/test-session", testSessionMethods);
+
+// trigger a test error to verify Airbrake source maps are working
+router.get("/test-airbrake", testAirbrake);
 
 export default router;
