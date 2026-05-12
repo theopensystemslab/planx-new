07ab513eaa0b0c399acb81f5ee67b03607c9ece4
Joe Carver <joecarver@live.co.uk>
2026-05-12 13:09:42 +0100

    install dependencies exit early if build found

--- e2e/tests/ui-driven/install-dependencies.sh
+++ e2e/tests/ui-driven/install-dependencies.sh
@@ -3,10 +3,12 @@ set -e
 
 cd $(dirname "$0")
 
+if [ -d "../../../apps/editor.planx.uk/build" ]; then
+  exit 0
+fi
+
 source ../../../.env
 
-if [ ! -d "../../../apps/editor.planx.uk/build" ]; then
-  cd ../../../apps/editor.planx.uk
-  pnpm install --frozen-lockfile
-  VITE_APP_ENV=test pnpm build
-fi
+cd ../../../apps/editor.planx.uk
+pnpm install --frozen-lockfile
+VITE_APP_ENV=test pnpm build
