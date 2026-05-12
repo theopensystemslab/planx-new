01b1e4af6dfaa707158a424cf5a2c3201c1bbfaa
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-05-12 09:30:58 +0100

    chore: Drop testing endpoint

--- apps/api.planx.uk/modules/test/controller.ts
+++ apps/api.planx.uk/modules/test/controller.ts
@@ -5,7 +5,3 @@ export const testSessionMethods: RequestHandler = (req, res) => {
   const hasSave = typeof req.session?.save === "function";
   res.status(200).json({ hasRegenerate, hasSave });
 };
-
-export const testAirbrake: RequestHandler = (_req, _res, next) => {
-  next(new Error("Test Airbrake error - checking source maps are working"));
-};
--- apps/api.planx.uk/modules/test/routes.ts
+++ apps/api.planx.uk/modules/test/routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import { testAirbrake, testSessionMethods } from "./controller.js";
+import { testSessionMethods } from "./controller.js";
 
 const router = Router();
 
 // in order to test the session setup, we need a dedicated test route
 router.get("/test-session", testSessionMethods);
-
-// trigger a test error to verify Airbrake source maps are working
-router.get("/test-airbrake", testAirbrake);
 
 export default router;
