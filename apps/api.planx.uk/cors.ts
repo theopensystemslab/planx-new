2d26b709efe779301f27e2f2ce295a931bd24dc6
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-05-13 14:48:59 +0100

    fix(api): Supress CORS error logging

--- apps/api.planx.uk/cors.ts
+++ apps/api.planx.uk/cors.ts
@@ -15,7 +15,7 @@ const checkAllowedOrigins: CorsOptions["origin"] = (origin, callback) => {
 
   isTest || isDevelopment || isAllowed || isMapDocs
     ? callback(null, true)
-    : callback(new Error(`Not allowed by CORS. Origin: ${origin}`));
+    : callback(null, false);
 };
 
 const apiCors = cors({
