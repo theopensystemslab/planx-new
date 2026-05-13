822d0cd9885b157aff57a9dacbdaf2db19e68238
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-05-13 16:55:34 +0100

    fix(api): Supress missing JWT errors

--- apps/api.planx.uk/errors/requestHandlers.ts
+++ apps/api.planx.uk/errors/requestHandlers.ts
@@ -22,6 +22,34 @@ export const expiredJWTHandler: ErrorRequestHandler = (
 };
 
 /**
+ * Handle missing JWTs from autheneticated routes - likely requests from bots, frontend errors, or frontend race conditions
+ * Log request context for debugging but do not send to Airbrake
+ */
+export const noTokenHandler: ErrorRequestHandler = (
+  errorObject,
+  req,
+  res,
+  next,
+) => {
+  const isMissingTokenError =
+    errorObject?.name === "UnauthorizedError" &&
+    errorObject?.message === "No authorization token was found";
+
+  if (!isMissingTokenError) return next(errorObject);
+
+  console.debug({
+    message: "Request missing auth token",
+    method: req.method,
+    path: req.path,
+    userAgent: req.headers["user-agent"],
+    referer: req.headers["referer"],
+    ip: req.ip,
+  });
+
+  return res.status(401).send({ error: "No authorization token was found" });
+};
+
+/**
  * Fallback error handler
  * Must be final Express middleware
  */
