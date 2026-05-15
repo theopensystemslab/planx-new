b3d605348e1878c44792a49a83c00fb5c24fa2d3
Joe Carver <joecarver@live.co.uk>
2026-05-15 09:40:37 +0100

    test change which should not trigger editor re-build

--- e2e/tests/ui-driven/install-dependencies.sh
+++ e2e/tests/ui-driven/install-dependencies.sh
@@ -3,6 +3,8 @@ set -e
 
 cd $(dirname "$0")
 
+# test change for editor.planx.uk build hash
+
 EDITOR_HASH=$(git -C ../../../ rev-parse HEAD:apps/editor.planx.uk)
 HASH_FILE="../../../apps/editor.planx.uk/build/.editor-hash"
 
