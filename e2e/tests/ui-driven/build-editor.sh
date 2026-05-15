ec1b9d47c577e11d74cbde7b2babf72cc939b832
joecarver <joe@opensystemslab.io>
2026-05-15 11:54:36 +0100

    ci: optimise editor re-builds for e2e tests (#6687)

---
+++ e2e/tests/ui-driven/build-editor.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# test change for editor.planx.uk build hash
+
+cd $(dirname "$0")
+
+EDITOR_HASH=$(git -C ../../../ rev-parse HEAD:apps/editor.planx.uk)
+HASH_FILE="../../../apps/editor.planx.uk/build/.editor-hash"
+
+if [ -f "$HASH_FILE" ] && [ "$(cat $HASH_FILE)" = "$EDITOR_HASH" ]; then
+  exit 0
+fi
+
+source ../../../.env
+
+cd ../../../apps/editor.planx.uk
+pnpm install --frozen-lockfile
+VITE_APP_ENV=test pnpm build
+echo "$EDITOR_HASH" > build/.editor-hash
