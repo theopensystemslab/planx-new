fde769d1c35af325eb1fe3bdd9fe1530dd394037
Joe Carver <joecarver@live.co.uk>
2026-05-15 09:22:55 +0100

    editor hash caching across different commits

--- e2e/tests/ui-driven/install-dependencies.sh
+++ e2e/tests/ui-driven/install-dependencies.sh
@@ -3,7 +3,10 @@ set -e
 
 cd $(dirname "$0")
 
-if [ -d "../../../apps/editor.planx.uk/build" ]; then
+EDITOR_HASH=$(git -C ../../../ rev-parse HEAD:apps/editor.planx.uk)
+HASH_FILE="../../../apps/editor.planx.uk/build/.editor-hash"
+
+if [ -f "$HASH_FILE" ] && [ "$(cat $HASH_FILE)" = "$EDITOR_HASH" ]; then
   exit 0
 fi
 
@@ -12,3 +15,4 @@ source ../../../.env
 cd ../../../apps/editor.planx.uk
 pnpm install --frozen-lockfile
 VITE_APP_ENV=test pnpm build
+echo "$EDITOR_HASH" > build/.editor-hash
