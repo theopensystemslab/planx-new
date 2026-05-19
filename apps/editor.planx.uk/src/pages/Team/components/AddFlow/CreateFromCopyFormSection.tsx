6d69f1392fdd6bc74de1fc3ed1ee15ad903624b7
zz-hh-aa <zzhhaa@protonmail.com>
2026-05-19 10:41:36 +0100

    ensure archived flows are not copiable

--- apps/editor.planx.uk/src/pages/Team/components/AddFlow/CreateFromCopyFormSection.tsx
+++ apps/editor.planx.uk/src/pages/Team/components/AddFlow/CreateFromCopyFormSection.tsx
@@ -1,4 +1,3 @@
-import { gql, useQuery } from "@apollo/client";
 import MenuItem from "@mui/material/MenuItem";
 import { SelectChangeEvent } from "@mui/material/Select";
 import { useFormikContext } from "formik";
@@ -7,33 +6,13 @@ import InputLabel from "ui/public/InputLabel";
 import SelectInput from "ui/shared/SelectInput/SelectInput";
 import { slugify } from "utils";
 
+import { useGetCopiableFlows } from "./hooks/useGetCopiableFlows";
 import { CreateFlow } from "./types";
-
-interface CopiableFlow {
-  id: string;
-  slug: string;
-  name: string;
-  team: { name: string };
-}
 
 export const CreateFromCopyFormSection: React.FC = () => {
   const { values, setFieldValue } = useFormikContext<CreateFlow>();
 
-  const { data } = useQuery<{ copiableFlows: CopiableFlow[] }>(gql`
-    query GetCopiableFlows {
-      copiableFlows: flows(
-        where: { can_create_from_copy: { _eq: true } }
-        order_by: { team: { name: asc }, name: asc }
-      ) {
-        id
-        slug
-        name
-        team {
-          name
-        }
-      }
-    }
-  `);
+  const { data } = useGetCopiableFlows();
 
   if (values.mode !== "copy" || !data?.copiableFlows?.length) return null;
 
---
+++ apps/editor.planx.uk/src/pages/Team/components/AddFlow/hooks/useGetCopiableFlows.ts
@@ -0,0 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_COPIABLE_FLOWS = gql`
+  query GetCopiableFlows {
+    copiableFlows: flows(
+      where: {
+        can_create_from_copy: { _eq: true }
+        archived_at: { _is_null: true }
+      }
+      order_by: { team: { name: asc }, name: asc }
+    ) {
+      id
+      slug
+      name
+      team {
+        name
+      }
+    }
+  }
+`;
+
+interface GetCopiableFlows {
+  copiableFlows: {
+    id: string;
+    slug: string;
+    name: string;
+    team: { name: string };
+  }[];
+}
+
+type GetCopiableFlowsVars = Record<string, never>;
+
+export const useGetCopiableFlows = () => {
+  return useQuery<GetCopiableFlows, GetCopiableFlowsVars>(
+    GET_COPIABLE_FLOWS,
+    {},
+  );
+};
