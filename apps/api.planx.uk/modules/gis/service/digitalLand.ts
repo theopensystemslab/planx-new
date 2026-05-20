f51f8d87a769dae18a3b21b8af8454446b0c9321
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2026-05-20 14:54:01 +0100

    add Liverpool metadata

--- apps/api.planx.uk/modules/gis/service/digitalLand.ts
+++ apps/api.planx.uk/modules/gis/service/digitalLand.ts
@@ -27,6 +27,7 @@ import * as gloucester from "./local_authorities/metadata/gloucester.js";
 import * as greaterCambridge from "./local_authorities/metadata/greaterCambridge.js";
 import * as horsham from "./local_authorities/metadata/horsham.js";
 import * as lambeth from "./local_authorities/metadata/lambeth.js";
+import * as liverpool from "./local_authorities/metadata/liverpool.js";
 import * as medway from "./local_authorities/metadata/medway.js";
 import * as newcastle from "./local_authorities/metadata/newcastle.js";
 import * as northumberland from "./local_authorities/metadata/northumberland.js";
@@ -70,6 +71,7 @@ export const localAuthorityMetadata: Record<string, LocalAuthorityMetadata> = {
   "greater-cambridge-shared-planning": greaterCambridge,
   horsham,
   lambeth,
+  liverpool,
   medway,
   newcastle,
   northumberland,
