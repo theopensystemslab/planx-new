4d1e5cef86363522ccc70c9a145826ac573e5a41
zz-hh-aa <zzhhaa@protonmail.com>
2026-05-26 10:52:48 +0100

    nit

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/Visibility/IsService/queries.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Flow/Visibility/IsService/queries.ts
@@ -1,48 +1,42 @@
-import { useMutation,useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 
-import { GetIsServiceResponse, IsServiceVars } from "./types"
+import { GetIsServiceResponse, IsServiceVars } from "./types";
 
 export const GET_IS_SERVICE = gql`
-query GetIsService($flowId: uuid!) {
-  flow: flows_by_pk(id: $flowId) {
-    isService: is_service
-    team {
-      settings: team_settings {
-        isTrial: is_trial
+  query GetIsService($flowId: uuid!) {
+    flow: flows_by_pk(id: $flowId) {
+      isService: is_service
+      team {
+        settings: team_settings {
+          isTrial: is_trial
+        }
       }
     }
   }
-}
-
 `;
 
 export const UPDATE_IS_SERVICE = gql`
   mutation SetIsService($flowId: uuid!) {
-    update_flows_by_pk(pk_columns: {id: $flowId}, _set: {is_service: true}) {
+    update_flows_by_pk(
+      pk_columns: { id: $flowId }
+      _set: { is_service: true }
+    ) {
       id
       isService: is_service
     }
   }
 `;
 
-
 export const useGetIsService = (flowId: string) => {
-  return useQuery<GetIsServiceResponse, IsServiceVars>(
-    GET_IS_SERVICE,
-    {
-      variables: { flowId },
-    }
-  );
+  return useQuery<GetIsServiceResponse, IsServiceVars>(GET_IS_SERVICE, {
+    variables: { flowId },
+  });
 };
 
 export const useUpdateIsService = (flowId: string) => {
-  return useMutation<GetIsServiceResponse, IsServiceVars>(
-    UPDATE_IS_SERVICE,
-    {
-      variables: { flowId },
-      refetchQueries: [{ query: GET_IS_SERVICE, variables: { flowId } }],
-    }
-  );
+  return useMutation<GetIsServiceResponse, IsServiceVars>(UPDATE_IS_SERVICE, {
+    variables: { flowId },
+    refetchQueries: [{ query: GET_IS_SERVICE, variables: { flowId } }],
+  });
 };
-
