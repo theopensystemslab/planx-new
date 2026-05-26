b4a4f9a326dec6b889ba531f5ea60ea9852cce7f
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-05-26 12:11:16 -0400

    fix: camelCase team slug for A4 `.caz` data fields (#6731)

--- apps/api.planx.uk/modules/gis/service/digitalLand.ts
+++ apps/api.planx.uk/modules/gis/service/digitalLand.ts
@@ -7,8 +7,9 @@ import {
 } from "@opensystemslab/planx-core/types";
 import { gql } from "graphql-request";
 import fetch from "isomorphic-fetch";
-import { addDesignatedVariable } from "./helpers.js";
+import camelCase from "lodash/camelCase.js";
 import { $api } from "../../../client/index.js";
+import { addDesignatedVariable } from "./helpers.js";
 
 import * as adurAndWorthing from "./local_authorities/metadata/adurAndWorthing.js";
 import * as barkingAndDagenham from "./local_authorities/metadata/barkingAndDagenham.js";
@@ -323,15 +324,8 @@ async function go(
     }
 
     // rename `articleFour.caz` to reflect localAuthority if applicable, ensuring `councilName` segment matches other A4 values (not always same as team-slug)
-    const customTeamSlugs: Record<string, string> = {
-      "barking-and-dagenham": "barkingAndDagenham",
-      "epsom-and-ewell": "epsomAndEwell",
-      "st-albans": "stAlbans",
-      "west-berkshire": "westBerkshire",
-    };
-    const localCaz = Object.keys(customTeamSlugs).includes(localAuthority)
-      ? `articleFour.${customTeamSlugs[localAuthority]}.caz`
-      : `articleFour.${localAuthority}.caz`;
+    const localCaz = `articleFour.${localAuthority.includes("-") ? camelCase(localAuthority) : localAuthority}.caz`;
+
     if (formattedResult["articleFour.caz"]) {
       formattedResult[localCaz] = formattedResult["articleFour.caz"];
       delete formattedResult["articleFour.caz"];
