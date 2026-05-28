07a66590f5cbbf32629a19c40e8385e7aad23d1d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-05-28 19:06:02 +0100

    fix(editor): Guard ShareDB connection using unique flow ID, not shared flow slug (#6757)

--- apps/editor.planx.uk/src/routes/_authenticated/app/$team/$flow/route.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/app/$team/$flow/route.tsx
@@ -1,22 +1,16 @@
 import {
   createFileRoute,
-  notFound,
   Outlet,
   stripSearchParams,
 } from "@tanstack/react-router";
 import { zodValidator } from "@tanstack/zod-adapter";
 import ErrorFallback from "components/Error/ErrorFallback";
 import FlowSkeleton from "pages/FlowEditor/FlowSkeleton";
-import React from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { CatchAllComponent } from "routes/$";
-import {
-  getBasicFlowData,
-  getFlowEditorData,
-} from "utils/routeUtils/queryUtils";
 
-import { useStore } from "../../../../../pages/FlowEditor/lib/store";
 import { teamSearchSchema } from "..";
+import { connectToFlowRoute } from "./-route.utils";
 
 export const Route = createFileRoute("/_authenticated/app/$team/$flow")({
   pendingComponent: FlowSkeleton,
@@ -38,44 +32,8 @@ export const Route = createFileRoute("/_authenticated/app/$team/$flow")({
 
     return { rootFlow, folderIds };
   },
-  beforeLoad: async ({ params, context: { rootFlow } }) => {
-    const { team: teamSlug } = params;
-    const store = useStore.getState();
-
-    // Don't re-connect to document when navigating to nested flows
-    if (store.flowSlug === rootFlow) return;
-
-    try {
-      // Ensure we only have a single active connection
-      store.disconnectFromFlow();
-
-      const flow = await getBasicFlowData(rootFlow, teamSlug);
-      await store.connectToFlow(flow.id);
-      useStore.setState({
-        flowName: flow.name,
-        flowSlug: rootFlow,
-      });
-
-      const flowEditorData = await getFlowEditorData(rootFlow, teamSlug);
-
-      useStore.setState({
-        id: flowEditorData.id,
-        flowStatus: flowEditorData.flowStatus,
-        isFlowPublished: flowEditorData.isFlowPublished,
-        isTemplate: flowEditorData.isTemplate,
-        isTemplatedFrom: Boolean(flowEditorData.templatedFrom),
-        template: flowEditorData.template,
-      });
-
-      if (flowEditorData.templatedFrom) {
-        store.setOrderedFlow();
-      }
-
-      store.getFlowInformation(rootFlow, teamSlug);
-    } catch (error) {
-      throw notFound();
-    }
-  },
+  beforeLoad: async ({ params: { team }, context: { rootFlow } }) =>
+    connectToFlowRoute(team, rootFlow),
   component: RouteComponent,
   notFoundComponent: CatchAllComponent,
 });
