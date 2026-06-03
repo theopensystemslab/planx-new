97b1da8e0e90ac8262a0e36eaf6b430a83af4df0
Joe Carver <joecarver@live.co.uk>
2026-06-03 08:31:17 +0100

    fix: use storybook/tanstack-react

--- apps/editor.planx.uk/src/pages/Dashboard/components/NotificationsWidget.stories.tsx
+++ apps/editor.planx.uk/src/pages/Dashboard/components/NotificationsWidget.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/tanstack-react";
 import { Notification } from "pages/FlowEditor/components/Notifications/types";
 
 import { NotificationsWidget } from "./NotificationsWidget";
