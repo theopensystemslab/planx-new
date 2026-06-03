aaea6e8b3c155bc9f72e8aca8d3f354db0a09f06
Joe Carver <joecarver@live.co.uk>
2026-06-03 13:07:21 +0100

    test: add test for option data fields being cleared

--- apps/editor.planx.uk/src/@planx/components/Question/Editor.test.tsx
+++ apps/editor.planx.uk/src/@planx/components/Question/Editor.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen, waitFor } from "@testing-library/react";
+import { fireEvent, screen, waitFor, within } from "@testing-library/react";
 import React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
@@ -26,6 +26,42 @@ it("displays the options editor when the 'Add option' button is clicked", async 
 
   const optionsEditor = await screen.findByPlaceholderText("Option");
   expect(optionsEditor).toBeInTheDocument();
+});
+
+describe("data field", () => {
+  it("clears option val fields when the question data field is cleared", async () => {
+    const { user } = await setup(
+      <DndProvider backend={HTML5Backend}>
+        <Question
+          node={{ data: { fn: "my.data.field", text: "A question" } }}
+          options={[
+            { id: "opt1", data: { text: "Yes", val: "yes" } },
+            { id: "opt2", data: { text: "No", val: "no" } },
+          ]}
+        />
+      </DndProvider>,
+    );
+
+    const questionDataField = screen.getByTestId("question-data-field");
+
+    await user.click(within(questionDataField).getByTitle("Clear"));
+
+    // Set a new fn value so option val fields become visible again
+    await user.click(within(questionDataField).getByRole("combobox"));
+    await user.paste("new.data.field");
+    await user.keyboard("{Enter}");
+
+    expect(
+      within(screen.getByTestId("data-field-autocomplete-option-0")).getByRole(
+        "combobox",
+      ),
+    ).toHaveValue("");
+    expect(
+      within(screen.getByTestId("data-field-autocomplete-option-1")).getByRole(
+        "combobox",
+      ),
+    ).toHaveValue("");
+  });
 });
 
 describe("validation", () => {
