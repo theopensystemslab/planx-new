1266692f164a3e760d2e826fbe695f7b60219232
zz-hh-aa <zzhhaa@protonmail.com>
2026-06-04 16:53:43 +0100

    nit

--- apps/editor.planx.uk/src/pages/Team/components/AddFlow/index.tsx
+++ apps/editor.planx.uk/src/pages/Team/components/AddFlow/index.tsx
@@ -46,7 +46,6 @@ export const AddFlow: React.FC = () => {
     values,
     { setFieldError, setStatus },
   ) => {
-    console.log({ values });
     setLoadingCompleteCallback(() => {
       toast.success("Flow created successfully");
       setLoadingCompleteCallback(undefined);
