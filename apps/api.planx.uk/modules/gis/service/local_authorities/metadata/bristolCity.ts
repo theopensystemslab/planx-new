5a825536f6341cdd6249835f2ee2a97e9855687a
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2026-06-04 17:27:18 +0100

    chore: add Bristol City a4 metadata (#6812)

---
+++ apps/api.planx.uk/modules/gis/service/local_authorities/metadata/bristolCity.ts
@@ -0,0 +1,32 @@
+/*
+LAD20NM: Bristol City
+LAD20CD: E06000023
+https://docs.google.com/spreadsheets/d/1dU584u4k8bY_R3VaPIBHB6gOErtjm95F8kqc0pHnMS0/edit?gid=0#gid=0
+*/
+
+import type { LocalAuthorityMetadata } from "../../digitalLand.js";
+
+const planningConstraints: LocalAuthorityMetadata["planningConstraints"] = {
+  articleFour: {
+    records: {
+      "articleFour.bristolCity.cliftonVillage.westMall": "A4D1",
+      "articleFour.bristolCity.cliftonVillage.cornwallis": "A4D2",
+      "articleFour.bristolCity.cliftonVillage.caledonia": "A4D3",
+      "articleFour.bristolCity.cliftonVillage.royalYork": "A4D4",
+      "articleFour.bristolCity.north": "A4D5",
+      "articleFour.bristolCity.east": "A4D6",
+      "articleFour.bristolCity.south": "A4D7",
+      "articleFour.bristolCity.avonmouth": "A4D8",
+      "articleFour.bristolCity.sherrinWay": "A4D9",
+      "articleFour.bristolCity.henbury": "A4D10",
+      "articleFour.bristolCity.redland": "A4D11",
+      "articleFour.bristolCity.cliftonWest": "A4D12",
+      "articleFour.bristolCity.lawrenceAshleyCabotCotham": "A4D13",
+      "articleFour.bristolCity.groveWood": "A4D14",
+      "articleFour.bristolCity.cothamRedland": "A4D15",
+      "articleFour.bristolCity.henleaze": "A4D16",
+    },
+  },
+};
+
+export { planningConstraints };
