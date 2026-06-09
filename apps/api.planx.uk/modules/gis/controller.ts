47454d2a5c564d47c39c51cb276bab0d90242a67
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-06-08 21:11:17 -0400

    wip

--- apps/api.planx.uk/modules/gis/controller.ts
+++ apps/api.planx.uk/modules/gis/controller.ts
@@ -1,6 +1,9 @@
+import { getValidSchemaValues } from "@opensystemslab/planx-core";
+
 import { ServerError } from "../../errors/serverError.js";
 import { findLpa } from "./service/findLpa/index.js";
 import type { FindLpaRequest } from "./service/findLpa/types.ts";
+import type { GetPlanningConstraintsSchemaRequest } from "./service/getPlanningConstraintsSchema/types.js";
 
 export const findLpaController: FindLpaRequest = async (_req, res, next) => {
   const { lon, lat } = res.locals.parsedReq.query;
@@ -13,6 +16,24 @@ export const findLpaController: FindLpaRequest = async (_req, res, next) => {
       new ServerError({
         status: error instanceof ServerError ? error.status : undefined,
         message: `Failed to find local planning authority. ${(error as Error).message}`,
+      }),
+    );
+  }
+};
+
+export const getPlanningConstraintsSchemaController: GetPlanningConstraintsSchemaRequest = async (_req, res, next) => {
+  const { localAuthority } = res.locals.parsedReq.query;
+
+  try {
+    const baseSchema = getValidSchemaValues("PlanningDesignation");
+    // TODO merge in granular a4s for applicable localAuthorities
+    
+    return res.status(200).json(baseSchema);
+  } catch (error) {
+    next(
+      new ServerError({
+        status: error instanceof ServerError ? error.status : undefined,
+        message: `Failed to get planning constraints schema (${localAuthority}). ${(error as Error).message}`,
       }),
     );
   }
--- apps/api.planx.uk/modules/gis/routes.ts
+++ apps/api.planx.uk/modules/gis/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
-import { locationSearch } from "./service/index.js";
-import { classifiedRoadsSearch } from "./service/classifiedRoads.js";
+
 import { validate } from "../../shared/middleware/validate.js";
+import { findLpaController, getPlanningConstraintsSchemaController } from "./controller.js";
+import { classifiedRoadsSearch } from "./service/classifiedRoads.js";
 import { findLpaSchema } from "./service/findLpa/types.js";
-import { findLpaController } from "./controller.js";
+import { getPlanningConstraintsSchemaRequestSchema } from "./service/getPlanningConstraintsSchema/types.js";
+import { locationSearch } from "./service/index.js";
 
 const router = Router();
 
 router.get("/gis/:localAuthority", locationSearch);
 router.get("/roads", classifiedRoadsSearch);
 router.get("/lpa", validate(findLpaSchema), findLpaController);
+router.get("/planning-constraints-schema", validate(getPlanningConstraintsSchemaRequestSchema), getPlanningConstraintsSchemaController);
 
 export default router;
