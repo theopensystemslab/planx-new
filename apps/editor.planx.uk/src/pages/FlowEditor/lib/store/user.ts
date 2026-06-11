7b6e416192ac0a0c2807c4054e6a6e9fd1b7ec61
zz-hh-aa <zzhhaa@protonmail.com>
2026-06-11 13:19:11 +0100

    get teamViewer if no team selected

--- apps/editor.planx.uk/src/pages/FlowEditor/lib/store/user.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/lib/store/user.ts
@@ -45,12 +45,11 @@ export const userStore: StateCreator<
 
     if (user.isPlatformAdmin) return "platformAdmin";
     if (user.isAnalyst) return "analyst";
+    if (!teamSlug) return "teamViewer"; // for EditorNavMenu, before teamSlug is set
 
     const currentUserTeam = user.teams.filter(
       ({ team: { slug } }) => slug === teamSlug,
     );
-
-    if (!currentUserTeam) return;
 
     const isUserTeamAdmin = currentUserTeam.some(
       (user) => user.role === "teamAdmin",
