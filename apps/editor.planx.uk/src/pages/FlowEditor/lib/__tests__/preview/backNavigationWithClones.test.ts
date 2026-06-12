c00b33462281e294250d190e8fef62c7f46c86a7
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-06-12 15:32:48 -0400

    wip

--- apps/editor.planx.uk/src/pages/FlowEditor/lib/__tests__/preview/backNavigationWithClones.test.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/lib/__tests__/preview/backNavigationWithClones.test.ts
@@ -1,21 +1,69 @@
-import { Store } from "../../store";
+import { ComponentType } from "@opensystemslab/planx-core/types";
+
+import { Store, useStore } from "../../store";
+import { clickContinue, visitedNodes } from "../utils";
+
+const { getState, setState } = useStore;
+const { resetPreview, upcomingCardIds } = getState();
+
+describe("Navigation", () => {
+  beforeEach(() => {
+    resetPreview();
+    setState({ flow });
+  });
+
+  test("It navigates forwards in the expected order", () => {
+    expect(upcomingCardIds()).toEqual(["RootQuestion", "FinalNotice"]);
+
+    // Answer the first question
+    clickContinue("RootQuestion", { auto: false, answers: ["RightPath"] });
+    // The clone has been incorrectly hoisted from the left branch into first position
+    expect(upcomingCardIds()).toEqual([
+      "ClonedSetValue",
+      "RightPathFirstQuestion",
+      "FinalNotice",
+    ]);
+
+    // Automate the SetValue and answer the next question
+    clickContinue("ClonedSetValue", { auto: true });
+    clickContinue("RightPathFirstQuestion", {
+      auto: false,
+      answers: ["FbbUQsAXnS"],
+    });
+
+    // This is our "forwards" order that we expect to be able to go "back" through in reverse
+    const forwardsSequence = [
+      "RootQuestion",
+      "ClonedSetValue",
+      "RightPathFirstQuestion",
+      "ClonedContent",
+      "FinalNotice",
+    ];
+    expect(visitedNodes().concat(upcomingCardIds())).toEqual(forwardsSequence);
+
+    // In a future where clones are fixed, forwards order _should_ be:
+    // ["RootQuestion", "RightPathFirstQuestion", "ClonedContent", "ClonedSetValue", "FinalNotice"]
+  });
+
+  test.todo("It navigates backwards in the same order");
+});
 
 // https://editor.planx.uk/app/testing/backwards-navigation
 const flow: Store.Flow = {
   _root: {
-    edges: ["2ZIGuFTUtP", "TGlsOIuh4O"],
+    edges: ["RootQuestion", "FinalNotice"],
   },
-  "2ZIGuFTUtP": {
+  RootQuestion: {
     data: {
       tags: [],
       text: "What do you want to do?",
       neverAutoAnswer: false,
       alwaysAutoAnswerBlank: false,
     },
-    type: 100,
-    edges: ["NNgsWrw2b9", "SGp9cmqbee"],
+    type: ComponentType.Question,
+    edges: ["LeftPath", "RightPath"],
   },
-  "71yccDrxOR": {
+  LeftPathFirstQuestion: {
     data: {
       fn: "proposal.projectType",
       text: "How will the outbuildings be used?",
@@ -24,7 +72,7 @@ const flow: Store.Flow = {
       alwaysAutoAnswerBlank: false,
       tags: [],
     },
-    type: 105,
+    type: ComponentType.Checklist,
     edges: ["Y4PpSxv4QQ", "Yger8jnqr8"],
   },
   FbbUQsAXnS: {
@@ -32,96 +80,96 @@ const flow: Store.Flow = {
       val: "false",
       text: "Building",
     },
-    type: 200,
-    edges: ["GPcbvqoYsU"],
+    type: ComponentType.Answer,
+    edges: ["ClonedContent"],
   },
-  GPcbvqoYsU: {
+  ClonedContent: {
     data: {
       content:
         "<h1>Cloned folder proxy</h1><p>Is it an incidental residential annexe or a new residential unit?</p><p></p>",
       resetButton: false,
     },
-    type: 250,
+    type: ComponentType.Content,
   },
   Jq48bumSEw: {
     data: {
       text: "Outside the garden of the house",
     },
-    type: 200,
+    type: ComponentType.Answer,
   },
-  MT3hvMBR61: {
+  RightPathSecondQuestion: {
     data: {
       text: "Where will the caravan be sited?",
       neverAutoAnswer: false,
       alwaysAutoAnswerBlank: false,
     },
-    type: 100,
+    type: ComponentType.Question,
     edges: ["zbE3thVeTe", "Jq48bumSEw"],
   },
-  NNgsWrw2b9: {
+  LeftPath: {
     data: {
       text: "Change how outbuildings are used",
     },
-    type: 200,
-    edges: ["71yccDrxOR"],
+    type: ComponentType.Answer,
+    edges: ["LeftPathFirstQuestion"],
   },
-  SGp9cmqbee: {
+  RightPath: {
     data: {
       text: "Site a caravan",
     },
-    type: 200,
-    edges: ["owsC195jOu", "ZJcX8tNydr"],
+    type: ComponentType.Answer,
+    edges: ["RightPathFirstQuestion", "ClonedSetValue"],
   },
   Y4PpSxv4QQ: {
     data: {
       val: "changeOfUse.let.part",
       text: "Long term residence",
     },
-    type: 200,
-    edges: ["GPcbvqoYsU", "ZJcX8tNydr"],
+    type: ComponentType.Answer,
+    edges: ["ClonedContent", "ClonedSetValue"],
   },
   Yger8jnqr8: {
     data: {
       val: "changeOfUse.outbuilding",
       text: "Something else",
     },
-    type: 200,
+    type: ComponentType.Answer,
   },
-  ZJcX8tNydr: {
+  ClonedSetValue: {
     data: {
       fn: "proposal.meansOfAccess",
       val: "true",
       operation: "append",
     },
-    type: 380,
+    type: ComponentType.SetValue,
   },
   ogQZmifptv: {
     data: {
       val: "true",
       text: "Caravan",
     },
-    type: 200,
-    edges: ["MT3hvMBR61"],
+    type: ComponentType.Answer,
+    edges: ["RightPathSecondQuestion"],
   },
-  owsC195jOu: {
+  RightPathFirstQuestion: {
     data: {
       fn: "proposal.caravan.building",
       text: "Is it a building or a caravan?",
       neverAutoAnswer: false,
       alwaysAutoAnswerBlank: false,
     },
-    type: 100,
+    type: ComponentType.Question,
     edges: ["FbbUQsAXnS", "ogQZmifptv"],
   },
   zbE3thVeTe: {
     data: {
       text: "Within the garden or driveway",
     },
-    type: 200,
-    edges: ["GPcbvqoYsU"],
+    type: ComponentType.Answer,
+    edges: ["ClonedContent"],
   },
-  TGlsOIuh4O: {
-    type: 8,
+  FinalNotice: {
+    type: ComponentType.Notice,
     data: {
       title: "End of test",
       color: "#EFEFEF",
