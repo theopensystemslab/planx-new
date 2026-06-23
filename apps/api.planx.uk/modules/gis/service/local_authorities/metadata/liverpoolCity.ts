6e190f7e27f86e55371405e349df3a4e23f16d0f
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2026-06-23 12:47:23 +0100

    remove planning conditions from A4 metadata

--- apps/api.planx.uk/modules/gis/service/local_authorities/metadata/liverpoolCity.ts
+++ apps/api.planx.uk/modules/gis/service/local_authorities/metadata/liverpoolCity.ts
@@ -10,24 +10,10 @@ import type { LocalAuthorityMetadata } from "../../digitalLand.js";
 
 const planningConstraints: LocalAuthorityMetadata["planningConstraints"] = {
   articleFour: {
-    // These entries include large properties with 'historic planning conditions'
+    // Only two actual A4 directions in Liverpool, others are planning conditions
     records: {
       "articleFour.liverpool.liverpoolDirection": "A4Da407",
       "articleFour.liverpool.dales": "A4Da408",
-      "articleFour.liverpool.waltonTriangle": "A4Da325",
-      "articleFour.liverpool.croxtethPark": "A4Da327",
-      "articleFour.liverpool.cravenWood": "A4Da339",
-      "articleFour.liverpool.liverpoolTwelve": "A4Da341",
-      "articleFour.liverpool.riverDrive": "A4Da392",
-      "articleFour.liverpool.dakotaDrive": "A4Da1242",
-      "articleFour.liverpool.newMersey": "A4Da1190",
-      "articleFour.liverpool.festivalSite.one": "A4Da711",
-      "articleFour.liverpool.festivalSite.two": "A4Da850",
-      "articleFour.liverpool.projectJennifer.one": "A4Da638",
-      "articleFour.liverpool.projectJennifer.two": "A4Da784",
-      "articleFour.liverpool.airport.one": "A4Da664",
-      "articleFour.liverpool.airport.two": "A4Da792",
-      "articleFour.liverpool.businessPark": "A4Da584",
     },
   },
 };
