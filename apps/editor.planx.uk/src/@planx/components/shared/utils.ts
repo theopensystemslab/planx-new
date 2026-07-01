82f3d96e9c73e76a424dfb5364b9689e10db1052
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-07-01 17:23:11 -0400

    add fns

--- apps/editor.planx.uk/src/@planx/components/shared/utils.ts
+++ apps/editor.planx.uk/src/@planx/components/shared/utils.ts
@@ -1,4 +1,7 @@
-import { getValidSchemaValues } from "@opensystemslab/planx-core";
+import {
+  getValidSchemaValues,
+  getValidSchemaValuesByEnumPath,
+} from "@opensystemslab/planx-core";
 import isNil from "lodash/isNil";
 
 export const validateEmail = (email: string) => {
@@ -79,11 +82,60 @@ export const getOptionsSchemaByFn = (
 
   // For certain data fields, suggest based on full ODP Schema enums rather than current flow schema
   //   Note that many `property.{}` fields which auto-suggest based on Planning Data entities set their schema one level up
-  if (fn === "application.type")
-    schema = getValidSchemaValues("ApplicationType");
-  if (fn === "proposal.projectType")
-    schema = getValidSchemaValues("ProjectType");
-  if (fn === "property.type") schema = getValidSchemaValues("PropertyType");
+  switch (fn) {
+    case "applicant.type":
+      schema = getValidSchemaValuesByEnumPath("BaseApplicant", "type");
+      break;
+    case "applicant.siteContact.role":
+      // .concat() on user role is lazy manual handling of discriminated union enum type for BaseApplicant/SiteContact/Role !
+      schema = getValidSchemaValuesByEnumPath("User", "role")?.concat("other");
+      break;
+    case "application.declaration.connection.form":
+      schema = getValidSchemaValuesByEnumPath("Declaration", "connection");
+      break;
+    case "application.leadDeveloper":
+      schema = getValidSchemaValuesByEnumPath("LeadDeveloper", "type");
+      break;
+    case "application.type":
+      schema = getValidSchemaValues("ApplicationType");
+      break;
+    case "property.EPCKnown.form":
+      schema = getValidSchemaValuesByEnumPath("LondonProperty", "EPC");
+      break;
+    case "property.occupation":
+      schema = getValidSchemaValuesByEnumPath(
+        "LondonProperty",
+        "occupation",
+        "status",
+      );
+      break;
+    case "property.ownership.status":
+      schema = getValidSchemaValuesByEnumPath(
+        "LondonProperty",
+        "ownership",
+        "status",
+      );
+      break;
+    case "property.titleNumberKnown.form":
+      schema = getValidSchemaValuesByEnumPath("LondonProperty", "titleNumber");
+      break;
+    case "property.type":
+      schema = getValidSchemaValues("PropertyType");
+      break;
+    case "proposal.cost.projected":
+      schema = getValidSchemaValuesByEnumPath(
+        "LondonProposal",
+        "cost",
+        "projected",
+      );
+      break;
+    case "proposal.projectType":
+      schema = getValidSchemaValues("ProjectType");
+      break;
+    case "user.role":
+      schema = getValidSchemaValuesByEnumPath("User", "role");
+      break;
+  }
 
   // Ensure that any initial values outside of ODP Schema enums will still be recognised/pre-populated when modal loads
   currentOptions?.forEach((option) => {
