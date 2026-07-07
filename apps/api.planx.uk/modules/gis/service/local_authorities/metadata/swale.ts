317cfc9b3d560558e8b0c31ba2166775507282a6
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2026-07-07 13:47:44 +0100

    add Swale a4s

---
+++ apps/api.planx.uk/modules/gis/service/local_authorities/metadata/swale.ts
@@ -0,0 +1,38 @@
+/*
+LAD20CD: E07000113
+LAD20NM: Swale
+
+*/
+
+import type { LocalAuthorityMetadata } from "../../helpers.js";
+
+const planningConstraints: LocalAuthorityMetadata["planningConstraints"] = {
+  articleFour: {
+    records: {
+      "articleFour.swale.a4d01": "A4D01",
+      "articleFour.swale.a4d02": "A4D02",
+      "articleFour.swale.a4d03": "A4D03",
+      "articleFour.swale.a4d04": "A4D04",
+      "articleFour.swale.a4d05": "A4D05",
+      "articleFour.swale.a4d06": "A4D06",
+      "articleFour.swale.a4d07": "A4D07",
+      "articleFour.swale.a4d08": "A4D08",
+      "articleFour.swale.a4d09": "A4D09",
+      "articleFour.swale.a4d10": "A4D10",
+      "articleFour.swale.a4d11": "A4D11",
+      "articleFour.swale.a4d12": "A4D12",
+      "articleFour.swale.a4d13": "A4D13",
+      "articleFour.swale.a4d14": "A4D14",
+      "articleFour.swale.a4d15": "A4D15",
+      "articleFour.swale.a4d16": "A4D16",
+      "articleFour.swale.a4d17": "A4D17",
+      "articleFour.swale.a4d18": "A4D18",
+      "articleFour.swale.a4d19": "A4D19",
+      "articleFour.swale.a4d20": "A4D20",
+      "articleFour.swale.a4d21": "A4D21",
+      "articleFour.swale.a4d22": "A4D22",
+    },
+  },
+};
+
+export { planningConstraints };
