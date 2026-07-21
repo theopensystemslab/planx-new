e710323ceea50af383af354badb8bf96c8257ed9
zz-hh-aa <zzhhaa@protonmail.com>
2026-07-21 17:24:52 +0100

    getPath function with mode prop

--- apps/editor.planx.uk/src/ui/editor/URLPrefix.tsx
+++ apps/editor.planx.uk/src/ui/editor/URLPrefix.tsx
@@ -1,9 +1,19 @@
 import InputAdornment from "@mui/material/InputAdornment";
 import { typographyClasses } from "@mui/material/Typography";
+import { useParams } from "@tanstack/react-router";
 import React, { useState } from "react";
 
 type Props = {
   mode: "team" | "flow";
+};
+
+const getPath = (mode: Props["mode"], team?: string) => {
+  switch (mode) {
+    case "team":
+      return "/app";
+    case "flow":
+      return `/app/${team}`;
+  }
 };
 
 /**
@@ -11,10 +21,11 @@ type Props = {
  * Used to display URLs when creating teams and flows
  */
 export const URLPrefix: React.FC<Props> = ({ mode }) => {
+  const { team } = useParams({ strict: false });
+
   // Store in component state so this does not update when user submits form
   const [urlPrefix] = useState(() => {
-    const { origin, pathname } = window.location;
-    const path = mode === "team" ? pathname : pathname.replace("/flows", ""); // if creating a new flow, the route of the flow itself should not be a child of `/flows`
+    const path = getPath(mode, team);
     return `${origin}${path}/`;
   });
 
