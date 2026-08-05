e482d8cbcf77692f82c2b9fffacaa5cece274727
Joe Carver <joecarver@live.co.uk>
2026-08-05 09:33:24 +0100

    fix: gate notes add/edit routes behind feature flag

--- apps/editor.planx.uk/src/routes/_authenticated/app/$team/$flow/_flowEditor/note/route.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/app/$team/$flow/_flowEditor/note/route.tsx
@@ -1,8 +1,14 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { hasFeatureFlag } from "lib/featureFlags";
 import React from "react";
 
 export const Route = createFileRoute(
   "/_authenticated/app/$team/$flow/_flowEditor/note",
 )({
+  beforeLoad: ({ params }) => {
+    if (!hasFeatureFlag("NOTES")) {
+      throw redirect({ to: "/app/$team/$flow", params });
+    }
+  },
   component: () => <Outlet />,
 });
