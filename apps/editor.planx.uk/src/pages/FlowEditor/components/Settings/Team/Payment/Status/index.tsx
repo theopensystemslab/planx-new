8cfd68f292b2b8cecb51d5be72504b151a9add2d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-08-07 14:03:00 +0100

    feat(editor): Dummy UI for Stripe onboarding workflow (#7138)

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Team/Payment/Status/index.tsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { FeaturePlaceholder } from "ui/editor/FeaturePlaceholder";
-
-export const Status: React.FC = () => <FeaturePlaceholder title="Status" />;
--- apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Team/Payment/index.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Settings/Team/Payment/index.tsx
@@ -2,12 +2,10 @@ import Stack from "@mui/material/Stack";
 
 import { Onboarding } from "./Onboarding";
 import { Provider } from "./Provider";
-import { Status } from "./Status";
 
 const PaymentSettings = () => (
   <Stack spacing={2}>
     <Onboarding />
-    <Status />
     <Provider />
   </Stack>
 );
