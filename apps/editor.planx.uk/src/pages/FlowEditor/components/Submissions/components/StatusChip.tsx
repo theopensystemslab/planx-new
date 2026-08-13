880cf90c0a0d22326776ce6007b1c6d77e496be1
zz-hh-aa <zzhhaa@protonmail.com>
2026-08-13 17:10:53 +0100

    fix status chip style

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/StatusChip.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/StatusChip.tsx
@@ -3,19 +3,9 @@ import React from "react";
 import type { RenderCellParams } from "ui/shared/DataTable/types";
 
 export const StatusChip = (params: RenderCellParams) => {
-  if (params.value.includes("failed")) {
-    return <Chip label={params.value} size="small" color="error" />;
-  }
-  if (params.value === "Sending") {
-    return <Chip label={params.value} size="small" color="sending" />;
-  }
-  if (
-    params.value === "Invited to pay" ||
-    params.value === "Payment in progress"
-  ) {
-    return <Chip label={params.value} size="small" color="paymentPending" />;
-  }
-  if (params.value === "Success") {
-    return <Chip label={params.value} size="small" color="success" />;
-  }
+  return params.value === "Success" ? (
+    <Chip label="Success" size="small" color="success" />
+  ) : (
+    <Chip label={params.value} size="small" color="error" />
+  );
 };
---
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/StatusChipGrouped.tsx
@@ -0,0 +1,21 @@
+import Chip from "@mui/material/Chip";
+import React from "react";
+import type { RenderCellParams } from "ui/shared/DataTable/types";
+
+export const StatusChipGrouped = (params: RenderCellParams) => {
+  if (params.value.includes("failed")) {
+    return <Chip label={params.value} size="small" color="error" />;
+  }
+  if (params.value === "Sending") {
+    return <Chip label={params.value} size="small" color="sending" />;
+  }
+  if (
+    params.value === "Invited to pay" ||
+    params.value === "Payment in progress"
+  ) {
+    return <Chip label={params.value} size="small" color="paymentPending" />;
+  }
+  if (params.value === "Success") {
+    return <Chip label={params.value} size="small" color="success" />;
+  }
+};
