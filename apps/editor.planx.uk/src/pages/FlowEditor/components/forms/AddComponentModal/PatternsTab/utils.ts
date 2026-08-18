829aa7f71fd49e44b0e7037fed63bb7f68eb10f7
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-08-18 11:16:33 +0100

    feat: Count components via traversla

--- apps/editor.planx.uk/src/pages/FlowEditor/components/forms/AddComponentModal/PatternsTab/utils.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/forms/AddComponentModal/PatternsTab/utils.ts
@@ -1,10 +1,32 @@
 import { ComponentType } from "@opensystemslab/planx-core/types";
 import type { Graph } from "@planx/graph";
 
+export interface PatternCounts {
+  components: number;
+  nestedFlows: number;
+}
+
 /**
- * Count all nodes (minus root and Answer components)
+ * Traverse graph to count components and nested flows
+ * Traversal is required to properly account for clones
  */
-export const getComponentCount = (graph: Graph) =>
-  Object.entries(graph).filter(
-    ([id, { type }]) => id !== "_root" && type !== ComponentType.Answer,
-  ).length;
+export const getPatternCounts = (graph: Graph): PatternCounts => {
+  let components = 0;
+  let nestedFlows = 0;
+
+  for (const node of Object.values(graph)) {
+    for (const childId of node.edges ?? []) {
+      const child = graph[childId];
+      if (!child) continue;
+      if (child.type === ComponentType.Answer) continue;
+
+      if (child.type === ComponentType.ExternalPortal) {
+        nestedFlows++;
+      } else {
+        components++;
+      }
+    }
+  }
+
+  return { components, nestedFlows };
+};
