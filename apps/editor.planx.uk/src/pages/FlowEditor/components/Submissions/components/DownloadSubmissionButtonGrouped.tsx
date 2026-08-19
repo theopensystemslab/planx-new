b3abc7291833e760c53c8bbf1e3ec13e4dbfefcd
zz-hh-aa <zzhhaa@protonmail.com>
2026-08-19 10:28:35 +0100

    download submission button

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/DownloadSubmissionButtonGrouped.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/DownloadSubmissionButtonGrouped.tsx
@@ -1,33 +1,36 @@
 import CloudDownload from "@mui/icons-material/CloudDownload";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
 import { addDays, isBefore } from "date-fns";
 import { DAYS_UNTIL_EXPIRY } from "lib/pay";
 import React from "react";
 
 type Props = {
   sessionId: string;
-  createdAt: string;
+  submittedAt?: string;
 };
 
 export const DownloadSubmissionButtonGrouped = (props: Props) => {
-  const submissionDataExpirationDate = addDays(
-    new Date(props.createdAt),
-    DAYS_UNTIL_EXPIRY,
-  );
+  const submissionDataExpirationDate = props.submittedAt
+    ? addDays(new Date(props.submittedAt), DAYS_UNTIL_EXPIRY)
+    : null;
 
-  if (!isBefore(new Date(), submissionDataExpirationDate)) return;
+  const showDownloadButton = submissionDataExpirationDate
+    ? isBefore(new Date(), submissionDataExpirationDate)
+    : false;
+
+  if (!showDownloadButton) return;
 
   const zipUrl = `${import.meta.env.VITE_APP_API_URL}/submission/${props.sessionId}/zip`;
 
   return (
-    <Tooltip title="Download application data">
-      <IconButton
-        aria-label="download application"
-        onClick={() => window.open(zipUrl, "_blank")}
-      >
-        <CloudDownload />
-      </IconButton>
-    </Tooltip>
+    <Button
+      color="primary"
+      variant="contained"
+      onClick={() => window.open(zipUrl, "_blank")}
+      disabled={!props.submittedAt}
+    >
+      <CloudDownload sx={{ mr: 1 }} />
+      Download
+    </Button>
   );
 };
