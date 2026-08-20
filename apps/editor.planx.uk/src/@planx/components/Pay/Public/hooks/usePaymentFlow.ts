9a03ce60c2eab7ce12e3e71de184c1948086cae7
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-08-20 09:05:43 +0100

    feat(editor): Stub out basic Stripe provider hook

---
+++ apps/editor.planx.uk/src/@planx/components/Pay/Public/hooks/usePaymentFlow.ts
@@ -0,0 +1,21 @@
+import type { GovPayMetadata } from "@opensystemslab/planx-core/types";
+
+import type { Props } from "../Pay";
+import type { UsePaymentProviderResult } from "../providers/types";
+import { useGovUkPay } from "../providers/useGovUkPay";
+import { useStripePay } from "../providers/useStripePay";
+import type { Action } from "../types";
+import { usePaymentProvider } from "./usePaymentProvider";
+
+export const usePaymentFlow = (
+  props: Props,
+  dispatch: React.Dispatch<Action>,
+  fee: number,
+  metadata: GovPayMetadata[],
+): UsePaymentProviderResult => {
+  const providerName = usePaymentProvider();
+  const govPay = useGovUkPay(props, dispatch, fee, metadata);
+  const stripe = useStripePay(props, dispatch, fee);
+
+  return providerName === "stripe" ? stripe : govPay;
+};
--- apps/editor.planx.uk/src/@planx/components/Pay/Public/providers/types.ts
+++ apps/editor.planx.uk/src/@planx/components/Pay/Public/providers/types.ts
@@ -2,6 +2,8 @@ import type { GOV_PAY_PASSPORT_KEY } from "@opensystemslab/planx-core/types";
 
 export type PaymentProviderName = "govpay" | "stripe";
 
+export const STRIPE_PASSPORT_KEY = "application.fee.reference.stripe";
+
 export interface PaymentActions {
   startNewPayment: () => Promise<void>;
   refetchPayment: () => Promise<void>;
@@ -11,6 +13,6 @@ export interface PaymentActions {
 
 export interface UsePaymentProviderResult {
   actions: PaymentActions;
-  passportKey: typeof GOV_PAY_PASSPORT_KEY;
+  passportKey: typeof GOV_PAY_PASSPORT_KEY | typeof STRIPE_PASSPORT_KEY;
   hasExistingPayment: boolean;
 }
