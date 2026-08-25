e9b88b53cc7dd87090338f20955fd0c763660f4a
zz-hh-aa <zzhhaa@protonmail.com>
2026-08-25 09:12:26 +0100

    fix modal header button alignment

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/SubmissionDetailModal.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/components/SubmissionDetailModal.tsx
@@ -1,10 +1,11 @@
 import { gql, useQuery } from "@apollo/client";
 import Close from "@mui/icons-material/CloseOutlined";
+import Box from "@mui/material/Box";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import Grid from "@mui/material/Grid";
-import { useNavigate, useParams } from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
 import DelayedLoadingIndicator from "components/DelayedLoadingIndicator/DelayedLoadingIndicator";
 import { useStore } from "pages/FlowEditor/lib/store";
 import { CloseButton } from "ui/icons/CloseButton";
@@ -37,6 +38,51 @@ const GET_SUBMISSION_EVENTS = gql`
 interface SubmissionDetailModalProps {
   sessionId: string;
 }
+
+const SubmissionModalWrapper = ({
+  handleClose,
+  children,
+}: {
+  handleClose: () => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Dialog
+      open
+      slotProps={{
+        paper: {
+          sx: {
+            width: "66.67%",
+            maxWidth: "66.67%",
+            margin: "auto",
+          },
+        },
+      }}
+      onClose={handleClose}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "flex-start",
+          marginX: 1,
+        }}
+      >
+        <DialogTitle variant="h2">Submission details</DialogTitle>
+        <CloseButton
+          aria-label="close"
+          onClick={handleClose}
+          size="large"
+          sx={{ paddingTop: 2.5 }}
+        >
+          <Close />
+        </CloseButton>
+      </Box>
+
+      <DialogContent children={children} />
+    </Dialog>
+  );
+};
 
 const getSubmittedAt = (events: Submission[]): string | undefined => {
   const successfulSend = events.find(
@@ -72,73 +118,37 @@ const SubmissionDetailModal: React.FC<SubmissionDetailModalProps> = ({
 
   if (loading) {
     return (
-      <Dialog
-        open
-        slotProps={{
-          paper: {
-            sx: {
-              width: "66.67%",
-              maxWidth: "66.67%",
-              margin: "auto",
-              minHeight: "30vh",
-            },
-          },
-        }}
-      >
-        <CloseButton aria-label="close" onClick={handleClose} size="large">
-          <Close />
-        </CloseButton>
-        <DialogTitle variant="h2">Submission details</DialogTitle>
-        <DialogContent>
-          <DelayedLoadingIndicator />
-        </DialogContent>
-      </Dialog>
+      <SubmissionModalWrapper handleClose={handleClose}>
+        <DelayedLoadingIndicator />
+      </SubmissionModalWrapper>
     );
   }
 
   if (error) throw error;
   return (
-    <Dialog
-      open
-      slotProps={{
-        paper: {
-          sx: {
-            width: "66.67%",
-            maxWidth: "66.67%",
-            margin: "auto",
-          },
-        },
-      }}
-      onClose={handleClose}
-    >
-      <CloseButton aria-label="close" onClick={handleClose} size="large">
-        <Close />
-      </CloseButton>
-      <DialogTitle variant="h2">Submission details</DialogTitle>
-      <DialogContent>
-        <Grid container>
-          <Grid
-            size={6}
-            sx={{
-              position: "sticky",
-              top: 0,
-              alignSelf: "flex-start",
-            }}
-          >
-            <SubmissionDetails
-              sessionId={sessionId}
-              latestEvent={latestEvent}
-              teamSlug={teamSlug}
-              submittedAt={submittedAt}
-            />
-          </Grid>
-
-          <Grid size={6}>
-            <SubmissionEventsHistory events={events} />
-          </Grid>
+    <SubmissionModalWrapper handleClose={handleClose}>
+      <Grid container>
+        <Grid
+          size={6}
+          sx={{
+            position: "sticky",
+            top: 0,
+            alignSelf: "flex-start",
+          }}
+        >
+          <SubmissionDetails
+            sessionId={sessionId}
+            latestEvent={latestEvent}
+            teamSlug={teamSlug}
+            submittedAt={submittedAt}
+          />
         </Grid>
-      </DialogContent>
-    </Dialog>
+
+        <Grid size={6}>
+          <SubmissionEventsHistory events={events} />
+        </Grid>
+      </Grid>
+    </SubmissionModalWrapper>
   );
 };
 
