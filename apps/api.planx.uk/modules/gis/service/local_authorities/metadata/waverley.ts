15535ab4dc55fcb6eb8c100f18b9e54e9089e5f0
augustlindemer <118665588+augustlindemer@users.noreply.github.com>
2026-08-27 12:51:15 +0100

    chore: add Waverley A4 metadata (#7275)

---
+++ apps/api.planx.uk/modules/gis/service/local_authorities/metadata/waverley.ts
@@ -0,0 +1,41 @@
+/*
+LAD20CD: E07000216
+LAD20NM: Waverley
+LAD20NMW:
+FID:
+
+https://docs.google.com/spreadsheets/d/1plCuIUcrSFKKpJA1IKLVVJ0RzptIo19L/edit?gid=130654674
+*/
+
+import type { LocalAuthorityMetadata } from "../../helpers.js";
+
+const planningConstraints: LocalAuthorityMetadata["planningConstraints"] = {
+  articleFour: {
+    records: {
+      "articleFour.waverley.sunray": "A4D-01",
+      "articleFour.waverley.laneEndMillbridge": "A4D-04",
+      "articleFour.waverley.munsteadOaks": "A4D-05",
+      "articleFour.waverley.buildingsWood": "A4D-06",
+      "articleFour.waverley.farnhamTrading": "A4D-07",
+      "articleFour.waverley.middleBourneLane": "A4D-08",
+      "articleFour.waverley.wishanger": "A4D-09",
+      "articleFour.waverley.westCottage": "A4D-10",
+      "articleFour.waverley.greenLane": "A4D-11",
+      "articleFour.waverley.potatoCopse": "A4D-12",
+      "articleFour.waverley.lytheHill": "A4D-13",
+      "articleFour.waverley.runfoldWest": "A4D-14",
+      "articleFour.waverley.chiddingfold": "A4D-15",
+      "articleFour.waverley.sydneyRosemary": "A4D-16",
+      "articleFour.waverley.winterfoldHouse": "A4D-17",
+      "articleFour.waverley.horshamRoad": "A4D-20",
+      "articleFour.waverley.woodFarm": "A4D-21",
+      "articleFour.waverley.longGardenWalk": "A4D-26",
+      "articleFour.waverley.lowndesBuildings": "A4D-27",
+      "articleFour.waverley.wildwoodGolf": "A4D-28",
+      "articleFour.waverley.godalmingStation": "A4D-31",
+      "articleFour.waverley.beaconHill": "A4D-32",
+    },
+  },
+};
+
+export { planningConstraints };
