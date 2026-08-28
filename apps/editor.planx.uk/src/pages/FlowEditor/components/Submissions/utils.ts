f4149d47aa87fba7df42598e8756e1a48c60bf1f
zz-hh-aa <zzhhaa@protonmail.com>
2026-08-28 15:34:48 +0100

    fix logic for Sending status

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/utils.ts
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Submissions/utils.ts
@@ -11,15 +11,18 @@ export const getConsolidatedStatus = (
   } else if (status === "Submitted") {
     // "Submitted" status only possible on Pay events, hence "Sending" status
     return "Sending";
+  } else if (eventType === "Pay" && status === "Success") {
+    // Pay succeeded but Send hasn't happened yet
+    return "Sending";
   } else if (status === "Success") {
+    // non-Pay success (Send events)
     return "Success";
   } else if (typeof status === "string" && status.includes("Failed")) {
-    return eventType.replace("(no notification)", "").concat(" failed"); // shortening possible status "Upload to AWS S3 (no notification)"
+    return eventType.replace(" (no notification)", "").concat(" failed");
   } else {
     return "Failed";
   }
 };
-
 export const getSucceededPayment = (
   events: Submission[],
 ): Submission | undefined =>
