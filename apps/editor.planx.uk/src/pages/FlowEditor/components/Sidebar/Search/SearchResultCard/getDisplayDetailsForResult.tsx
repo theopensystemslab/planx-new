ddcd0cd35552224f8d35148b1ef318009868297a
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-08-27 20:24:12 -0400

    align search facet names

--- apps/editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Search/SearchResultCard/getDisplayDetailsForResult.tsx
+++ apps/editor.planx.uk/src/pages/FlowEditor/components/Sidebar/Search/SearchResultCard/getDisplayDetailsForResult.tsx
@@ -34,8 +34,8 @@ const keyFormatters: KeyMap = {
         ? "Option (description)"
         : "Description",
   },
-  "data.note": {
-    getDisplayKey: () => "Note",
+  "data.text": {
+    getDisplayKey: () => "Note", // Standalone component type
   },
   "data.notes": {
     getDisplayKey: () => "Internal notes", // TODO rename here and in-modals?
