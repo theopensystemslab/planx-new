5e6e5126d8b61ddb0068e26966e9ec0147ec56cb
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-09-02 11:20:31 +0100

    fix: Add team_integrations columns also

--- scripts/seed-database/write/team_integrations.sql
+++ scripts/seed-database/write/team_integrations.sql
@@ -6,13 +6,22 @@ CREATE TEMPORARY TABLE sync_team_integrations (
   staging_bops_secret text,
   staging_govpay_secret text,
   staging_file_api_key text,
-  staging_power_automate_api_key text
+  staging_power_automate_api_key text,
+  staging_stripe_account_id text
 );
 
 \COPY sync_team_integrations FROM '/tmp/team_integrations.csv' WITH (FORMAT csv, DELIMITER ';');
 
-INSERT INTO
-  team_integrations (id, team_id, staging_bops_submission_url, staging_bops_secret, staging_govpay_secret, staging_file_api_key, staging_power_automate_api_key)
+INSERT INTO team_integrations (
+  id, 
+  team_id, 
+  staging_bops_submission_url, 
+  staging_bops_secret, 
+  staging_govpay_secret, 
+  staging_file_api_key, 
+  staging_power_automate_api_key,
+  staging_stripe_account_id
+)
 SELECT
   id,
   team_id,
@@ -20,7 +29,8 @@ SELECT
   staging_bops_secret,
   staging_govpay_secret,
   staging_file_api_key,
-  staging_power_automate_api_key
+  staging_power_automate_api_key,
+  staging_stripe_account_id
 FROM
   sync_team_integrations ON CONFLICT (id) DO
 UPDATE
@@ -30,7 +40,8 @@ SET
   staging_bops_secret = EXCLUDED.staging_bops_secret,
   staging_govpay_secret = EXCLUDED.staging_govpay_secret,
   staging_file_api_key = EXCLUDED.staging_file_api_key,
-  staging_power_automate_api_key = EXCLUDED.staging_power_automate_api_key;
+  staging_power_automate_api_key = EXCLUDED.staging_power_automate_api_key,
+  staging_stripe_account_id = EXCLUDED.staging_stripe_account_id;
 SELECT
   setval('team_integrations_id_seq', max(id))
 FROM
