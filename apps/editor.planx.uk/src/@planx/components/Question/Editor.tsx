1e561dbeccabd8aae103c988dc603d0e0dd81c7d
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-09-02 11:45:41 -0400

    fix: update Question and Checklist error messages (#7328)

--- apps/editor.planx.uk/src/@planx/components/Question/Editor.tsx
+++ apps/editor.planx.uk/src/@planx/components/Question/Editor.tsx
@@ -37,12 +37,7 @@ export const QuestionComponent: React.FC<Props> = (props) => {
       validate: ({ options, ...values }) => {
         const errors: FormikErrors<FormikValues> = {};
         if (!options.length) {
-          if (values.text) {
-            errors.text =
-              "Questions can no longer be used as notes and must have at least one option";
-          } else {
-            errors.options = "Add at least one option";
-          }
+          errors.options = "Add at least one option";
         }
         if (values.fn && !options.some((option) => option.data.val)) {
           errors.fn = "At least one option must also set a data field";
