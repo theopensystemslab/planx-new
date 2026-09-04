5fa65d58c3df5fc2ce7457c47fb9b45b18fc136a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-09-04 12:34:14 +0100

    fix(ci): Ensure update.sh doesn't trigger xcaddy build

--- scripts/pullrequest/update.sh
+++ scripts/pullrequest/update.sh
@@ -4,6 +4,15 @@ set -o errexit -o pipefail
 # run from project root
 cd "$(dirname $0)/../.."
 
+# caddy runs up against kernel buffer limits of host OS, so we increase them
+# see https://github.com/quic-go/quic-go/wiki/UDP-Buffer-Sizes
+sysctl -w net.core.rmem_max=7500000
+sysctl -w net.core.wmem_max=7500000
+
+# ensure docker does not pull caddy image via ipv6 (no subnet configured, it will fail)
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
 # set env for this shell
 set -o allexport
 source .env.pizza
