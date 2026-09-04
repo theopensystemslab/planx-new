ee82982a804677f699781bc879faa6bd69469b5f
Jessica McInchak <jessica.mcinchak@gmail.com>
2026-09-04 10:09:52 -0400

    formik import

--- apps/editor.planx.uk/src/@planx/components/Note/Editor.tsx
+++ apps/editor.planx.uk/src/@planx/components/Note/Editor.tsx
@@ -1,5 +1,6 @@
 import { ComponentType } from "@opensystemslab/planx-core/types";
 import { useFormikWithRef } from "@planx/components/shared/useFormikWithRef";
+import { getIn } from "formik";
 import { ModalFooter } from "ui/editor/ModalFooter";
 import ModalSection from "ui/editor/ModalSection";
 import ModalSectionContent from "ui/editor/ModalSectionContent";
