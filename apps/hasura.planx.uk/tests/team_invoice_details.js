eb8f66225d8f96998058875507ff331795c9d42c
zz-hh-aa <zzhhaa@protonmail.com>
2026-09-11 16:28:00 +0100

    update introspection tests

--- apps/hasura.planx.uk/tests/team_invoice_details.js
+++ apps/hasura.planx.uk/tests/team_invoice_details.js
@@ -61,9 +61,9 @@ describe("team_invoice_details", () => {
       expect(i.queries).toContain("team_invoice_details");
     });
 
-    test("cannot update team_invoice_details", () => {
-      expect(i.mutations).not.toContain("update_team_invoice_details");
-      expect(i.mutations).not.toContain("update_team_invoice_details_by_pk");
+    test("can update team_invoice_details", () => {
+      expect(i.mutations).toContain("update_team_invoice_details");
+      expect(i.mutations).toContain("update_team_invoice_details_by_pk");
     });
 
     test("cannot delete team_invoice_details", async () => {
@@ -81,20 +81,14 @@ describe("team_invoice_details", () => {
       i = await introspectAs("teamAdmin");
     });
 
-    test("can query team_invoice_details", () => {
-      expect(i.queries).toContain("team_invoice_details");
+    test("cannot query team_invoice_details", () => {
+      expect(i.queries).not.toContain("team_invoice_details");
     });
 
-    test("can update team_invoice_details", () => {
-      expect(i.mutations).toContain("update_team_invoice_details");
-      expect(i.mutations).toContain("update_team_invoice_details_by_pk");
-    });
-
-    test("cannot delete team_invoice_details", async () => {
+    test("cannot update, delete or insert team_invoice_details", () => {
+      expect(i.mutations).not.toContain("update_team_invoice_details");
+      expect(i.mutations).not.toContain("update_team_invoice_details_by_pk");
       expect(i.mutations).not.toContain("delete_team_invoice_details");
-    });
-
-    test("cannot insert team_invoice_details", async () => {
       expect(i.mutations).not.toContain("insert_team_invoice_details");
     });
   });
