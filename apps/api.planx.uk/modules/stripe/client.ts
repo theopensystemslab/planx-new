8440e3a84686844813f1fef1238a13bc73ff8f3d
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-09-11 17:37:24 +0100

    chore: Setup webhook secret

--- apps/api.planx.uk/modules/stripe/client.ts
+++ apps/api.planx.uk/modules/stripe/client.ts
@@ -2,7 +2,6 @@ import assert from "assert";
 import Stripe from "stripe";
 
 assert(process.env.STRIPE_SECRET_KEY);
-assert(process.env.STRIPE_WEBHOOK_SECRET);
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2026-08-26.dahlia",
