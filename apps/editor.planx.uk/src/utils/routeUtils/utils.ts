02b1afe16fa0832a8524d82996be1ab67907529a
Dafydd Llŷr Pearson <DafyddLlyr@gmail.com>
2026-09-14 06:32:31 +0100

    chore: Finalise A&W subdomain [skip pizza]

--- apps/editor.planx.uk/src/utils/routeUtils/utils.ts
+++ apps/editor.planx.uk/src/utils/routeUtils/utils.ts
@@ -59,6 +59,7 @@ const PREVIEW_ONLY_DOMAINS = [
   "planningservices.stoke.gov.uk",
   "planningservices.tewkesbury.gov.uk",
   "planningservices.westberks.gov.uk",
+  "planx.adur-worthing.gov.uk",
   // XXX: un-comment the next line to test custom domains locally
   // "localhost",
 ];
