4df27d2fa658a4134c702338fe57a6efed347643
zz-hh-aa <zzhhaa@protonmail.com>
2026-09-15 10:47:16 +0100

    fix tests

--- apps/hasura.planx.uk/tests/team_invoice_details.test.js
+++ apps/hasura.planx.uk/tests/team_invoice_details.test.js
@@ -81,13 +81,16 @@ describe("team_invoice_details", () => {
       i = await introspectAs("teamAdmin");
     });
 
-    test("cannot query team_invoice_details", () => {
-      expect(i.queries).not.toContain("team_invoice_details");
+    test("can query team_invoice_details", () => {
+      expect(i.queries).toContain("team_invoice_details");
     });
 
-    test("cannot update, delete or insert team_invoice_details", () => {
-      expect(i.mutations).not.toContain("update_team_invoice_details");
-      expect(i.mutations).not.toContain("update_team_invoice_details_by_pk");
+    test("can update team_invoice_details", () => {
+      expect(i.mutations).toContain("update_team_invoice_details");
+      expect(i.mutations).toContain("update_team_invoice_details_by_pk");
+    });
+
+    test("cannot delete or insert team_invoice_details", () => {
       expect(i.mutations).not.toContain("delete_team_invoice_details");
       expect(i.mutations).not.toContain("insert_team_invoice_details");
     });
@@ -100,7 +103,7 @@ describe("team_invoice_details", () => {
     });
 
     test("can query team_invoice_details", () => {
-      expect(i.queries).toContain("team_invoice_details");
+      expect(i.queries).not.toContain("team_invoice_details");
     });
 
     test("cannot create, update, or delete team_invoice_details", () => {
