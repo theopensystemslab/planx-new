972b11b186bb54eff76ea1895db5d0463d1e23f8
ollie <107874766+zz-hh-aa@users.noreply.github.com>
2026-09-17 10:22:58 +0100

    chore(infra): advance gloucester to shared-final (#7463)

--- infrastructure/common/customDomains.ts
+++ infrastructure/common/customDomains.ts
@@ -64,7 +64,7 @@ export const getCustomDomains = (env: string): CustomDomain[] =>
         {
           name: "gloucester",
           domain: "planningservices.gloucester.gov.uk",
-          cloudFrontState: "cutover-ongoing",
+          cloudFrontState: "shared-final",
         },
         {
           name: "epsom-and-ewell",
