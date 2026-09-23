e6dd830151b755b7f2ff7082a2f70cdd0104452f
Joe Carver <joecarver@live.co.uk>
2026-09-23 17:19:47 +0100

    add non-lpa chip to team list

--- apps/editor.planx.uk/src/routes/_authenticated/app/route.tsx
+++ apps/editor.planx.uk/src/routes/_authenticated/app/route.tsx
@@ -18,6 +18,7 @@ export const Route = createFileRoute("/_authenticated/app")({
             id
             name
             slug
+            isLpa: is_lpa
             settings: team_settings {
               isTrial: is_trial
             }
