02e4abc0984f1926f43fff94a93ec03523b13d01
Joe Carver <joecarver@live.co.uk>
2026-09-24 09:24:42 +0100

    chore: add is_lpa to teams sync

--- scripts/seed-database/write/teams.sql
+++ scripts/seed-database/write/teams.sql
@@ -5,7 +5,8 @@ CREATE TEMPORARY TABLE sync_teams (
   slug text,
   created_at timestamptz,
   updated_at timestamptz,
-  domain text
+  domain text,
+  is_lpa boolean
 );
 
 \copy sync_teams FROM '/tmp/teams.csv' WITH (FORMAT csv, DELIMITER ';');
@@ -13,16 +14,19 @@ CREATE TEMPORARY TABLE sync_teams (
 INSERT INTO teams (
   id,
   name,
-  slug
+  slug,
+  is_lpa
 )
 SELECT
   id,
   name,
-  slug
+  slug,
+  is_lpa
 FROM sync_teams
 ON CONFLICT (id) DO UPDATE
 SET
   name = EXCLUDED.name,
-  slug = EXCLUDED.slug;
+  slug = EXCLUDED.slug,
+  is_lpa = EXCLUDED.is_lpa;
 
 SELECT setval('teams_id_seq', max(id)) FROM teams;
